e5d72715350c5bc9b5a5e5870fb7253e5b807881
prestonlimlianjie <prestonlimlianjie@gmail.com>
2019-10-17 11:18:16 +0800

    test React Router link on Home page

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import uuid from 'uuid';
+import { Link } from "react-router-dom";
 
 const UUID = uuid.v4()
 
@@ -7,6 +8,8 @@ export default function Home() {
   return (
     <div>
       <a href={`https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&state=${UUID}&scope=public_repo`}>Sign in with GitHub</a>
+      <br />
+      <Link to={`${process.env.PUBLIC_URL}/sites`}>Sites</Link>
     </div>
   );
 }
