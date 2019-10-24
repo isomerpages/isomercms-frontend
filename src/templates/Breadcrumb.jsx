38b84d879fef8cf6f077f5456e418cbd37464880
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2019-10-24 18:22:34 +0800

    Format JSX for HTML page templates

--- src/templates/Breadcrumb.jsx
+++ src/templates/Breadcrumb.jsx
@@ -1,30 +1,27 @@
 import React from 'react';
 
-// eslint-disable-next-line arrow-body-style
-const Breadcrumb = () => {
-  return (
-    <section className="bp-section is-small bp-section-pagetitle" style={{ width: `${100 / 0.65}%` }}>
-      <div className="bp-container ">
-        <div className="row">
-          <div className="col">
-            <nav className="bp-breadcrumb" aria-label="breadcrumbs">
-              <ul>
-                <li><a href="/"><small>HOME</small></a></li>
-                <li><a href="/"><small>GENERIC BREADCRUMB</small></a></li>
-              </ul>
-            </nav>
-          </div>
+const Breadcrumb = () => (
+  <section className="bp-section is-small bp-section-pagetitle" style={{ width: `${100 / 0.65}%` }}>
+    <div className="bp-container ">
+      <div className="row">
+        <div className="col">
+          <nav className="bp-breadcrumb" aria-label="breadcrumbs">
+            <ul>
+              <li><a href="/"><small>HOME</small></a></li>
+              <li><a href="/"><small>GENERIC BREADCRUMB</small></a></li>
+            </ul>
+          </nav>
         </div>
       </div>
-      <div className="bp-container">
-        <div className="row">
-          <div className="col">
-            <h1 className="has-text-white"><b>Generic Breadcrumb</b></h1>
-          </div>
+    </div>
+    <div className="bp-container">
+      <div className="row">
+        <div className="col">
+          <h1 className="has-text-white"><b>Generic Breadcrumb</b></h1>
         </div>
       </div>
-    </section>
-  );
-};
+    </div>
+  </section>
+);
 
 export default Breadcrumb;
--- src/templates/SimplePage.jsx
+++ src/templates/SimplePage.jsx
@@ -3,21 +3,18 @@ import PropTypes from 'prop-types';
 import Breadcrumb from './Breadcrumb';
 
 // This following template was taken from the 'Simple Page'
-// eslint-disable-next-line arrow-body-style
-const SimplePage = ({ chunk }) => {
-  return (
-    <div>
-      <Breadcrumb />
-      <section className="bp-section">
-        <div className="bp-container content padding--top--lg padding--bottom--xl">
-          <div className="row">
-            <div className="col is-8 is-offset-2 print-content" dangerouslySetInnerHTML={{ __html: chunk }} />
-          </div>
+const SimplePage = ({ chunk }) => (
+  <div>
+    <Breadcrumb />
+    <section className="bp-section">
+      <div className="bp-container content padding--top--lg padding--bottom--xl">
+        <div className="row">
+          <div className="col is-8 is-offset-2 print-content" dangerouslySetInnerHTML={{ __html: chunk }} />
         </div>
-      </section>
-    </div>
-  );
-};
+      </div>
+    </section>
+  </div>
+);
 
 SimplePage.propTypes = {
   chunk: PropTypes.string.isRequired,
