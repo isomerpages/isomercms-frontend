2c120f00016610ea9cf88290ce616e4b362087e6
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2019-10-30 16:05:56 +0800

    Add cheerio package and create a util function to convert permalinks to absolute URLs

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,6 @@
+// import dependencies
 import yaml from 'js-yaml';
+import cheerio from 'cheerio';
 
 // extracts yaml front matter from a markdown file path
 export function frontMatterParser(content) {
@@ -13,14 +15,28 @@ export function frontMatterParser(content) {
   };
 }
 
+// this function concatenates the front matter with the main content body
+// of the markdown file
 export function concatFrontMatterMdBody(frontMatter, mdBody) {
   return ['---\n', yaml.safeDump(frontMatter), '---\n', mdBody].join('');
 }
 
+// this function converts file names into readable form
+// for example, '0-this-is-a-file.md' -> 'This Is A File'
 export function deslugifyCollectionPage(collectionPageName) {
   return collectionPageName
     .split('.')[0] // remove the file extension
     .split('-').slice(1) // remove the number at the start
     .map((string) => string.charAt(0).toUpperCase() + string.slice(1)) // capitalize first letter
     .join(' '); // join it back together
+}
+
+// prepends image src urls so that images are rendered on the right pane
+// of the edit pages
+export function prependImageSrc(repoName, imagePath, chunk) {
+  const $ = cheerio.load(chunk);
+  $('img').each((i, elem) => {
+    $(elem).attr('src', `https://github.com/isomerpages/${repoName}/blob/staging/${$(elem).attr('src')}?raw=true`);
+    return $.html();
+  });
 }
