610e661526e9e2ce3d9e84b844bbf4813df3308d
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2019-11-18 13:05:27 +0800

    Add header to sites page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import elementStyles from '../styles/isomer-cms/Elements.module.scss';
 
-const Header = () => (
+const Header = ({ showButton }) => (
   <div className={elementStyles.header}>
     <div className={elementStyles.headerLeft}>
-      <a href="/sites">
-        <button type="button">
-          <i className="bx bx-chevron-left" />
-Back to Sites
-        </button>
-      </a>
+      { !showButton ? null : (
+        <a href="/sites">
+          <button type="button">
+            <i className="bx bx-chevron-left" />
+            Back to Sites
+          </button>
+        </a>
+      )}
     </div>
     <div className={elementStyles.headerCenter}>
       <div className={elementStyles.logo}>
@@ -23,5 +26,13 @@ Back to Sites
     </div>
   </div>
 );
+
+Header.defaultProps = {
+  showButton: true,
+};
+
+Header.propTypes = {
+  showButton: PropTypes.bool,
+};
 
 export default Header;
