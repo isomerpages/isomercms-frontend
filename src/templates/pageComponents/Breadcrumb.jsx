b3a9e01fa2f60a15d28ad0157c7d95b51e199b32
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2019-11-21 15:52:41 +0800

    style: shift breadcrumb to the left for readability

--- src/templates/pageComponents/Breadcrumb.jsx
+++ src/templates/pageComponents/Breadcrumb.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Breadcrumb = () => (
   <section className="bp-section is-small bp-section-pagetitle" style={{ width: `${100 / 0.65}%` }}>
-    <div className="bp-container ">
+    <div className="bp-container" style={{ right: '50px' }}>
       <div className="row">
         <div className="col">
           <nav className="bp-breadcrumb" aria-label="breadcrumbs">
@@ -14,7 +14,7 @@ const Breadcrumb = () => (
         </div>
       </div>
     </div>
-    <div className="bp-container">
+    <div className="bp-container" style={{ right: '50px' }}>
       <div className="row">
         <div className="col">
           <h1 className="has-text-white"><b>Generic Breadcrumb</b></h1>
