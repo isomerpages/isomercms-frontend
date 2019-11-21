4f7ccc1eb2d876c0448fb65e7320cd23c4eaa058
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2019-11-21 17:18:06 +0800

    fix: modify breadcrumbs to reflect collection information

--- src/templates/pageComponents/Breadcrumb.jsx
+++ src/templates/pageComponents/Breadcrumb.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Breadcrumb = ({ title }) => (
+const Breadcrumb = ({ title, collection }) => (
   <section className="bp-section is-small bp-section-pagetitle" style={{ width: `${100 / 0.65}%` }}>
     <div className="bp-container" style={{ right: '50px' }}>
       <div className="row">
@@ -9,6 +9,9 @@ const Breadcrumb = ({ title }) => (
           <nav className="bp-breadcrumb" aria-label="breadcrumbs">
             <ul>
               <li><a href="/"><small>HOME</small></a></li>
+              { collection ? (
+                <li><a href="/"><small>{ collection.toUpperCase() }</small></a></li>
+              ) : null}
               <li><a href="/"><small>{ title.toUpperCase() }</small></a></li>
             </ul>
           </nav>
@@ -27,6 +30,7 @@ const Breadcrumb = ({ title }) => (
 
 Breadcrumb.propTypes = {
   title: PropTypes.string.isRequired,
+  collection: PropTypes.string.isRequired,
 };
 
 export default Breadcrumb;
