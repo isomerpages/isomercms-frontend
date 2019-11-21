3fe2275bfcb5599721d1f633c02f3ac08acc9d8f
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2019-11-21 15:56:21 +0800

    feat: modify breadcrumb to reflect accurate information on EditPage

--- src/templates/pageComponents/Breadcrumb.jsx
+++ src/templates/pageComponents/Breadcrumb.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Breadcrumb = () => (
+const Breadcrumb = ({ title }) => (
   <section className="bp-section is-small bp-section-pagetitle" style={{ width: `${100 / 0.65}%` }}>
     <div className="bp-container" style={{ right: '50px' }}>
       <div className="row">
@@ -8,7 +9,7 @@ const Breadcrumb = () => (
           <nav className="bp-breadcrumb" aria-label="breadcrumbs">
             <ul>
               <li><a href="/"><small>HOME</small></a></li>
-              <li><a href="/"><small>GENERIC BREADCRUMB</small></a></li>
+              <li><a href="/"><small>{ title.toUpperCase() }</small></a></li>
             </ul>
           </nav>
         </div>
@@ -17,11 +18,15 @@ const Breadcrumb = () => (
     <div className="bp-container" style={{ right: '50px' }}>
       <div className="row">
         <div className="col">
-          <h1 className="has-text-white"><b>Generic Breadcrumb</b></h1>
+          <h1 className="has-text-white"><b>{ title }</b></h1>
         </div>
       </div>
     </div>
   </section>
 );
+
+Breadcrumb.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default Breadcrumb;
