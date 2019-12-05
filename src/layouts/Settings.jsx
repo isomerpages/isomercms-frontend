4359ac912fb25cdbd7857f6f8d802f686e162c94
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2019-12-05 20:54:02 +0800

    feat: store on state and render all necessary state values

--- src/layouts/Settings.jsx
+++ src/layouts/Settings.jsx
@@ -1,69 +1,63 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
+import update from 'immutability-helper';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import FormField from '../components/FormField';
+import FormFieldImage from '../components/FormFieldImage';
+import FormFieldHorizontal from '../components/FormFieldHorizontal';
 import elementStyles from '../styles/isomer-cms/Elements.module.scss';
 import contentStyles from '../styles/isomer-cms/pages/Content.module.scss';
+
+const stateFields = {
+  title: '',
+  favicon: '',
+  resources_name: '',
+  colors: {
+    "primary-color": '',
+    "secondary-color": '',
+    "media-colors": [
+      {
+        title: '',
+        color: '',
+      },
+      {
+        title: '',
+        color: '',
+      },
+      {
+        title: '',
+        color: '',
+      },
+      {
+        title: '',
+        color: '',
+      },
+      {
+        title: '',
+        color: '',
+      },
+    ],
+  },
+  socialMediaContent: {
+    facebook: '',
+    linkedin: '',
+    twitter: '',
+    youtube: '',
+    instagram: '',
+  },
+};
 
 export default class Settings extends Component {
   constructor(props) {
     super(props);
     this.state = {
       siteName: '',
-      settings: {
-        configContent: {
-          title: '',
-          favicon: '',
-          resources_name: '',
-          colors: {
-            "primary-color": '',
-            "secondary-color": '',
-            "media-colors": {
-              "media-color-one": '',
-              "media-color-two": '',
-              "media-color-three": '',
-              "media-color-four": '',
-              "media-color-five": '',
-            },
-          },
-        },
-        socialMediaContent: {
-          facebook: '',
-          linkedin: '',
-          twitter: '',
-          youtube: '',
-          instagram: '',
-        },
-      },
+      ...stateFields,
       errors: {
-        settings: {
-          configContent: {
-            title: '',
-            favicon: '',
-            resources_name: '',
-            colors: {
-              "primary-color": '',
-              "secondary-color": '',
-              "media-colors": {
-                "media-color-one": '',
-                "media-color-two": '',
-                "media-color-three": '',
-                "media-color-four": '',
-                "media-color-five": '',
-              },
-            },
-          },
-          socialMediaContent: {
-            facebook: '',
-            linkedin: '',
-            twitter: '',
-            youtube: '',
-            instagram: '',
-          }
-        }
-      }
+        ...stateFields,
+      },
     };
   }
 
@@ -74,35 +68,86 @@ export default class Settings extends Component {
       const resp = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/sites/${siteName}/settings`, {
         withCredentials: true,
       });
-      const { settings } = resp.data;
-      this.setState({ siteName, settings });
-      console.log('abc')
+      const { settings: { configContent, socialMediaContent } } = resp.data;
+      // set state properly
+      this.setState((currState) => ({
+        ...currState,
+        siteName,
+        ...configContent,
+        socialMediaContent: {
+          ...currState.socialMediaContent,
+          ...socialMediaContent,
+        },
+      }));
+      console.log('abc');
     } catch (err) {
       console.log(err);
     }
   }
 
   changeHandler = (event) => {
-    const { id, value } = event.target;
+    const { id, value, parentElement: { id: parentElementID } } = event.target;
     // const errorMessage = validateSettings(id, value);
 
-    this.setState({
-      // errors: {
-      //   [id]: errorMessage,
-      // },
-      [id]: value,
-    });
-  }
+    if (parentElementID === 'social-media-fields') {
+      this.setState((currState) => ({
+        // errors: {
+        //  socialMediaContent:
+        //    [id]: errorMessage,
+        // },
+        socialMediaContent: {
+          ...currState.socialMediaContent,
+          [id]: value,
+        },
+      }));
+    } else if (parentElementID === 'color-fields') {
+      console.log('a');
+    } else if (parentElementID === 'media-color-fields') {
+      const index = id.split('@')[id.split('@').length - 1];
+      this.setState((currState) => ({
+        // errors: {
+        //  socialMediaContent:
+        //    [id]: errorMessage,
+        // },
+        colors: {
+          ...currState.colors,
+          'media-colors': update(currState.colors['media-colors'], {
+            [index]: {
+              color: {
+                $set: value,
+              },
+            },
+          }),
+        },
+      }));
+    } else {
+      this.setState((currState) => ({
+        // errors: {
+        //   [id]: errorMessage,
+        // },
+        ...currState,
+        [id]: value,
+      }));
+    }
+  };
 
   render() {
-    const { siteName, settings, errors } = this.state;
-    const { configContent: { title, favicon, resources_name: resourcesName, colors }, socialMediaContent } = settings
+    const {
+      siteName,
+      title,
+      favicon,
+      resources_name: resourcesName,
+      colors,
+      socialMediaContent,
+      errors,
+    } = this.state;
+    const { 'primary-color': primaryColor, 'secondary-color': secondaryColor, 'media-colors': mediaColors } = colors;
     const { match, location } = this.props;
     return (
       <>
         <Header showButton={false} />
         {/* main bottom section */}
-        <div className={elementStyles.wrapper}>
+        <form onSubmit={this.saveSettings} className={elementStyles.wrapper}>
           <Sidebar siteName={siteName} currPath={location.pathname} />
 
           {/* main section starts here */}
@@ -113,7 +158,6 @@ export default class Settings extends Component {
             {/* container for settings fields */}
             <div className={contentStyles.contentContainerCards}>
               <div className={contentStyles.cardContainer}>
-                <h1>Hi</h1>
                 {/* Title field */}
                 <FormField
                   title="Title"
@@ -123,8 +167,8 @@ export default class Settings extends Component {
                   isRequired
                   onFieldChange={this.changeHandler}
                 />
-                {/* Title field */}
-                <FormField
+                {/* Favicon field */}
+                <FormFieldImage
                   title="Favicon"
                   id="favicon"
                   value={favicon}
@@ -132,19 +176,67 @@ export default class Settings extends Component {
                   isRequired
                   onFieldChange={this.changeHandler}
                 />
-                {/* Title field */}
+                {/* Color fields */}
+                <div id="color-fields">
+                  <p className={elementStyles.formLabel}>Colors</p>
+                  <FormField
+                    title="Primary"
+                    id="primary-color"
+                    value={primaryColor}
+                    errorMessage={errors.colors['primary-color']}
+                    isRequired
+                    onFieldChange={this.changeHandler}
+                  />
+                  <FormField
+                    title="Secondary Color"
+                    id="secondary-color"
+                    value={secondaryColor}
+                    errorMessage={errors.colors['secondary-color']}
+                    isRequired
+                    onFieldChange={this.changeHandler}
+                  />
+                  <div id="media-color-fields">
+                    {Object.keys(mediaColors).map((category, index) => {
+                      const { title: categoryId, color } = mediaColors[index];
+                      return (
+                        <FormField
+                          title={`Resource ${index + 1}`}
+                          id={`media-color@${index}`}
+                          value={color}
+                          errorMessage={errors.colors['media-colors'][categoryId]}
+                          isRequired
+                          onFieldChange={this.changeHandler}
+                        />
+                      );
+                    })}
+                  </div>
+                </div>
+                {/* Resource room name field */}
                 <FormField
                   title="Resource Room Name"
                   id="resources_name"
                   value={resourcesName}
-                  errorMessage={errors.title}
+                  errorMessage={errors.resources_name}
                   isRequired
                   onFieldChange={this.changeHandler}
                 />
+                {/* Social media fields */}
+                <div id="social-media-fields">
+                  {Object.keys(socialMediaContent).map((socialMediaPage) => (
+                    <FormFieldHorizontal
+                      title={socialMediaPage.charAt(0).toUpperCase() + socialMediaPage.slice(1)}
+                      id={socialMediaPage}
+                      value={socialMediaContent[socialMediaPage]}
+                      errorMessage={errors.socialMediaContent[socialMediaPage]}
+                      isRequired
+                      onFieldChange={this.changeHandler}
+                    />
+                  ))}
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </form>
       </>
     );
   }
