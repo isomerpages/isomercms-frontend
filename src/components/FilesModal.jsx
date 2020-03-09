9bf162a9569f3712234912462ab73a73bf966b94
Taufiq Mohammed <taufiqmmhd@gmail.com>
2020-03-09 11:01:58 +0800

    feat(files-modal): allow uploads through modal

--- src/components/FilesModal.jsx
+++ src/components/FilesModal.jsx
@@ -6,6 +6,7 @@ import elementStyles from '../styles/isomer-cms/Elements.module.scss';
 import MediaCard from './media/MediaCard';
 import MediaUploadCard from './media/MediaUploadCard';
 import LoadingButton from './LoadingButton';
+import MediaSettingsModal from './media/MediaSettingsModal';
 
 export default class FilesModal extends Component {
   constructor(props) {
@@ -28,61 +29,99 @@ export default class FilesModal extends Component {
     }
   }
 
+  stageFileForUpload = (fileName, fileData) => {
+    this.setState({
+      stagedFileForUpload: {
+        path: `files%2F${fileName}`,
+        content: fileData,
+        fileName,
+      },
+    });
+  }
+
+  readFileToStageUpload = async (event) => {
+    const fileReader = new FileReader();
+    const fileName = event.target.files[0].name;
+    fileReader.onload = (() => {
+      /** Github only requires the content of the image
+         * imgReader returns  `data:application/pdf;base64, {fileContent}`
+         * hence the split
+         */
+
+      const fileData = fileReader.result.split(',')[1];
+      this.stageFileForUpload(fileName, fileData);
+    });
+    fileReader.readAsDataURL(event.target.files[0]);
+  }
+
   render() {
     const {
       siteName,
       onClose,
       onFileSelect,
-      readImageToUpload,
     } = this.props;
-    const { files, selectedFile } = this.state;
+    const { files, selectedFile, stagedFileForUpload } = this.state;
     return (!!files.length
       && (
-        <div className={elementStyles.overlay}>
-          <div className={mediaStyles.mediaModal}>
-            <div className={elementStyles.modalHeader}>
-              <h1 style={{ flexGrow: 1 }}>Select File</h1>
-              <LoadingButton
-                label="Select file"
-                disabledStyle={elementStyles.disabled}
-                className={elementStyles.blue}
-                callback={() => onFileSelect(`/${decodeURIComponent(selectedFile.path)}`)}
-              />
-              <button type="button" onClick={onClose}>
-                <i className="bx bx-x" />
-              </button>
-            </div>
-            <div className={mediaStyles.mediaCards}>
-              {/* Upload image */}
-              <MediaUploadCard
-                type="file"
-                onClick={() => document.getElementById('file-upload').click()}
-              />
-              <input
-                onChange={readImageToUpload}
-                onClick={(event) => {
-                  // eslint-disable-next-line no-param-reassign
-                  event.target.value = '';
-                }}
-                type="file"
-                id="file-upload"
-                accept="image/png, image/jpeg, image/gif"
-                hidden
-              />
-              {/* Render files */}
-              {files.map((file) => (
-                <MediaCard
-                  type="file"
-                  media={file}
-                  siteName={siteName}
-                  onClick={() => this.setState({ selectedFile: file })}
-                  key={file.path}
-                  isSelected={file.path === selectedFile?.path}
+        <>
+          <div className={elementStyles.overlay}>
+            <div className={mediaStyles.mediaModal}>
+              <div className={elementStyles.modalHeader}>
+                <h1 style={{ flexGrow: 1 }}>Select File</h1>
+                <LoadingButton
+                  label="Select file"
+                  disabledStyle={elementStyles.disabled}
+                  className={elementStyles.blue}
+                  callback={() => onFileSelect(`/${decodeURIComponent(selectedFile.path)}`)}
                 />
-              ))}
+                <button type="button" onClick={onClose}>
+                  <i className="bx bx-x" />
+                </button>
+              </div>
+              <div className={mediaStyles.mediaCards}>
+                {/* Upload image */}
+                <MediaUploadCard
+                  type="file"
+                  onClick={() => document.getElementById('file-upload').click()}
+                />
+                <input
+                  onChange={this.readFileToStageUpload}
+                  onClick={(event) => {
+                    // eslint-disable-next-line no-param-reassign
+                    event.target.value = '';
+                  }}
+                  type="file"
+                  id="file-upload"
+                  accept="application/msword, application/vnd.ms-excel, application/vnd.ms-powerpoint,
+                  text/plain, application/pdf"
+                  hidden
+                />
+                {/* Render files */}
+                {files.map((file) => (
+                  <MediaCard
+                    type="file"
+                    media={file}
+                    siteName={siteName}
+                    onClick={() => this.setState({ selectedFile: file })}
+                    key={file.path}
+                    isSelected={file.path === selectedFile?.path}
+                  />
+                ))}
+              </div>
             </div>
           </div>
-        </div>
+          {
+            stagedFileForUpload && (
+              <MediaSettingsModal
+                type="file"
+                siteName={siteName}
+                onClose={() => this.setState({ selectedFile: null })}
+                media={stagedFileForUpload}
+                isPendingUpload
+              />
+            )
+          }
+        </>
       )
     );
   }
@@ -92,5 +131,4 @@ FilesModal.propTypes = {
   onClose: PropTypes.func.isRequired,
   siteName: PropTypes.string.isRequired,
   onFileSelect: PropTypes.func.isRequired,
-  readImageToUpload: PropTypes.func.isRequired,
 };
