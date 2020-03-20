isomerpages/isomercms-frontend

commit e5932554b52fb8ae06a0820947a3ab301aa98886

improvement: show modal only after data is fetched
Previously, the modal would load and the form fields would be empty.
Then the data is fetched and the fields are populated. This is slightly
trippy for users.

This commit now just loads the overlay first, and once the data is
fetchd to populate the modal, the modal then renders.

src/components/ResourceSettingsModal.jsx
@@ -224,6 +224,8 @@ export default class ResourceSettingsModal extends Component {
     return (
       <>
         <div className={elementStyles.overlay}>
+          { sha
+          && (
           <div className={elementStyles.modal}>
             <div className={elementStyles.modalHeader}>
               <h1>Resource Settings</h1>
@@ -342,6 +344,7 @@ export default class ResourceSettingsModal extends Component {
               </div>
             </div>
           </div>
+          )}
         </div>
         {
           canShowDeleteWarningModal

commit 51f49578059234f110d4d15e1e9966188bac0d6e

fix(resources): re-enable creation of new resources
This fixes a bug introduced in commit e593255 (a commit to show the resource settings modal only
after the data is fetched for the selected resource). The condition to show the modal was only after the `sha`
of the resource was fetched. I forgot to account for the fact that the resource settings modal is also
used when creating a new resource. Since new resources have no `sha`, the modal doesn't show. To fix
that, the condition to show the modal checks for either the `sha` of the fetched resource or checks if the `isNewPost` prop is true.

src/components/ResourceSettingsModal.jsx
@@ -224,7 +224,7 @@ export default class ResourceSettingsModal extends Component {
     return (
       <>
         <div className={elementStyles.overlay}>
-          { sha
+          { (sha || isNewPost)
           && (
           <div className={elementStyles.modal}>
             <div className={elementStyles.modalHeader}>