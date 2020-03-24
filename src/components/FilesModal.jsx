isomerpages/isomercms-frontend

commit e5932554b52fb8ae06a0820947a3ab301aa98886

improvement: show modal only after data is fetched
Previously, the modal would load and the form fields would be empty.
Then the data is fetched and the fields are populated. This is slightly
trippy for users.

This commit now just loads the overlay first, and once the data is
fetchd to populate the modal, the modal then renders.

src/components/ResourceSettingsModal.jsx
@@ -224,6 +224,8 @@ export default class ResourceSettingsModal extends Component {
     return (
       <>
         <div className={elementStyles.overlay}>
+          { sha
+          && (
           <div className={elementStyles.modal}>
             <div className={elementStyles.modalHeader}>
               <h1>Resource Settings</h1>
@@ -342,6 +344,7 @@ export default class ResourceSettingsModal extends Component {
               </div>
             </div>
           </div>
+          )}
         </div>
         {
           canShowDeleteWarningModal

commit 9d8fa3a49847f3a1a703123056a723aae6556071

improvement: resources file url (#134)
* feat(resource-settings): introduce 'select file' button and disable url input

Users should not need to remember the permalinks of where their files are and input them. Hence this
branch will introduce commits for users to select their file from a modal and the permalink will be
added in for them.

* refactor(resource-settings): rename button click handler

* build: update packages

* feat(files-modal): implement files modal

* feat(files-modal): allow uploads through modal

* refactor((images)): switch to using base media components

Initially, media-related components were initialised into their type before they are used. (i.e
constructing `MediaCard` with type `image`, then assigning it to `ImageCard` and using `ImageCard`).
This was a problem as new props being passed through ImageCard in the `render` function had to be
updated in the `MediaCard` initialization call as well. Instead of updating two places, we should
just use the default `MediaCard` component

* fix(resource-settings): nest modal buttons into modalContent div to apply intended styling

The modal buttons div was not a child to the div with class `modalContent`. This caused the buttons
to not have the intended css styling. To fix it, it was made to be a child of the `modalContent`
div.

* feat(resource-settings): validate file selection

Validation is in place for file selection in the resource settings modal.

 The way validation is checked is slightly different for the FormFieldFile component. Usually, validation is done when the form field onChange handler is called. This is easily done since users can key into the input and invoke it.

Since the FormFieldFile input's value changes based on the file selected, the onChange handler isn't called at all. To mimic a similar onChange behaviour like other FormField components, `useEffect` is used to call `onFieldChange` whenever the input value changes.

* fix(resource-settings): erase file url validation error if user switches to permalink

* improvement: show modal only after data is fetched

Previously, the modal would load and the form fields would be empty.
Then the data is fetched and the fields are populated. This is slightly
trippy for users.

This commit now just loads the overlay first, and once the data is
fetchd to populate the modal, the modal then renders.

* fix: remove blue border around cards in images tab

Introduced a bug where a blue border appeared on all the image cards in the image tab.

This commit fixes it.

* refactor: replace type-specified media components

We were declaring components (`ImageCard`/`FileCard`/`ImageUploadCard`, etc.) that were constructed from the corresponding base media component, but with the type specified.

This proved to be a problem as when we include a new prop for the base
media component, we would then have to include that prop for the
type-specified component and make sure that new prop is passed into the
base media component it was created from.

It can get pretty messy as there are now multiple points to update the
code, hence it'd be better to just use the base media components when
rendering and specify the type there instead of creating a component
that builds on top of that base, but with the type specified.

* build: update package-lock.json

* fix(resources): re-enable creation of new resources

This fixes a bug introduced in commit e593255 (a commit to show the resource settings modal only

src/components/FilesModal.jsx
@@ -1,0 +1,134 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import mediaStyles from '../styles/isomer-cms/pages/Media.module.scss';
+import elementStyles from '../styles/isomer-cms/Elements.module.scss';
+import MediaCard from './media/MediaCard';
+import MediaUploadCard from './media/MediaUploadCard';
+import LoadingButton from './LoadingButton';
+import MediaSettingsModal from './media/MediaSettingsModal';
+
+export default class FilesModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      files: [],
+      selectedFile: null,
+    };
+  }
+
+  async componentDidMount() {
+    const { siteName } = this.props;
+    try {
+      const { data: { documents } } = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/sites/${siteName}/documents`, {
+        withCredentials: true,
+      });
+      this.setState({ files: documents });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  stageFileForUpload = (fileName, fileData) => {
+    this.setState({
+      stagedFileForUpload: {
+        path: `files%2F${fileName}`,
+        content: fileData,
+        fileName,
+      },
+    });
+  }
+
+  readFileToStageUpload = async (event) => {
+    const fileReader = new FileReader();
+    const fileName = event.target.files[0].name;
+    fileReader.onload = (() => {
+      /** Github only requires the content of the image
+         * imgReader returns  `data:application/pdf;base64, {fileContent}`
+         * hence the split
+         */
+
+      const fileData = fileReader.result.split(',')[1];
+      this.stageFileForUpload(fileName, fileData);
+    });
+    fileReader.readAsDataURL(event.target.files[0]);
+  }
+
+  render() {
+    const {
+      siteName,
+      onClose,
+      onFileSelect,
+    } = this.props;
+    const { files, selectedFile, stagedFileForUpload } = this.state;
+    return (!!files.length
+      && (
+        <>
+          <div className={elementStyles.overlay}>
+            <div className={mediaStyles.mediaModal}>
+              <div className={elementStyles.modalHeader}>
+                <h1 style={{ flexGrow: 1 }}>Select File</h1>
+                <LoadingButton
+                  label="Select file"
+                  disabledStyle={elementStyles.disabled}
+                  className={elementStyles.blue}
+                  callback={() => onFileSelect(`/${decodeURIComponent(selectedFile.path)}`)}
+                />
+                <button type="button" onClick={onClose}>
+                  <i className="bx bx-x" />
+                </button>
+              </div>
+              <div className={mediaStyles.mediaCards}>
+                {/* Upload image */}
+                <MediaUploadCard
+                  type="file"
+                  onClick={() => document.getElementById('file-upload').click()}
+                />
+                <input
+                  onChange={this.readFileToStageUpload}
+                  onClick={(event) => {
+                    // eslint-disable-next-line no-param-reassign
+                    event.target.value = '';
+                  }}
+                  type="file"
+                  id="file-upload"
+                  accept="application/msword, application/vnd.ms-excel, application/vnd.ms-powerpoint,
+                  text/plain, application/pdf"
+                  hidden
+                />
+                {/* Render files */}
+                {files.map((file) => (
+                  <MediaCard
+                    type="file"
+                    media={file}
+                    siteName={siteName}
+                    onClick={() => this.setState({ selectedFile: file })}
+                    key={file.path}
+                    isSelected={file.path === selectedFile?.path}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+          {
+            stagedFileForUpload && (
+              <MediaSettingsModal
+                type="file"
+                siteName={siteName}
+                onClose={() => this.setState({ selectedFile: null })}
+                media={stagedFileForUpload}
+                isPendingUpload
+              />
+            )
+          }
+        </>
+      )
+    );
+  }
+}
+
+FilesModal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  siteName: PropTypes.string.isRequired,
+  onFileSelect: PropTypes.func.isRequired,
+};

src/components/FormField.jsx
@@ -14,18 +14,20 @@ const FormField = ({
 }) => (
   <>
     <p className={elementStyles.formLabel}>{title}</p>
-    <input
-      type="text"
-      placeholder={title}
-      value={value}
-      defaultValue={defaultValue}
-      id={id}
-      autoComplete="off"
-      required={isRequired}
-      className={errorMessage ? `${elementStyles.error}` : null}
-      style={style}
-      onChange={onFieldChange}
-    />
+    <div className="d-flex">
+      <input
+        type="text"
+        placeholder={title}
+        value={value}
+        defaultValue={defaultValue}
+        id={id}
+        autoComplete="off"
+        required={isRequired}
+        className={errorMessage ? `${elementStyles.error}` : null}
+        style={style}
+        onChange={onFieldChange}
+      />
+    </div>
     <span className={elementStyles.error}>{errorMessage}</span>
   </>
 );
@@ -37,7 +39,7 @@ FormField.propTypes = {
   defaultValue: PropTypes.string,
   value: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
-  errorMessage: PropTypes.string.isRequired,
+  errorMessage: PropTypes.string,
   onFieldChange: PropTypes.func.isRequired,
   isRequired: PropTypes.bool.isRequired,
   style: PropTypes.string,
@@ -46,4 +48,5 @@ FormField.propTypes = {
 FormField.defaultProps = {
   defaultValue: undefined,
   style: undefined,
+  errorMessage: null,
 };

src/components/FormFieldFile.jsx
@@ -1,0 +1,82 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+import elementStyles from '../styles/isomer-cms/Elements.module.scss';
+
+const FormFieldFile = ({
+  title,
+  defaultValue,
+  value,
+  id,
+  errorMessage,
+  isRequired,
+  style,
+  onInlineButtonClick,
+  inlineButtonText,
+  onFieldChange,
+}) => {
+  useEffect(() => {
+    if (_.isString(value)) {
+      const event = {
+        target: {
+          id: 'fileUrl',
+          value,
+        },
+      };
+      onFieldChange(event);
+    }
+  }, [value]);
+  return (
+    <>
+      <p className={elementStyles.formLabel}>{title}</p>
+      <div className="d-flex border">
+        <input
+          type="text"
+          placeholder={title}
+          value={value}
+          defaultValue={defaultValue}
+          id={id}
+          autoComplete="off"
+          required={isRequired}
+          className={errorMessage ? `${elementStyles.error}` : 'border-0'}
+          style={style}
+          disabled
+        />
+        {
+        inlineButtonText
+        && (
+        <button
+          type="button"
+          className={`${elementStyles.blue} text-nowrap`}
+          onClick={onInlineButtonClick}
+        >
+          { inlineButtonText }
+        </button>
+        )
+      }
+      </div>
+      <span className={elementStyles.error}>{errorMessage}</span>
+    </>
+  );
+};
+
+export default FormFieldFile;
+
+FormFieldFile.propTypes = {
+  title: PropTypes.string.isRequired,
+  defaultValue: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  errorMessage: PropTypes.string.isRequired,
+  isRequired: PropTypes.bool.isRequired,
+  style: PropTypes.string,
+  inlineButtonText: PropTypes.string,
+  onInlineButtonClick: PropTypes.func.isRequired,
+  onFieldChange: PropTypes.func.isRequired,
+};
+
+FormFieldFile.defaultProps = {
+  defaultValue: undefined,
+  style: undefined,
+  inlineButtonText: '',
+};

src/components/ImagesModal.jsx
@@ -2,35 +2,9 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import mediaStyles from '../styles/isomer-cms/pages/Media.module.scss';
 import elementStyles from '../styles/isomer-cms/Elements.module.scss';
-import { ImageUploadCard } from '../layouts/Images';
+import MediaUploadCard from './media/MediaUploadCard';
+import MediaCard from './media/MediaCard';
 import LoadingButton from './LoadingButton';
-
-export const ImageCard = ({
-  image,
-  siteName,
-  onClick,
-  isSelected,
-}) => (
-  <div
-    className={isSelected ? mediaStyles.selectedMediaCard : mediaStyles.mediaCard}
-    key={image.path}
-  >
-    <a href="/" onClick={(e) => { e.preventDefault(); onClick(image.path); }}>
-      <div className={mediaStyles.mediaCardImagePreviewContainer}>
-        <img
-          className={mediaStyles.mediaCardImage}
-          alt={`${image.fileName}`}
-          // The sanitise parameter is for SVGs. It converts the raw svg data into an image
-          src={`https://raw.githubusercontent.com/isomerpages/${siteName}/staging/${image.path}${image.path.endsWith('.svg') ? '?sanitize=true' : ''}`}
-        />
-      </div>
-      <div className={mediaStyles.mediaCardDescription}>
-        <div className={mediaStyles.mediaCardName}>{image.fileName}</div>
-      </div>
-    </a>
-  </div>
-);
-
 
 export default class ImagesModal extends PureComponent {
   render() {
@@ -61,7 +35,8 @@ export default class ImagesModal extends PureComponent {
             </div>
             <div className={mediaStyles.mediaCards}>
               {/* Upload image */}
-              <ImageUploadCard
+              <MediaUploadCard
+                type="image"
                 onClick={() => document.getElementById('file-upload').click()}
               />
               <input
@@ -77,10 +52,11 @@ export default class ImagesModal extends PureComponent {
               />
               {/* Render images */}
               {images.map((image) => (
-                <ImageCard
-                  image={image}
+                <MediaCard
+                  type="image"
+                  media={image}
                   siteName={siteName}
-                  onClick={setSelectedImage}
+                  onClick={() => setSelectedImage(image.path)}
                   key={image.fileName}
                   isSelected={image.path === selectedImage}
                 />
@@ -92,16 +68,6 @@ export default class ImagesModal extends PureComponent {
     );
   }
 }
-
-ImageCard.propTypes = {
-  image: PropTypes.shape({
-    fileName: PropTypes.string,
-    path: PropTypes.string,
-  }).isRequired,
-  siteName: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
-  isSelected: PropTypes.bool.isRequired,
-};
 
 ImagesModal.propTypes = {
   images: PropTypes.arrayOf(PropTypes.shape({

src/components/ResourceSettingsModal.jsx
@@ -4,6 +4,7 @@ import { Base64 } from 'js-base64';
 import PropTypes from 'prop-types';
 import * as _ from 'lodash';
 import FormField from './FormField';
+import FormFieldFile from './FormFieldFile';
 import LoadingButton from './LoadingButton';
 import {
   prettifyResourceCategory,
@@ -17,6 +18,7 @@ import {
 import elementStyles from '../styles/isomer-cms/Elements.module.scss';
 import { validateResourceSettings } from '../utils/validators';
 import DeleteWarningModal from './DeleteWarningModal';
+import FilesModal from './FilesModal';
 
 export default class ResourceSettingsModal extends Component {
   constructor(props) {
@@ -39,6 +41,7 @@ export default class ResourceSettingsModal extends Component {
         prevCategory: '',
       },
       canShowDeleteWarningModal: false,
+      canShowFilesModal: false,
     };
   }
 
@@ -93,9 +96,18 @@ export default class ResourceSettingsModal extends Component {
   handlePermalinkFileUrlToggle = (event) => {
     const { target: { value } } = event;
     if (value === 'file') {
-      this.setState({ permalink: null, fileUrl: '/file/url/' });
+      this.setState({
+        permalink: null,
+        fileUrl: '',
+      });
     } else {
-      this.setState({ permalink: '/permalink/', fileUrl: null });
+      this.setState({
+        permalink: '/permalink/',
+        fileUrl: null,
+        errors: {
+          fileUrl: '',
+        },
+      });
     }
   }
 
@@ -202,8 +214,9 @@ export default class ResourceSettingsModal extends Component {
       errors,
       canShowDeleteWarningModal,
       sha,
+      canShowFilesModal,
     } = this.state;
-    const { settingsToggle, isNewPost } = this.props;
+    const { settingsToggle, isNewPost, siteName } = this.props;
 
     // Resource settings form has errors - disable save button
     const hasErrors = _.some(errors, (field) => field.length > 0);
@@ -211,6 +224,8 @@ export default class ResourceSettingsModal extends Component {
     return (
       <>
         <div className={elementStyles.overlay}>
+          { (sha || isNewPost)
+          && (
           <div className={elementStyles.modal}>
             <div className={elementStyles.modalHeader}>
               <h1>Resource Settings</h1>
@@ -261,15 +276,29 @@ export default class ResourceSettingsModal extends Component {
                 {/* Permalink or File URL */}
                 <div className="d-flex">
                   <label htmlFor="radio-post" className="flex-fill">
-                    <input type="radio" id="radio-post" name="resource-type" value="post" onClick={this.handlePermalinkFileUrlToggle} checked={!!permalink} />
+                    <input
+                      type="radio"
+                      id="radio-post"
+                      name="resource-type"
+                      value="post"
+                      onClick={this.handlePermalinkFileUrlToggle}
+                      checked={permalink || fileUrl === null}
+                    />
                     Post Content
                   </label>
                   <label htmlFor="radio-post" className="flex-fill">
-                    <input type="radio" id="radio-file" name="resource-type" value="file" onClick={this.handlePermalinkFileUrlToggle} checked={!permalink} />
+                    <input
+                      type="radio"
+                      id="radio-file"
+                      name="resource-type"
+                      value="file"
+                      onClick={this.handlePermalinkFileUrlToggle}
+                      checked={!permalink && fileUrl !== null}
+                    />
                     Downloadable File
                   </label>
                 </div>
-                {permalink
+                {permalink || fileUrl === null
                   ? (
                     <>
                       {/* Permalink */}
@@ -286,33 +315,36 @@ export default class ResourceSettingsModal extends Component {
                   : (
                     <>
                       {/* File URL */}
-                      <FormField
-                        title="File URL"
+                      <FormFieldFile
+                        title="Select File"
                         id="fileUrl"
-                        value={fileUrl}
+                        value={fileUrl?.split('/').pop()}
                         errorMessage={errors.fileUrl}
                         isRequired
                         onFieldChange={this.changeHandler}
+                        inlineButtonText="Select File"
+                        onInlineButtonClick={() => this.setState({ canShowFilesModal: true })}
                       />
                     </>
                   )}
               </div>
-            </div>
-            {/* Save or Delete buttons */}
-            <div className={elementStyles.modalButtons}>
-              <LoadingButton
-                label="Save"
-                disabled={hasErrors}
-                disabledStyle={elementStyles.disabled}
-                className={(isNewPost ? hasErrors : (hasErrors || !sha)) ? elementStyles.disabled : elementStyles.blue}
-                callback={this.saveHandler}
-              />
-              { !isNewPost
-                ? (
-                  <button type="button" className={elementStyles.warning} onClick={() => this.setState({ canShowDeleteWarningModal: true })}>Delete</button>
-                ) : null}
+              {/* Save or Delete buttons */}
+              <div className={elementStyles.modalButtons}>
+                <LoadingButton
+                  label="Save"
+                  disabled={hasErrors}
+                  disabledStyle={elementStyles.disabled}
+                  className={(isNewPost ? hasErrors : (hasErrors || !sha)) ? elementStyles.disabled : elementStyles.blue}
+                  callback={this.saveHandler}
+                />
+                { !isNewPost
+                  ? (
+                    <button type="button" className={elementStyles.warning} onClick={() => this.setState({ canShowDeleteWarningModal: true })}>Delete</button>
+                  ) : null}
+              </div>
             </div>
           </div>
+          )}
         </div>
         {
           canShowDeleteWarningModal
@@ -321,6 +353,16 @@ export default class ResourceSettingsModal extends Component {
               onCancel={() => this.setState({ canShowDeleteWarningModal: false })}
               onDelete={this.deleteHandler}
               type="resource"
+            />
+          )
+        }
+        {
+          canShowFilesModal
+          && (
+            <FilesModal
+              siteName={siteName}
+              onClose={() => this.setState({ canShowFilesModal: false })}
+              onFileSelect={(filePath) => this.setState({ fileUrl: filePath, canShowFilesModal: false })}
             />
           )
         }

src/components/media/MediaCard.jsx
@@ -3,9 +3,12 @@ import PropTypes from 'prop-types';
 import mediaStyles from '../../styles/isomer-cms/pages/Media.module.scss';
 
 const MediaCard = ({
-  type, siteName, onClick, media,
+  type, siteName, onClick, media, isSelected, canShowEditIcon,
 }) => (
-  <div className={mediaStyles.mediaCard} key={media.path}>
+  <div
+    className={isSelected ? mediaStyles.selectedMediaCard : mediaStyles.mediaCard}
+    key={media.path}
+  >
     <a href="/" onClick={(e) => { e.preventDefault(); onClick(); }}>
       {
         type === 'image' && (
@@ -28,7 +31,7 @@ const MediaCard = ({
       }
       <div className={mediaStyles.mediaCardDescription}>
         <div className={mediaStyles.mediaCardName}>{media.fileName}</div>
-        <i className="bx bxs-edit" />
+        { canShowEditIcon && <i className="bx bxs-edit" /> }
       </div>
     </a>
   </div>
@@ -42,10 +45,14 @@ MediaCard.propTypes = {
     fileName: PropTypes.string,
     path: PropTypes.string,
   }).isRequired,
+  isSelected: PropTypes.bool,
+  canShowEditIcon: PropTypes.string,
 };
 
 MediaCard.defaultProps = {
   siteName: '',
+  isSelected: false,
+  canShowEditIcon: false,
 };
 
 export default MediaCard;

src/layouts/Files.jsx
@@ -10,10 +10,6 @@ import MediaUploadCard from '../components/media/MediaUploadCard';
 import MediaCard from '../components/media/MediaCard';
 import MediaSettingsModal from '../components/media/MediaSettingsModal';
 
-const FileCard = ({ file, onClick }) => MediaCard({ type: 'file', onClick, media: file });
-
-const FileUploadCard = ({ onClick }) => MediaUploadCard({ onClick, type: 'file' });
-
 export default class Files extends Component {
   constructor(props) {
     super(props);
@@ -96,7 +92,8 @@ export default class Files extends Component {
             <div className={contentStyles.contentContainerBars}>
               {/* File cards */}
               <div className={mediaStyles.mediaCards}>
-                <FileUploadCard
+                <MediaUploadCard
+                  type="file"
                   onClick={() => document.getElementById('file-upload').click()}
                 />
                 <input
@@ -109,9 +106,10 @@ export default class Files extends Component {
                 />
                 {files.length > 0
                   && files.map((file) => (
-                    <FileCard
+                    <MediaCard
+                      type="file"
+                      media={file}
                       siteName={siteName}
-                      file={file}
                       key={file.fileName}
                       onClick={() => this.setState({ chosenFile: file })}
                     />
@@ -162,12 +160,4 @@ Files.propTypes = {
   location: PropTypes.shape({
     pathname: PropTypes.string.isRequired,
   }).isRequired,
-};
-
-FileCard.propTypes = {
-  file: PropTypes.shape({
-    fileName: PropTypes.string,
-    path: PropTypes.string,
-  }).isRequired,
-  onClick: PropTypes.func.isRequired,
 };

src/layouts/Images.jsx
@@ -10,12 +10,6 @@ import MediaUploadCard from '../components/media/MediaUploadCard';
 import MediaCard from '../components/media/MediaCard';
 import MediaSettingsModal from '../components/media/MediaSettingsModal';
 
-const ImageCard = ({ image, siteName, onClick }) => MediaCard({
-  type: 'image', siteName, onClick, media: image,
-});
-
-const ImageUploadCard = ({ onClick }) => MediaUploadCard({ onClick, type: 'image' });
-
 export default class Images extends Component {
   constructor(props) {
     super(props);
@@ -90,7 +84,8 @@ export default class Images extends Component {
               <div className={contentStyles.boxesContainer}>
                 <div className={mediaStyles.mediaCards}>
                   {/* Upload Image */}
-                  <ImageUploadCard
+                  <MediaUploadCard
+                    type="image"
                     onClick={() => document.getElementById('file-upload').click()}
                   />
                   <input
@@ -106,8 +101,9 @@ export default class Images extends Component {
                   />
                   {/* Images */}
                   {images.map((image) => (
-                    <ImageCard
-                      image={image}
+                    <MediaCard
+                      type="image"
+                      media={image}
                       siteName={siteName}
                       onClick={() => this.setState({ chosenImage: image })}
                       key={image.fileName}
@@ -160,14 +156,3 @@ Images.propTypes = {
     pathname: PropTypes.string.isRequired,
   }).isRequired,
 };
-
-ImageCard.propTypes = {
-  image: PropTypes.shape({
-    fileName: PropTypes.string,
-    path: PropTypes.string,
-  }).isRequired,
-  siteName: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
-};
-
-export { ImageUploadCard, ImageCard };

src/utils/validators.js
@@ -470,23 +470,9 @@ const validateResourceSettings = (id, value) => {
       break;
     }
     case 'fileUrl': {
-      // File URL is too short
-      // if (value.length < RESOURCE_SETTINGS_PERMALINK_MIN_LENGTH) {
-      //   errorMessage = `The file URL should be longer than ${RESOURCE_SETTINGS_PERMALINK_MIN_LENGTH} characters.`;
-      // }
-      // // File URL is too long
-      // if (value.length > RESOURCE_SETTINGS_PERMALINK_MAX_LENGTH) {
-      //   errorMessage = `The file URL should be shorter than ${RESOURCE_SETTINGS_PERMALINK_MAX_LENGTH} characters.`;
-      // }
-      // File URL fails regex
-      // if (!permalinkRegexTest.test(value)) {
-      //   console.log('FAILED');
-      //   errorMessage = `The permalink should start and end with slashes and contain
-      //     lowercase words separated by hyphens only.
-      //     `;
-      // }
-      // TO-DO
-      // Check if file exists
+      if (value.length === 0) {
+        errorMessage = 'Please choose a file';
+      }
       break;
     }
     default: {