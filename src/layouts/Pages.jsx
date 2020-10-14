isomerpages/isomercms-frontend

commit 02fcccabea2aa44b396736c6d0b120b20e7d163f

Fix/misc ui bugs (#153)
* fix: display "Back to sites" button on Settings page

This commit makes the Settings page consistent with other pages by
enabling the "Back to sites" button for the Settings page

* fix: set default btn bg color to be white, center align btn icon

This commit accomplishes two things:
1. It center aligns button icons by removing the right padding and
sets "text-align: center" on the base button class

2. It sets the default button bg color to be white to blend in with
the CMS background.

* fix: make entire PageCard component clickable

The Pages layout displays a list of PageCard components which act
as links towards individual EditPages for users to modify their
markdown content. However, currently, each PageCard component is
only clickable within a narrow band. When the cursor is on the edge
of the PageCard component, clicking on it does not bring you to the
EditPage to start editing.

The desired behavior is to ensure that the entire component is clickable.
To do this, we need to allow the Link component to make up the entire
<li> element.

This commit makes the necessary stylistic changes to allow the Link component
to take up the entire <li> component so the whole PageCard component is
clickable. Style changes were also made to the "Homepage" list element
to keep the Pages layout visually consistent.

* fix: make all homepage preview elements non-clickable

This commit addresses issue #152 by removing the url values from all
homepage preview template components, and converting all <a href> tags
to <div> tags so that they're not actually clickable. For more details
on why we're doing this, refer to the description for issue #152.

* fix: display sgds icons properly

This commit fixes issue #151. Previously, we had blindly copied the
sgds icons into our the isomer-template.scss file but we had not
checked to ensure that it was working. This installation of the SGDS

src/layouts/Pages.jsx
@@ -17,13 +17,20 @@ const RADIX_PARSE_INT = 10;
 const PageCard = ({
   siteName, pageName, pageIndex, settingsToggle, collectionName,
 }) => (
-  <li>
-    { collectionName
-      ? <Link to={`/sites/${siteName}/collections/${collectionName}/${pageName}`}>{prettifyPageFileName(pageName)}</Link>
-      : <Link to={`/sites/${siteName}/pages/${pageName}`}>{prettifyPageFileName(pageName)}</Link>}
-    <button type="button" className="d-flex flex-column pr-0" onClick={settingsToggle} id={`settings-${pageIndex}`}>
-      <i id={`settingsIcon-${pageIndex}`} className="bx bx-cog" />
-    </button>
+  // Set padding to 0 so that Link component takes up the entire element
+  // and entire element is clickable
+  <li className="p-0">
+    <Link
+      className="d-flex align-items-center px-5 py-3"
+      to={collectionName
+        ? `/sites/${siteName}/collections/${collectionName}/${pageName}`
+        : `/sites/${siteName}/pages/${pageName}`}
+    >
+      <div className="mr-auto">{prettifyPageFileName(pageName)}</div>
+      <button type="button" onClick={settingsToggle} id={`settings-${pageIndex}`}>
+        <i id={`settingsIcon-${pageIndex}`} className="bx bx-cog" />
+      </button>
+    </Link>
   </li>
 );
 
@@ -142,8 +149,17 @@ Create New Page
               { pages
                 ? (
                   <ul>
-                    <li>
-                      <Link to={`/sites/${siteName}/homepage`}>Homepage</Link>
+                    {
+                    // Set padding of li element to 0 to be compatible with other PageCard
+                    // components
+                    }
+                    <li className="p-0">
+                      <Link
+                        className="d-flex align-items-center px-5 py-3"
+                        to={`/sites/${siteName}/homepage`}
+                      >
+                        Homepage
+                      </Link>
                     </li>
                     {pages.length > 0
                       ? pages.map((page, pageIndex) => (

src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = ({
     <div className={elementStyles.headerLeft}>
       { !showButton ? null : (
         <a href={backButtonUrl}>
-          <button type="button">
+          <button className={elementStyles.default} type="button">
             <i className="bx bx-chevron-left" />
             {backButtonText}
           </button>

src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
+import 'sgds-govtech/css/sgds.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));

src/layouts/EditHomepage.jsx
@@ -1195,7 +1195,6 @@ export default class EditHomepage extends Component {
                         title={section.infobar.title}
                         subtitle={section.infobar.subtitle}
                         description={section.infobar.description}
-                        url={section.infobar.url}
                         button={section.infobar.button}
                         sectionIndex={sectionIndex}
                       />
@@ -1213,7 +1212,6 @@ export default class EditHomepage extends Component {
                             title={section.infopic.title}
                             subtitle={section.infopic.subtitle}
                             description={section.infopic.description}
-                            url={section.infopic.url}
                             imageUrl={section.infopic.imageUrl}
                             imageAlt={section.infopic.imageAlt}
                             button={section.infopic.button}
@@ -1227,7 +1225,6 @@ export default class EditHomepage extends Component {
                             title={section.infopic.title}
                             subtitle={section.infopic.subtitle}
                             description={section.infopic.description}
-                            url={section.infopic.url}
                             imageUrl={section.infopic.imageUrl}
                             imageAlt={section.infopic.imageAlt}
                             button={section.infopic.button}

src/layouts/Settings.jsx
@@ -349,7 +349,7 @@ export default class Settings extends Component {
     const hasErrors = hasConfigErrors || hasColorErrors || hasMediaColorErrors || hasSocialMediaErrors;
     return (
       <>
-        <Header showButton={false} />
+        <Header />
         {/* main bottom section */}
         <form
           className={elementStyles.wrapper}

src/styles/isomer-cms/elements/buttons.scss
@@ -29,6 +29,8 @@
 }
 
 button{
+  background: $default-background;
+  text-align: center;
   font-family: 'Lato', sans-serif;
   font-size: 14px;
   font-weight: 700;
@@ -40,7 +42,6 @@ button{
   border: none;
   flex-wrap: nowrap;
   padding: 0;
-  padding-right: 14px;
 
   &:focus {
     outline: 0;
@@ -64,6 +65,7 @@ button{
       color: $isomer-blue;
     }
   }
+
   &.warning{
     @include buttonColor($error-red);
   }

src/styles/isomer-cms/elements/variable.scss
@@ -7,6 +7,7 @@ $body-text: #333;
 $body-grey: #878787;
 $login-green: #259a36;
 
+$default-background: white;
 $base-background: #E5E9F1;
 $base-background-light: #F2F4F8;
 $base-background-dark: #BBC4D5;

src/styles/isomer-template.scss
@@ -10125,526 +10125,6 @@ li .highlight {
     font-family: Lato,sans-serif
 }
 
-@font-face {
-    font-family: sgds-icons;
-    src: url(/b499878413ae391d623ae38b6c2d5e930dd37b7a/b385f/assets/css/fonts/sgds-icons.eot#iefix) format("embedded-opentype"),url(/b9a830da3a89eb3eef563de6cf3ae5fc2c4441b0/30d0e/assets/css/fonts/sgds-icons.ttf) format("truetype"),url(/875c12db8ee785c34f1e97d127b74605d90dd675/a4cd0/assets/css/fonts/sgds-icons.woff) format("woff"),url(/859a3ccb64dad305441460944be994f0f6485107/f2f81/assets/css/fonts/sgds-icons.svg#sgds-icons) format("svg");
-    font-weight: 400;
-    font-style: normal
-}
-
-.sgds-icon {
-    font-family: sgds-icons!important;
-    speak: none;
-    font-style: normal;
-    font-weight: 400;
-    font-variant: normal;
-    text-transform: none;
-    line-height: 1;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale
-}
-
-.sgds-icon-add-image:before {
-    content: ""
-}
-
-.sgds-icon-attachment:before {
-    content: ""
-}
-
-.sgds-icon-cloud:before {
-    content: ""
-}
-
-.sgds-icon-contact:before {
-    content: ""
-}
-
-.sgds-icon-create-folder:before {
-    content: ""
-}
-
-.sgds-icon-delay:before {
-    content: ""
-}
-
-.sgds-icon-file-copy:before {
-    content: ""
-}
-
-.sgds-icon-file-pdf:before {
-    content: ""
-}
-
-.sgds-icon-folder:before {
-    content: ""
-}
-
-.sgds-icon-telephone:before {
-    content: ""
-}
-
-.sgds-icon-add-photo:before {
-    content: ""
-}
-
-.sgds-icon-browser:before {
-    content: ""
-}
-
-.sgds-icon-calculator:before {
-    content: ""
-}
-
-.sgds-icon-camera:before {
-    content: ""
-}
-
-.sgds-icon-clock:before {
-    content: ""
-}
-
-.sgds-icon-cloud-download:before {
-    content: ""
-}
-
-.sgds-icon-cloud-success:before {
-    content: ""
-}
-
-.sgds-icon-cloud-upload:before {
-    content: ""
-}
-
-.sgds-icon-database:before {
-    content: ""
-}
-
-.sgds-icon-download:before {
-    content: ""
-}
-
-.sgds-icon-file-add:before {
-    content: ""
-}
-
-.sgds-icon-file-alt:before {
-    content: ""
-}
-
-.sgds-icon-file:before {
-    content: ""
-}
-
-.sgds-icon-hdd:before {
-    content: ""
-}
-
-.sgds-icon-image:before {
-    content: ""
-}
-
-.sgds-icon-link:before {
-    content: ""
-}
-
-.sgds-icon-lock:before {
-    content: ""
-}
-
-.sgds-icon-mail:before {
-    content: ""
-}
-
-.sgds-icon-print:before {
-    content: ""
-}
-
-.sgds-icon-radar:before {
-    content: ""
-}
-
-.sgds-icon-save:before {
-    content: ""
-}
-
-.sgds-icon-share:before {
-    content: ""
-}
-
-.sgds-icon-unlock:before {
-    content: ""
-}
-
-.sgds-icon-upload:before {
-    content: ""
-}
-
-.sgds-icon-volume-mute:before {
-    content: ""
-}
-
-.sgds-icon-volume:before {
-    content: ""
-}
-
-.sgds-icon-docker:before {
-    content: ""
-}
-
-.sgds-icon-facebook-alt:before {
-    content: ""
-}
-
-.sgds-icon-facebook:before {
-    content: ""
-}
-
-.sgds-icon-google:before {
-    content: ""
-}
-
-.sgds-icon-instagram:before {
-    content: ""
-}
-
-.sgds-icon-linkedin-alt:before {
-    content: ""
-}
-
-.sgds-icon-linkedin:before {
-    content: ""
-}
-
-.sgds-icon-person-add:before {
-    content: ""
-}
-
-.sgds-icon-person-cancel:before {
-    content: ""
-}
-
-.sgds-icon-person-remove:before {
-    content: ""
-}
-
-.sgds-icon-person:before {
-    content: ""
-}
-
-.sgds-icon-rss:before {
-    content: ""
-}
-
-.sgds-icon-thumbs-down:before {
-    content: ""
-}
-
-.sgds-icon-thumbs-up:before {
-    content: ""
-}
-
-.sgds-icon-twitter:before {
-    content: ""
-}
-
-.sgds-icon-youtube:before {
-    content: ""
-}
-
-.sgds-icon-add-place:before {
-    content: ""
-}
-
-.sgds-icon-boat:before {
-    content: ""
-}
-
-.sgds-icon-bus:before {
-    content: ""
-}
-
-.sgds-icon-car:before {
-    content: ""
-}
-
-.sgds-icon-layers:before {
-    content: ""
-}
-
-.sgds-icon-map:before {
-    content: ""
-}
-
-.sgds-icon-my-location:before {
-    content: ""
-}
-
-.sgds-icon-place:before {
-    content: ""
-}
-
-.sgds-icon-taxi:before {
-    content: ""
-}
-
-.sgds-icon-traffic:before {
-    content: ""
-}
-
-.sgds-icon-train:before {
-    content: ""
-}
-
-.sgds-icon-arrow-down:before {
-    content: ""
-}
-
-.sgds-icon-arrow-left:before {
-    content: ""
-}
-
-.sgds-icon-arrow-right:before {
-    content: ""
-}
-
-.sgds-icon-arrow-top-left:before {
-    content: ""
-}
-
-.sgds-icon-arrow-top-right:before {
-    content: ""
-}
-
-.sgds-icon-arrow-up:before {
-    content: ""
-}
-
-.sgds-icon-check:before {
-    content: ""
-}
-
-.sgds-icon-chevron-down:before {
-    content: ""
-}
-
-.sgds-icon-chevron-left:before {
-    content: ""
-}
-
-.sgds-icon-chevron-right:before {
-    content: ""
-}
-
-.sgds-icon-chevron-up:before {
-    content: ""
-}
-
-.sgds-icon-cog:before {
-    content: ""
-}
-
-.sgds-icon-cross:before {
-    content: ""
-}
-
-.sgds-icon-ellipsis:before {
-    content: ""
-}
-
-.sgds-icon-home:before {
-    content: ""
-}
-
-.sgds-icon-menu:before {
-    content: ""
-}
-
-.sgds-icon-minus:before {
-    content: ""
-}
-
-.sgds-icon-notification:before {
-    content: ""
-}
-
-.sgds-icon-plus:before {
-    content: ""
-}
-
-.sgds-icon-sg-crest:before {
-    content: "";
-    color: #db0000
-}
-
-.sgds-icon-text-center:before {
-    content: ""
-}
-
-.sgds-icon-text-justify:before {
-    content: ""
-}
-
-.sgds-icon-text-left:before {
-    content: ""
-}
-
-.sgds-icon-text-right:before {
-    content: ""
-}
-
-.sgds-icon-add-comment:before {
-    content: ""
-}
-
-.sgds-icon-ban:before {
-    content: ""
-}
-
-.sgds-icon-bookmark:before {
-    content: ""
-}
-
-.sgds-icon-calendar:before {
-    content: ""
-}
-
-.sgds-icon-chat:before {
-    content: ""
-}
-
-.sgds-icon-circle-info:before {
-    content: ""
-}
-
-.sgds-icon-circle-minus:before {
-    content: ""
-}
-
-.sgds-icon-circle-plus:before {
-    content: ""
-}
-
-.sgds-icon-circle-question:before {
-    content: ""
-}
-
-.sgds-icon-circle-warning:before {
-    content: ""
-}
-
-.sgds-icon-compress-alt:before {
-    content: ""
-}
-
-.sgds-icon-compress:before {
-    content: ""
-}
-
-.sgds-icon-edit:before {
-    content: ""
-}
-
-.sgds-icon-expand-alt:before {
-    content: ""
-}
-
-.sgds-icon-expand:before {
-    content: ""
-}
-
-.sgds-icon-external:before {
-    content: ""
-}
-
-.sgds-icon-feedback:before {
-    content: ""
-}
-
-.sgds-icon-filter:before {
-    content: ""
-}
-
-.sgds-icon-info:before {
-    content: ""
-}
-
-.sgds-icon-list:before {
-    content: ""
-}
-
-.sgds-icon-pin:before {
-    content: ""
-}
-
-.sgds-icon-question:before {
-    content: ""
-}
-
-.sgds-icon-refresh:before {
-    content: ""
-}
-
-.sgds-icon-rsvp-no:before {
-    content: ""
-}
-
-.sgds-icon-rsvp-yes:before {
-    content: ""
-}
-
-.sgds-icon-search:before {
-    content: ""
-}
-
-.sgds-icon-slider-horizontal:before {
-    content: ""
-}
-
-.sgds-icon-slider-vertical:before {
-    content: ""
-}
-
-.sgds-icon-star:before {
-    content: ""
-}
-
-.sgds-icon-sync:before {
-    content: ""
-}
-
-.sgds-icon-toggle-minus:before {
-    content: ""
-}
-
-.sgds-icon-toggle-off:before {
-    content: ""
-}
-
-.sgds-icon-toggle-on:before {
-    content: ""
-}
-
-.sgds-icon-toggle-plus:before {
-    content: ""
-}
-
-.sgds-icon-triangle-warning:before {
-    content: ""
-}
-
-.sgds-icon-warning:before {
-    content: ""
-}
-
-.sgds-icon-zoom-in:before {
-    content: ""
-}
-
-.sgds-icon-zoom-out:before {
-    content: ""
-}
-
 html.has-navbar-fixed-top-widescreen {
     padding-top: 128px!important
 }

commit 770067975f7566485f7b6df5c109d050fb1d944d

fix: allow settings button in PageCard to be clicked
Previously, the settings button in the PageCard was nested inside
the Link component's <a> tag, which is not allowed (interactive elements
are not allowed to be nested in each other, or the nested component
loses its interactivity) and resulted in the settings button being
unclickable.

This commit fixes the bug, which was introduced in PR #153, by moving the
button element outside the Link component.

src/layouts/Pages.jsx
@@ -20,17 +20,21 @@ const PageCard = ({
   // Set padding to 0 so that Link component takes up the entire element
   // and entire element is clickable
   <li className="p-0">
-    <Link
-      className="d-flex align-items-center px-5 py-3"
-      to={collectionName
-        ? `/sites/${siteName}/collections/${collectionName}/${pageName}`
-        : `/sites/${siteName}/pages/${pageName}`}
+    <div
+      className="d-flex w-100 h-100"
     >
-      <div className="mr-auto">{prettifyPageFileName(pageName)}</div>
-      <button type="button" onClick={settingsToggle} id={`settings-${pageIndex}`}>
+      <Link
+        className="d-flex align-items-center px-5 py-3 mr-auto h-100"
+        to={collectionName
+          ? `/sites/${siteName}/collections/${collectionName}/${pageName}`
+          : `/sites/${siteName}/pages/${pageName}`}
+      >
+        {prettifyPageFileName(pageName)}
+      </Link>
+      <button className="px-5 py-3 h-100" type="button" onClick={settingsToggle} id={`settings-${pageIndex}`}>
         <i id={`settingsIcon-${pageIndex}`} className="bx bx-cog" />
       </button>
-    </Link>
+    </div>
   </li>
 );
 