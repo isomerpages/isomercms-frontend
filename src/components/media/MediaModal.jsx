e58e29f9011ea351ab0ddc975b60401daa7d3b8c
Jie Hao Kwa <jiehaokwa@Jies-MacBook-Pro.local>
2020-10-20 13:51:02 +0800

    feat: clean up image/file modal design
    This commit cleans up the image/file modal design in the following ways:
    - uniform spacing between images across the modal
    - padding the "Select File" header

--- src/components/media/MediaModal.jsx
+++ src/components/media/MediaModal.jsx
@@ -44,7 +44,7 @@ export default class MediasModal extends Component {
           <div className={elementStyles.overlay}>
             <div className={mediaStyles.mediaModal}>
               <div className={elementStyles.modalHeader}>
-                <h1 style={{ flexGrow: 1 }}>Select File</h1>
+                <h1 className="pl-5" style={{ flexGrow: 1 }}>Select File</h1>
                 <button type="button" onClick={onClose}>
                   <i className="bx bx-x" />
                 </button>
