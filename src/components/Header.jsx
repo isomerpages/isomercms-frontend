24f034ea322744800e0ce4cdec04af43603bf83a
Alexander Lee <alexleegs@gmail.com>
2020-10-20 12:59:07 +0800

    Fix: handle behaviour on logout (#154)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import elementStyles from '../styles/isomer-cms/Elements.module.scss';
 
+// axios settings
+axios.defaults.withCredentials = true
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
+
 const Header = ({
   showButton, title, backButtonText, backButtonUrl,
-}) => (
-  <div className={elementStyles.header}>
-    {/* Back button section */}
-    <div className={elementStyles.headerLeft}>
-      { !showButton ? null : (
-        <a href={backButtonUrl}>
-          <button className={elementStyles.default} type="button">
-            <i className="bx bx-chevron-left" />
-            {backButtonText}
-          </button>
-        </a>
-      )}
-    </div>
-    {/* Middle section */}
-    <div className={elementStyles.headerCenter}>
-      { title
-        ? <h1>{title}</h1>
-        : (
-          <div className={elementStyles.logo}>
-            <img src={`${process.env.PUBLIC_URL}/img/logo.svg`} alt="Isomer CMS logo" />
-          </div>
+}) => {
+  const [shouldRedirect, setShouldRedirect] = useState(false)
+
+  const clearCookie = async () => {
+    try {
+      // Call the logout endpoint in the API server to clear the browser cookie
+      await axios.get(`${BACKEND_URL}/auth/logout`)
+      setShouldRedirect(true)
+    } catch (err) {
+      console.error(err)
+      setShouldRedirect(false)
+    }
+  }
+
+  return (
+    <div className={elementStyles.header}>
+      {/* Back button section */}
+      <div className={elementStyles.headerLeft}>
+        { !showButton ? null : (
+          <a href={backButtonUrl}>
+            <button className={elementStyles.default} type="button">
+              <i className="bx bx-chevron-left" />
+              {backButtonText}
+            </button>
+          </a>
         )}
-    </div>
-    {/* Right section */}
-    <div className={elementStyles.headerRight}>
-      <a href={`${process.env.REACT_APP_BACKEND_URL}/auth/logout`}>
-        <button type="button" className={elementStyles.blue}>
+      </div>
+      {/* Middle section */}
+      <div className={elementStyles.headerCenter}>
+        { title
+          ? <h1>{title}</h1>
+          : (
+            <div className={elementStyles.logo}>
+              <img src={`${process.env.PUBLIC_URL}/img/logo.svg`} alt="Isomer CMS logo" />
+            </div>
+          )}
+      </div>
+      {/* Right section */}
+      <div className={elementStyles.headerRight}>
+        <button type="button" className={elementStyles.blue} onClick={clearCookie}>
           Log Out
         </button>
-      </a>
+      </div>
+      { shouldRedirect && 
+        <Redirect
+          to={{
+            pathname: '/'
+          }}
+        />
+      }
     </div>
-  </div>
-);
+  )
+};
 
 Header.defaultProps = {
   showButton: true,
