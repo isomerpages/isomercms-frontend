38ff84ff0ef04a0a3a64171965dd6cda9954ca2a
Jie Hao Kwa <jiehaokwa@Jies-MacBook-Pro.local>
2020-10-26 15:38:51 +0800

    fix: remove unused `type` prop for GenericWarningModal
    This commit accomplishes a couple of things:
    - it removes the unused `type` prop for the GenericWarningModal component
    - it updates the proptypes for the GenericWarningModal to make `displayTitle` and
    `displayText` required

--- src/components/GenericWarningModal.jsx
+++ src/components/GenericWarningModal.jsx
@@ -4,14 +4,12 @@ import elementStyles from '../styles/isomer-cms/Elements.module.scss';
 import LoadingButton from './LoadingButton';
 
 
-const GenericWarningModal = ({ displayTitle, displayText, onProceed, onCancel, type }) => (
+const GenericWarningModal = ({ displayTitle, displayText, onProceed, onCancel }) => (
   <div className={elementStyles.overlay}>
     <div className={elementStyles['modal-warning']}>
       <div className={elementStyles.modalHeader}>
         <h1>
           {displayTitle}
-          {' '}
-          { type }
         </h1>
       </div>
       <form className={elementStyles.modalContent}>
@@ -36,9 +34,10 @@ const GenericWarningModal = ({ displayTitle, displayText, onProceed, onCancel, t
 );
 
 GenericWarningModal.propTypes = {
+  displayTitle: PropTypes.string.isRequired,
+  displayText: PropTypes.string.isRequired,
   onProceed: PropTypes.func.isRequired,
   onCancel: PropTypes.func.isRequired,
-  type: PropTypes.string.isRequired,
 };
 
 export default GenericWarningModal;
