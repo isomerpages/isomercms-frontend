2797a71afa238f5e4676ad6be49a9a602481c4a6
Jie Hao Kwa <jiehaokwa@Jies-MacBook-Pro.local>
2020-10-28 06:43:54 +0800

    fix: replace inline styles on LeftNavPage
    Previously, the LeftNavPage template used inline styles to forcibly
    shift page elements. This resulted in a collision between the page
    content html and the left navbar element, since the left navbar was
    placed by manually shifting the element to the right.
    
    This commit fixes that by removing the inline styles and reusing the
    page-content-body CSS class instead.

--- src/templates/pageComponents/LeftNav.jsx
+++ src/templates/pageComponents/LeftNav.jsx
@@ -11,7 +11,7 @@ import '../../styles/isomer-template.scss';
 const LeftNav = ({ leftNavPages, fileName }) => (
   <div className="col is-2 is-position-relative has-side-nav is-hidden-touch">
     <div className="sidenav">
-      <aside className="bp-menu is-gt sidebar__inner" style={{ position: 'relative', left: '110px' }}>
+      <aside className="bp-menu is-gt sidebar__inner">
         <ul className="bp-menu-list">
           {
             leftNavPages.map((page) => {
