71931e28209915b5ca89ba8c10ab19d81e3ce36a
Jie Hao Kwa <jiehaokwa@Jies-MacBook-Pro.local>
2020-10-28 16:38:35 +0800

    feat: revamp left navigation bar on LeftNav template
    Previously, the left navigation bar accordion inside the LeftNav
    template did not render third nav elements. This means that we needed
    to both modify the html template, as well as to use javascript to
    create the dynamic accordion behavior of hiding and displaying
    accordion child elements.
    
    This commit makes those changes by introducing functions to toggle DOM
    element class names (to toggle the visibility of the accordion elements)
    and to generate the HTML itself.
    
    However, in doing so, we removed the behavior where the selected left
    nav element is highlighted. This will be re-introduced in subsequent
    commits.

--- src/templates/pageComponents/LeftNav.jsx
+++ src/templates/pageComponents/LeftNav.jsx
@@ -1,12 +1,116 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import _ from 'lodash';
 import PropTypes from 'prop-types';
 import { deslugifyCollectionPage } from '../../utils';
 import '../../styles/isomer-template.scss';
 
-// to-do
-// generate the bp-menu-list dynamically
-// detect which one is the active bar
+const accordionIconToggle = (accordionIconClass) => {
+  const upChevronIconClassName = 'sgds-icon-chevron-up'
+  const downChevronIconClassName = 'sgds-icon-chevron-down'
+
+  if (accordionIconClass.includes(upChevronIconClassName)) {
+    return accordionIconClass.replace(upChevronIconClassName, downChevronIconClassName)
+  }
+  if (accordionIconClass.includes(downChevronIconClassName)) {
+    return accordionIconClass.replace(downChevronIconClassName, upChevronIconClassName)
+  }
+}
+
+const accordionElementToggle = (accordionElementClass) => {
+  if (accordionElementClass.includes(' is-hidden')) {
+    return accordionElementClass.replace(' is-hidden', '')
+  }
+  return accordionElementClass + ' is-hidden'
+}
+
+const accordionHandler = (e) => {
+  let accordionElement
+  if (e.target.className.includes('third-level-nav-header')) {
+    accordionElement = e.target.parentElement
+
+  // There are only two levels, the child element, third-level-nav-header, or the grandchild,
+  // the accordion icon
+  } else {
+    accordionElement = e.target.parentElement.parentElement
+  }
+
+  // Hierarchy is li > a > i
+  const accordionIcon = accordionElement.children[0].children[0]
+  accordionIcon.className = accordionIconToggle(accordionIcon.className)
+
+  accordionElement.nextSibling.className = accordionElementToggle(accordionElement.nextSibling.className)
+}
+
+const generateThirdNavDiv = (thirdNavElements) => (
+  thirdNavElements.length > 0
+    ? (
+      <div className="third-level-nav-div">
+        {thirdNavElements.map((thirdNav) => thirdNav)}
+      </div>
+    )
+    : ''
+)
+
+const generateLeftNav = (leftNavPages) => {
+  let isPartOfThirdNav = false;
+  let thirdNavElements = [];
+  const leftNavBar = leftNavPages.map((page) => {
+    if (page.third_nav_title) {
+      // Begin third nav section
+      if (!isPartOfThirdNav) {
+        isPartOfThirdNav = true
+
+        // Add to third nav array
+        thirdNavElements.push(
+          <li key={page.fileName}>
+            <a className="third-level-nav-item padding--top--none ">
+              {deslugifyCollectionPage(page.fileName)}
+            </a>
+          </li>
+        )
+        return (
+          <li className="third-level-nav-header" key={page.fileName} onClick={accordionHandler}>
+            <a className="third-level-nav-header">
+              {page.third_nav_title}
+              <i className="sgds-icon is-pulled-right is-size-4 sgds-icon-chevron-up" aria-hidden="true"></i>
+            </a>
+          </li>
+        )
+      } else {
+        // Add to third nav array
+        thirdNavElements.push(
+          <li key={page.fileName}>
+            <a className="third-level-nav-item padding--top--none ">
+              {deslugifyCollectionPage(page.fileName)}
+            </a>
+          </li>
+        )
+        return
+      }
+    }
+
+    // Generate left nav entries for remaining third nav elements
+    const prevThirdNavElements = _.cloneDeep(thirdNavElements);
+    isPartOfThirdNav = false;
+    thirdNavElements = [];
+    return (
+      <>
+        {
+          generateThirdNavDiv(prevThirdNavElements)
+        }
+        <li>
+          <a>
+            {deslugifyCollectionPage(page.fileName)}
+          </a>
+        </li>
+      </>
+    )
+  })
+
+  if (thirdNavElements.length > 0) leftNavBar.push(generateThirdNavDiv(thirdNavElements))
+  return leftNavBar
+}
 
 const LeftNav = ({ leftNavPages, fileName }) => (
   <div className="col is-2 is-position-relative has-side-nav is-hidden-touch">
@@ -14,15 +118,7 @@ const LeftNav = ({ leftNavPages, fileName }) => (
       <aside className="bp-menu is-gt sidebar__inner">
         <ul className="bp-menu-list">
           {
-            leftNavPages.map((page) => (
-                <li>
-                  {
-                    page.fileName === fileName
-                      ? <a className="is-active">{deslugifyCollectionPage(page.fileName)}</a>
-                      : <a>{deslugifyCollectionPage(page.fileName)}</a>
-                  }
-                </li>
-            ))
+            generateLeftNav(leftNavPages)
           }
         </ul>
         <div
