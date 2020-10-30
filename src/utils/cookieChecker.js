c7f84d809d406f4763a07dab0638a426f29cd4bc
Jie Hao Kwa <jiehaokwa@Jies-MacBook-Pro.local>
2020-10-30 18:02:51 +0800

    chore: typo

--- src/utils/cookieChecker.js
+++ src/utils/cookieChecker.js
@@ -4,7 +4,7 @@ export const doesHttpOnlyCookieExist = (cookieName) => {
     const expires = "expires=" + d.toUTCString();
 
     document.cookie = cookieName + "=new_value;path=/;" + expires;
-    if (document.cookie.indexOf(cookieName + '=') == -1) {
+    if (document.cookie.indexOf(cookieName + '=') === -1) {
         return true;
     } else {
         return false;
