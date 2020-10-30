ff742c44edda4d67f648ed937fabfab651444886
Jie Hao Kwa <jiehaokwa@Jies-MacBook-Pro.local>
2020-10-30 23:06:09 +0800

    feat: initialize login state by checking whether cookie exists
    Previously, we initialized login state to be false. The downside of this
    is that when you enter a random isomerCMS url into the url bar, due to
    the routing rules in place, the user might be shown a flicker of the
    login page first before being redirected to the correct page.
    
    However, if we detect that the cookie exists, we can initialize login state
    to be true. If upon cookie validation, the login state is shown to be
    false, the app can then redirect back to the login page. In the future,
    we will also show a modal which indicates that we're automatically logging
    the user out.
    
    This is made possible by the `cookieChecker` util function introduced
    in this commit. Previously, we did not think it was possible to check for
    whether the isomercms cookie exists, since we set it to be httponly, which
    prevents javascript from reading the cookie or even detecting whether it
    exists. However, there is a trick which allows us to detect whether the
    cookie exists, by attempting to write to the same namespace. If the cookie
    does not exist, we will be allowed to create a cookie with that name, but
    if it does, then we will be blocked from doing so.

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,15 @@ import EditNav from './layouts/EditNav';
 import Settings from './layouts/Settings';
 import ProtectedRoute from './components/ProtectedRoute'
 
+// Utils
+const { doesHttpOnlyCookieExist } = require('./utils/cookieChecker')
+
+// Constants
+const COOKIE_NAME = 'isomercms'
+
 function App() {
   // Keep track of whether user is logged in
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(doesHttpOnlyCookieExist(COOKIE_NAME))
 
   return (
     <Router basename={process.env.PUBLIC_URL}>
---
+++ src/utils/cookieChecker.js
@@ -0,0 +1,12 @@
+export const doesHttpOnlyCookieExist = (cookieName) => {
+    const d = new Date();
+    d.setTime(d.getTime() + (1000));
+    const expires = "expires=" + d.toUTCString();
+
+    document.cookie = cookieName + "=new_value;path=/;" + expires;
+    if (document.cookie.indexOf(cookieName + '=') == -1) {
+        return true;
+    } else {
+        return false;
+    }
+} 
