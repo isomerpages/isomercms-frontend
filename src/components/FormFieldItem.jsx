6f41cb37ce5a8ca324ff3f41f44fabab61a79c33
Alexander Lee <alexander@open.gov.sg>
2020-11-04 15:46:53 +0800

    Nit: remove unused import

--- src/components/FormFieldItem.jsx
+++ src/components/FormFieldItem.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import elementStyles from '../styles/isomer-cms/Elements.module.scss';
 import MediasModal from '../components/media/MediaModal';
