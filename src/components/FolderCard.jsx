aee334d123eda2a320221879bf2eaa886b8cc55a
Jie Hao Kwa <jiehaokwa@Jies-MacBook-Pro.local>
2020-11-05 22:51:17 +0800

    fix: FolderCard CSS bug
    This commit fixes the FolderCard CSS bug which causes neighboring
    FolderCard components to be rendered vertically instead of side-by-side.
    The cause of the bug is that the following two divs which FolderCard
    is currently wrapped in:
    
    <div className={contentStyles.folderContainerBoxes}>
       <div className={contentStyles.boxesContainer}>
         ...
       </div>
    </div>
    
    should not be a part of the FolderCard component, since those divs
    act as containers for FolderCard components.
    
    This commit also generalizes the component to deal with resources,
    collections, and homepage type pages so that the correct link is
    generated for each type of page.
    
    Finally, it also updates the component's proptypes.

--- src/components/FolderCard.jsx
+++ src/components/FolderCard.jsx
@@ -6,47 +6,56 @@ import contentStyles from '../styles/isomer-cms/pages/Content.module.scss';
 
 const FolderCard = ({
   displayText,
-  link,
   settingsToggle,
   itemIndex,
   isHomepage,
-}) => (
-  <div className={contentStyles.folderContainerBoxes}>
-    <div className={contentStyles.boxesContainer}>
-      <Link className={`${contentStyles.component} ${contentStyles.card} ${elementStyles.folderCard}`} to={link}>
-        <div id={itemIndex} className={contentStyles.folderInfo}>
-          <i className={`bx bx-md text-dark ${isHomepage ? 'bxs-home-circle' : 'bxs-folder'}`} />
-          <span className={`${contentStyles.componentFolderName} align-self-center ml-4 mr-auto`}>{displayText}</span>
-          {
-            isHomepage
-            ? ''
-            : (
-              <div className={contentStyles.componentIcon}>
-                <button
-                  className={contentStyles.componentIcon}
-                  type="button"
-                  id={`settings-folder-${itemIndex}`}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    settingsToggle(e)}}
-                  className={contentStyles.componentIcon}
-                >
-                  <i id={`settingsIcon-${itemIndex}`} className="bx bx-dots-vertical-rounded" />
-                </button>
-              </div>
-            )
-          }
-        </div>
-      </Link>
-    </div>
-  </div>
-)
+  isCollection,
+  siteName,
+  category,
+}) => {
+  const generateLink = () => {
+    if (isHomepage) return `/sites/${siteName}/homepage`
+    if (isCollection) return `/sites/${siteName}/collections/${category}`
+    return `/sites/${siteName}/resources/${category}`
+  }
+
+  return (
+    <Link className={`${contentStyles.component} ${contentStyles.card} ${elementStyles.folderCard}`} to={generateLink()}>
+      <div id={itemIndex} className={contentStyles.folderInfo}>
+        <i className={`bx bx-md text-dark ${isHomepage ? 'bxs-home-circle' : 'bxs-folder'}`} />
+        <span className={`${contentStyles.componentFolderName} align-self-center ml-4 mr-auto`}>{displayText}</span>
+        {
+          isHomepage
+          ? ''
+          : (
+            <div className={contentStyles.componentIcon}>
+              <button
+                className={contentStyles.componentIcon}
+                type="button"
+                id={`settings-folder-${itemIndex}`}
+                onClick={(e) => {
+                  e.preventDefault();
+                  settingsToggle(e)}}
+                className={contentStyles.componentIcon}
+              >
+                <i id={`settingsIcon-${itemIndex}`} className="bx bx-dots-vertical-rounded" />
+              </button>
+            </div>
+          )
+        }
+      </div>
+    </Link>
+  )
+}
 
 FolderCard.propTypes = {
   displayText: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
   settingsToggle: PropTypes.func.isRequired,
-  itemIndex: PropTypes.number.isRequired,
+  itemIndex: PropTypes.number,
+  isHomePage: PropTypes.bool.isRequired,
+  isCollection: PropTypes.bool.isRequired,
+  siteName: PropTypes.string.isRequired,
 };
 
 export default FolderCard;
