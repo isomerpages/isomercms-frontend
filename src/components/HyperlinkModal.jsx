bdd10bd3e3b12f2c9d6cf10d1bd4447c43ac321d
gweiying <39231249+gweiying@users.noreply.github.com>
2020-11-13 10:35:22 +0100

    feat: adds hyperlink modal to prompt users for input text and link (#209)
    * feat: adds hyperlink modal to prompt users for input text and link
    
    * fix: fixes missing semicolons
    
    * fix: fixes broken link icon reference
    
    * fix: fixes line spacing
    
    * fix: fixes line spacing
    
    * fix: removes unused linkButton export

---
+++ src/components/HyperlinkModal.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import FormField from './FormField';
+import elementStyles from '../styles/isomer-cms/Elements.module.scss';
+import SaveDeleteButtons from './SaveDeleteButtons';
+
+// axios settings
+axios.defaults.withCredentials = true
+
+export default class HyperlinkModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: props.text,
+      link: '',
+    };
+  }
+
+  changeHandler = (event) => {
+    const { id, value } = event.target;
+    this.setState({
+      [id]: value,
+    });
+  };
+
+  render() {
+    const {
+      onSave,
+      onClose,
+    } = this.props;
+		
+    const {
+      text,
+      link,
+    } = this.state;
+
+    return (
+      <>
+        <div className={elementStyles.overlay}>
+          
+          <div className={elementStyles['modal-settings']}>
+            <div className={elementStyles.modalHeader}>
+              <h1>Insert hyperlink</h1>
+              <button type="button" onClick={onClose}>
+                <i className="bx bx-x" />
+              </button>
+            </div>
+            <div className={elementStyles.modalContent}>
+              <div className={elementStyles.modalFormFields}>
+                <FormField
+                  title="Text"
+                  id="text"
+                  value={text}
+                  isRequired={true}
+                  onFieldChange={this.changeHandler}
+                />
+                <FormField	
+                  title="Link"
+                  id="link"
+                  value={link}
+                  isRequired={true}
+                  onFieldChange={this.changeHandler}
+                />
+              </div>
+              <SaveDeleteButtons 
+                isDisabled={false}
+                hasDeleteButton={false}
+                saveCallback={() => {onSave(text,link)}}
+              />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+HyperlinkModal.propTypes = {
+  onSave: PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
