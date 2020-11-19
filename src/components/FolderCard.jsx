002095059b9089e5fa23fb861fe55e13cdc51dac
Alexander Lee <alexander@open.gov.sg>
2020-11-19 14:55:43 +0800

    Feat: add folderModal to allow users to change name of folders

--- src/components/FolderCard.jsx
+++ src/components/FolderCard.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import elementStyles from '../styles/isomer-cms/Elements.module.scss';
 import contentStyles from '../styles/isomer-cms/pages/Content.module.scss';
+import FolderModal from './FolderModal';
 
 const FolderCard = ({
   displayText,
@@ -13,6 +14,8 @@ const FolderCard = ({
   siteName,
   category,
 }) => {
+  const [isFolderModalOpen, setIsFolderModalOpen] = useState(false)
+
   const generateLink = () => {
     if (isHomepage) return `/sites/${siteName}/homepage`
     if (isCollection) return `/sites/${siteName}/collections/${category}`
@@ -20,31 +23,48 @@ const FolderCard = ({
   }
 
   return (
-    <Link className={`${contentStyles.component} ${contentStyles.card} ${elementStyles.folderCard}`} to={generateLink()}>
-      <div id={itemIndex} className={contentStyles.folderInfo}>
-        <i className={`bx bx-md text-dark ${isHomepage ? 'bxs-home-circle' : 'bxs-folder'} ${contentStyles.componentIcon}`} />
-        <span className={`${contentStyles.componentFolderName} align-self-center ml-4 mr-auto`}>{displayText}</span>
-        {
-          isHomepage
-          ? ''
-          : (
-            <div className={contentStyles.componentIcon}>
-              <button
-                className={contentStyles.componentIcon}
-                type="button"
-                id={`settings-folder-${itemIndex}`}
-                onClick={(e) => {
-                  e.preventDefault();
-                  settingsToggle(e)}}
-                className={contentStyles.componentIcon}
-              >
-                <i id={`settingsIcon-${itemIndex}`} className="bx bx-dots-vertical-rounded" />
-              </button>
-            </div>
-          )
-        }
-      </div>
-    </Link>
+    <>
+      {
+        isFolderModalOpen
+        && (
+          <FolderModal
+            displayTitle={isCollection ? 'Rename Collection' : 'Rename Resource Category'}
+            displayText={isCollection ? 'Collection name' : "Resource category name"}
+            onClose={() => setIsFolderModalOpen(false)}
+            category={category}
+            siteName={siteName}
+            isCollection={isCollection}
+          />
+        )
+      }
+      <Link className={`${contentStyles.component} ${contentStyles.card} ${elementStyles.folderCard}`} to={generateLink()}>
+        <div id={itemIndex} className={contentStyles.folderInfo}>
+          <i className={`bx bx-md text-dark ${isHomepage ? 'bxs-home-circle' : 'bxs-folder'} ${contentStyles.componentIcon}`} />
+          <span className={`${contentStyles.componentFolderName} align-self-center ml-4 mr-auto`}>{displayText}</span>
+          {
+            isHomepage
+            ? ''
+            : (
+              <div className={contentStyles.componentIcon}>
+                <button
+                  className={contentStyles.componentIcon}
+                  type="button"
+                  id={`settings-folder-${itemIndex}`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    settingsToggle(e);
+                    setIsFolderModalOpen(true)
+                  }}
+                  className={contentStyles.componentIcon}
+                >
+                  <i id={`settingsIcon-${itemIndex}`} className="bx bx-dots-vertical-rounded" />
+                </button>
+              </div>
+            )
+          }
+        </div>
+      </Link>
+    </>
   )
 }
 
