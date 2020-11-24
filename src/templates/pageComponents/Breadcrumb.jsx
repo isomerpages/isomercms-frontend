542ec2cc99d0f7d3519cd8906aea643a550f45c3
Alexis <gweiying@gmail.com>
2020-11-24 18:49:27 +0100

    feat: refactors Breadcrumb into a separate component for reuse in contact-us template page, renames old Breadcrumb as PageHeader and updates Breadcrumb references in LeftNavPage and SimplePage as well as isomer-template.scss

--- src/templates/pageComponents/Breadcrumb.jsx
+++ src/templates/pageComponents/Breadcrumb.jsx
@@ -1,40 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Breadcrumb = ({ title, date, collection }) => (
-  <section className="bp-section is-small bp-section-pagetitle">
-    <div className="bp-container breadcrumb-container">
-      <div className="row">
-        <div className="col">
-          <nav className="bp-breadcrumb" aria-label="breadcrumbs">
-            <ul>
-              <li><small>HOME</small></li>
-              { collection ? (
-                <li><small>{ collection.toUpperCase() }</small></li>
-              ) : null}
-              <li><small>{ title.toUpperCase() }</small></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </div>
-    <div className="bp-container breadcrumb-container">
-      <div className="row">
-        <div className="col">
-          <h1 className="has-text-white"><b>{ title.split(' ').map((string) => string.charAt(0).toUpperCase() + string.slice(1)).join(' ') }</b></h1>
-        </div>
-      </div>
-    </div>
-    {date &&
-      <div className="bp-container breadcrumb-container">
-        <div className="row">
-          <div className="col">
-            <p className="has-text-white">{ date }</p>
-          </div>
-        </div>
-      </div>
-    }
-  </section>
+const Breadcrumb = ({ title, collection }) => (
+  <nav className="bp-breadcrumb" aria-label="breadcrumbs">
+    <ul>
+      <li><small><a>HOME</a></small></li>
+      { collection ? (
+        <li><small><a>{ collection.toUpperCase() }</a></small></li>
+      ) : null}
+      <li><small><a>{ title.toUpperCase() }</a></small></li>
+    </ul>
+  </nav>
 );
 
 Breadcrumb.propTypes = {
