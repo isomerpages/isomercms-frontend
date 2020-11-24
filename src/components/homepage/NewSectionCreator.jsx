908ecbd1218d896c490351268b99fa49490a86d2
Alexis <gweiying@gmail.com>
2020-11-24 18:57:16 +0100

    feat: refactors Dropdown into a separate component for reuse, originally intended for reuse in contact contents; updates NewSectionCreator to use Dropdown component

--- src/components/homepage/NewSectionCreator.jsx
+++ src/components/homepage/NewSectionCreator.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import elementStyles from '../../styles/isomer-cms/Elements.module.scss';
+import Dropdown from '../Dropdown'
 
 const NewSectionCreator = ({ createHandler, hasResources }) => {
   const [newSectionType, setNewSectionType] = useState()
@@ -13,22 +14,22 @@ const NewSectionCreator = ({ createHandler, hasResources }) => {
     }
     createHandler(event)
   }
+  const options = hasResources ? ['Infobar', 'Infopic'] : ['Infobar', 'Infopic', 'Resources']
+  const defaultText = '--Choose a new section--'
   return (
   <div className={`d-flex flex-column ${elementStyles.card} ${elementStyles.addNewHomepageSection}`}>
     <h2>
       Add a new section
     </h2>
     <div className='d-flex justify-content-between'>
-      <select className={`${elementStyles.form} ${elementStyles.formHorizontal} ${elementStyles.formHorizontalInput}`} name="newSection" id="section-new" onChange={(e) => setNewSectionType(e.target.value)}>
-        <option value="">--Choose a new section--</option>
-        <option value="infobar">Infobar</option>
-        <option value="infopic">Infopic</option>
-        {/* If homepage already has a Resources section,
-          don't display the option to create one */}
-        {hasResources
-          ? null
-          : <option value="resources">Resources</option>}
-      </select>
+      <Dropdown 
+        options={options}
+        defaultOption={defaultText}
+        emptyDefault={true}
+        name='newSection'
+        id='section-new'
+        onFieldChange={(e) => setNewSectionType(e.target.value)}
+      />
       <div>
         <button type="button" className={newSectionType ? elementStyles.blue: elementStyles.disabled} onClick={onFormSubmit} disabled={!newSectionType}>Select</button>
       </div>
