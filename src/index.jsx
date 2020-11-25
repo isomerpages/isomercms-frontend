789b53ff77e5d964bedaed65b99158f64e27c8c6
Jie Hao Kwa <jiehaokwa@Jies-MacBook-Pro.local>
2020-11-25 15:06:34 +0800

    feat: initialize Sentry
    This commit initializes Sentry for our app if the environment is not
    a local dev environment

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import './styles/index.scss';
 import 'sgds-govtech/css/sgds.css';
 import App from './App';
+
+if (process.env.ENV !== 'dev') {
+    Sentry.init({
+        dsn: process.env.SENTRY_DSN,
+        integrations: [
+          new Integrations.BrowserTracing(),
+        ],
+    
+        // We recommend adjusting this value in production, or using tracesSampler
+        // for finer control
+        tracesSampleRate: 1.0,
+    });
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
