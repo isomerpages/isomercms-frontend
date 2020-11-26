a3e50960e31c5739d6de21aacb4d194945a50953
Jie Hao Kwa <jiehaokwa@Jies-MacBook-Pro.local>
2020-11-26 13:59:16 +0800

    chore: rename env vars to begin with REACT_APP_
    Reference: https://create-react-app.dev/docs/adding-custom-environment-variables/

--- src/index.jsx
+++ src/index.jsx
@@ -6,17 +6,17 @@ import './styles/index.scss';
 import 'sgds-govtech/css/sgds.css';
 import App from './App';
 
-if (process.env.ENV !== 'staging' || process.env.ENV !== 'production') {
-    Sentry.init({
-        dsn: process.env.SENTRY_DSN,
-        integrations: [
-          new Integrations.BrowserTracing(),
-        ],
-    
-        // We recommend adjusting this value in production, or using tracesSampler
-        // for finer control
-        tracesSampleRate: 1.0,
-    });
+if (process.env.REACT_APP_SENTRY_ENV === 'staging' || process.env.REACT_APP_SENTRY_ENV == 'production') {
+  Sentry.init({
+      dsn: process.env.REACT_APP_SENTRY_DSN,
+      integrations: [
+        new Integrations.BrowserTracing(),
+      ],
+  
+      // We recommend adjusting this value in production, or using tracesSampler
+      // for finer control
+      tracesSampleRate: 1.0,
+  });
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
