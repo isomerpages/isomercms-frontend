4b648f0b34cc5f73fc066a11f661a60a829c4a25
Alexis <gweiying@gmail.com>
2020-11-30 17:48:01 +0100

    fix: rectifies relative imports

--- src/components/contact-us/ContactFields.jsx
+++ src/components/contact-us/ContactFields.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import FormField from '../FormField';
-import InputMaskFormField from './PhoneFormField';
+import InputMaskFormField from '../InputMaskFormField';
 import Dropdown from '../Dropdown';
 
 const ContactFields = ({ 
