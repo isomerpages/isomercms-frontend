9566f14249307228c63863791ffa16c9e13ebf0a
Alexis <gweiying@gmail.com>
2020-11-30 17:42:05 +0100

    feat: creates InputMaskFormField using react-input-mask for FormFields that can use masked input styling, for example dates, phone numbers and so on

---
+++ src/components/InputMaskFormField.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import elementStyles from '../../styles/isomer-cms/Elements.module.scss';
+import InputMask from 'react-input-mask';
+
+const InputMaskFormField = ({
+  title,
+  value,
+  mask,
+  maskChar,
+  alwaysShowMask,
+  id,
+  hasError, 
+  errorMessage,
+  onFieldChange,
+  style,
+  disabled,
+  fixedMessage,
+  maxWidth,
+}) => (
+  <>
+    { title && <label className={elementStyles.formLabel}>{title}</label> }
+    <div className={`d-flex text-nowrap ${maxWidth ? 'w-100' : ''}`}>
+      { fixedMessage && <p className={elementStyles.formFixedText}>{fixedMessage}</p> }
+      <InputMask
+        mask={mask}
+        maskChar={maskChar}
+        alwaysShowMask={alwaysShowMask}
+        id={id}
+        value={value} 
+        style={style}
+        onChange={onFieldChange}
+        className={hasError || errorMessage ? `${elementStyles.error}` : null}
+        disabled={disabled}
+      />
+    </div>
+    { errorMessage && <span className={elementStyles.error}>{errorMessage}</span> }
+  </>
+);
+
+export default InputMaskFormField;
+
+InputMaskFormField.propTypes = {
+  title: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  mask: PropTypes.string.isRequired,
+  maskChar: PropTypes.string,
+  alwaysShowMask: PropTypes.bool,
+  id: PropTypes.string.isRequired,
+  hasError: PropTypes.bool,
+  errorMessage: PropTypes.string,
+  onFieldChange: PropTypes.func.isRequired,
+  isRequired: PropTypes.bool,
+  style: PropTypes.string,
+  maxWidth: PropTypes.bool,
+};
+
+InputMaskFormField.defaultProps = {
+  style: undefined,
+  errorMessage: null,
+};
