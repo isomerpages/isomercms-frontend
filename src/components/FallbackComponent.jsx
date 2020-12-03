a9e4127d8a38dd5319fd6d692777a232b780f02e
Alexander Lee <alexander@open.gov.sg>
2020-12-03 16:37:42 +0800

    Feat: update fallback component with generic error page

--- src/components/FallbackComponent.jsx
+++ src/components/FallbackComponent.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 
-function FallbackComponent(err) {
-    console.log(err)
-    return <div>An error has occurred</div>;
+import errorStyles from '../styles/isomer-cms/pages/Error.module.scss';
+import elementStyles from '../styles/isomer-cms/Elements.module.scss';
+
+function FallbackComponent({error}) {
+  console.log(error)
+
+  return (
+    <>
+      <div className={errorStyles.errorPageMain}>
+        <img
+          className={errorStyles.errorImage}
+          alt={`Generic Error Image`}
+          src={`/genericError.svg`}
+        />
+        <div className={errorStyles.errorText}>
+          Whoops, something went wrong. 
+          <br/> 
+          Click below to return to IsomerCMS.
+        </div>
+          <button className={`${errorStyles.errorButton} ${elementStyles.blue}`} onClick={() => window.location.reload()}>
+            Back to IsomerCMS
+          </button>
+      </div>
+    </>
+  )
 }
 
 export default FallbackComponent;
