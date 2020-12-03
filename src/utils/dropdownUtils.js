420166f7b95ba225c8e44ea725e060154caf5139
Jie Hao Kwa <jiehaokwa@Jies-MacBook-Pro.local>
2020-12-04 00:18:02 +0800

    feat: add error toast when we fail to retrieve third nav options
    This commit adds an error toast when we fail to retrieve third nav
    options in the ComponentSettingsModal

--- src/utils/dropdownUtils.js
+++ src/utils/dropdownUtils.js
@@ -1,24 +1,36 @@
 import axios from 'axios';
+import React from 'react';
+import elementStyles from '../styles/isomer-cms/Elements.module.scss';
+import { toast } from 'react-toastify';
+import Toast from '../components/Toast';
 
 // axios settings
 axios.defaults.withCredentials = true
 
 export const retrieveThirdNavOptions = async (siteName, collectionName, isExistingCollection) => {
-    let thirdNavArr = [], allCollectionPages = []
-    if (isExistingCollection) {
-        const endpoint = `${process.env.REACT_APP_BACKEND_URL}/sites/${siteName}/collections/${collectionName}/pages`
-        const { data : { collectionPages } } = await axios.get(endpoint)
-        thirdNavArr = collectionPages.filter((elem) => elem.type === 'third-nav')
-        allCollectionPages = collectionPages
-    }
-    const thirdNavOptions = [''].concat(thirdNavArr).map((thirdNav) => (
-        {
-            value:thirdNav.title,
-            label:thirdNav.title ? thirdNav.title : 'None',
+    try {
+        let thirdNavArr = [], allCollectionPages = []
+        if (isExistingCollection) {
+            const endpoint = `${process.env.REACT_APP_BACKEND_URL}/sites/${siteName}/collections/${collectionName}/pages`
+            const { data : { collectionPages } } = await axios.get(endpoint)
+            thirdNavArr = collectionPages.filter((elem) => elem.type === 'third-nav')
+            allCollectionPages = collectionPages
         }
-    ))
-    return {
-        collectionPages: allCollectionPages,
-        thirdNavOptions,
+        const thirdNavOptions = [''].concat(thirdNavArr).map((thirdNav) => (
+            {
+                value:thirdNav.title,
+                label:thirdNav.title ? thirdNav.title : 'None',
+            }
+        ))
+        return {
+            collectionPages: allCollectionPages,
+            thirdNavOptions,
+        }
+    } catch (err) {
+        toast(
+            <Toast notificationType='error' text={`There was a problem trying to retrieve data from your repo. Please try again or check your internet connection.`}/>,
+            {className: `${elementStyles.toastError} ${elementStyles.toastLong}`},
+          );
+        console.log(err);
     }
 }
