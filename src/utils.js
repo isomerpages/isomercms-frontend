381cb6eca8f51b82a7bd3c6a0e201e7a9ee7374f
Jie Hao Kwa <jiehaokwa@Jies-MacBook-Pro.local>
2020-12-08 23:49:37 +0800

    feat: util function for deep comparing objects
    This commit introduces a util function i found on stack overflow,
    https://stackoverflow.com/a/39813128, which deep compares two objects and
    identifies which attributes have changed.

--- src/utils.js
+++ src/utils.js
@@ -501,3 +501,25 @@ export const checkIsOutOfViewport = (bounding, posArr) => {
   
   return posArr.some((pos) => {return out[pos]})
 }
+
+export const getObjectDiff = (obj1, obj2) => {
+  // const diff = Object.keys(obj1).reduce((result, key) => {
+  //     if (!obj2.hasOwnProperty(key)) {
+  //         result.push(key);
+  //     } else if (_.isEqual(obj1[key], obj2[key])) {
+  //         const resultKeyIndex = result.indexOf(key);
+  //         result.splice(resultKeyIndex, 1);
+  //     }
+  //     return result;
+  // }, Object.keys(obj2));
+
+  // return diff;
+  const allkeys = _.union(_.keys(obj1), _.keys(obj2));
+  const difference = _.reduce(allkeys, function (result, key) {
+    if ( !_.isEqual(obj1[key], obj2[key]) ) {
+      result[key] = {obj1: obj1[key], obj2: obj2[key]}
+    }
+    return result;
+  }, {});
+  return difference
+}
