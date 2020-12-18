b390c02575aaa4dd8492a5c4fc04bdb9ace890c1
Alexis <gweiying@gmail.com>
2020-12-18 17:26:51 +0100

    Feat: add validation functions to run onMount in utils/contact-us/validators

---
+++ src/utils/contact-us/validators.js
@@ -0,0 +1,62 @@
+import { validateContactType, validateLocationType } from '../validators';
+
+function validateOperatingHours(operatingHours) {
+  const { days, time, description } = operatingHours
+  return {
+    days: validateLocationType('days', days),
+    time: validateLocationType('time', time),
+    description: validateLocationType('description', description)
+  }
+}
+
+function validateOperatingHoursArr(operatingHours) {
+  return operatingHours.map(hours => validateOperatingHours(hours))
+}
+
+function validateLocation(location) {
+  const { title, address, operating_hours: operatingHours, maps_link: mapUrl } = location
+
+  return {
+    title: validateLocationType('title', title),
+    address: validateLocationType('address', address),
+    operating_hours: validateOperatingHoursArr(operatingHours),
+    maps_link: validateLocationType('maps_link', mapUrl)
+  }
+}
+
+function validateContact(contact) {
+  const { title, content } = contact
+  return {
+    title: validateContactType('title', title),
+    content: [
+      validateContactType('phone', content[0].phone),
+      validateContactType('email', content[1].email),
+      validateContactType('other', content[2].other),
+    ]
+  }
+}
+
+export function validateFrontMatter(frontMatter) {
+    const { contacts, locations } = frontMatter;
+  
+    let contactsErrors = [];
+    let locationsErrors = [];
+  
+    if (contacts !== undefined) {
+      contacts.forEach(contact => 
+        contactsErrors.push(validateContact(contact))
+      )
+    }
+    if (locations !== undefined) {
+      locations.forEach(location => 
+        locationsErrors.push(validateLocation(location))
+      )
+    }
+  
+    const frontMatterErrors = {
+      contactsErrors,
+      locationsErrors,
+    }
+    
+    return frontMatterErrors
+  }
