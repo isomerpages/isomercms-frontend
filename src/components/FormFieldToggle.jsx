3f35ed3fb7251616299de18de4c0f3849f5d166a
Alexander Lee <alexander@open.gov.sg>
2020-12-30 14:58:16 +0800

    Feat: add FormFieldToggle

---
+++ src/components/FormFieldToggle.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ToggleButton from 'react-toggle-button'
+import elementStyles from '../styles/isomer-cms/Elements.module.scss';
+
+const creatableSelectHandler = (callback, dropdownEvent, id) => {
+    callback({
+        target: {
+            id,
+            value: dropdownEvent,
+        },
+    })
+}
+
+const FormFieldDropdown = ({
+    title,
+    value,
+    id,
+    onFieldChange,
+}) => (
+    <>
+      <div className={elementStyles.formHorizontal}>
+        <div className={`d-flex align-items-center ${elementStyles.formHorizontalLabel}`}>
+            <span>{`${title}:`}</span>
+        </div>
+        <div className={`d-flex align-items-center ${elementStyles.formHorizontalLabel}`}>
+            <ToggleButton
+                className={elementStyles.formHorizontalInput}
+                inactiveLabel={''}
+                activeLabel={''}
+                thumbIcon={<span className={`bx bx-xs ${value ? `${elementStyles.formToggleThumbIconEnabled} bx-check` : `${elementStyles.formToggleThumbIconDisabled} bx-x`}`}/>}
+                value={value}
+                onToggle={(value) => creatableSelectHandler(onFieldChange, !value, id)}
+            />
+        </div>
+      </div>
+    </>
+);
+  
+export default FormFieldDropdown;
+  
+FormFieldDropdown.propTypes = {
+    title: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    onFieldChange: PropTypes.func.isRequired,
+};
