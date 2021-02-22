928363381159032760e311494423b7ef31b149ad
jiehao <jiehao@open.gov.sg>
2021-02-22 14:02:06 +0800

    feat: create api file for storing data fetching and mutation functions

---
+++ src/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+// axios settings
+axios.defaults.withCredentials = true
+
+// Constants
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
+
+const getDirectoryFile = async (siteName, folderName) => {
+    return await axios.get(`${BACKEND_URL}/sites/${siteName}/collections/${folderName}/pages/collection.yml`);
+}
+
+const getFolderContents = async (siteName, folderName, subfolderName) => {
+    return await axios.get(`${BACKEND_URL}/sites/${siteName}/folders?path=_${folderName}${subfolderName ? `/${subfolderName}` : ''}`);
+}
+
+export {
+    getDirectoryFile,
+    getFolderContents,
+}
