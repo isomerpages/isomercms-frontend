a81c1b1c47cbcc0a3fc402fa493e52170ae8888a
jiehao <jiehao@open.gov.sg>
2021-02-22 15:13:48 +0800

    fix: modify folder content items to navigate to EditPage
    This commit updates the FolderContentItem component so that if it's
    a directory, it links to the subfolder and if it's a file, it links
    to the EditPage layout

--- src/components/folders/FolderContent.jsx
+++ src/components/folders/FolderContent.jsx
@@ -1,45 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { deslugifyPage } from '../../utils'
 
 // Import styles
 import elementStyles from '../../styles/isomer-cms/Elements.module.scss';
 import contentStyles from '../../styles/isomer-cms/pages/Content.module.scss';
 
-const FolderContentItem = ({ title, isFile, numItems }) => {
+const FolderContentItem = ({ title, isFile, numItems, link }) => {
     return (
-        <div type="button" className={`${elementStyles.card} ${contentStyles.card} ${elementStyles.folderItem}`}>
-            <div className={contentStyles.contentContainerFolderRow}>
-                {
-                    isFile
-                    ? <i className={`bx bxs-file-blank ${elementStyles.folderItemIcon}`} />
-                    : <i className={`bx bxs-folder ${elementStyles.folderItemIcon}`} />
-                }
-                <span className={`${elementStyles.folderItemText} mr-auto`} >{title}</span>
-                {
-                    numItems
-                    ? <span className={elementStyles.folderItemText}>{numItems} item{numItems === '1' ? 's' : ''}</span>
-                    : null
-                }
-                <button
-                  className={`${contentStyles.componentIcon} ml-5 mr-3`}
-                  type="button"
-                >
-                  <i className="bx bx-dots-vertical-rounded" />
-                </button>
+        <Link to={link}>
+            <div type="button" className={`${elementStyles.card} ${contentStyles.card} ${elementStyles.folderItem}`}>
+                <div className={contentStyles.contentContainerFolderRow}>
+                    {
+                        isFile
+                        ? <i className={`bx bxs-file-blank ${elementStyles.folderItemIcon}`} />
+                        : <i className={`bx bxs-folder ${elementStyles.folderItemIcon}`} />
+                    }
+                    <span className={`${elementStyles.folderItemText} mr-auto`} >{title}</span>
+                    {
+                        numItems
+                        ? <span className={elementStyles.folderItemText}>{numItems} item{numItems === '1' ? 's' : ''}</span>
+                        : null
+                    }
+                    <button
+                    className={`${contentStyles.componentIcon} ml-5 mr-3`}
+                    type="button"
+                    >
+                    <i className="bx bx-dots-vertical-rounded" />
+                    </button>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
-const FolderContent = ({ data }) => {
+const FolderContent = ({ data, siteName, folderName }) => {
     return (
         <div className={`${contentStyles.contentContainerFolderColumn} mb-5`}>
             {
                 data.map((folderContentItem) => (
                     <FolderContentItem
                         key={folderContentItem.title}
-                        title={folderContentItem.title}
-                        numItems={folderContentItem.children ? folderContentItem.children.length : null}
-                        isFile={folderContentItem.type === 'dir' ? false : true} />
+                        title={deslugifyPage(folderContentItem.title)}
+                        numItems={folderContentItem.type === 'dir' ? folderContentItem.children.length : null}
+                        isFile={folderContentItem.type === 'dir' ? false: true}
+                        link={folderContentItem.type === 'dir' ? `/sites/${siteName}/folder/${folderName}/${folderContentItem.title}` : `/sites/${siteName}/collections/${folderName}/${folderContentItem.path}`}
+                    />
                 ))
             }
         </div>
