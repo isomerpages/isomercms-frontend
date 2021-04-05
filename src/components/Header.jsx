41c4778ef8e9559d4a4e31adf6bad24f488166e0
Alexander Lee <alexander@open.gov.sg>
2021-04-05 12:42:28 +0800

    Feat: modify header format

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import GenericWarningModal from './GenericWarningModal'
 import useRedirectHook from '../hooks/useRedirectHook';
+import useSiteUrlHook from '../hooks/useSiteUrlHook';
 import elementStyles from '../styles/isomer-cms/Elements.module.scss';
 
 // Import context
@@ -17,12 +18,31 @@ const userIdKey = "userId"
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
 
 const Header = ({
-  showButton, title, isEditPage, shouldAllowEditPageBackNav, backButtonText, backButtonUrl,
+  siteName, showButton, title, isEditPage, shouldAllowEditPageBackNav, backButtonText, backButtonUrl,
 }) => {
   const { setRedirectToPage, setRedirectToLogout } = useRedirectHook()
+  const { retrieveStagingUrl } = useSiteUrlHook()
   const setLogoutState = useContext(LoginContext)
 
   const [showBackNavWarningModal, setShowBackNavWarningModal] = useState(false)
+  const [showStagingWarningModal, setShowStagingWarningModal] = useState(false)
+  const [stagingUrl, setStagingUrl] = useState()
+
+  useEffect(() => {
+    let _isMounted = true
+
+    const loadStagingUrl = async () => {
+      if (siteName) {
+        const retrievedStagingUrl = await retrieveStagingUrl(siteName)
+        if (_isMounted) setStagingUrl(retrievedStagingUrl)
+      }
+    }
+
+    loadStagingUrl()
+    return () => {
+      _isMounted = false
+    }
+  }, [])
 
   const clearCookie = async () => {
     try {
@@ -43,6 +63,16 @@ const Header = ({
   const handleBackNav = () => {
     if (isEditPage && !shouldAllowEditPageBackNav) setShowBackNavWarningModal(true)
     else toggleBackNav()
+  }
+
+  const handleViewPullRequest = () => {
+    const githubUrl = `https://github.com/isomerpages/${siteName}/pulls`
+    window.open(githubUrl, '_blank')
+  }
+
+  const handleViewStaging = () => {
+    window.open(stagingUrl, '_blank')
+    setShowStagingWarningModal(false)
   }
 
   return (
@@ -70,11 +100,11 @@ const Header = ({
       </div>
       {/* Right section */}
       <div className={elementStyles.headerRight}>
-        <div className={`${elementStyles.info} mr-3`}>
-          Logged in as @{localStorage.getItem(userIdKey)}
-        </div>
-        <button type="button" className={`${elementStyles.blue} float-right`} onClick={clearCookie}>
-          Log Out
+        <button type="button" className={`${elementStyles.green} float-right text-nowrap`} onClick={handleViewPullRequest}>
+          Pull Request
+        </button>
+        <button type="button" className={`${elementStyles.blue} float-right text-nowrap`} onClick={() => setShowStagingWarningModal(true)}>
+          View Staging
         </button>
       </div>
       {
@@ -88,20 +118,39 @@ const Header = ({
           cancelText="No"
         />
       }
+      {
+        showStagingWarningModal &&
+        <GenericWarningModal
+          displayTitle=""
+          displayText="Your changes may take some time to be reflected. <br/> Refresh your page to see if your changes have been built."
+          displayImg="/publishModal.svg"
+          displayImgAlt="View Staging Modal Image"
+          onProceed={handleViewStaging}
+          onCancel={() => setShowStagingWarningModal(false)}
+          proceedText="Proceed to staging site"
+          cancelText="Cancel"
+        />
+      }
     </div>
   )
 };
 
 Header.defaultProps = {
+  siteName: undefined,
   showButton: true,
   title: undefined,
+  isEditPage: false,
+  shouldAllowEditPageBackNav: true,
   backButtonText: 'Back to Sites',
   backButtonUrl: '/sites',
 };
 
 Header.propTypes = {
+  siteName: PropTypes.string,
   showButton: PropTypes.bool,
   title: PropTypes.string,
+  isEditPage: PropTypes.bool,
+  shouldAllowEditPageBackNav: PropTypes.bool,
   backButtonText: PropTypes.string,
   backButtonUrl: PropTypes.string,
 };
