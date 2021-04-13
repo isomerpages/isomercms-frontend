cc91e62bd564fd4f69c9aee1bb9db56507721463
Alexander Lee <alexander@open.gov.sg>
2021-04-13 10:27:51 +0800

    Fix: remove encoding when saving edit contact us

--- src/layouts/EditContactUs.jsx
+++ src/layouts/EditContactUs.jsx
@@ -2,7 +2,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import _ from 'lodash';
-import { Base64 } from 'js-base64';
 import PropTypes from 'prop-types';
 import update from 'immutability-helper';
 import { DragDropContext } from 'react-beautiful-dnd';
@@ -583,10 +582,9 @@ const EditContactUs =  ({ match }) => {
       if (!filteredFrontMatter.locations.length) delete filteredFrontMatter.locations
 
       const content = concatFrontMatterMdBody(filteredFrontMatter, '');
-      const base64EncodedContent = Base64.encode(content);
 
       const frontMatterParams = {
-        content: base64EncodedContent,
+        content,
         sha: frontMatterSha,
       };
 
