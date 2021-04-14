38905ab9891916f947e65d696f84a52e0adb7e46
Alexander Lee <alexander@open.gov.sg>
2021-04-14 09:50:00 +0800

    Feat: add ability to disable save and delete buttons separately

--- src/components/SaveDeleteButtons.jsx
+++ src/components/SaveDeleteButtons.jsx
@@ -3,24 +3,26 @@ import PropTypes from 'prop-types';
 import LoadingButton from './LoadingButton'
 import elementStyles from '../styles/isomer-cms/Elements.module.scss';
 
-const SaveDeleteButtons = ({ saveLabel, deleteLabel, isDisabled, hasDeleteButton, saveCallback, deleteCallback }) => {
+const SaveDeleteButtons = ({ saveLabel, deleteLabel, isDisabled, isSaveDisabled, isDeleteDisabled, hasDeleteButton, saveCallback, deleteCallback }) => {
+  const shouldDisableSave = isSaveDisabled ? isSaveDisabled : isDisabled
+  const shouldDisableDelete = isDeleteDisabled ? isDeleteDisabled : isDisabled
   return (
     <div className={elementStyles.modalButtons}>
       { hasDeleteButton
         ? (
           <LoadingButton
             label={deleteLabel || 'Delete'}
-            disabled={isDisabled}
+            disabled={shouldDisableDelete}
             disabledStyle={elementStyles.disabled}
-            className={`ml-auto ${isDisabled ? elementStyles.disabled : elementStyles.warning}`}
+            className={`ml-auto ${shouldDisableDelete ? elementStyles.disabled : elementStyles.warning}`}
             callback={deleteCallback}
           />
         ) : null}
       <LoadingButton
         label={saveLabel || 'Save'}
-        disabled={isDisabled}
+        disabled={shouldDisableSave}
         disabledStyle={elementStyles.disabled}
-        className={`${hasDeleteButton ? null : `ml-auto`} ${isDisabled ? elementStyles.disabled : elementStyles.blue}`}
+        className={`${hasDeleteButton ? null : `ml-auto`} ${shouldDisableSave ? elementStyles.disabled : elementStyles.blue}`}
         callback={saveCallback}
       />
       
@@ -32,6 +34,8 @@ SaveDeleteButtons.propTypes = {
   saveLabel: PropTypes.string,
   deleteLabel: PropTypes.string,
   isDisabled: PropTypes.bool,
+  isSaveDisabled: PropTypes.bool,
+  isDeleteDisabled: PropTypes.bool,
   hasDeleteButton: PropTypes.bool.isRequired,
   saveCallback: PropTypes.func.isRequired,
   deleteCallback: PropTypes.func.isRequired,
