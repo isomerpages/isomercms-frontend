7a48d45aa999c2d27492992be7bbc9af0b31124c
kwajiehao <31984694+kwajiehao@users.noreply.github.com>
2021-04-19 19:26:45 +0800

    Feat/nested images functionality (#370)

--- src/api.js
+++ src/api.js
@@ -240,6 +240,24 @@ const moveFile = async ({selectedFile, siteName, resourceName, folderName, subfo
     return await axios.post(apiEndpoint, params)
 }
 
+const getImages = async (siteName, customPath) => {
+    const resp = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/sites/${siteName}/files/${customPath ? encodeURIComponent(`images/${customPath}`) : 'images'}`);
+    const { directoryContents } = resp.data;
+
+    let respImages = []
+    let respDirectories = []
+    directoryContents.forEach((fileOrDir) => {
+        const modifiedFileOrDir = { ...fileOrDir, fileName: fileOrDir.name }
+        if (fileOrDir.type === 'file') respImages.push(modifiedFileOrDir)
+        if (fileOrDir.type === 'dir') respDirectories.push(modifiedFileOrDir)
+    })
+
+    return {
+        respImages,
+        respDirectories,
+    }
+}
+
 export {
     getDirectoryFile,
     setDirectoryFile,
@@ -263,4 +281,5 @@ export {
     getAllCategories,
     moveFiles,
     moveFile,
+    getImages,
 }
