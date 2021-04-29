36a689d930b066ef0442551483a55191b59e176b
Preston Lim <prestonlimlianjie@gmail.com>
2021-04-29 15:39:09 +0800

    refactor: clean up ProtectedRoute and LoginContext (#431)
    * refactor: clean up ProtectedRoute and LoginContext
    
    * refactor: make LoginContext testable
    
    create 3 exports: LoginContext itself, LoginProvider, LoginConsumer
    
    * refactor: make route selector testable in App.jsx
    
    * refactor: group routing components
    
    * feat: add basic routing tests
    
    * refactor: move __tests__ folder to correct place
    
    Required for jest to find the test files
    
    * style: delete unnecessary div
    
    * refactor: make exports more obvious for LoginContext.js
    
    * refactor: delete duplicate route
    
    * chore: add rest of routing tests
    
    * style: remove unused declarations

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -6,21 +6,19 @@ const UUID = uuid.v4();
 
 export default function Home() {
   return (
-    <>
-      <div className={elementStyles.loginDiv}>
-        <div>
-          <img className={elementStyles.loginImage} src={`${process.env.PUBLIC_URL}/img/logo.svg`} alt="Isomer CMS logo" />
-          <button
-            type="button"
-            onClick={() => {
-              window.location.href = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&state=${UUID}&scope=repo`
-            }}
-            className={`${elementStyles.green} ${elementStyles.loginButton}`}
-          >
-            Login with GitHub
-          </button>
-        </div>
+    <div className={elementStyles.loginDiv}>
+      <div>
+        <img className={elementStyles.loginImage} src={`${process.env.PUBLIC_URL}/img/logo.svg`} alt="Isomer CMS logo" />
+        <button
+          type="button"
+          onClick={() => {
+            window.location.href = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&state=${UUID}&scope=repo`
+          }}
+          className={`${elementStyles.green} ${elementStyles.loginButton}`}
+        >
+          Login with GitHub
+        </button>
       </div>
-    </>
-  );
+    </div>
+  )
 }
---
+++ src/routing/ProtectedRoute.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Redirect, Route } from 'react-router-dom'
+import axios from 'axios'
+
+// Import contexts
+import { LoginConsumer } from '../contexts/LoginContext'
+
+// axios settings
+axios.defaults.withCredentials = true
+
+const ProtectedRoute = ({ children, component: WrappedComponent, ...rest }) => {
+  return (
+    <LoginConsumer>
+      {({userId}) => (
+        userId ? (
+        children || WrappedComponent && <Route {...rest} component={WrappedComponent}/>
+      ) : (
+        <Redirect to="/" /> 
+      ))}
+    </LoginConsumer>
+  )
+}
+
+export default ProtectedRoute;
