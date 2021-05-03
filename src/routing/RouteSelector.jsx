c66e67ef877382479b5f5a431486b2b7aecc1d50
Alexander Lee <alexander@open.gov.sg>
2021-05-03 15:51:15 +0800

    Fix: rebase errors

--- src/routing/RouteSelector.jsx
+++ src/routing/RouteSelector.jsx
@@ -51,6 +51,7 @@ export const RouteSelector = () => (
     <ProtectedRouteWithProps path="/sites/:siteName/resources/:resourceName/:fileName" component={EditPage} isCollectionPage={false} isResourcePage={true} />
     <ProtectedRouteWithProps path="/sites/:siteName/resources/:collectionName" component={CategoryPages} isResource={true}/>
     <ProtectedRouteWithProps path="/sites/:siteName/resources" component={Resources} />
+    <ProtectedRouteWithProps path="/sites/:siteName/navbar" component={EditNavBar} />
     <ProtectedRouteWithProps path="/sites/:siteName/settings" component={Settings} />
     <ProtectedRouteWithProps exact path="/sites" component={Sites} />
     <ProtectedRouteWithProps path="/" component={NotFoundPage}/>
