1649ac73c659f502b42815eee43ed9d597b63b2c
Preston Lim <prestonlimlianjie@gmail.com>
2021-05-10 17:57:52 +0800

    chore: set up basic Cypress E2E tests (#443)
    * chore: set up basic Cypress E2E tests
    
    * chore: remove unnecessary beforeEach in homepage test
    
    * chore: add instructions on how to run e2e tests
    
    * chore: add instructions to open Cypress UI
    
    * chore: remove unused var

---
+++ cypress/integration/homepage.spec.js
@@ -0,0 +1,24 @@
+describe('Login flow', () => {
+  const CMS_BASEURL = Cypress.env('BASEURL')
+  const LOGIN_BUTTON_TEXT = 'Login with GitHub'
+  const GITHUB_LOGIN_URL = 'https://github.com/login'
+
+  it('Home page should have login button', () => {
+    cy.visit(CMS_BASEURL)
+    cy.get('button').should('have.text', LOGIN_BUTTON_TEXT)
+  })
+
+  it('Login button should redirect to GitHub login on click', () => {
+    cy.visit(CMS_BASEURL)
+
+    // This is necessary to prevent the frontend from throwing the 401 error from the backend.
+    // TODO: We should handle this properly in the axios interceptor.
+    cy.on('uncaught:exception', (err, runnable) => {
+      // returning false here prevents Cypress from failing the test
+      return false
+    })
+    
+    cy.contains(LOGIN_BUTTON_TEXT).click()
+    cy.url().should('include', GITHUB_LOGIN_URL)
+  })
+})
