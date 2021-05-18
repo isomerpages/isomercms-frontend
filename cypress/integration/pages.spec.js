82104154c5a1542a197cf9bad6b5a1430bca5957
Preston Lim <prestonlimlianjie@gmail.com>
2021-05-18 16:12:20 +0800

    feat: add create, delete, edit page tests in folder

--- cypress/integration/pages.spec.js
+++ cypress/integration/pages.spec.js
@@ -1,4 +1,3 @@
-import { after } from 'lodash'
 import { deslugifyPage, deslugifyDirectory, generatePageFileName, slugifyCategory } from '../../src/utils'
 
 describe('Pages flow', () => {
@@ -14,6 +13,30 @@ describe('Pages flow', () => {
   const EDITED_TEST_PAGE_FILENAME = generatePageFileName(EDITED_TEST_PAGE_TITLE)
   const PRETTIFIED_PAGE_TITLE = deslugifyPage(TEST_PAGE_FILENAME)
   const EDITED_PRETTIFIED_PAGE_TITLE = deslugifyPage(EDITED_TEST_PAGE_FILENAME)
+
+  const TEST_FOLDER_NO_PAGES_TITLE = 'test folder title no pages'
+  const PARSED_TEST_FOLDER_NO_PAGES_TITLE = slugifyCategory(TEST_FOLDER_NO_PAGES_TITLE)
+  const PRETTIFIED_FOLDER_NO_PAGES_TITLE = deslugifyDirectory(PARSED_TEST_FOLDER_NO_PAGES_TITLE)
+
+  const TEST_FOLDER_WITH_PAGES_TITLE = 'test folder title with pages'
+  const PARSED_TEST_FOLDER_WITH_PAGES_TITLE = slugifyCategory(TEST_FOLDER_WITH_PAGES_TITLE)
+  const PRETTIFIED_FOLDER_WITH_PAGES_TITLE = deslugifyDirectory(PARSED_TEST_FOLDER_WITH_PAGES_TITLE)
+
+  const TEST_SUBFOLDER_NO_PAGES_TITLE = 'test subfolder title no pages'
+  const PARSED_TEST_SUBFOLDER_NO_PAGES_TITLE = slugifyCategory(TEST_SUBFOLDER_NO_PAGES_TITLE)
+  const PRETTIFIED_SUBFOLDER_NO_PAGES_TITLE = deslugifyDirectory(PARSED_TEST_SUBFOLDER_NO_PAGES_TITLE)
+
+  const TEST_SUBFOLDER_WITH_PAGES_TITLE = 'test subfolder title no pages'
+  const PARSED_TEST_SUBFOLDER_WITH_PAGES_TITLE = slugifyCategory(TEST_SUBFOLDER_WITH_PAGES_TITLE)
+  const PRETTIFIED_SUBFOLDER_WITH_PAGES_TITLE = deslugifyDirectory(PARSED_TEST_SUBFOLDER_WITH_PAGES_TITLE)
+
+  const EDITED_TEST_FOLDER_WITH_PAGES_TITLE = 'edited folder with pages'
+  const PARSED_EDITED_TEST_FOLDER_WITH_PAGES_TITLE = slugifyCategory(EDITED_TEST_FOLDER_WITH_PAGES_TITLE)
+  const PRETTIFIED_EDITED_FOLDER_WITH_PAGES_TITLE = deslugifyDirectory(PARSED_EDITED_TEST_FOLDER_WITH_PAGES_TITLE)
+
+  const EDITED_TEST_SUBFOLDER_WITH_PAGES_TITLE = 'edited subfolder with pages'
+  const PARSED_EDITED_TEST_SUBFOLDER_WITH_PAGES_TITLE = slugifyCategory(EDITED_TEST_SUBFOLDER_WITH_PAGES_TITLE)
+  const PRETTIFIED_EDITED_SUBFOLDER_WITH_PAGES_TITLE = deslugifyDirectory(PARSED_EDITED_TEST_SUBFOLDER_WITH_PAGES_TITLE)
 
   describe('Create page, delete page, edit page settings in Workspace', () => {
     beforeEach(() => {
@@ -111,30 +134,6 @@ describe('Pages flow', () => {
   })
 
   describe('Create folder/subfolder, rename folder/subfolder, delete folder/subfolder', () => {
-    const TEST_FOLDER_NO_PAGES_TITLE = 'test folder title no pages'
-    const PARSED_TEST_FOLDER_NO_PAGES_TITLE = slugifyCategory(TEST_FOLDER_NO_PAGES_TITLE)
-    const PRETTIFIED_FOLDER_NO_PAGES_TITLE = deslugifyDirectory(PARSED_TEST_FOLDER_NO_PAGES_TITLE)
-
-    const TEST_FOLDER_WITH_PAGES_TITLE = 'test folder title with pages'
-    const PARSED_TEST_FOLDER_WITH_PAGES_TITLE = slugifyCategory(TEST_FOLDER_WITH_PAGES_TITLE)
-    const PRETTIFIED_FOLDER_WITH_PAGES_TITLE = deslugifyDirectory(PARSED_TEST_FOLDER_WITH_PAGES_TITLE)
-
-    const TEST_SUBFOLDER_NO_PAGES_TITLE = 'test subfolder title no pages'
-    const PARSED_TEST_SUBFOLDER_NO_PAGES_TITLE = slugifyCategory(TEST_SUBFOLDER_NO_PAGES_TITLE)
-    const PRETTIFIED_SUBFOLDER_NO_PAGES_TITLE = deslugifyDirectory(PARSED_TEST_SUBFOLDER_NO_PAGES_TITLE)
-
-    const TEST_SUBFOLDER_WITH_PAGES_TITLE = 'test subfolder title no pages'
-    const PARSED_TEST_SUBFOLDER_WITH_PAGES_TITLE = slugifyCategory(TEST_SUBFOLDER_WITH_PAGES_TITLE)
-    const PRETTIFIED_SUBFOLDER_WITH_PAGES_TITLE = deslugifyDirectory(PARSED_TEST_SUBFOLDER_WITH_PAGES_TITLE)
-
-    const EDITED_TEST_FOLDER_WITH_PAGES_TITLE = 'edited folder with pages'
-    const PARSED_EDITED_TEST_FOLDER_WITH_PAGES_TITLE = slugifyCategory(EDITED_TEST_FOLDER_WITH_PAGES_TITLE)
-    const PRETTIFIED_EDITED_FOLDER_WITH_PAGES_TITLE = deslugifyDirectory(PARSED_EDITED_TEST_FOLDER_WITH_PAGES_TITLE)
-
-    const EDITED_TEST_SUBFOLDER_WITH_PAGES_TITLE = 'edited subfolder with pages'
-    const PARSED_EDITED_TEST_SUBFOLDER_WITH_PAGES_TITLE = slugifyCategory(EDITED_TEST_SUBFOLDER_WITH_PAGES_TITLE)
-    const PRETTIFIED_EDITED_SUBFOLDER_WITH_PAGES_TITLE = deslugifyDirectory(PARSED_EDITED_TEST_SUBFOLDER_WITH_PAGES_TITLE)
-
     beforeEach(() => {
       cy.setCookie(COOKIE_NAME, COOKIE_VALUE)
       window.localStorage.setItem('userId', 'test')
@@ -317,6 +316,104 @@ describe('Pages flow', () => {
 
       cy.wait(4000)
       cy.contains(PRETTIFIED_EDITED_FOLDER_WITH_PAGES_TITLE).should('not.exist')
+    })
+  })
+
+  describe('Create page, delete page, edit page settings in folder', () => {
+    before(() => {
+      cy.setCookie(COOKIE_NAME, COOKIE_VALUE)
+      window.localStorage.setItem('userId', 'test')
+      cy.visit(`${CMS_BASEURL}/sites/${TEST_REPO_NAME}/workspace`)
+      cy.contains('Create new folder').click()
+      cy.get('input#folder').clear().type(TEST_FOLDER_NO_PAGES_TITLE)
+      cy.contains('Select pages').click()
+      cy.contains('Done').click()
+
+      // Assert
+      cy.wait(4000)
+      cy.contains(PRETTIFIED_FOLDER_NO_PAGES_TITLE)
+      cy.url().should('include', `${CMS_BASEURL}/sites/${TEST_REPO_NAME}/folder/${PARSED_TEST_FOLDER_NO_PAGES_TITLE}`)
+      cy.contains('There are no pages in this folder.')
+    })
+
+    beforeEach(() => {
+      cy.setCookie(COOKIE_NAME, COOKIE_VALUE)
+      window.localStorage.setItem('userId', 'test')
+      cy.visit(`${CMS_BASEURL}/sites/${TEST_REPO_NAME}/folder/${PARSED_TEST_FOLDER_NO_PAGES_TITLE}`)
+    })
+  
+    it('Should be able to create a new page with valid title and permalink', () => {
+      cy.contains('Create new page').click()
+      cy.get('#title').clear().type(TEST_PAGE_TITLE)
+      cy.get('#permalink').clear().type(TEST_PAGE_PERMALNK)
+      cy.contains('Save').click()
+
+      // Asserts
+      // 1. User should be redirected to correct EditPage
+      cy.url().should('include', `${CMS_BASEURL}/sites/${TEST_REPO_NAME}/folder/${PARSED_TEST_FOLDER_NO_PAGES_TITLE}/${TEST_PAGE_FILENAME}`)
+      cy.contains(PRETTIFIED_PAGE_TITLE)
+
+      // 2. If user goes back to the workspace, they should be able to see that the page exists
+      cy.contains('button', PRETTIFIED_FOLDER_NO_PAGES_TITLE).click()
+      cy.contains(PRETTIFIED_PAGE_TITLE)
+    })
+
+    it('Should not be able to create page with invalid title or permalink', () => {
+      const SHORT_TITLE = 'abc'
+      const SHORT_PERMALINK = '/12'
+      const INVALID_PERMALINK = 'test-'
+
+      cy.contains('Create new page').click()
+
+      // Page title has to be more than 4 characters long
+      cy.get('#title').clear().type(SHORT_TITLE)
+      cy.get('#permalink').clear().type(TEST_PAGE_PERMALNK)
+      cy.contains('The title should be longer than 4 characters.')
+      cy.contains('button', 'Save').should('be.disabled')
+  
+      // Page title must not already exist
+      cy.get('#title').clear().type(TEST_PAGE_TITLE)
+      cy.contains('This title is already in use. Please choose a different title.')
+      cy.contains('button', 'Save').should('be.disabled')
+
+      // Permalink needs to be longer than 4 characters
+      cy.get('#title').clear().type(EDITED_TEST_PAGE_TITLE)
+      cy.get('#permalink').clear().type(SHORT_PERMALINK)
+      cy.contains('The permalink should be longer than 4 characters.')
+      cy.contains('button', 'Save').should('be.disabled')
+  
+      // Permalink should start with a slash, and contain alphanumeric characters separated by hyphens and slashes only
+      cy.get('#title').clear().type(EDITED_TEST_PAGE_TITLE)
+      cy.get('#permalink').clear().type(INVALID_PERMALINK)
+      cy.contains('The url should start with a slash, and contain alphanumeric characters separated by hyphens and slashes only.')
+      cy.contains('button', 'Save').should('be.disabled')
+    })
+
+    it('Should be able to edit existing page details with valid title and permalink', () => {
+      const testPageCard = cy.contains(PRETTIFIED_PAGE_TITLE)
+
+      // User should be able edit page details
+      cy.get('.bx-dots-vertical-rounded').parent().click()
+      cy.get('div[id^=settings-]').first().click()
+      cy.get('#title').clear().type(EDITED_TEST_PAGE_TITLE)
+      cy.contains('button', 'Save').click()
+
+      // New page title should be reflected in the Workspace
+      cy.contains(EDITED_PRETTIFIED_PAGE_TITLE)
+    })
+
+    it('Should be able to delete existing page on workspace', () => {
+      const testPageCard = cy.contains(EDITED_PRETTIFIED_PAGE_TITLE)
+
+      // Assert
+      // User should be able to remove the created test page card
+      cy.contains(EDITED_PRETTIFIED_PAGE_TITLE)
+      cy.get('.bx-dots-vertical-rounded').parent().click()
+      cy.get('div[id^=delete-]').first().click()
+      cy.contains('button', 'Delete').click()
+      cy.wait(2000)
+
+      cy.contains(EDITED_PRETTIFIED_PAGE_TITLE).should('not.exist')
     })
   })
 })
