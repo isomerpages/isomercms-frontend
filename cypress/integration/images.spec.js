bb00b2eccbd6e60a6f9504d8c6764a1c3636f686
Alexis <gweiying@gmail.com>
2021-05-31 13:42:07 +0800

    fix: reorders structure of Image tests
    This commit restructures the ordering of image tests to set the cookie value within each test, in line with what is being done in Pages. This allows for a before and after hook to run to setup and tear down an image album as part of the tests for image flows inside an image album

--- cypress/integration/images.spec.js
+++ cypress/integration/images.spec.js
@@ -8,24 +8,32 @@ describe('Images', () => {
   const TEST_REPO_NAME = Cypress.env('TEST_REPO_NAME')
   const CUSTOM_TIMEOUT = Cypress.env('CUSTOM_TIMEOUT')
 
-  const TEST_IMAGE_PATH = 'images/singapore.jpg'
   const IMAGE_TITLE = 'singapore.jpg'
-  const ALBUM_TITLE = 'Singapore'
+  const OTHER_IMAGE_TITLE = 'america.jpg'
+  const TEST_IMAGE_PATH = 'images/singapore.jpg'
+  
+  const ALBUM_TITLE = 'Purple'
+  const OTHER_ALBUM_TITLE = 'Green'     
+  const SLUGIFIED_ALBUM_TITLE = slugifyCategory(ALBUM_TITLE)
 
-  beforeEach(() => {
-    cy.setCookie(COOKIE_NAME, COOKIE_VALUE)
-    window.localStorage.setItem('userId', 'test')
-    cy.visit(`/sites/${TEST_REPO_NAME}/images`)
-  })
+  const MISSING_EXTENSION = 'singapore'
+  const INVALID_CHARACTER = '!!.png'
+  const ACTION_DISABLED = 'true'
 
-  it('Images should contain Albums and Ungrouped Images', () => {
-    cy.contains('Albums')
-    cy.contains('Ungrouped images')
-    cy.contains('Upload new image')
-    cy.contains('Create new album')
-  })
+  describe('Create image, delete image, edit image settings in Images', () => {
+    beforeEach(() => {
+      cy.setCookie(COOKIE_NAME, COOKIE_VALUE)
+      window.localStorage.setItem('userId', 'test')
 
-  describe('Images flow', () => {
+      cy.visit(`/sites/${TEST_REPO_NAME}/images`)
+    })
+
+    it('Images should contain albums and ungrouped images', () => {
+      cy.contains('Albums')
+      cy.contains('Ungrouped images')
+      cy.contains('Upload new image')
+      cy.contains('Create new album')
+    })
 
     it('Should be able to create new image with valid title', () => {
       // Set intercept to listen for POST request on server
@@ -39,23 +47,17 @@ describe('Images', () => {
     })
 
     it('Should be able to edit an image', () => {
-      const NEW_IMAGE_TITLE = 'america.jpg'
-      
-      cy.renameMedia(IMAGE_TITLE, NEW_IMAGE_TITLE)
+      cy.renameMedia(IMAGE_TITLE, OTHER_IMAGE_TITLE)
       // ASSERTS
-      cy.contains(NEW_IMAGE_TITLE) // Image should be contained in Images
+      cy.contains(OTHER_IMAGE_TITLE) // Image should be contained in Images
 
-      cy.renameMedia(NEW_IMAGE_TITLE, IMAGE_TITLE) // Rename image to original title 
+      cy.renameMedia(OTHER_IMAGE_TITLE, IMAGE_TITLE) // Rename image to original title 
       // ASSERTS
       cy.contains(IMAGE_TITLE)
     })
 
-
     it('Should not be able to create image with invalid title', () => {
-      const MISSING_EXTENSION = 'singapore'
-      const INVALID_CHARACTER = '!!.png'
-      const ACTION_DISABLED = 'true'
-      
+
       // should not be able to save with invalid characters in title 
       cy.uploadMedia(INVALID_CHARACTER, TEST_IMAGE_PATH, ACTION_DISABLED)
       // ASSERTS
@@ -78,11 +80,7 @@ describe('Images', () => {
     })
 
     it('Should not be able to edit image and save with invalid title', () => {
-      const MISSING_EXTENSION = 'singapore'
-      const INVALID_CHARACTER = '!!.png'
-      const ACTION_DISABLED = 'true'
-      const OTHER_IMAGE_TITLE = 'america.png'
-      
+
       // should not be able to save with invalid characters in title 
       cy.renameMedia(IMAGE_TITLE, INVALID_CHARACTER, ACTION_DISABLED)
       // ASSERTS
@@ -124,7 +122,14 @@ describe('Images', () => {
     })
   })
 
-  describe('Image Albums flows', () => {
+  describe('Create image album, delete image album, edit image album settings in Images', () => {
+    
+    beforeEach(() => {
+      cy.setCookie(COOKIE_NAME, COOKIE_VALUE)
+      window.localStorage.setItem('userId', 'test')
+
+      cy.visit(`/sites/${TEST_REPO_NAME}/images`)
+    })
     
     it('Should be able to create new image album', () => {
       // Set intercept to listen for POST request on server
@@ -137,24 +142,23 @@ describe('Images', () => {
 
       // ASSERTS
       cy.wait('@createAlbum') // Should intercept POST request
-      cy.contains(ALBUM_TITLE) // Album name should be contained in Images
+      cy.contains(ALBUM_TITLE, { timeout: CUSTOM_TIMEOUT} ).should('exist') // Album name should be contained in Images
     })
 
     it('Should be able to edit image album name', () => {
-      const NEW_ALBUM_TITLE = 'America'
       
       // User should be able edit album details
       const testAlbumCard = cy.contains(ALBUM_TITLE)
       testAlbumCard.children().within(() => cy.get('[id^=settings-]').click())
       cy.get('div[id^=settings-]').first().click() // .first() is necessary because the get returns multiple elements (refer to MenuDropdown.jsx)
-      cy.get('#newDirectoryName').clear().type(NEW_ALBUM_TITLE)
+      cy.get('#newDirectoryName').clear().type(OTHER_ALBUM_TITLE)
       cy.contains('button', 'Save').click()
 
       // ASSERTS
-      cy.contains(NEW_ALBUM_TITLE, { timeout: CUSTOM_TIMEOUT} ).should('exist') // New image album name should be contained in Images
+      cy.contains(OTHER_ALBUM_TITLE, { timeout: CUSTOM_TIMEOUT} ).should('exist') // New image album name should be contained in Images
 
       // Rename album to original album title
-      const newTestAlbumCard = cy.contains(NEW_ALBUM_TITLE)
+      const newTestAlbumCard = cy.contains(OTHER_ALBUM_TITLE)
       newTestAlbumCard.children().within(() => cy.get('[id^=settings-]').click())
       cy.get('div[id^=settings-]').first().click() // .first() is necessary because the get returns multiple elements (refer to MenuDropdown.jsx)
       cy.get('#newDirectoryName').clear().type(ALBUM_TITLE)
@@ -180,12 +184,25 @@ describe('Images', () => {
     })
   })
 
-  describe('Nested Images flow', () => {
-    
-    const EXISTING_ALBUM = 'hello-new-album' // Should be existing  since we don't want the tests to fail in cascade 
+  describe('Create image, delete image, edit image settings, and move images in image albums', () => {
+    before(() => {
+      cy.setCookie(COOKIE_NAME, COOKIE_VALUE)
+      window.localStorage.setItem('userId', 'test')
+
+      cy.visit(`/sites/${TEST_REPO_NAME}/images`)
+      cy.contains('Create new album').click()
+      cy.get("[id='image album']").clear().type(ALBUM_TITLE)  
+      cy.get('button').contains(/^Create$/).click() 
+
+      // Assert
+      cy.contains(ALBUM_TITLE, { timeout: CUSTOM_TIMEOUT}).should('exist')
+    })
 
     beforeEach(() => {
-      cy.visit(`/sites/${TEST_REPO_NAME}/images/${EXISTING_ALBUM}`)
+      cy.setCookie(COOKIE_NAME, COOKIE_VALUE)
+      window.localStorage.setItem('userId', 'test')
+      
+      cy.visit(`/sites/${TEST_REPO_NAME}/images/${SLUGIFIED_ALBUM_TITLE}`)
     })
 
     it('Should be able to add image to image album', () => {
@@ -199,20 +216,18 @@ describe('Images', () => {
     })
 
     it('Should be able to edit an image in image album', () => {
-      const NEW_IMAGE_TITLE = 'america.jpg'
-      
-      cy.renameMedia(IMAGE_TITLE, NEW_IMAGE_TITLE)
+      cy.renameMedia(IMAGE_TITLE, OTHER_IMAGE_TITLE)
       // ASSERTS
-      cy.contains(NEW_IMAGE_TITLE) // Image should be contained in Images
+      cy.contains(OTHER_IMAGE_TITLE) // Image should be contained in Images
 
-      cy.renameMedia(NEW_IMAGE_TITLE, IMAGE_TITLE) // Rename image to original title 
+      cy.renameMedia(OTHER_IMAGE_TITLE, IMAGE_TITLE) // Rename image to original title 
       // ASSERTS
       cy.contains(IMAGE_TITLE)
     })
 
     it('Should be able to delete image from image album', () => {
       // Set intercept to listen for POST request on server
-      cy.intercept({ method:'DELETE', url: `/v1/sites/${TEST_REPO_NAME}/images/${generateImageorFilePath(EXISTING_ALBUM, IMAGE_TITLE)}` }).as('deleteImage')
+      cy.intercept({ method:'DELETE', url: `/v1/sites/${TEST_REPO_NAME}/images/${generateImageorFilePath(SLUGIFIED_ALBUM_TITLE, IMAGE_TITLE)}` }).as('deleteImage')
 
       cy.deleteMedia(IMAGE_TITLE)
       // ASSERTS
@@ -222,7 +237,7 @@ describe('Images', () => {
 
     it('Should be able to move image from image album to Images', () => {
       // Set intercept to listen for POST request on server
-      cy.intercept({ method:'POST', url: `/v1/sites/${TEST_REPO_NAME}/images/${encodeURIComponent(`${EXISTING_ALBUM}/${IMAGE_TITLE}`)}/move/${IMAGE_TITLE}`}).as('moveImage')
+      cy.intercept({ method:'POST', url: `/v1/sites/${TEST_REPO_NAME}/images/${encodeURIComponent(`${SLUGIFIED_ALBUM_TITLE}/${IMAGE_TITLE}`)}/move/${IMAGE_TITLE}`}).as('moveImage')
 
       cy.uploadMedia(IMAGE_TITLE, TEST_IMAGE_PATH)
       cy.moveMedia(IMAGE_TITLE)
@@ -236,17 +251,28 @@ describe('Images', () => {
 
     it('Should be able to move image from Images to image album', () => {
       // Set intercept to listen for POST request on server
-      cy.intercept({ method:'POST', url: `/v1/sites/${TEST_REPO_NAME}/images/${IMAGE_TITLE}/move/${encodeURIComponent(`${EXISTING_ALBUM}/${IMAGE_TITLE}`)}`}).as('moveImage')
+      cy.intercept({ method:'POST', url: `/v1/sites/${TEST_REPO_NAME}/images/${IMAGE_TITLE}/move/${encodeURIComponent(`${SLUGIFIED_ALBUM_TITLE}/${IMAGE_TITLE}`)}`}).as('moveImage')
 
       cy.visit(`/sites/${TEST_REPO_NAME}/images`)
       cy.uploadMedia(IMAGE_TITLE, TEST_IMAGE_PATH)
-      cy.moveMedia(IMAGE_TITLE, EXISTING_ALBUM)
+      cy.moveMedia(IMAGE_TITLE, SLUGIFIED_ALBUM_TITLE)
       cy.wait('@moveImage')  // should intercept POST request
 
-      cy.visit(`/sites/${TEST_REPO_NAME}/images/${EXISTING_ALBUM}`)
+      cy.visit(`/sites/${TEST_REPO_NAME}/images/${SLUGIFIED_ALBUM_TITLE}`)
       cy.contains(IMAGE_TITLE) // image should be contained in album
       
       cy.deleteMedia(IMAGE_TITLE) // cleanup
+    })
+
+    after(() => {
+      cy.visit(`/sites/${TEST_REPO_NAME}/images`)
+      const testAlbumCard = cy.contains(ALBUM_TITLE)
+      testAlbumCard.children().within(() => cy.get('[id^=settings-]').click())
+      cy.get('div[id^=delete-]').first().click() // .first() is necessary because the get returns multiple elements (refer to MenuDropdown.jsx)
+      cy.contains('button', 'Delete').click()
+
+      // ASSERTS
+      cy.contains(ALBUM_TITLE, { timeout: CUSTOM_TIMEOUT}).should('not.exist')
     })
   })
 })
