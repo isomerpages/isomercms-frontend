d00bbcb5e4aba2056d3caccbc30e18ce5f542c1d
Preston Lim <prestonlimlianjie@gmail.com>
2021-06-07 17:21:41 +0800

    style: import correct lib

--- src/__tests__/RouteSelector.js
+++ src/__tests__/RouteSelector.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
-import { MemoryRouter } from "react-router"
+import { MemoryRouter } from "react-router-dom"
 import "@testing-library/jest-dom/extend-expect"
 
 import { RouteSelector } from "../routing/RouteSelector"
