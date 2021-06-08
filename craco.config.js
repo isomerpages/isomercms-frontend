10604a9ce3b7a3a93051a773a68f9fea58a589e0
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2021-06-08 10:29:55 +0800

    BREAKING CHANGE: use craco instead of react-scripts
    Craco stands for create react app configuration override, and it's a
    library which does just that. The reason we're introducing craco is
    because create-react-app has complicated webpack config which means
    that module-alias does not work with create-react-app. The solution
    is to use craco to define webpack aliases instead, through the
    craco.config.js file.
    
    This commit does the following:
    - uninstall module-alias, and remove module aliases from package.json
    - install craco
    - replace `react-scripts` with `craco` in package.json scripts
    - define path aliases in craco config

---
+++ craco.config.js
@@ -0,0 +1,19 @@
+/* craco.config.js */
+const path = require(`path`)
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@root': path.resolve(__dirname, '.'),
+      '@src': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@contexts': path.resolve(__dirname, 'src/contexts'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@layouts': path.resolve(__dirname, 'src/layouts'),
+      '@routing': path.resolve(__dirname, 'src/routing'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@templates': path.resolve(__dirname, 'src/templates'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    }
+  },
+}
