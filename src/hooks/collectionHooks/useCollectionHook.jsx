7469e713e0bb1a2e8a436a158b843411d4defb5b
Alexis <gweiying@gmail.com>
2021-08-10 15:14:56 +0800

    feat: add v1 useCollectionHook, to be refactored

---
+++ src/hooks/collectionHooks/useCollectionHook.jsx
@@ -0,0 +1,41 @@
+// used in EditPageV2
+// currently uses the v1 collection endpoints, to be refactored after v2 collection endpoints are completed
+
+import axios from "axios"
+
+import { useQuery } from "react-query"
+import { errorToast } from "../../utils/toasts"
+import { parseDirectoryFile } from "../../utils"
+import { DIR_CONTENT_KEY } from "../queryKeys"
+import useRedirectHook from "../useRedirectHook"
+
+const getDirectoryFile = async ({ siteName, folderName }) => {
+  if (!folderName) return undefined
+  const resp = await axios.get(
+    `${process.env.REACT_APP_BACKEND_URL}/sites/${siteName}/collections/${folderName}/pages/collection.yml`
+  )
+  const { content: dirContent } = resp.data
+  const { order: parsedFolderContents } = parseDirectoryFile(dirContent)
+  // Filter out placeholder files
+  const filteredFolderContents = parsedFolderContents.filter(
+    (name) => !name.includes(".keep")
+  )
+  return filteredFolderContents
+}
+
+// get directory data
+export function useCollectionHook(params) {
+  const { setRedirectToNotFound } = useRedirectHook()
+  return useQuery([DIR_CONTENT_KEY, params], () => getDirectoryFile(params), {
+    retry: false,
+    onError: (err) => {
+      if (err.response && err.response.status === 404) {
+        setRedirectToNotFound(siteName)
+      } else {
+        errorToast(
+          `There was a problem trying to load your page. ${DEFAULT_RETRY_MSG}`
+        )
+      }
+    },
+  })
+}
