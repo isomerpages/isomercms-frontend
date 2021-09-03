0c6893eead9ab6e2cc68c9ad155e66f71296d548
Alexis <gweiying@gmail.com>
2021-09-03 11:59:02 +0800

    feat(screens): add layout screens for react query hooks

---
+++ src/layouts/foldersScreens/FoldersPageMoveScreen.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { useParams } from "react-router-dom"
+
+import { useMoveHook } from "../../hooks/moveHooks"
+
+import MoveModal from "../../components/MoveModal"
+
+export const FoldersPageMoveScreen = ({ onClose }) => {
+  const params = useParams()
+
+  const { mutateAsync: moveHandler } = useMoveHook(
+    (({ fileName, ...p }) => p)(params),
+    {
+      onSuccess: () => onClose(),
+    }
+  )
+
+  return <MoveModal params={params} onProceed={moveHandler} onClose={onClose} />
+}
+
+FoldersPageMoveScreen.propTypes = {
+  onClose: PropTypes.func.isRequired,
+}
