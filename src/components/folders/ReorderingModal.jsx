eaa159d9e3379b0554f26708949b21df27032e4f
Alexis <gweiying@gmail.com>
2021-09-03 11:51:52 +0800

    feat(reorder): update reorder modal

---
+++ src/components/folders/ReorderingModal.jsx
@@ -0,0 +1,171 @@
+import React, { useEffect, useState } from "react"
+import PropTypes from "prop-types"
+import { Droppable, Draggable, DragDropContext } from "react-beautiful-dnd"
+
+import update from "immutability-helper"
+
+import { FolderItem } from "./FolderContent"
+import LoadingButton from "../LoadingButton"
+
+import { deslugifyDirectory } from "../../utils"
+
+// Import styles
+import adminStyles from "../../styles/isomer-cms/pages/Admin.module.scss"
+import elementStyles from "../../styles/isomer-cms/Elements.module.scss"
+import contentStyles from "../../styles/isomer-cms/pages/Content.module.scss"
+
+import Breadcrumb from "./Breadcrumb"
+
+const ReorderingModal = ({ params, pagesData, onProceed, onClose }) => {
+  const { collectionName, subCollectionName } = params
+  const [pagesOrder, setPagesOrder] = useState([])
+
+  /** ******************************** */
+  /*     useEffects to load data     */
+  /** ******************************** */
+
+  useEffect(() => {
+    setPagesOrder(pagesData)
+  }, [pagesData])
+
+  /** ******************************** */
+  /*     handler functions    */
+  /** ******************************** */
+
+  const onDragEnd = (result) => {
+    const { source, destination } = result
+
+    // If the user dropped the draggable to no known droppable
+    if (!destination) return
+
+    // The draggable elem was returned to its original position
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return
+
+    const elem = pagesOrder[source.index]
+    const newPagesOrder = update(pagesOrder, {
+      $splice: [
+        [source.index, 1], // Remove elem from its original position
+        [destination.index, 0, elem], // Splice elem into its new position
+      ],
+    })
+    setPagesOrder(newPagesOrder)
+  }
+
+  return (
+    <>
+      <div className={elementStyles.overlay}>
+        <div className={`${elementStyles.fullscreenWrapper}`}>
+          <div
+            className={`${adminStyles.adminSidebar} ${elementStyles.wrappedContent} bg-transparent`}
+          />
+          <div
+            className={`${contentStyles.mainSection} ${elementStyles.wrappedContent} bg-light`}
+          >
+            {/* Page title */}
+            <div className={contentStyles.sectionHeader}>
+              <h1 className={contentStyles.sectionTitle}>{`Rearrange items in ${
+                subCollectionName
+                  ? deslugifyDirectory(subCollectionName)
+                  : deslugifyDirectory(collectionName)
+              }`}</h1>
+            </div>
+            <div className={contentStyles.segment}>
+              <i className="bx bx-sm bx-bulb text-dark" />
+              <span>
+                <strong className="ml-1">Pro tip:</strong> Drag and drop the
+                items below to rearrange their order in your site.
+              </span>
+            </div>
+            {/* Segment divider  */}
+            <div className={contentStyles.segmentDividerContainer}>
+              <hr className="w-100 mt-3 mb-5" />
+            </div>
+            <br />
+            {/* Breadcrumb */}
+            <div className={contentStyles.segment}>
+              <Breadcrumb params={params} />
+            </div>
+            {/* Pages */}
+            <DragDropContext onDragEnd={onDragEnd}>
+              <Droppable droppableId="folder" type="folder">
+                {(droppableProvided) => (
+                  <div
+                    className={`${contentStyles.contentContainerFolderColumn} mb-5`}
+                    ref={droppableProvided.innerRef}
+                    {...droppableProvided.droppableProps}
+                  >
+                    {pagesOrder.map((folderContentItem, folderContentIndex) => (
+                      <Draggable
+                        draggableId={folderContentItem.name}
+                        index={folderContentIndex}
+                        key={folderContentItem.name}
+                      >
+                        {(draggableProvided) => (
+                          <div
+                            key={folderContentIndex}
+                            {...draggableProvided.draggableProps}
+                            {...draggableProvided.dragHandleProps}
+                            ref={draggableProvided.innerRef}
+                          >
+                            <FolderItem
+                              key={folderContentItem.name}
+                              item={folderContentItem}
+                              itemIndex={folderContentIndex}
+                              isLinkDisabled
+                            />
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {droppableProvided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </div>
+          <div className={contentStyles.sectionFooter}>
+            <LoadingButton
+              label="Cancel"
+              disabledStyle={elementStyles.disabled}
+              className={`${elementStyles.warning}`}
+              callback={onClose}
+            />
+            <LoadingButton
+              label="Save"
+              className={elementStyles.blue}
+              callback={() =>
+                onProceed({
+                  items: pagesOrder,
+                })
+              }
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ReorderingModal
+
+ReorderingModal.propTypes = {
+  siteName: PropTypes.string.isRequired,
+  folderName: PropTypes.string.isRequired,
+  subfolderName: PropTypes.string,
+  folderOrderArray: PropTypes.arrayOf(
+    PropTypes.shape({
+      fileName: PropTypes.string.isRequired,
+      path: PropTypes.string.isRequired,
+      sha: PropTypes.string,
+      type: PropTypes.string,
+    })
+  ).isRequired,
+  setIsRearrangeActive: PropTypes.func.isRequired,
+  directoryFileSha: PropTypes.string.isRequired,
+  parsedFolderContents: PropTypes.arrayOf(PropTypes.string).isRequired,
+  isFolderLive: PropTypes.bool.isRequired,
+}
