83b39f8bd927b0aa5fb3e014c308b26c993b04c1
Alexis <gweiying@gmail.com>
2021-09-16 16:53:11 +0800

    fix(validation): allows capital letters on permalink

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -10,7 +10,7 @@ import {
 
 // Common regexes and constants
 // ==============
-const PERMALINK_REGEX = "^((/([a-z0-9]+-)*[a-z0-9]+)+)/?$"
+const PERMALINK_REGEX = "^((/([a-zA-Z0-9]+-)*[a-z0-9]+)+)/?$"
 const URL_REGEX_PART_1 = "^(https://)?(www.)?("
 const URL_REGEX_PART_2 = ".com/)([a-zA-Z0-9_-]+([/.])?)+$"
 const TELEGRAM_REGEX = "telegram|t).me/([a-zA-Z0-9_-]+([/.])?)+$"
