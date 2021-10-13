0021dbfabdd75d1b53b4a085fe67dcba45e3ce6d
Alexis <gweiying@gmail.com>
2021-10-13 17:41:24 +0800

    tests: update Cypress tests for BackButton text

--- cypress/integration/workspace.spec.js
+++ cypress/integration/workspace.spec.js
@@ -83,7 +83,7 @@ describe("Workspace Pages flow", () => {
       cy.contains(TEST_PAGE_TITLE, { timeout: CUSTOM_TIMEOUT }).should("exist")
 
       // 2. If user goes back to the workspace, they should be able to see that the page exists
-      cy.contains("My Workspace", { timeout: CUSTOM_TIMEOUT })
+      cy.contains("Back to Workspace", { timeout: CUSTOM_TIMEOUT })
         .should("exist")
         .click()
       cy.contains(TEST_PAGE_FILENAME, { timeout: CUSTOM_TIMEOUT }).should(
