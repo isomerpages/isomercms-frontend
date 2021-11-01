dc4cbde0a71efca831a3586a026ef454a8e04119
Alexander Lee <alexander@open.gov.sg>
2021-11-01 10:36:55 +0800

    Chore: shift settings components into separate folder

--- src/components/settings/ColorPicker.jsx
+++ src/components/settings/ColorPicker.jsx
@@ -1,7 +1,6 @@
 import PropTypes from "prop-types"
 import React, { useState } from "react"
 import { SketchPicker } from "react-color"
-
 import elementStyles from "styles/isomer-cms/Elements.module.scss"
 
 const ColorPicker = ({ value, onColorSelect, setRealTimeColor }) => {
