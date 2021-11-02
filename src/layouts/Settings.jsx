2aa1077bba95141f4d30215aeb8ebc7bd334669d
Alexander Lee <alexander@open.gov.sg>
2021-11-02 13:12:15 +0800

    Chore: clean up unused imports

--- src/layouts/Settings.jsx
+++ src/layouts/Settings.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import * as _ from "lodash"
 import PropTypes from "prop-types"
 
