b75274b552e2be86d396d3312a6aabdbb7c9056a
Alexis <gweiying@gmail.com>
2021-11-02 12:45:34 +0800

    feat: add useForm and Yup object schema to PageSettingsModal

--- src/components/PageSettingsModal/PageSettingsModalV2.jsx
+++ src/components/PageSettingsModal/PageSettingsModalV2.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 import axios from "axios"
 import * as _ from "lodash"
 
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
 import elementStyles from "../../styles/isomer-cms/Elements.module.scss"
-
-import { validatePageSettings } from "../../utils/validators"
 
 import { getDefaultFrontMatter } from "../../utils"
 
@@ -13,6 +13,8 @@ import FormField from "../FormField"
 import FormFieldHorizontal from "../FormFieldHorizontal"
 import SaveDeleteButtons from "../SaveDeleteButtons"
 import Breadcrumb from "../folders/Breadcrumb"
+
+import { PageSettingsSchema } from "."
 
 // axios settings
 axios.defaults.withCredentials = true
@@ -27,80 +29,66 @@ export const PageSettingsModal = ({
 }) => {
   const { fileName } = params
 
+  const existingTitlesArray = dirData
+    .filter((item) => item.type === "file")
+    .map((page) => page.name)
+    .filter((pageName) => pageName !== fileName)
+    .map((pageName) => pageName.split(".")[0])
+
   const { exampleTitle, examplePermalink } = getDefaultFrontMatter(
     params,
-    dirData.filter((item) => item.type === "file").map((item) => item.name)
+    existingTitlesArray
   )
 
-  const [errors, setErrors] = useState({})
-  const [hasErrors, setHasErrors] = useState(false)
-  const [hasChanges, setHasChanges] = useState(false)
-
-  // Base hooks
-  const [title, setTitle] = useState(fileName ? "" : exampleTitle)
-  const [permalink, setPermalink] = useState(fileName ? "" : examplePermalink)
-
-  const idToSetterFuncMap = {
-    title: setTitle,
-    permalink: setPermalink,
-  }
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm({
+    mode: "onBlur",
+    resolver: yupResolver(PageSettingsSchema(existingTitlesArray)),
+    defaultValues: {
+      title: fileName ? "" : exampleTitle,
+      permalink: fileName ? "" : examplePermalink,
+    },
+  })
 
   /** ******************************** */
   /*     useEffects to load data     */
   /** ******************************** */
 
   useEffect(() => {
-    if (fileName && pageData && !hasChanges && pageData.content) {
-      setTitle(pageData.content.frontMatter.title)
-      setPermalink(pageData.content.frontMatter.permalink)
-      setErrors({
-        title: validatePageSettings(
-          "title",
-          pageData.content.frontMatter.title
-        ),
-        permalink: validatePageSettings(
-          "permalink",
-          pageData.content.frontMatter.permalink
-        ),
+    if (fileName && pageData && pageData.content) {
+      setValue("title", pageData.content.frontMatter.title, {
+        shouldValidate: true,
+      })
+      setValue("permalink", pageData.content.frontMatter.permalink, {
+        shouldValidate: true,
       })
     }
-  }, [pageData])
-
-  useEffect(() => {
-    setHasErrors(_.some(errors, (field) => field.length > 0))
-  }, [errors])
-
-  useEffect(() => {
-    setHasChanges(
-      fileName && pageData
-        ? title !== pageData.content.frontMatter.title ||
-            permalink !== pageData.content.frontMatter.permalink
-        : fileName && !pageData
-        ? false
-        : title !== exampleTitle && permalink !== examplePermalink
-    )
-  }, [title, permalink])
+  }, [pageData, setValue])
 
   /** ******************************** */
   /*     handler functions    */
   /** ******************************** */
 
-  const changeHandler = (event) => {
-    const { id, value } = event.target
-    const errorMessage = validatePageSettings(
-      id,
-      value,
-      dirData
-        .filter((item) => item.type === "file")
-        .map((page) => page.name)
-        .filter((pageName) => pageName !== fileName)
-    )
-
-    setErrors((prevState) => ({
-      ...prevState,
-      [id]: errorMessage,
-    }))
-    idToSetterFuncMap[id](value)
+  const onSubmit = (data) => {
+    return onProceed({
+      frontMatter:
+        pageData && pageData.content
+          ? {
+              ...pageData.content.frontMatter,
+              ...data,
+            }
+          : {
+              ...data,
+            },
+      sha: pageData?.sha || "",
+      pageBody: pageData?.content?.pageBody || "",
+      newFileName: `${data.title}.md`,
+    })
   }
 
   return (
@@ -117,54 +105,35 @@ export const PageSettingsModal = ({
             <div className={elementStyles.modalFormFields}>
               {!fileName ? "You may edit page details anytime. " : ""}
               To edit page content, simply click on the page title. <br />
-              <Breadcrumb params={params} title={title} />
+              <Breadcrumb params={params} title={watch("title")} />
               {/* Title */}
               <FormField
+                register={register}
                 title="Page title"
                 id="title"
-                value={title}
-                errorMessage={errors.title}
+                errorMessage={errors.title?.message}
                 isRequired
-                onFieldChange={changeHandler}
               />
               <br />
               <p className={elementStyles.formLabel}>Page URL</p>
               {/* Permalink */}
               <FormFieldHorizontal
+                register={register}
                 title={siteUrl}
                 id="permalink"
-                value={permalink || ""}
-                errorMessage={errors.permalink}
+                errorMessage={errors.permalink?.message}
                 isRequired
-                onFieldChange={changeHandler}
-                placeholder=" "
+                placeholder=""
               />
             </div>
             <SaveDeleteButtons
               isDisabled={
                 !fileName
-                  ? hasErrors
-                  : !hasChanges || hasErrors || !pageData?.sha
+                  ? !_.isEmpty(errors)
+                  : !_.isEmpty(errors) || !pageData?.sha
               }
               hasDeleteButton={false}
-              saveCallback={() =>
-                onProceed({
-                  frontMatter:
-                    pageData && pageData.content
-                      ? {
-                          ...pageData.content.frontMatter,
-                          title,
-                          permalink,
-                        }
-                      : {
-                          title,
-                          permalink,
-                        },
-                  sha: pageData?.sha || "",
-                  pageBody: pageData?.content?.pageBody || "",
-                  newFileName: `${title}.md`,
-                })
-              }
+              saveCallback={handleSubmit(onSubmit)}
             />
           </div>
         </div>
