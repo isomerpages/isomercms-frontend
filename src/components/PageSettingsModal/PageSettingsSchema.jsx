4a41e98faadb881680872cd56621e148634c2004
Alexis <gweiying@gmail.com>
2021-11-10 09:29:54 +0800

    fix: run transform in Yup validation schema

--- src/components/PageSettingsModal/PageSettingsSchema.jsx
+++ src/components/PageSettingsModal/PageSettingsSchema.jsx
@@ -1,4 +1,5 @@
 import * as Yup from "yup"
+import { titleToPageFileName } from "../../utils"
 import {
   permalinkRegexTest,
   specialCharactersRegexTest,
@@ -12,22 +13,25 @@ export const PageSettingsSchema = (existingTitlesArray = []) =>
   Yup.object().shape({
     title: Yup.string()
       .required("Title is required")
+      .transform((value) =>
+        value !== null ? titleToPageFileName(value) : value
+      )
+      .test(
+        "Special characters found",
+        'Title cannot contain any of the following special characters: ~%^*_+-./\\`;~{}[]"<>',
+        (value) => !specialCharactersRegexTest.test(value.split(".")[0])
+      )
       .min(
-        PAGE_SETTINGS_TITLE_MIN_LENGTH,
+        PAGE_SETTINGS_TITLE_MIN_LENGTH + 3, // + 3 as '.md' is 3 characters long
         `Title must be longer than ${PAGE_SETTINGS_TITLE_MIN_LENGTH} characters`
       )
       .max(
-        PAGE_SETTINGS_TITLE_MAX_LENGTH,
+        PAGE_SETTINGS_TITLE_MAX_LENGTH + 3, // + 3 as '.md' is 3 characters long
         `Title must be shorter than ${PAGE_SETTINGS_TITLE_MAX_LENGTH} characters`
       )
       .notOneOf(
         existingTitlesArray,
         "Title is already in use. Please choose a different title."
-      )
-      .test(
-        "Special characters found",
-        'Title cannot contain any of the following special characters: ~%^*_+-./\\`;~{}[]"<>',
-        (value) => !specialCharactersRegexTest.test(value)
       ),
     permalink: Yup.string()
       .required("Permalink is required")
