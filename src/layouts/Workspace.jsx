084df4712df710311eb8f89a18026ac7107ddce3
Alexis <gweiying@gmail.com>
2021-11-10 09:30:57 +0800

    refactor: update Workspace to use refactored components

--- src/layouts/Workspace.jsx
+++ src/layouts/Workspace.jsx
@@ -9,6 +9,8 @@ import React, { useEffect, useState } from "react"
 import { Switch, useRouteMatch, useHistory } from "react-router-dom"
 
 // Import styles
+import elementStyles from "styles/isomer-cms/Elements.module.scss"
+import contentStyles from "styles/isomer-cms/pages/Content.module.scss"
 
 // Import utils
 
