d56f69b3b48fbd4852c7104af23db20505454749
Alexis <gweiying@gmail.com>
2021-11-29 10:06:00 +0800

    feat: update ResourceFormField with useForm hooks

--- src/components/ResourceFormFields.jsx
+++ src/components/ResourceFormFields.jsx
@@ -1,63 +1,59 @@
-import React from "react"
-
 import FormField from "components/FormField"
 import FormFieldMedia from "components/FormFieldMedia"
+import React from "react"
 
 import elementStyles from "styles/isomer-cms/Elements.module.scss"
 
 const ResourceFormFields = ({
-  date,
+  register,
+  setValue,
+  watch,
+  trigger,
   errors,
-  changeHandler,
-  onToggle,
-  isPost,
-  setIsPost,
   siteName,
-  fileUrl,
 }) => {
-  const onToggleType = (e) => {
-    onToggle(e)
-    setIsPost(!isPost)
-  }
-
   return (
     <>
       <div className="d-flex row m-0">
         <div className="col-4 m-0 p-0">
           {/* Date */}
           <FormField
+            register={register}
             title="Date (YYYY-MM-DD)"
             id="date"
-            value={date}
-            errorMessage={errors.date}
+            errorMessage={errors.date?.message}
             isRequired
-            onFieldChange={(e) => changeHandler(e)}
           />
         </div>
-
         <div className="col-8">
           <p className={elementStyles.formLabel}>Resource Type</p>
           {/* Permalink or File URL */}
           <div className="d-flex">
             <label htmlFor="radio-post" className="flex-fill">
               <input
+                {...register("layout")}
+                onChange={(e) => {
+                  register("layout").onChange(e)
+                  setValue("file_url", undefined)
+                }}
                 type="radio"
                 id="radio-post"
-                name="resource-type"
+                name="layout"
                 value="post"
-                onChange={onToggleType}
-                checked={isPost}
               />
               Post Content
             </label>
             <label htmlFor="radio-file" className="flex-fill">
               <input
+                {...register("layout")}
                 type="radio"
                 id="radio-file"
-                name="resource-type"
+                name="layout"
                 value="file"
-                onChange={onToggleType}
-                checked={!isPost}
+                onChange={(e) => {
+                  register("layout").onChange(e)
+                  setValue("permalink", undefined)
+                }}
               />
               Downloadable File
             </label>
@@ -65,19 +61,24 @@ const ResourceFormFields = ({
         </div>
       </div>
       {/* File URL */}
-      <FormFieldMedia
-        title="Select File"
-        id="fileUrl"
-        value={fileUrl?.split("/").pop()}
-        errorMessage={errors.fileUrl}
-        onFieldChange={changeHandler}
-        inlineButtonText="Select File"
-        siteName={siteName}
-        placeholder=" "
-        type="files"
-        errorMessage={errors.fileUrl}
-        isDisabled={isPost}
-      />
+      {watch("layout") === "file" && (
+        <>
+          <br />
+          <FormFieldMedia
+            register={register}
+            title="Select File"
+            id="file_url"
+            onFieldChange={(e) => {
+              setValue("file_url", e.target.value)
+              trigger("file_url")
+            }}
+            errorMessage={errors.file_url?.message}
+            inlineButtonText="Select File"
+            siteName={siteName}
+            type="files"
+          />
+        </>
+      )}
     </>
   )
 }
