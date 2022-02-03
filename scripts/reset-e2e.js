469db6ccfde87f67ef7ad72c0f5a7193b0756a50
seaerchin <jiachin@open.gov.sg>
2022-02-03 12:02:33 +0800

    ci(run-ci): adds a build script and npm command for ci
    ci(netlify.toml): update build command for netlify to use ci command
    
    ci(package): updates ci command

--- scripts/reset-e2e.js
+++ scripts/reset-e2e.js
@@ -19,6 +19,8 @@ const cypressCommand = process.argv[2] // either `run` or `open`
 const srcPath = path.resolve(__dirname, "..")
 const envFilePath = `${srcPath}/.env`
 const runCypressCommand = `source ${envFilePath} && npx cypress ${cypressCommand}`
+// NOTE: This is not prefixed with source because the env vars should be set in the hosting platform
+const buildCommand = "npm run build"
 
 // reset e2e-test-repo
 const { E2E_COMMIT_HASH } = process.env
@@ -28,17 +30,21 @@ const REPO_NAME = "e2e-test-repo"
 const resetE2eTestRepo = async () => {
   const baseRefEndpoint = `https://api.github.com/repos/${GITHUB_ORG_NAME}/${REPO_NAME}/git/refs`
   const refEndpoint = `${baseRefEndpoint}/heads/staging`
-  await axios
-    .patch(
-      refEndpoint,
-      {
-        sha: E2E_COMMIT_HASH,
-        force: true,
-      },
-      {
-        headers,
-      }
-    )
+  await axios.patch(
+    refEndpoint,
+    {
+      sha: E2E_COMMIT_HASH,
+      force: true,
+    },
+    {
+      headers,
+    }
+  )
+}
+
+// resets the e2e repo then runs the corresponding cypress command
+const runE2eTests = async () =>
+  resetE2eTestRepo()
     .then(() => {
       console.log(`Successfully reset e2e-test-repo to ${E2E_COMMIT_HASH}`)
 
@@ -54,22 +60,27 @@ const resetE2eTestRepo = async () => {
       })
     })
     .catch((err) => console.error(err))
+
+const runCi = async () =>
+  resetE2eTestRepo()
+    .then(() => {
+      console.log(`Successfully reset e2e-test-repo to ${E2E_COMMIT_HASH}`)
+
+      const child = spawn(buildCommand, { shell: true })
+      child.stderr.on("data", (data) => {
+        console.error(data.toString().trim())
+      })
+      child.stdout.on("data", (data) => {
+        console.log(data.toString().trim())
+      })
+      child.on("exit", (exitCode) => {
+        console.log(`Child exited with code: ${exitCode}`)
+      })
+    })
+    .catch((err) => console.error(err))
+
+module.exports = {
+  runE2eTests,
+  resetE2eTestRepo,
+  runCi,
 }
-
-// resets the e2e repo then runs the corresponding cypress command
-const runE2eTests = resetE2eTestRepo()
-  .then(() => {
-    console.log(`Successfully reset e2e-test-repo to ${E2E_COMMIT_HASH}`)
-
-    const child = spawn(runCypressCommand, { shell: true })
-    child.stderr.on("data", (data) => {
-      console.error(data.toString().trim())
-    })
-    child.stdout.on("data", (data) => {
-      console.log(data.toString().trim())
-    })
-    child.on("exit", (exitCode) => {
-      console.log(`Child exited with code: ${exitCode}`)
-    })
-  })
-  .catch((err) => console.error(err))
---
+++ scripts/run-ci.js
@@ -0,0 +1,3 @@
+const { runCi } = require("./reset-e2e")
+
+runCi()
