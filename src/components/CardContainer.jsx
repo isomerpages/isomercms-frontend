3b042bb4337afeb29e23572ccaa3167adb514559
Alexis <gweiying@gmail.com>
2022-02-09 15:20:34 +0800

    feat: create CardContainer for EditXX section cards

---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,30 @@
+import elementStyles from "styles/isomer-cms/Elements.module.scss"
+import React, { useState } from "react"
+
+export const CardContainer = ({ cardTitle, children, isError }) => {
+  const [shouldDisplay, setShouldDisplay] = useState(false)
+
+  return (
+    <div
+      className={`${elementStyles.card} ${
+        !shouldDisplay && isError ? elementStyles.error : ""
+      }`}
+    >
+      <div className={elementStyles.cardHeader}>
+        <h2>{cardTitle}</h2>
+        <button
+          className="pl-3"
+          type="button"
+          onClick={() => setShouldDisplay(!shouldDisplay)}
+        >
+          <i
+            className={`bx ${
+              shouldDisplay ? "bx-chevron-down" : "bx-chevron-right"
+            }`}
+          />
+        </button>
+      </div>
+      {shouldDisplay && <>{children}</>}
+    </div>
+  )
+}
