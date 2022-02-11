31bd8e7d9a8bb593841838274190b6b1eb1b3bd9
Alexis <gweiying@gmail.com>
2022-02-11 09:05:05 +0800

    refactor: create HeroDropdown and HeroHighlight with useForm and validation

--- src/components/homepage/HeroSection/HeroHighlightSchema.jsx
+++ src/components/homepage/HeroSection/HeroHighlightSchema.jsx
@@ -1,5 +1,7 @@
 import * as Yup from "yup"
 import {
+  HERO_BUTTON_MIN_LENGTH,
+  HERO_BUTTON_MAX_LENGTH,
   HIGHLIGHTS_DESCRIPTION_MIN_LENGTH,
   HIGHLIGHTS_DESCRIPTION_MAX_LENGTH,
   HIGHLIGHTS_TITLE_MIN_LENGTH,
@@ -26,4 +28,18 @@ export const HeroHighlightOptionSchema = Yup.object({
       HIGHLIGHTS_DESCRIPTION_MAX_LENGTH,
       `Title must be shorter than ${HIGHLIGHTS_DESCRIPTION_MAX_LENGTH} characters`
     ),
+})
+
+export const HeroHighlightSchema = Yup.object().shape({
+  button: Yup.string()
+    .min(
+      HERO_BUTTON_MIN_LENGTH,
+      `Title must be longer than ${HERO_BUTTON_MIN_LENGTH} characters`
+    )
+    .max(
+      HERO_BUTTON_MAX_LENGTH,
+      `Title must be shorter than ${HERO_BUTTON_MAX_LENGTH} characters`
+    ),
+  url: Yup.string(),
+  key_highlights: Yup.array().of(HeroHighlightOptionSchema),
 })
