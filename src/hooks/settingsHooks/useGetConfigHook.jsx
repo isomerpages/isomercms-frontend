7f2a9b19e059e1b271709822d5bb64eaaa815f95
Alexis <gweiying@gmail.com>
2022-02-14 10:28:34 +0800

    chore: rename Settings hooks to Config hooks

--- src/hooks/settingsHooks/useGetConfigHook.jsx
+++ src/hooks/settingsHooks/useGetConfigHook.jsx
@@ -2,21 +2,21 @@ import { useContext } from "react"
 import { useQuery } from "react-query"
 import { DEFAULT_RETRY_MSG } from "../../utils"
 import { errorToast } from "../../utils/toasts"
-import { SETTINGS_CONTENT_KEY } from "../queryKeys"
+import { CONFIG_CONTENT_KEY } from "../queryKeys"
 
 import { ServicesContext } from "../../contexts/ServicesContext"
 
-export function useGetSettingsHook(params, queryParams) {
-  const { settingsService } = useContext(ServicesContext)
+export function useGetConfigHook(params, queryParams) {
+  const { configService } = useContext(ServicesContext)
   return useQuery(
-    [SETTINGS_CONTENT_KEY, { ...params }],
-    () => settingsService.get(params),
+    [CONFIG_CONTENT_KEY, params],
+    () => configService.get(params),
     {
       ...queryParams,
       retry: false,
       onError: () => {
         errorToast(
-          `The settings data could not be retrieved. ${DEFAULT_RETRY_MSG}`
+          `The configurations could not be retrieved. ${DEFAULT_RETRY_MSG}`
         )
         queryParams && queryParams.onError && queryParams.onError()
       },
