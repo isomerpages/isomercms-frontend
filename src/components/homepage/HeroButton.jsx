72ffa107f99975b646414fb3a9d98538af1abceb
seaerchin <jiachin@open.gov.sg>
2022-02-15 15:30:06 +0800

    refactor(herobutton): refactor to use new api

--- src/components/homepage/HeroButton.jsx
+++ src/components/homepage/HeroButton.jsx
@@ -1,7 +1,9 @@
+import FormContext from "components/Form/FormContext"
+import FormError from "components/Form/FormError"
+import FormTitle from "components/Form/FormTitle"
+import FormField from "components/FormField"
 import PropTypes from "prop-types"
 import React from "react"
-
-import FormField from "components/FormField"
 
 /* eslint
   react/no-array-index-key: 0
@@ -9,23 +11,25 @@ import FormField from "components/FormField"
 
 const HeroButton = ({ button, url, sectionIndex, onFieldChange, errors }) => (
   <>
-    <FormField
-      title="Hero button"
-      id={`section-${sectionIndex}-hero-button`}
-      value={button}
-      errorMessage={errors.button}
-      isRequired={false}
-      onFieldChange={onFieldChange}
-    />
-    <FormField
-      title="Hero button URL"
-      placeholder="Insert permalink or external URL"
-      id={`section-${sectionIndex}-hero-url`}
-      value={url}
-      errorMessage={errors.url}
-      isRequired={false}
-      onFieldChange={onFieldChange}
-    />
+    <FormContext hasError={!!errors.button}>
+      <FormTitle>Hero button</FormTitle>
+      <FormField
+        id={`section-${sectionIndex}-hero-button`}
+        value={button}
+        onChange={onFieldChange}
+      />
+      <FormError>{errors.button}</FormError>
+    </FormContext>
+    <FormContext hasError={!!errors.url}>
+      <FormTitle>Hero button URL</FormTitle>
+      <FormField
+        placeholder="Insert permalink or external URL"
+        id={`section-${sectionIndex}-hero-url`}
+        value={url}
+        onChange={onFieldChange}
+      />
+      <FormError>{errors.url}</FormError>
+    </FormContext>
   </>
 )
 
