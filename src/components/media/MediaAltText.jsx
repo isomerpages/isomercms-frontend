171494889cbe6084b80561489115b3d316ea3506
seaerchin <jiachin@open.gov.sg>
2022-02-15 16:56:53 +0800

    refactor(mediaalttext): refactor to use new api

--- src/components/media/MediaAltText.jsx
+++ src/components/media/MediaAltText.jsx
@@ -1,4 +1,8 @@
 import axios from "axios"
+import FormContext from "components/Form/FormContext"
+import FormDescription from "components/Form/FormDescription"
+import FormError from "components/Form/FormError"
+import FormTitle from "components/Form/FormTitle"
 import FormField from "components/FormField"
 import SaveDeleteButtons from "components/SaveDeleteButtons"
 import React from "react"
@@ -6,10 +10,10 @@ import { useFormContext } from "react-hook-form"
 
 import elementStyles from "styles/isomer-cms/Elements.module.scss"
 import mediaStyles from "styles/isomer-cms/pages/Media.module.scss"
-
 // axios settings
 axios.defaults.withCredentials = true
 
+// eslint-disable-next-line import/prefer-default-export
 export const MediaAltText = ({ onProceed, onClose, type }) => {
   const {
     watch,
@@ -24,6 +28,7 @@ export const MediaAltText = ({ onProceed, onClose, type }) => {
         <div className={elementStyles.modalHeader}>
           <h1>Insert media</h1>
           <button
+            type="button"
             mediaType="button"
             id="closeMediaSettingsModal"
             onClick={onClose}
@@ -59,35 +64,39 @@ export const MediaAltText = ({ onProceed, onClose, type }) => {
             )}
             <form className={elementStyles.modalContent}>
               <div className={elementStyles.modalFormFields}>
-                <FormField
-                  title="File name"
-                  value={
-                    watch("selectedMedia").name ||
-                    watch("selectedMedia").newFileName
-                  }
-                  disabled
-                />
+                <FormContext isDisabled>
+                  <FormTitle>File name</FormTitle>
+                  <FormField
+                    value={
+                      watch("selectedMedia").name ||
+                      watch("selectedMedia").newFileName
+                    }
+                  />
+                </FormContext>
                 <br />
-                <FormField
-                  register={register}
-                  title={type === "images" ? "Alt text" : "Text"}
-                  id="altText"
-                  errorMessage={errors.altText?.message}
-                  children={
-                    type === "images" ? (
-                      <p className={elementStyles.formDescription}>
+                <FormContext hasError={!!errors.altText?.message}>
+                  <FormTitle>
+                    {type === "images" ? "Alt text" : "Text"}
+                  </FormTitle>
+                  <FormDescription>
+                    {type === "images" ? (
+                      <>
                         Short description of image used for accessibility.{" "}
-                        <a href="https://go.gov.sg/isomer-meta" target="_blank">
+                        <a
+                          href="https://go.gov.sg/isomer-meta"
+                          target="_blank"
+                          rel="noreferrer"
+                        >
                           Learn more
                         </a>
-                      </p>
+                      </>
                     ) : (
-                      <p className={elementStyles.formDescription}>
-                        Description of file displayed in hyperlink
-                      </p>
-                    )
-                  }
-                />
+                      "Description of file displayed in hyperlink"
+                    )}
+                  </FormDescription>
+                  <FormField {...register("altText")} id="altText" />
+                  <FormError>{errors.altText?.message}</FormError>
+                </FormContext>
               </div>
               <SaveDeleteButtons
                 saveLabel="Save"
