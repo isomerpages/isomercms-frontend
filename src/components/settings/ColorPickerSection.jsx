6a608455f874824112526f3a5a37f210e9da533a
seaerchin <jiachin@open.gov.sg>
2022-02-18 14:48:23 +0800

    refactor(colorpickersection): updates to new formfieldcolour api

--- src/components/settings/ColorPickerSection.jsx
+++ src/components/settings/ColorPickerSection.jsx
@@ -1,5 +1,7 @@
+import FormContext from "components/Form/FormContext"
+import FormTitle from "components/Form/FormTitle"
 import FormFieldColor from "components/settings/FormFieldColor"
-import * as _ from "lodash"
+import _ from "lodash"
 import PropTypes from "prop-types"
 import React from "react"
 
@@ -9,52 +11,63 @@ const ColorPickerSection = ({ colors, saveChanges }) => {
   return (
     <div id="color-fields">
       <p className={elementStyles.formSectionHeader}>Colors</p>
-      <FormFieldColor
-        title="Primary"
-        id="primary-color"
-        value={colors["primary-color"]}
-        isRequired
-        saveChanges={(selectedColor) => {
-          saveChanges({
-            ...colors,
-            "primary-color": selectedColor,
-          })
-        }}
-      />
-      <FormFieldColor
-        title="Secondary"
-        id="secondary-color"
-        value={colors["secondary-color"]}
-        isRequired
-        saveChanges={(selectedColor) => {
-          saveChanges({
-            ...colors,
-            "secondary-color": selectedColor,
-          })
-        }}
-      />
-      <div id="media-color-fields">
-        {colors["media-colors"].map((category, index) => {
-          const { title: mediaColorName, color } = category
-          return (
-            <FormFieldColor
-              title={`Resource ${index + 1}`}
-              id={`media-color@${index}`}
-              value={color}
-              key={mediaColorName}
-              isRequired
-              saveChanges={(selectedColor) => {
-                const newMediaColors = _.cloneDeep(colors["media-colors"])
-                newMediaColors[index].color = selectedColor
-                saveChanges({
-                  ...colors,
-                  "media-colors": newMediaColors,
-                })
-              }}
-            />
-          )
-        })}
-      </div>
+      <FormContext isRequired>
+        <div className={elementStyles.formColor}>
+          <FormTitle className={elementStyles.formColorLabel}>
+            Primary
+          </FormTitle>
+          <FormFieldColor
+            id="primary-color"
+            value={colors["primary-color"]}
+            saveChanges={(selectedColor) => {
+              saveChanges({
+                ...colors,
+                "primary-color": selectedColor,
+              })
+            }}
+          />
+        </div>
+        <div className={elementStyles.formColor}>
+          <FormTitle className={elementStyles.formColorLabel}>
+            Secondary
+          </FormTitle>
+          <FormFieldColor
+            id="secondary-color"
+            value={colors["secondary-color"]}
+            saveChanges={(selectedColor) => {
+              saveChanges({
+                ...colors,
+                "secondary-color": selectedColor,
+              })
+            }}
+          />
+        </div>
+        <div id="media-color-fields">
+          {colors["media-colors"].map((category, index) => {
+            const { title: mediaColorName, color } = category
+            return (
+              <div className={elementStyles.formColor}>
+                <FormTitle className={elementStyles.formColorLabel}>
+                  {`Resource ${index + 1}`}
+                </FormTitle>
+                <FormFieldColor
+                  id={`media-color@${index}`}
+                  value={color}
+                  key={mediaColorName}
+                  saveChanges={(selectedColor) => {
+                    const newMediaColors = _.cloneDeep(colors["media-colors"])
+                    newMediaColors[index].color = selectedColor
+                    saveChanges({
+                      ...colors,
+                      "media-colors": newMediaColors,
+                    })
+                  }}
+                />
+              </div>
+            )
+          })}
+        </div>
+      </FormContext>
     </div>
   )
 }
