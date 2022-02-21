c09fa04fe2f867cc94e9c45f2b175cc420e73935
seaerchin <jiachin@open.gov.sg>
2022-02-21 19:42:17 +0800

    refactor(mediamovemodal): mediamovemodal
    refactor to avoid nested ternary

--- src/components/MediaMoveModal/MediaMoveModal.jsx
+++ src/components/MediaMoveModal/MediaMoveModal.jsx
@@ -11,11 +11,73 @@ import elementStyles from "styles/isomer-cms/Elements.module.scss"
 import { pageFileNameToTitle, getMediaDirectoryName } from "utils"
 
 export const MediaMoveModal = ({ queryParams, params, onProceed, onClose }) => {
-  const [moveQuery, setMoveQuery] = useState(
-    (({ fileName, ...p }) => p)(queryParams)
-  )
+  const [moveQuery, setMoveQuery] = useState((({ ...p }) => p)(queryParams))
   const [moveTo, setMoveTo] = useState(params)
   const { data: dirData } = useGetDirectoryHook(moveQuery)
+
+  const MenuItems = () => {
+    if (dirData && dirData.length)
+      return (
+        <>
+          {/* directories */}
+          {dirData
+            .filter((item) => item.type === "dir")
+            .map((item, itemIndex) => (
+              <MoveMenuItem
+                item={item}
+                id={itemIndex}
+                isItemSelected={
+                  getMediaDirectoryName(moveTo.mediaDirectoryName, {
+                    splitOn: "/",
+                  }) === `${moveQuery.mediaDirectoryName}%2F${item.name}`
+                }
+                onItemSelect={() =>
+                  setMoveTo({
+                    ...moveQuery,
+                    mediaDirectoryName: `${getMediaDirectoryName(
+                      moveQuery.mediaDirectoryName,
+                      { joinOn: "/", decode: true }
+                    )}/${item.name}`,
+                  })
+                }
+                onForward={() => {
+                  setMoveTo({
+                    ...moveQuery,
+                    mediaDirectoryName: `${getMediaDirectoryName(
+                      moveQuery.mediaDirectoryName,
+                      { joinOn: "/", decode: true }
+                    )}/${item.name}`,
+                  })
+                  setMoveQuery((prevState) => ({
+                    ...prevState,
+                    mediaDirectoryName: `${
+                      moveQuery.mediaDirectoryName
+                    }%2F${encodeURIComponent(item.name)}`,
+                  }))
+                }}
+              />
+            ))}
+          {/* files */}
+          {dirData
+            .filter((item) => item.type === "file")
+            .map((item, itemIndex) => (
+              <MoveMenuItem item={item} id={itemIndex} />
+            ))}
+        </>
+      )
+
+    return (
+      <div
+        className={`${elementStyles.dropdownItemDisabled} d-flex justify-content-center`}
+      >
+        {dirData ? (
+          `No ${moveQuery.mediaRoom} here yet.`
+        ) : (
+          <div className="spinner-border text-primary" role="status" />
+        )}
+      </div>
+    )
+  }
 
   return (
     <div className={elementStyles.overlay}>
@@ -71,67 +133,7 @@ export const MediaMoveModal = ({ queryParams, params, onProceed, onClose }) => {
                   { start: -1, decode: true }
                 )}
               />
-              {dirData && dirData.length ? (
-                <>
-                  {/* directories */}
-                  {dirData
-                    .filter((item) => item.type === "dir")
-                    .map((item, itemIndex) => (
-                      <MoveMenuItem
-                        item={item}
-                        id={itemIndex}
-                        isItemSelected={
-                          getMediaDirectoryName(moveTo.mediaDirectoryName, {
-                            splitOn: "/",
-                          }) ===
-                          `${moveQuery.mediaDirectoryName}%2F${item.name}`
-                        }
-                        onItemSelect={() =>
-                          setMoveTo({
-                            ...moveQuery,
-                            mediaDirectoryName: `${getMediaDirectoryName(
-                              moveQuery.mediaDirectoryName,
-                              { joinOn: "/", decode: true }
-                            )}/${item.name}`,
-                          })
-                        }
-                        onForward={() => {
-                          setMoveTo({
-                            ...moveQuery,
-                            mediaDirectoryName: `${getMediaDirectoryName(
-                              moveQuery.mediaDirectoryName,
-                              { joinOn: "/", decode: true }
-                            )}/${item.name}`,
-                          })
-                          setMoveQuery((prevState) => ({
-                            ...prevState,
-                            mediaDirectoryName: `${
-                              moveQuery.mediaDirectoryName
-                            }%2F${encodeURIComponent(item.name)}`,
-                          }))
-                        }}
-                      />
-                    ))}
-                  {/* files */}
-                  {dirData
-                    .filter((item) => item.type === "file")
-                    .map((item, itemIndex) => (
-                      <MoveMenuItem item={item} id={itemIndex} />
-                    ))}
-                </>
-              ) : dirData ? (
-                <div
-                  className={`${elementStyles.dropdownItemDisabled} d-flex justify-content-center`}
-                >
-                  {`No ${moveQuery.mediaRoom} here yet.`}
-                </div>
-              ) : (
-                <div
-                  className={`${elementStyles.dropdownItemDisabled} d-flex justify-content-center`}
-                >
-                  <div className="spinner-border text-primary" role="status" />
-                </div>
-              )}
+              <MenuItems />
             </div>
             Moving to: <br />
             <Breadcrumb
