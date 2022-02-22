b600d6c94b5a07842e31d4065b159fd991fb6eaa
Alexis <gweiying@gmail.com>
2022-02-22 11:00:21 +0800

    chore: update EditorSections schema to include section object

--- src/components/homepage/EditorInfobarSection/EditorInfobarSchema.jsx
+++ src/components/homepage/EditorInfobarSection/EditorInfobarSchema.jsx
@@ -11,41 +11,43 @@ import {
 } from "utils/validators"
 
 export const EditorInfobarSchema = Yup.object().shape({
-  title: Yup.string()
-    .min(
-      INFOBAR_TITLE_MIN_LENGTH,
-      `Title must be longer than ${INFOBAR_TITLE_MIN_LENGTH} characters`
-    )
-    .max(
-      INFOBAR_TITLE_MAX_LENGTH,
-      `Title must be shorter than ${INFOBAR_TITLE_MAX_LENGTH} characters`
-    ),
-  subtitle: Yup.string()
-    .min(
-      INFOBAR_SUBTITLE_MIN_LENGTH,
-      `Title must be longer than ${INFOBAR_SUBTITLE_MIN_LENGTH} characters`
-    )
-    .max(
-      INFOBAR_SUBTITLE_MAX_LENGTH,
-      `Title must be shorter than ${INFOBAR_SUBTITLE_MAX_LENGTH} characters`
-    ),
-  description: Yup.string()
-    .min(
-      INFOBAR_DESCRIPTION_MIN_LENGTH,
-      `Title must be longer than ${INFOBAR_DESCRIPTION_MIN_LENGTH} characters`
-    )
-    .max(
-      INFOBAR_DESCRIPTION_MAX_LENGTH,
-      `Title must be shorter than ${INFOBAR_DESCRIPTION_MAX_LENGTH} characters`
-    ),
-  button: Yup.string()
-    .min(
-      INFOBAR_BUTTON_MIN_LENGTH,
-      `Title must be longer than ${INFOBAR_BUTTON_MIN_LENGTH} characters`
-    )
-    .max(
-      INFOBAR_BUTTON_MAX_LENGTH,
-      `Title must be shorter than ${INFOBAR_BUTTON_MAX_LENGTH} characters`
-    ),
-  url: Yup.string(),
+  infobar: Yup.object().shape({
+    title: Yup.string()
+      .min(
+        INFOBAR_TITLE_MIN_LENGTH,
+        `Title must be longer than ${INFOBAR_TITLE_MIN_LENGTH} characters`
+      )
+      .max(
+        INFOBAR_TITLE_MAX_LENGTH,
+        `Title must be shorter than ${INFOBAR_TITLE_MAX_LENGTH} characters`
+      ),
+    subtitle: Yup.string()
+      .min(
+        INFOBAR_SUBTITLE_MIN_LENGTH,
+        `Title must be longer than ${INFOBAR_SUBTITLE_MIN_LENGTH} characters`
+      )
+      .max(
+        INFOBAR_SUBTITLE_MAX_LENGTH,
+        `Title must be shorter than ${INFOBAR_SUBTITLE_MAX_LENGTH} characters`
+      ),
+    description: Yup.string()
+      .min(
+        INFOBAR_DESCRIPTION_MIN_LENGTH,
+        `Title must be longer than ${INFOBAR_DESCRIPTION_MIN_LENGTH} characters`
+      )
+      .max(
+        INFOBAR_DESCRIPTION_MAX_LENGTH,
+        `Title must be shorter than ${INFOBAR_DESCRIPTION_MAX_LENGTH} characters`
+      ),
+    button: Yup.string()
+      .min(
+        INFOBAR_BUTTON_MIN_LENGTH,
+        `Title must be longer than ${INFOBAR_BUTTON_MIN_LENGTH} characters`
+      )
+      .max(
+        INFOBAR_BUTTON_MAX_LENGTH,
+        `Title must be shorter than ${INFOBAR_BUTTON_MAX_LENGTH} characters`
+      ),
+    url: Yup.string(),
+  }),
 })
