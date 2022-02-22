61a75bbcace1c9b15e5187fd0d7be17830a2ae03
Alexis <gweiying@gmail.com>
2022-02-22 10:59:35 +0800

    chore: update Hero components to use FormContext from parent

--- src/components/homepage/HeroSection/HeroDropdown.jsx
+++ src/components/homepage/HeroSection/HeroDropdown.jsx
@@ -1,104 +1,95 @@
-import React, { useEffect } from "react"
-import { Droppable, Draggable } from "react-beautiful-dnd"
+import React from "react"
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
 
-import { useForm, useFieldArray } from "react-hook-form"
+import { useFormContext, useFieldArray } from "react-hook-form"
 
 import FormField from "components/FormField"
 import { HeroOption } from "components/homepage/HeroSection/HeroOption"
-import { HeroDropdownSchema } from "components/homepage/HeroSection/HeroDropdownSchema"
 import styles from "styles/App.module.scss"
 import elementStyles from "styles/isomer-cms/Elements.module.scss"
-import { yupResolver } from "@hookform/resolvers/yup"
 
 const defaultDropdownOption = {
   title: "Hero Dropdown Element Title",
   url: "", // No default value so that no broken link is created
 }
 
-export const HeroDropdown = ({ dropdownContent, onUpdate }) => {
+export const HeroDropdown = ({
+  fieldId, // sections.0.hero.dropdown
+}) => {
   const {
     register,
     formState: { errors },
-    watch,
     control,
-    trigger,
-  } = useForm({
-    defaultValues: dropdownContent,
-    resolver: yupResolver(HeroDropdownSchema),
-    mode: "onBlur",
-  })
-  const { append, remove, update } = useFieldArray({
+  } = useFormContext()
+
+  const { fields, append, remove, move } = useFieldArray({
     control,
-    name: "options", // key for dropdown array
+    name: `${fieldId}.options`, // key for dropdown array
   })
-
-  /** ****************** */
-  /*   useForm effects   */
-  /** ****************** */
-
-  watch((data) => !_.isEqual(data, dropdownContent) && onUpdate(data)) // updates parent component (EditorHeroSection) when form values are changed
-  useEffect(() => {
-    trigger()
-  }, []) // triggers validation when component is mounted
+  const sectionErrors = _.get(errors, fieldId)
 
   return (
     <div>
       <FormField
         register={register}
         title="Hero dropdown title"
-        id="title"
-        errorMessage={errors.title?.message}
+        id={`${fieldId}.title`}
+        errorMessage={sectionErrors?.title?.message}
         isRequired
       />
-      <Droppable droppableId="dropdownelem" type="dropdownelem">
-        {(droppableProvided) => (
-          /* eslint-disable react/jsx-props-no-spreading */
-          <div
-            className={styles.card}
-            ref={droppableProvided.innerRef}
-            {...droppableProvided.droppableProps}
-          >
-            {watch("options") && watch("options").length > 0 ? (
-              <>
-                <b>Hero dropdown options</b>
-                {watch("options").map((option, dropdownsIndex) => (
-                  <Draggable
-                    draggableId={`dropdownelem-${dropdownsIndex}-draggable`}
-                    index={dropdownsIndex}
-                  >
-                    {(draggableProvided) => (
-                      <div
-                        className={styles.card}
-                        key={dropdownsIndex}
-                        {...draggableProvided.draggableProps}
-                        {...draggableProvided.dragHandleProps}
-                        ref={draggableProvided.innerRef}
-                      >
-                        <HeroOption
-                          optionContent={option}
-                          key={`dropdownelem-${dropdownsIndex}`}
-                          dropdownsIndex={dropdownsIndex}
-                          deleteHandler={() => remove(dropdownsIndex)}
-                          onUpdate={(data) => update(dropdownsIndex, data)}
-                        />
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-              </>
-            ) : null}
-            {droppableProvided.placeholder}
-            <button
-              type="button"
-              id={`dropdownelem-create`}
-              className={`ml-auto ${elementStyles.blue}`}
-              onClick={() => append(defaultDropdownOption)}
+      <DragDropContext
+        onDragEnd={({ source, destination }) =>
+          move(source.index, destination.index)
+        }
+      >
+        <Droppable droppableId="dropdownelem" type="dropdownelem">
+          {(droppableProvided) => (
+            /* eslint-disable react/jsx-props-no-spreading */
+            <div
+              className={styles.card}
+              ref={droppableProvided.innerRef}
+              {...droppableProvided.droppableProps}
             >
-              Create dropdown element
-            </button>
-          </div>
-        )}
-      </Droppable>
+              {fields && fields.length > 0 ? (
+                <>
+                  <b>Hero dropdown options</b>
+                  {fields.map((option, dropdownsIndex) => (
+                    <Draggable
+                      draggableId={`dropdownelem-${dropdownsIndex}-draggable`}
+                      index={dropdownsIndex}
+                    >
+                      {(draggableProvided) => (
+                        <div
+                          className={styles.card}
+                          key={dropdownsIndex}
+                          {...draggableProvided.draggableProps}
+                          {...draggableProvided.dragHandleProps}
+                          ref={draggableProvided.innerRef}
+                        >
+                          <HeroOption
+                            fieldId={`${fieldId}.options.${dropdownsIndex}`}
+                            key={`dropdownelem-${dropdownsIndex}`}
+                            deleteHandler={() => remove(dropdownsIndex)}
+                          />
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                </>
+              ) : null}
+              {droppableProvided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+      <button
+        type="button"
+        id={`dropdownelem-create`}
+        className={`ml-auto ${elementStyles.blue}`}
+        onClick={() => append(defaultDropdownOption)}
+      >
+        Create dropdown element
+      </button>
     </div>
   )
 }
--- src/components/homepage/HeroSection/HeroHighlight.jsx
+++ src/components/homepage/HeroSection/HeroHighlight.jsx
@@ -1,13 +1,11 @@
-import React, { useEffect } from "react"
-import { Droppable, Draggable } from "react-beautiful-dnd"
+import React from "react"
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
 
 import FormField from "components/FormField"
 import { HeroOption } from "components/homepage/HeroSection/HeroOption"
-import { HeroHighlightSchema } from "components/homepage/HeroSection/HeroHighlightSchema"
 import styles from "styles/App.module.scss"
 import elementStyles from "styles/isomer-cms/Elements.module.scss"
-import { useForm, useFieldArray } from "react-hook-form"
-import { yupResolver } from "@hookform/resolvers/yup"
+import { useFormContext, useFieldArray } from "react-hook-form"
 
 // Constants
 const MAX_NUM_KEY_HIGHLIGHTS = 4
@@ -18,104 +16,90 @@ const defaultHighlightOption = {
   url: "", // No default value so that no broken link is created
 }
 
-export const HeroHighlight = ({ highlightsContent, onUpdate }) => {
+export const HeroHighlight = ({ fieldId }) => {
   const {
     register,
     formState: { errors },
     control,
-    watch,
-    trigger,
-  } = useForm({
-    defaultValues: highlightsContent,
-    resolver: yupResolver(HeroHighlightSchema),
-    mode: "onBlur",
-  })
-  const { append, remove, update } = useFieldArray({
+  } = useFormContext()
+  const { fields, move, append, remove } = useFieldArray({
     control,
-    name: "key_highlights", // key for highlights array
+    name: `${fieldId}.key_highlights`, // key for highlights array
   })
-
-  /** ****************** */
-  /*   useForm effects   */
-  /** ****************** */
-
-  watch((data) => !_.isEqual(data, highlightsContent) && onUpdate(data)) // updates parent component (EditorHeroSection) when form values are changed
-  useEffect(() => {
-    trigger()
-  }, []) // triggers validation when component is mounted
-
+  const sectionErrors = _.get(errors, fieldId)
   return (
     <>
       <FormField
         register={register}
         title="Button name"
-        id="button"
-        errorMessage={errors.button?.message}
+        id={`${fieldId}.button`}
+        errorMessage={sectionErrors?.button?.message}
       />
       <FormField
         register={register}
         title="Button url"
-        id="url"
-        errorMessage={errors.url?.message}
+        id={`${fieldId}.url`}
+        errorMessage={sectionErrors?.url?.message}
       />
-      <Droppable droppableId="highlight" type="highlight">
-        {(droppableProvided) => (
-          /* eslint-disable react/jsx-props-no-spreading */
-          <div
-            className={styles.card}
-            ref={droppableProvided.innerRef}
-            {...droppableProvided.droppableProps}
-          >
-            {watch("key_highlights") && watch("key_highlights").length > 0 ? (
-              <>
-                <b>Hero highlights</b>
-                {watch("key_highlights").map((highlight, highlightIndex) => (
-                  <Draggable
-                    draggableId={`highlight-${highlightIndex}-draggable`}
-                    index={highlightIndex}
-                  >
-                    {(draggableProvided) => (
-                      <div
-                        className={styles.card}
-                        key={highlightIndex}
-                        {...draggableProvided}
-                        ref={draggableProvided.innerRef}
-                      >
-                        <HeroOption
-                          optionContent={highlight}
-                          key={`highlight-${highlightIndex}`}
-                          dropdownsIndex={highlightIndex}
-                          deleteHandler={() => remove(highlightIndex)}
-                          onUpdate={(data) => update(highlightIndex, data)}
-                          isHighlight
-                        />
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-              </>
-            ) : null}
-            {droppableProvided.placeholder}
-            <button
-              type="button"
-              id={`highlight-create`}
-              disabled={
-                watch("key_highlights") &&
-                watch("key_highlights").length >= MAX_NUM_KEY_HIGHLIGHTS
-              }
-              className={
-                watch("key_highlights") &&
-                watch("key_highlights").length < MAX_NUM_KEY_HIGHLIGHTS
-                  ? `ml-auto ${elementStyles.blue}`
-                  : `ml-auto ${elementStyles.disabled}`
-              }
-              onClick={() => append(defaultHighlightOption)}
+      <DragDropContext
+        onDragEnd={({ source, destination }) =>
+          move(source.index, destination.index)
+        }
+      >
+        <Droppable droppableId="highlight" type="highlight">
+          {(droppableProvided) => (
+            /* eslint-disable react/jsx-props-no-spreading */
+            <div
+              className={styles.card}
+              ref={droppableProvided.innerRef}
+              {...droppableProvided.droppableProps}
             >
-              Create highlight
-            </button>
-          </div>
-        )}
-      </Droppable>
+              {fields.length > 0 ? (
+                <>
+                  <b>Hero highlights</b>
+                  {fields.map((highlight, highlightIndex) => (
+                    <Draggable
+                      draggableId={`highlight-${highlightIndex}-draggable`}
+                      index={highlightIndex}
+                    >
+                      {(draggableProvided) => (
+                        <div
+                          className={styles.card}
+                          key={highlightIndex}
+                          {...draggableProvided.draggableProps}
+                          {...draggableProvided.dragHandleProps}
+                          ref={draggableProvided.innerRef}
+                        >
+                          <HeroOption
+                            fieldId={`${fieldId}.key_highlights.${highlightIndex}`}
+                            key={`highlight-${highlightIndex}`}
+                            deleteHandler={() => remove(highlightIndex)}
+                            isHighlight
+                          />
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                </>
+              ) : null}
+              {droppableProvided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+      <button
+        type="button"
+        id={`highlight-create`}
+        disabled={fields && fields.length >= MAX_NUM_KEY_HIGHLIGHTS}
+        className={
+          fields && fields.length < MAX_NUM_KEY_HIGHLIGHTS
+            ? `ml-auto ${elementStyles.blue}`
+            : `ml-auto ${elementStyles.disabled}`
+        }
+        onClick={() => append(defaultHighlightOption)}
+      >
+        Create highlight
+      </button>
     </>
   )
 }
