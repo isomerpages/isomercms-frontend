0b4a145520655bf672156f684170e3717e8ab4a5
Alexis <gweiying@gmail.com>
2022-02-22 10:56:49 +0800

    chore: update EditorSections to use FormContext from parent

--- src/components/homepage/EditorInfobarSection/EditorInfobarSection.jsx
+++ src/components/homepage/EditorInfobarSection/EditorInfobarSection.jsx
@@ -1,101 +1,71 @@
 import PropTypes from "prop-types"
-import React, { useEffect } from "react"
+import React from "react"
 
 import FormField from "components/FormField"
 import elementStyles from "styles/isomer-cms/Elements.module.scss"
-import { useForm } from "react-hook-form"
+import { useFormContext } from "react-hook-form"
 import { CardContainer } from "components/CardContainer"
 
-import { EditorInfobarSchema } from "."
-import { yupResolver } from "@hookform/resolvers/yup"
 import _ from "lodash"
 
 export const EditorInfobarSection = ({
-  sectionContent,
-  sectionIndex,
-  onUpdate,
+  fieldId, // sections.[sectionId].infobar
   deleteHandler,
 }) => {
   const {
     register,
     formState: { errors },
-    setValue,
     watch,
-  } = useForm({
-    mode: "onBlur",
-    resolver: yupResolver(EditorInfobarSchema),
-    defaultValues: {
-      title: "",
-      subtitle: "",
-      description: "",
-      button: "",
-      url: "",
-    },
-  })
-
-  watch((data) => !_.isEqual(data, sectionContent) && onUpdate(data)) // updates parent component (EditHomepage) when form values are changed
-
-  /** ******************************** */
-  /*     useEffects to load data     */
-  /** ******************************** */
-
-  useEffect(() => {
-    if (sectionContent) {
-      Object.entries(sectionContent).forEach(([key, value]) => {
-        setValue(key, value, {
-          shouldValidate: true,
-        })
-      })
-    }
-  }, [])
+  } = useFormContext()
+  const sectionErrors = _.get(errors, fieldId)
 
   return (
     <CardContainer
       cardTitle={`Infobar section: ${watch("title")}`}
-      isError={!_.isEmpty(errors)}
+      isError={!_.isEmpty(sectionErrors)}
     >
       <div className={elementStyles.cardContent}>
         <FormField
           register={register}
           title="Infobar subtitle"
-          id="subtitle"
-          errorMessage={errors.subtitle?.message}
+          id={`${fieldId}.subtitle`}
+          errorMessage={sectionErrors?.subtitle?.message}
           isRequired
         />
         <FormField
           register={register}
           title="Infobar title"
-          id={`title`}
-          errorMessage={errors.title?.message}
+          id={`${fieldId}.title`}
+          errorMessage={sectionErrors?.title?.message}
           isRequired
         />
         <FormField
           register={register}
           title="Infobar description"
-          id={`description`}
-          errorMessage={errors.description?.message}
+          id={`${fieldId}.description`}
+          errorMessage={sectionErrors?.description?.message}
           isRequired
         />
         <FormField
           register={register}
           title="Infobar button name"
           id={`button`}
-          errorMessage={errors.button?.message}
+          errorMessage={sectionErrors?.button?.message}
           isRequired
         />
         <FormField
           register={register}
           title="Infobar button URL"
           placeholder="Insert permalink or external URL"
-          id={`url`}
-          errorMessage={errors.url?.message}
+          id={`${fieldId}.url`}
+          errorMessage={sectionErrors?.url?.message}
           isRequired
         />
       </div>
       <div className={elementStyles.inputGroup}>
         <button
           type="button"
-          id={`section-${sectionIndex}`}
+          id={`${fieldId}-delete`}
           className={`ml-auto ${elementStyles.warning}`}
           onClick={deleteHandler}
         >
