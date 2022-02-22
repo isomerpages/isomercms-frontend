75cc8c6abbec74118412a4baadda18b77812d910
Alexis <gweiying@gmail.com>
2022-02-22 11:00:21 +0800

    chore: update EditorSections schema to include section object

--- src/components/homepage/EditorHeroSection/EditorHeroSchema.jsx
+++ src/components/homepage/EditorHeroSection/EditorHeroSchema.jsx
@@ -11,28 +11,31 @@ import {
   HeroDropdownSchema,
 } from "components/homepage/HeroSection"
 
-export const EditorHeroSchema = Yup.object()
-  .shape({
-    title: Yup.string()
-      .min(
-        HERO_TITLE_MIN_LENGTH,
-        `Title must be longer than ${HERO_TITLE_MIN_LENGTH} characters`
-      )
-      .max(
-        HERO_TITLE_MAX_LENGTH,
-        `Title must be shorter than ${HERO_TITLE_MAX_LENGTH} characters`
-      ),
-    subtitle: Yup.string()
-      .min(
-        HERO_SUBTITLE_MIN_LENGTH,
-        `Title must be longer than ${HERO_SUBTITLE_MIN_LENGTH} characters`
-      )
-      .max(
-        HERO_SUBTITLE_MAX_LENGTH,
-        `Title must be shorter than ${HERO_SUBTITLE_MAX_LENGTH} characters`
-      ),
-    background: Yup.string(),
-    heroType: Yup.string(),
-    dropdown: HeroDropdownSchema,
-  })
-  .concat(HeroHighlightSchema)
+export const EditorHeroSchema = Yup.object().shape({
+  hero: Yup.object()
+    .shape({
+      title: Yup.string()
+        .min(
+          HERO_TITLE_MIN_LENGTH,
+          `Title must be longer than ${HERO_TITLE_MIN_LENGTH} characters`
+        )
+        .max(
+          HERO_TITLE_MAX_LENGTH,
+          `Title must be shorter than ${HERO_TITLE_MAX_LENGTH} characters`
+        ),
+      subtitle: Yup.string()
+        .min(
+          HERO_SUBTITLE_MIN_LENGTH,
+          `Title must be longer than ${HERO_SUBTITLE_MIN_LENGTH} characters`
+        )
+        .max(
+          HERO_SUBTITLE_MAX_LENGTH,
+          `Title must be shorter than ${HERO_SUBTITLE_MAX_LENGTH} characters`
+        ),
+      background: Yup.string(),
+      heroType: Yup.string(),
+      options: Yup.array(),
+      dropdown: HeroDropdownSchema,
+    })
+    .concat(HeroHighlightSchema),
+})
--- src/components/homepage/EditorResourcesSection/EditorResourcesSchema.jsx
+++ src/components/homepage/EditorResourcesSection/EditorResourcesSchema.jsx
@@ -9,31 +9,33 @@ import {
 } from "utils/validators"
 
 export const EditorResourcesSchema = Yup.object().shape({
-  title: Yup.string()
-    .min(
-      RESOURCES_TITLE_MIN_LENGTH,
-      `Title must be longer than ${RESOURCES_TITLE_MIN_LENGTH} characters`
-    )
-    .max(
-      RESOURCES_TITLE_MAX_LENGTH,
-      `Title must be shorter than ${RESOURCES_TITLE_MAX_LENGTH} characters`
-    ),
-  subtitle: Yup.string()
-    .min(
-      RESOURCES_SUBTITLE_MIN_LENGTH,
-      `Title must be longer than ${RESOURCES_SUBTITLE_MIN_LENGTH} characters`
-    )
-    .max(
-      RESOURCES_SUBTITLE_MAX_LENGTH,
-      `Title must be shorter than ${RESOURCES_SUBTITLE_MAX_LENGTH} characters`
-    ),
-  button: Yup.string()
-    .min(
-      RESOURCES_BUTTON_MIN_LENGTH,
-      `Title must be longer than ${RESOURCES_BUTTON_MIN_LENGTH} characters`
-    )
-    .max(
-      RESOURCES_BUTTON_MAX_LENGTH,
-      `Title must be shorter than ${RESOURCES_BUTTON_MAX_LENGTH} characters`
-    ),
+  resources: Yup.object().shape({
+    title: Yup.string()
+      .min(
+        RESOURCES_TITLE_MIN_LENGTH,
+        `Title must be longer than ${RESOURCES_TITLE_MIN_LENGTH} characters`
+      )
+      .max(
+        RESOURCES_TITLE_MAX_LENGTH,
+        `Title must be shorter than ${RESOURCES_TITLE_MAX_LENGTH} characters`
+      ),
+    subtitle: Yup.string()
+      .min(
+        RESOURCES_SUBTITLE_MIN_LENGTH,
+        `Title must be longer than ${RESOURCES_SUBTITLE_MIN_LENGTH} characters`
+      )
+      .max(
+        RESOURCES_SUBTITLE_MAX_LENGTH,
+        `Title must be shorter than ${RESOURCES_SUBTITLE_MAX_LENGTH} characters`
+      ),
+    button: Yup.string()
+      .min(
+        RESOURCES_BUTTON_MIN_LENGTH,
+        `Title must be longer than ${RESOURCES_BUTTON_MIN_LENGTH} characters`
+      )
+      .max(
+        RESOURCES_BUTTON_MAX_LENGTH,
+        `Title must be shorter than ${RESOURCES_BUTTON_MAX_LENGTH} characters`
+      ),
+  }),
 })
