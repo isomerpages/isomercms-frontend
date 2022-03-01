f517261e57b2c08a614cae3a4b946d810ccbb807
Alexis <gweiying@gmail.com>
2022-03-01 10:58:11 +0800

    refactor: create HeroDropdown, HeroHighlight with useFormContext and validation

--- src/components/homepage/HeroSection/index.js
+++ src/components/homepage/HeroSection/index.js
@@ -1,1 +1,11 @@
+export { HeroDropdown } from "./HeroDropdown"
+export { HeroHighlight } from "./HeroHighlight"
 export { HeroOption } from "./HeroOption"
+export {
+  HeroDropdownSchema,
+  HeroDropdownOptionSchema,
+} from "./HeroDropdownSchema"
+export {
+  HeroHighlightSchema,
+  HeroHighlightOptionSchema,
+} from "./HeroHighlightSchema"
