6ba0f1c1731401fe398389d78a8b9bd9b13d7edc
seaerchin <44049504+seaerchin@users.noreply.github.com>
2022-03-09 11:14:17 +0800

    ref(formFieldColour): refactored to use base form components (#792)
    * refactor(formtitle): added optional classname prop
    
    * refactor(formfieldcolor): refactor formfieldcolor to remove extra concerns
    
    * refactor(colorpickersection): updates to new formfieldcolour api
    
    * chore(formfieldcolour): removed extra autocomplete prop

--- src/components/Form/FormTitle.jsx
+++ src/components/Form/FormTitle.jsx
@@ -3,11 +3,12 @@ import React from "react"
 
 import elementStyles from "styles/isomer-cms/Elements.module.scss"
 
-const FormTitle = ({ children }) => (
-  <p className={elementStyles.formLabel}>{children}</p>
+const FormTitle = ({ className = elementStyles.formLabel, children }) => (
+  <p className={className}>{children}</p>
 )
 
 FormTitle.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.node,
 }
 
--- src/components/settings/FormFieldColor.jsx
+++ src/components/settings/FormFieldColor.jsx
@@ -1,20 +1,18 @@
+import FormInput from "components/Form/FormInput"
 import ColorPickerModal from "components/settings/ColorPickerModal"
 import PropTypes from "prop-types"
 import React, { useCallback, useEffect, useState } from "react"
 
 import elementStyles from "styles/isomer-cms/Elements.module.scss"
 
-const FormFieldColor = ({
-  title,
-  defaultValue,
-  value,
-  id,
-  isRequired,
-  style,
-  saveChanges,
-}) => {
+const FormFieldColor = ({ value, id, saveChanges }) => {
   const [originalColor, setOriginalColor] = useState()
   const [colorPickerToggle, setColorPickerToggle] = useState(false)
+
+  const setRealTimeColor = (color) => {
+    const hex = color.hex ? color.hex : `#${color}`
+    saveChanges(hex)
+  }
 
   // event listener callback function that resets ColorPicker modal
   // when escape key is pressed while modal is active
@@ -89,11 +87,6 @@ const FormFieldColor = ({
     setColorPickerToggle(false)
   }
 
-  const setRealTimeColor = (color) => {
-    const hex = color.hex ? color.hex : `#${color}`
-    saveChanges(hex)
-  }
-
   return (
     <>
       {/* Color picker modal */}
@@ -105,27 +98,20 @@ const FormFieldColor = ({
           elementId={id}
         />
       )}
-      <div className={elementStyles.formColor}>
-        <p className={elementStyles.formColorLabel}>{title}</p>
-        <input
-          type="text"
-          placeholder={title}
-          value={value}
-          defaultValue={defaultValue}
-          id={id}
-          autoComplete="off"
-          required={isRequired}
-          className={elementStyles.formColorInput}
-          style={style}
-          disabled
-        />
-        <div
-          className={elementStyles.formColorBox}
-          id={`${id}-box`}
-          style={{ background: value }}
-          onClick={activateColorPicker}
-        />
-      </div>
+      <FormInput
+        value={value}
+        id={id}
+        // This component is purely presentational.
+        // It is used to display the hex code and hence, is alwaysDisabled.
+        alwaysDisabled
+        className={elementStyles.formColorInput}
+      />
+      <div
+        className={elementStyles.formColorBox}
+        id={`${id}-box`}
+        style={{ background: value }}
+        onClick={activateColorPicker}
+      />
     </>
   )
 }
@@ -133,16 +119,7 @@ const FormFieldColor = ({
 export default FormFieldColor
 
 FormFieldColor.propTypes = {
-  title: PropTypes.string.isRequired,
-  defaultValue: PropTypes.string,
   value: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
-  isRequired: PropTypes.bool.isRequired,
-  style: PropTypes.string,
   saveChanges: PropTypes.func.isRequired,
-}
-
-FormFieldColor.defaultProps = {
-  defaultValue: undefined,
-  style: undefined,
 }
