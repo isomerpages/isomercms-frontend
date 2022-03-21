6e80087d2d7b786fc9f210055d62f8403d4db11f
seaerchin <jiachin@open.gov.sg>
2022-03-21 16:11:48 +0800

    refactor(sites): removed nested ternary, used lodash set

--- src/layouts/Sites.jsx
+++ src/layouts/Sites.jsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@opengovsg/design-system-react"
 import axios from "axios"
 import Banner from "components/Banner"
 import Header from "components/Header"
+import _ from "lodash"
 import { Component } from "react"
 import { Link } from "react-router-dom"
 
@@ -10,6 +11,33 @@ import { SITES_IS_PRIVATE_KEY } from "constants/constants"
 
 import elementStyles from "styles/isomer-cms/Elements.module.scss"
 import siteStyles from "styles/isomer-cms/pages/Sites.module.scss"
+
+const Site = ({ siteNames }) => {
+  if (siteNames && siteNames.length > 0)
+    return siteNames.map((siteName) => (
+      <div className={siteStyles.siteContainer} key={siteName.repoName}>
+        <div className={siteStyles.site}>
+          <Link to={`/sites/${siteName.repoName}/workspace`}>
+            <div className={siteStyles.siteImage} />
+            <div className={siteStyles.siteDescription}>
+              <div className={siteStyles.siteName}>{siteName.repoName}</div>
+              <div className={siteStyles.siteInfo}>{siteName.lastUpdated}</div>
+            </div>
+          </Link>
+        </div>
+      </div>
+    ))
+
+  if (siteNames && siteNames.length === 0)
+    return (
+      <div className={siteStyles.infoText}>
+        You do not have access to any sites at the moment. Please contact your
+        system administrator.
+      </div>
+    )
+
+  return <div className={siteStyles.infoText}>Loading sites...</div>
+}
 
 export default class Sites extends Component {
   _isMounted = false
@@ -35,10 +63,11 @@ export default class Sites extends Component {
       window.localStorage.setItem(
         SITES_IS_PRIVATE_KEY,
         JSON.stringify(
-          siteNames.reduce((map, siteName) => {
-            map[siteName.repoName] = siteName.isPrivate
-            return map
-          }, {})
+          siteNames.reduce(
+            (map, siteName) =>
+              _.set(map, siteName.repoName, siteName.isPrivate),
+            {}
+          )
         )
       )
 
@@ -80,35 +109,7 @@ export default class Sites extends Component {
               </div>
             </div>
             <div className={siteStyles.sites}>
-              {siteNames && siteNames.length > 0 ? (
-                siteNames.map((siteName) => (
-                  <div
-                    className={siteStyles.siteContainer}
-                    key={siteName.repoName}
-                  >
-                    <div className={siteStyles.site}>
-                      <Link to={`/sites/${siteName.repoName}/workspace`}>
-                        <div className={siteStyles.siteImage} />
-                        <div className={siteStyles.siteDescription}>
-                          <div className={siteStyles.siteName}>
-                            {siteName.repoName}
-                          </div>
-                          <div className={siteStyles.siteInfo}>
-                            {siteName.lastUpdated}
-                          </div>
-                        </div>
-                      </Link>
-                    </div>
-                  </div>
-                ))
-              ) : siteNames && siteNames.length === 0 ? (
-                <div className={siteStyles.infoText}>
-                  You do not have access to any sites at the moment. Please
-                  contact your system administrator.
-                </div>
-              ) : (
-                <div className={siteStyles.infoText}>Loading sites...</div>
-              )}
+              <Site siteNames={siteNames} />
             </div>
           </div>
         </div>
