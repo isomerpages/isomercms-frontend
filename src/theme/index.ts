4c0ff027ce7f2e9d1e19d9db871a5e8960cb8100
seaerchin <jiachin@open.gov.sg>
2022-04-13 12:56:48 +0800

    chore(theme): unset global overrides in chakra theme

---
+++ src/theme/index.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: null,
+      },
+    },
+  },
+})
+
+export default theme
