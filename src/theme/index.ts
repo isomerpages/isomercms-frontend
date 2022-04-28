3fdf36c11e985217602b5a2c59c733752805e483
seaerchin <jiachin@open.gov.sg>
2022-04-28 17:32:05 +0800

    fix(theme): extend ogp theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,8 +1,9 @@
 import { extendTheme } from "@chakra-ui/react"
+import { theme as designSystemTheme } from "@opengovsg/design-system-react"
 
 import { colours } from "./foundations/colours"
 
-const theme = extendTheme({
+const theme = extendTheme(designSystemTheme, {
   styles: {
     global: {
       body: {
