7ca0225753a64c832db34f4da47e90b77f74fcf4
seaerchin <jiachin@open.gov.sg>
2022-05-11 13:48:28 +0800

    refactor(movemenubackbutton): uses ogp button and refactors component structured

--- src/components/move/MoveMenuBackButton.jsx
+++ src/components/move/MoveMenuBackButton.jsx
@@ -1,22 +1,30 @@
-import elementStyles from "styles/isomer-cms/Elements.module.scss"
+import { Flex, Box } from "@chakra-ui/react"
+import { Button } from "@opengovsg/design-system-react"
 
 import { deslugifyDirectory } from "utils"
 
 // eslint-disable-next-line import/prefer-default-export
 export const MoveMenuBackButton = ({ onBack, isDisabled, backButtonText }) => {
   return (
-    <button
-      type="button"
+    <Button
       id="moveModal-backButton"
-      className={`${elementStyles.dropdownHeader} ${elementStyles.dropdownTextButton}`}
-      onMouseDown={!isDisabled && onBack}
+      onClick={!isDisabled && onBack}
+      borderRadius={0}
+      top={0}
+      position="sticky"
+      // NOTE: Design system button wraps in a box, which means that the Flex component
+      // is unable to stretch to fill the width of the button hence this prop is here
+      justifyContent="flex-start"
+      py="0.3rem"
+      paddingStart={isDisabled ? "3rem" : "1.5rem"}
+      w="100%"
     >
-      <i
-        className={`${elementStyles.dropdownIcon} ${
-          !isDisabled && "bx bx-sm bx-arrow-back text-white"
-        }`}
-      />
-      {deslugifyDirectory(backButtonText)}
-    </button>
+      <Flex alignItems="center">
+        {!isDisabled && (
+          <Box pr="1.5rem" className="bx bx-sm bx-arrow-back text-white" />
+        )}
+        {deslugifyDirectory(backButtonText)}
+      </Flex>
+    </Button>
   )
 }
