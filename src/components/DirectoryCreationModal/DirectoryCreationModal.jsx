30301fe56b7e4bb2d17cf407d5979685d80562a9
seaerchin <jiachin@open.gov.sg>
2022-05-13 21:27:31 +0800

    refactor(directorycreationmodal): refactor dir creation modal

--- src/components/DirectoryCreationModal/DirectoryCreationModal.jsx
+++ src/components/DirectoryCreationModal/DirectoryCreationModal.jsx
@@ -8,6 +8,7 @@ import {
   Text,
   HStack,
   Divider,
+  VStack,
 } from "@chakra-ui/react"
 import { yupResolver } from "@hookform/resolvers/yup"
 import { ModalCloseButton, Button } from "@opengovsg/design-system-react"
@@ -17,6 +18,7 @@ import {
   DirectorySettingsModal,
 } from "components/DirectorySettingsModal"
 import { FolderCard } from "components/FolderCard"
+import _ from "lodash"
 import PropTypes from "prop-types"
 import { useState } from "react"
 import { useFieldArray, useForm, FormProvider } from "react-hook-form"
@@ -81,32 +83,43 @@ export const DirectoryCreationModal = ({
   // Sub-component used here for clarity
   const FolderContents = () => {
     if (pagesData && pagesData.length > 0) {
-      return pagesData.map((pageData, pageIdx) => (
-        <FolderCard
-          displayText={pageFileNameToTitle(pageData.name)}
-          settingsToggle={() => {}}
-          key={pageData.name}
-          pageType="file"
-          siteName={siteName}
-          itemIndex={pageIdx}
-          selectedIndex={
-            fields.findIndex((item) => item.name === pageData.name) !== -1
-              ? fields.findIndex((item) => item.name === pageData.name) + 1
-              : null
-          }
-          onClick={() => {
-            const indexOfItem = fields.findIndex(
-              (item) => item.name === pageData.name
-            )
+      return (
+        <VStack alignItems="flex-start" spacing={8} w="100%">
+          {_.chunk(pagesData, 3).map((rows) => (
+            <HStack w="100%" spacing={8}>
+              {rows.map((pageData, pageIdx) => (
+                <FolderCard
+                  displayText={pageFileNameToTitle(pageData.name)}
+                  settingsToggle={() => {}}
+                  key={pageData.name}
+                  pageType="file"
+                  siteName={siteName}
+                  itemIndex={pageIdx}
+                  selectedIndex={
+                    fields.findIndex((item) => item.name === pageData.name) !==
+                    -1
+                      ? fields.findIndex(
+                          (item) => item.name === pageData.name
+                        ) + 1
+                      : null
+                  }
+                  onClick={() => {
+                    const indexOfItem = fields.findIndex(
+                      (item) => item.name === pageData.name
+                    )
 
-            if (indexOfItem !== -1) {
-              remove(indexOfItem)
-            } else {
-              append({ name: pageData.name, type: "file" })
-            }
-          }}
-        />
-      ))
+                    if (indexOfItem !== -1) {
+                      remove(indexOfItem)
+                    } else {
+                      append({ name: pageData.name, type: "file" })
+                    }
+                  }}
+                />
+              ))}
+            </HStack>
+          ))}
+        </VStack>
+      )
     }
 
     if (pagesData) {
@@ -139,27 +152,32 @@ export const DirectoryCreationModal = ({
         scrollBehavior="inside"
       >
         <ModalOverlay />
-        <ModalContent overflow="scroll" pt={24.5}>
+        <ModalContent pt={24.5} bgColor="#F9F9F9">
           <ModalCloseButton colorScheme="primary" />
           {/*
            * NOTE: We have to set padding separately for header/body;
            * This is because using a box and setting flex = 1 + padding causes the scrollbar to not appear
            */}
-          <ModalHeader paddingInline={66}>
-            <Text textStyle="display-2">
-              {`Select items to add into '${methods.watch(
-                "newDirectoryName"
-              )}'`}
-            </Text>
-            <Text textStyle="body-2">
-              Pages will be ordered by the order of selection
-            </Text>
-            <Divider mt={8} mb={11.5} />
+          <ModalHeader paddingInline="264px">
+            <VStack spacing={1.5} align="flex-start">
+              <Text textStyle="display-2">
+                {`Select items to add into '${methods.watch(
+                  "newDirectoryName"
+                )}'`}
+              </Text>
+              <Text textStyle="body-2">
+                Pages will be ordered by the order of selection
+              </Text>
+            </VStack>
           </ModalHeader>
-          <ModalBody px={66}>
-            <FolderContents />
+          <ModalBody px="264px">
+            <VStack spacing="46px" mt={8} align="flex-start">
+              <Divider borderColor="#E9E9E9" />
+              <Text textStyle="body-2">Ungrouped pages</Text>
+              <FolderContents />
+            </VStack>
           </ModalBody>
-          <ModalFooter>
+          <ModalFooter bg="white" borderTop="1px solid" borderColor="#E9E9E9">
             <HStack spacing={2}>
               <Button onClick={onClose} variant="clear">
                 Cancel
