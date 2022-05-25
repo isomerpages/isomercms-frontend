0f47b6e4ba1861816651005d6dfb22f2b1f827a6
Alexander Lee <alexander@open.gov.sg>
2022-05-25 15:15:22 +0800

    Chore: replace preview css with scoped variants

--- src/templates/contact-us/FeedbackSection.jsx
+++ src/templates/contact-us/FeedbackSection.jsx
@@ -2,16 +2,38 @@ import { sanitizeUrl } from "@braintree/sanitize-url"
 import PropTypes from "prop-types"
 import { forwardRef } from "react"
 
+import editorStyles from "styles/isomer-cms/pages/Editor.module.scss"
+
+import { getClassNames } from "utils"
+
 const TemplateFeedbackSection = forwardRef(({ feedback }, ref) => (
   <div ref={ref}>
     {feedback && (
-      <div className="row is-multiline margin--bottom--lg">
-        <div className="col is-12 padding--bottom--none">
-          <h5 className="has-text-secondary has-text-weight-semibold">
+      <div
+        className={getClassNames(editorStyles, [
+          "row",
+          "is-multiline",
+          "margin--bottom--lg",
+        ])}
+      >
+        <div
+          className={getClassNames(editorStyles, [
+            "col",
+            "is-12",
+            "padding--bottom--none",
+          ])}
+        >
+          <h5
+            className={getClassNames(
+              editorStyles,
+              ["has-text-weight-semibold"],
+              ["has-text-secondary"]
+            )}
+          >
             Send us your feedback
           </h5>
         </div>
-        <div className="col is-8">
+        <div className={getClassNames(editorStyles, ["col", "is-8"])}>
           {/* fixing target='_blank' warning: https://www.jitbit.com/alexblog/256-targetblank---the-most-underestimated-vulnerability-ever/ */}
           <p>
             If you have a query, feedback or wish to report a problem related to
