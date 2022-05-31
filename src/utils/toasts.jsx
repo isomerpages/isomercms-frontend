c61a6f3b55ba54fbc4b950cbd10840d0605d9249
Alexander Lee <alexander@open.gov.sg>
2022-05-31 17:22:09 +0800

    Refactor: move default toast props

--- src/utils/toasts.jsx
+++ src/utils/toasts.jsx
@@ -2,18 +2,21 @@ import { useToast } from "@opengovsg/design-system-react"
 
 import { DEFAULT_ERROR_TOAST_MSG } from "utils"
 
+const DEFAULT_TOAST_PROPS = {
+  duration: 5000,
+  isClosable: true,
+}
+
 export const useErrorToast = () =>
   useToast({
+    ...DEFAULT_TOAST_PROPS,
     description: DEFAULT_ERROR_TOAST_MSG,
     status: "danger",
-    duration: 5000,
-    isClosable: true,
   })
 
 export const useSuccessToast = () =>
   useToast({
+    ...DEFAULT_TOAST_PROPS,
     description: "Success!",
     status: "success",
-    duration: 5000,
-    isClosable: true,
   })
