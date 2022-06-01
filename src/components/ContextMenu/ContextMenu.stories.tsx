a6b9a11f2a900c25925c2c76fcb82c43a4464b93
seaerchin <jiachin@open.gov.sg>
2022-06-01 15:26:55 +0800

    feat(contextmenu): add story

---
+++ src/components/ContextMenu/ContextMenu.stories.tsx
@@ -0,0 +1,46 @@
+import "@opengovsg/design-system-react/build/fonts/inter.css"
+import { Flex, Icon, Text } from "@chakra-ui/react"
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+import { BiChevronRight, BiEditAlt, BiFolder, BiWrench } from "react-icons/bi"
+
+import { ContextMenuButton } from "./ContextMenuButton"
+import { ContextMenuItem } from "./ContextMenuItem"
+
+const contextMenuMeta = {
+  title: "Components/Context Menu",
+  component: ContextMenuButton,
+} as ComponentMeta<typeof ContextMenuButton>
+
+const BaseComponent = (args: TemplateArgs) => {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <ContextMenuButton {...args}>
+      <ContextMenuItem icon={<BiEditAlt />}>
+        <Text textStyle="body-1">Edit page</Text>
+      </ContextMenuItem>
+      <ContextMenuItem icon={<BiWrench />}>Page settings</ContextMenuItem>
+      <ContextMenuItem icon={<BiFolder />}>
+        <Flex justify="space-between" alignItems="center">
+          <Text>Move to</Text>
+          <Icon as={BiChevronRight} fontSize="1.25rem" />
+        </Flex>
+      </ContextMenuItem>
+    </ContextMenuButton>
+  )
+}
+
+interface TemplateArgs {
+  defaultIsOpen: boolean
+}
+
+const Template: ComponentStory<typeof BaseComponent> = (args: TemplateArgs) => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <BaseComponent {...args} />
+)
+
+export const ContextMenu = Template.bind({})
+export const OpenContextMenu = Template.bind({})
+OpenContextMenu.args = {
+  defaultIsOpen: true,
+}
+export default contextMenuMeta
