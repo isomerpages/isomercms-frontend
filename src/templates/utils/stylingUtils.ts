e00e84a9ef274227bf2888e1b3ea4d0c1ed21f55
Alexander Lee <alexander@open.gov.sg>
2022-06-02 13:34:14 +0800

    Chore: rename class names and add comments

--- src/templates/utils/stylingUtils.ts
+++ src/templates/utils/stylingUtils.ts
@@ -1,6 +1,7 @@
 import classNames from "classnames"
 import _ from "lodash"
 
+// Styles which are used in both imported stylesheet and inserted in siteColorUtils
 const OVERRIDDEN_CLASS_NAMES = [
   "bp-section",
   "bp-container",
@@ -10,7 +11,10 @@ const OVERRIDDEN_CLASS_NAMES = [
   "col",
   "sgds-icon",
 ]
-const UNSTYLED_CLASS_NAMES = [
+
+// Styles related to colours - only use the original name so that our colours take precedence
+// over the !important styles defined in the imported stylesheets
+const INSERTED_CLASS_NAMES = [
   "has-text-secondary",
   "bg-secondary",
   "is-secondary",
@@ -23,9 +27,9 @@ export const getClassNames = (
   // Generates relevant class, with classes using the provided styles
   return classNames(
     classes
-      .filter((className) => !UNSTYLED_CLASS_NAMES.includes(className))
+      .filter((className) => !INSERTED_CLASS_NAMES.includes(className))
       .map((className) => styles[className])
       .concat(_.intersection(classes, OVERRIDDEN_CLASS_NAMES))
-      .concat(_.intersection(classes, UNSTYLED_CLASS_NAMES))
+      .concat(_.intersection(classes, INSERTED_CLASS_NAMES))
   )
 }
