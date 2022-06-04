575988730a444d8948fb00cbe6f00933789a059e
seaerchin <jiachin@open.gov.sg>
2022-06-05 04:22:07 +0800

    feat(cards): add workspace card components

---
+++ src/layouts/Workspace/components/Cards/NavigationCard.tsx
@@ -0,0 +1,29 @@
+import { LinkOverlay, LinkBox, Text, Icon } from "@chakra-ui/react"
+import { Card, CardBody } from "components/Card"
+import { BiWorld } from "react-icons/bi"
+import { Link as RouterLink } from "react-router-dom"
+
+interface NavigationCardProps {
+  siteName: string
+}
+
+export const NavigationCard = ({
+  siteName,
+}: NavigationCardProps): JSX.Element => {
+  const generatedLink = `/sites/${siteName}/navbar`
+
+  return (
+    <LinkBox>
+      <LinkOverlay as={RouterLink} to={generatedLink}>
+        <Card variant="single">
+          <CardBody>
+            <Icon as={BiWorld} fontSize="1.5rem" fill="secondary.400" />
+            <Text textStyle="subhead-1" color="text.label">
+              Navigation Bar
+            </Text>
+          </CardBody>
+        </Card>
+      </LinkOverlay>
+    </LinkBox>
+  )
+}
