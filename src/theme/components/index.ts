c6614dac2ca3f86b409baac2674e89ebf3fefc37
seaerchin <jiachin@open.gov.sg>
2022-06-09 14:03:30 +0800

    feat(tabs): add new styling for tabs component

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,6 +1,8 @@
 import { Card, CARD_THEME_KEY } from "./Card"
+import { Tabs } from "./Tabs"
 
 // eslint-disable-next-line import/prefer-default-export
 export const components = {
   [CARD_THEME_KEY]: Card,
+  Tabs,
 }
