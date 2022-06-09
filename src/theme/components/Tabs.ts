a2240d488a535fc2a72a2b321e73bb06d869e689
seaerchin <jiachin@open.gov.sg>
2022-06-09 14:03:30 +0800

    feat(tabs): add new styling for tabs component

---
+++ src/theme/components/Tabs.ts
@@ -0,0 +1,45 @@
+export const Tabs = {
+  variants: {
+    "line-vertical": (props) => {
+      const { colorScheme: c, orientation } = props
+      const borderProp =
+        orientation === "vertical" ? "borderStart" : "borderBottom"
+
+      return {
+        tablist: {
+          // NOTE: The original chakra implementation does a vertical line indicator
+          // through having a border of 2px on the tablist.
+          // The tabs then have a negative margin of 2px, overlapping this border.
+          // On select, the tabs would then activate and have their border colour set.
+          // However, this does not seem to work when replicated here (tab borders don't display over tablist).
+          // Hence, border colour has been set on tabs rather than tablist
+          border: "0px",
+        },
+        tab: {
+          boxSizing: "border-box",
+          borderRadius: 0,
+          [borderProp]: "2px solid",
+          borderColor: "border.divider.disabled",
+          textColor: "text.link.disabled",
+          py: "1rem",
+          _focus: {
+            boxShadow: `0 0 0 2px var(--chakra-colors-border-action-default)`,
+          },
+          _hover: {
+            textColor: "text.link.hover",
+          },
+          _selected: {
+            color: "text.link.default",
+            borderColor: "border.divider.active",
+          },
+          _active: {
+            bg: "background.action.infoInverse",
+          },
+          _disabled: {
+            _active: { bg: "none" },
+          },
+        },
+      }
+    },
+  },
+}
