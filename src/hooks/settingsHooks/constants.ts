fdde9b1d7300f1f2d9726ddc4a512be747583c59
seaerchin <jiachin@open.gov.sg>
2022-06-10 19:49:41 +0800

    chore(settingshooks): add constants

---
+++ src/hooks/settingsHooks/constants.ts
@@ -0,0 +1,26 @@
+import { invert } from "lodash"
+import type { StringKeyOf } from "type-fest"
+
+import { BackendSiteSettings, SiteSettings } from "types/settings"
+
+export const BE_TO_FE: {
+  [K in keyof BackendSiteSettings]: StringKeyOf<SiteSettings>
+} = {
+  title: "title",
+  description: "description",
+  shareicon: "shareicon",
+  favicon: "favicon",
+  facebook_pixel: "pixel",
+  google_analytics: "ga",
+  linkedin_insights: "insights",
+  is_government: "displayGovMasthead",
+  colors: "colours",
+  contact_us: "contact",
+  feedback: "feedback",
+  faq: "faq",
+  show_reach: "showReach",
+  logo: "agencyLogo",
+  social_media: "socialMediaContent",
+}
+
+export const FE_TO_BE = invert(BE_TO_FE)
