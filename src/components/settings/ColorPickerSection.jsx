f0f14452a3bf5cd7d19bf1bb94e6b16d20eaf903
seaerchin <jiachin@open.gov.sg>
2022-06-10 19:54:38 +0800

    chore(colorpickersection): remove outer div

--- src/components/settings/ColorPickerSection.jsx
+++ src/components/settings/ColorPickerSection.jsx
@@ -8,66 +8,59 @@ import elementStyles from "styles/isomer-cms/Elements.module.scss"
 
 const ColorPickerSection = ({ colors, saveChanges }) => {
   return (
-    <div id="color-fields">
-      <p className={elementStyles.formSectionHeader}>Colors</p>
-      <FormContext isRequired>
-        <div className={elementStyles.formColor}>
-          <FormTitle className={elementStyles.formColorLabel}>
-            Primary
-          </FormTitle>
-          <FormFieldColor
-            id="primary-color"
-            value={colors["primary-color"]}
-            saveChanges={(selectedColor) => {
-              saveChanges({
-                ...colors,
-                "primary-color": selectedColor,
-              })
-            }}
-          />
-        </div>
-        <div className={elementStyles.formColor}>
-          <FormTitle className={elementStyles.formColorLabel}>
-            Secondary
-          </FormTitle>
-          <FormFieldColor
-            id="secondary-color"
-            value={colors["secondary-color"]}
-            saveChanges={(selectedColor) => {
-              saveChanges({
-                ...colors,
-                "secondary-color": selectedColor,
-              })
-            }}
-          />
-        </div>
-        <div id="media-color-fields">
-          {colors["media-colors"].map((category, index) => {
-            const { title: mediaColorName, color } = category
-            return (
-              <div className={elementStyles.formColor}>
-                <FormTitle className={elementStyles.formColorLabel}>
-                  {`Resource ${index + 1}`}
-                </FormTitle>
-                <FormFieldColor
-                  id={`media-color@${index}`}
-                  value={color}
-                  key={mediaColorName}
-                  saveChanges={(selectedColor) => {
-                    const newMediaColors = _.cloneDeep(colors["media-colors"])
-                    newMediaColors[index].color = selectedColor
-                    saveChanges({
-                      ...colors,
-                      "media-colors": newMediaColors,
-                    })
-                  }}
-                />
-              </div>
-            )
-          })}
-        </div>
-      </FormContext>
-    </div>
+    <FormContext isRequired>
+      <div className={elementStyles.formColor}>
+        <FormTitle className={elementStyles.formColorLabel}>Primary</FormTitle>
+        <FormFieldColor
+          id="primary-color"
+          value={colors["primary-color"]}
+          saveChanges={(selectedColor) => {
+            saveChanges({
+              ...colors,
+              "primary-color": selectedColor,
+            })
+          }}
+        />
+      </div>
+      <div className={elementStyles.formColor}>
+        <FormTitle className={elementStyles.formColorLabel}>
+          Secondary
+        </FormTitle>
+        <FormFieldColor
+          id="secondary-color"
+          value={colors["secondary-color"]}
+          saveChanges={(selectedColor) => {
+            saveChanges({
+              ...colors,
+              "secondary-color": selectedColor,
+            })
+          }}
+        />
+      </div>
+      {colors["media-colors"].map((category, index) => {
+        const { title: mediaColorName, color } = category
+        return (
+          <div className={elementStyles.formColor}>
+            <FormTitle className={elementStyles.formColorLabel}>
+              {`Resource ${index + 1}`}
+            </FormTitle>
+            <FormFieldColor
+              id={`media-color@${index}`}
+              value={color}
+              key={mediaColorName}
+              saveChanges={(selectedColor) => {
+                const newMediaColors = _.cloneDeep(colors["media-colors"])
+                newMediaColors[index].color = selectedColor
+                saveChanges({
+                  ...colors,
+                  "media-colors": newMediaColors,
+                })
+              }}
+            />
+          </div>
+        )
+      })}
+    </FormContext>
   )
 }
 
