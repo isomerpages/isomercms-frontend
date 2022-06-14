d500d25223fe6876424ad3dc0b0cba4e503c5e38
seaerchin <jiachin@open.gov.sg>
2022-06-14 11:58:50 +0800

    feat(types): add new types for  dircetory and resources

--- src/types/directory.ts
+++ src/types/directory.ts
@@ -1,5 +1,34 @@
+import type { SetRequired } from "type-fest"
+
 export type DirectoryType =
   | "mediaDirectoryName"
   | "subCollectionName"
   | "collectionName"
   | "resourceRoomName"
+
+export interface DirectoryInfoProps {
+  data: {
+    items?: unknown
+    newDirectoryName: string
+  }
+  mediaDirectoryName?: string
+}
+
+export interface DirectoryInfoReturn {
+  items: DirectoryInfoProps["data"]["items"]
+  newDirectoryName: string
+}
+
+export interface DirectoryData {
+  name: string
+}
+
+export interface PageData {
+  name: string
+  date?: string
+  resourceType?: "file" | "post"
+}
+
+export type ResourcePageData = SetRequired<PageData, "date"> & {
+  resourceType: "post"
+}
