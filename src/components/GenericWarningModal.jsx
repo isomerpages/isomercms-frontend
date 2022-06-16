ac65b9b83739ed098c8679d32958a98c9b577b00
Alexander Lee <alexander@open.gov.sg>
2022-06-16 15:45:37 +0800

    Feat: use chakra components for warning modal

--- src/components/GenericWarningModal.jsx
+++ src/components/GenericWarningModal.jsx
@@ -1,58 +1,38 @@
-import { HStack } from "@chakra-ui/react"
-import { LoadingButton } from "components/LoadingButton"
+import {
+  Text,
+  HStack,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react"
 import parse from "html-react-parser"
-import PropTypes from "prop-types"
-
-import elementStyles from "styles/isomer-cms/Elements.module.scss"
 
 const GenericWarningModal = ({
+  isOpen,
+  onClose,
   displayTitle,
   displayText,
-  displayImage,
-  displayImgAlt,
-  onProceed,
-  onCancel,
-  proceedText,
-  cancelText,
+  children,
 }) => (
-  <div className={elementStyles.overlay}>
-    <div className={elementStyles["modal-warning"]}>
-      <div className={elementStyles.modalHeader}>
-        <h1>{displayTitle}</h1>
-      </div>
-      {displayImage && (
-        <img
-          className="align-self-center"
-          alt={displayImgAlt}
-          src={displayImage}
-        />
-      )}
-      <div className={elementStyles.modalContent}>
-        <p>{parse(displayText)}</p>
-      </div>
-      <HStack w="100%" pt="20px" spacing={2} justifyContent="flex-end">
-        {cancelText && onCancel && (
-          <LoadingButton colorScheme="danger" onClick={onCancel}>
-            {cancelText}
-          </LoadingButton>
-        )}
-        {proceedText && onProceed && (
-          <LoadingButton onClick={onProceed}>{proceedText}</LoadingButton>
-        )}
-      </HStack>
-    </div>
-  </div>
+  <Modal isOpen={isOpen} onClose={onClose}>
+    <ModalOverlay />
+    <ModalContent>
+      <ModalHeader>{displayTitle}</ModalHeader>
+      <ModalCloseButton />
+      <ModalBody>
+        <Text textStyle="body-2">{parse(displayText)}</Text>
+      </ModalBody>
+      <ModalFooter>
+        <HStack w="100%" spacing={2} justifyContent="flex-end">
+          {children}
+        </HStack>
+      </ModalFooter>
+    </ModalContent>
+  </Modal>
 )
-
-GenericWarningModal.propTypes = {
-  displayTitle: PropTypes.string.isRequired,
-  displayText: PropTypes.string.isRequired,
-  displayImage: PropTypes.string,
-  displayImgAlt: PropTypes.string,
-  onProceed: PropTypes.func,
-  onCancel: PropTypes.func,
-  proceedText: PropTypes.string,
-  cancelText: PropTypes.string,
-}
 
 export default GenericWarningModal
