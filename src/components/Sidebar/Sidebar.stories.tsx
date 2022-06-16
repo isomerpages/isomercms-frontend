11ab74456541e252d80e4d4bb229fea69ec1136f
seaerchin <jiachin@open.gov.sg>
2022-06-16 16:20:37 +0800

    test(sidebar): update sidebar story

--- src/components/Sidebar/Sidebar.stories.tsx
+++ src/components/Sidebar/Sidebar.stories.tsx
@@ -1,22 +1,47 @@
-import { Box, Divider, Icon, Text, HStack } from "@chakra-ui/react"
-import { ComponentStory, ComponentMeta, StoryFn } from "@storybook/react"
-import {
-  BiChevronRight,
-  BiEditAlt,
-  BiFolder,
-  BiTrash,
-  BiWrench,
-} from "react-icons/bi"
-import { Route } from "react-router-dom"
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+import { MemoryRouter, Redirect, Route } from "react-router-dom"
+
+import { handlers } from "../../mocks/handlers"
 
 import { Sidebar } from "./Sidebar"
 
 const SidebarMeta = {
   title: "Components/Sidebar",
   component: Sidebar,
+  decorators: [
+    (Story) => {
+      return (
+        <MemoryRouter initialEntries={["/sites/:siteName/"]}>
+          <Redirect to="/sites/storybook/workspace" />
+          <Route path="/sites/:siteName/workspace">
+            <Story />
+          </Route>
+          <Route path="/sites/:siteName/resourceRoom">
+            <Story />
+          </Route>
+          <Route path="/sites/:siteName/settings">
+            <Story />
+          </Route>
+          <Route path="/sites/:siteName/media/files">
+            <Story />
+          </Route>
+          <Route path="/sites/:siteName/media/images">
+            <Story />
+          </Route>
+        </MemoryRouter>
+      )
+    },
+  ],
+} as ComponentMeta<typeof Sidebar>
+
+const Template: ComponentStory<typeof Sidebar> = () => {
+  return <Sidebar />
 }
 
-const Template: ComponentStory<typeof Sidebar> = () => <Sidebar />
-
 export const Default = Template.bind({})
+Default.parameters = {
+  msw: {
+    handlers,
+  },
+}
 export default SidebarMeta
