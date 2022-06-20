85e445592aca035fba93ce08b8d6b406d24fe0e4
seaerchin <jiachin@open.gov.sg>
2022-06-21 02:31:29 +0800

    test(workspace): wip first describe done

--- cypress/integration/workspace.spec.js
+++ cypress/integration/workspace.spec.js
@@ -53,16 +53,16 @@ describe("Workspace Pages flow", () => {
       cy.setCookie(COOKIE_NAME, COOKIE_VALUE)
       window.localStorage.setItem("userId", "test")
       cy.visit(`${CMS_BASEURL}/sites/${TEST_REPO_NAME}/workspace`)
+      cy.contains("Verify").should("not.exist")
     })
 
     it("Test site workspace page should have unlinked pages section", () => {
-      cy.contains("Add a new page")
+      cy.contains("Create page")
     })
 
     it("Should be able to create a new page with valid title and permalink", () => {
-      cy.get("#settings-NEW", { timeout: E2E_EXTENDED_TIMEOUT })
-        .should("exist")
-        .click()
+      // Act
+      cy.contains("button", "Create page").click()
       cy.get("#title").clear().type(TEST_PAGE_TITLE)
       cy.get("#permalink").clear().type(TEST_PAGE_PERMALNK)
       cy.intercept("POST", "/v2/**").as("awaitSave")
@@ -80,7 +80,7 @@ describe("Workspace Pages flow", () => {
           newFileName: `${TEST_PAGE_TITLE}.md`,
         })
 
-      // Asserts
+      // Assert
       // 1. User should be redirected to correct EditPage
       cy.url().should(
         "include",
@@ -91,15 +91,12 @@ describe("Workspace Pages flow", () => {
       )
 
       // 2. If user goes back to the workspace, they should be able to see that the page exists
-      cy.contains("Back to Workspace", { timeout: E2E_EXTENDED_TIMEOUT })
-        .should("exist")
-        .click()
-      cy.contains(TEST_PAGE_TITLE, { timeout: E2E_EXTENDED_TIMEOUT }).should(
-        "exist"
-      )
+      cy.contains("Back to Workspace").should("exist").click()
+      cy.contains("a", TEST_PAGE_TITLE).should("exist")
     })
 
     it("Should not be able to create page with invalid title", () => {
+      // Arrange
       const INVALID_TEST_PAGE_TITLES = [
         "Ab",
         "Lorem Ipsum<",
@@ -112,16 +109,15 @@ describe("Workspace Pages flow", () => {
         "]Lorem Ipsum",
       ]
 
-      cy.get("#settings-NEW", { timeout: E2E_EXTENDED_TIMEOUT })
-        .should("exist")
-        .click()
-
+      // Act
+      cy.contains("button", "Create page").click()
       // Cannot use titles shorter than 4 characters or containing symbols ~!@#$%^&*_+-./\`:;~{}()[]"'<>,?
       INVALID_TEST_PAGE_TITLES.forEach((invalidTitle) => {
         cy.get("#title").clear().type(invalidTitle).blur()
         cy.contains("button", "Save").should("be.disabled")
       })
 
+      // Assert
       // Page title must not already exist
       cy.get("#title").clear().type(TEST_PAGE_TITLE).blur()
       cy.contains("Title is already in use. Please choose a different title.")
@@ -129,12 +125,13 @@ describe("Workspace Pages flow", () => {
     })
 
     it("Should not be able to create page with invalid permalink", () => {
+      // Arrange
       const INVALID_TEST_PAGE_PERMALINKS = ["/12", "test-", "/abcd?"]
 
-      cy.get("#settings-NEW", { timeout: E2E_EXTENDED_TIMEOUT })
-        .should("exist")
-        .click()
+      // Act
+      cy.contains("button", "Create page").click()
 
+      // Assert
       // Permalink needs to be longer than 4 characters, should start with a slash, and contain alphanumeric characters separated by hyphens and slashes only
       INVALID_TEST_PAGE_PERMALINKS.forEach((invalidPermalink) => {
         cy.get("#permalink").clear().type(invalidPermalink).blur()
@@ -143,24 +140,20 @@ describe("Workspace Pages flow", () => {
     })
 
     it("Should be able to edit existing page details with Chinese title and valid permalink", () => {
-      const testPageCard = cy
-        .contains(TEST_PAGE_TITLE, { timeout: E2E_EXTENDED_TIMEOUT })
-        .should("exist")
+      // Arrange
+      cy.contains("a", TEST_PAGE_TITLE).as("pageCard").should("exist")
 
+      // Act
       // User should be able edit page details
-      testPageCard
-        .children()
-        .within(() => cy.get("[id^=pageCard-dropdown-]").click())
-      cy.get("div[id^=settings-]").first().click() // .first() is necessary because the get returns multiple elements (refer to MenuDropdown.jsx)
-
+      cy.clickContextMenuItem("@pageCard", "settings")
       cy.get("#title").should("have.value", TEST_PAGE_TITLE, {
         timeout: E2E_EXTENDED_TIMEOUT,
       })
-
       cy.get("#title").clear().type(EDITED_TEST_PAGE_TITLE)
       cy.contains("button", "Save").click()
 
-      // ASSERT: New page title should be reflected in the Workspace
+      // Assert
+      // New page title should be reflected in the Workspace
       cy.contains(EDITED_TEST_PAGE_TITLE, {
         timeout: E2E_EXTENDED_TIMEOUT,
       }).should("exist")
@@ -169,53 +162,43 @@ describe("Workspace Pages flow", () => {
     })
 
     it("Should be able to edit existing page details with Tamil title and valid permalink", () => {
-      const testPageCard = cy
-        .contains(EDITED_TEST_PAGE_TITLE, {
-          timeout: E2E_EXTENDED_TIMEOUT,
-        })
-        .should("exist")
+      // Arrange
+      cy.contains("a", EDITED_TEST_PAGE_TITLE).as("pageCard").should("exist")
 
+      // Act
       // User should be able edit page details
-      testPageCard
-        .children()
-        .within(() => cy.get("[id^=pageCard-dropdown-]").click())
-      cy.get("div[id^=settings-]").first().click()
-
+      cy.clickContextMenuItem("@pageCard", "settings")
       cy.get("#title").should("have.value", EDITED_TEST_PAGE_TITLE, {
         timeout: E2E_EXTENDED_TIMEOUT,
       })
-
       cy.get("#title").clear().type(EDITED_TEST_PAGE_TITLE_2)
       cy.contains("button", "Save").click()
 
+      // Asserts
+      // Should show modal
       cy.contains("Successfully updated page!", {
         timeout: E2E_EXTENDED_TIMEOUT,
       }).should("exist")
-
-      // Asserts
-      // 1. New page title should be reflected in Folders
+      // New page title should be reflected in Folders
       cy.contains(EDITED_TEST_PAGE_TITLE_2, {
         timeout: E2E_EXTENDED_TIMEOUT,
       }).should("exist")
     })
 
     it("Should be able to delete existing page on workspace", () => {
+      // Arrange
       // Ensure that the frontend has been updated
       cy.contains(EDITED_TEST_PAGE_TITLE, {
         timeout: E2E_EXTENDED_TIMEOUT,
       }).should("not.exist")
 
-      // Assert
+      // Act
       // User should be able to remove the created test page card
-      cy.contains(EDITED_TEST_PAGE_TITLE_2, { timeout: E2E_EXTENDED_TIMEOUT })
-        .should("exist")
-        .children()
-        .within(() => cy.get("[id^=pageCard-dropdown-]").click())
-      cy.get("div[id^=delete-]").first().click() // .first() is necessary because the get returns multiple elements (refer to MenuDropdown.jsx)
-      cy.contains("button", "Delete", { timeout: E2E_EXTENDED_TIMEOUT })
-        .should("exist")
-        .click()
+      cy.contains("a", EDITED_TEST_PAGE_TITLE_2).as("pageCard").should("exist")
+      cy.clickContextMenuItem("@pageCard", "Delete")
+      cy.contains("button", "Delete").should("exist").click()
 
+      // Assert
       cy.contains(EDITED_TEST_PAGE_TITLE_2, {
         timeout: E2E_EXTENDED_TIMEOUT,
       }).should("not.exist")
