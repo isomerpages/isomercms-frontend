bcdfba9084dbd028c0c26678c6a724c7450b932d
seaerchin <44049504+seaerchin@users.noreply.github.com>
2022-06-29 16:10:48 +0800

    ref(design-system): Cards (#926)
    * build(chakra): install chakra theme tools and  utils
    
    * chore(coilours): update colours
    
    * feat(card): initial single card component
    
    * chore(eslint): remove annoying lint messages
    
    * chore(colours): add theme colours
    
    * refactor(card): update card API and stories
    
    * chore(card.stories): fix stoires
    
    * chore(card): fix card styling
    
    * fix(card): make textbox full height of grid item
    
    * fix(card): shift grid size to 4.5rme

---
+++ src/components/Card/Card.stories.tsx
@@ -0,0 +1,107 @@
+import { Icon, Text, useToast, Box } from "@chakra-ui/react"
+import { ComponentMeta, Story } from "@storybook/react"
+import { ContextMenu } from "components/ContextMenu"
+import { BiFolder } from "react-icons/bi"
+
+import { CardBody, Card, CardFooter } from "./Card"
+
+const cardMeta = {
+  title: "Components/Card",
+  component: Card,
+} as ComponentMeta<typeof Card>
+
+interface TemplateArgs {
+  text: string
+}
+
+const SingleCardTemplate: Story<TemplateArgs> = ({ text }: TemplateArgs) => {
+  const toast = useToast()
+
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Card
+      onClick={() =>
+        toast({
+          title: "Account created.",
+          description: "We've created your account for you.",
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        })
+      }
+      variant="single"
+    >
+      <CardBody>
+        <Icon as={BiFolder} fontSize="1.5rem" fill="icon.alt" />
+        <Text textStyle="body-1" color="text.label" noOfLines={1}>
+          {text}
+        </Text>
+      </CardBody>
+      <ContextMenu>
+        <ContextMenu.Button />
+        <ContextMenu.List>
+          <ContextMenu.Item>Something</ContextMenu.Item>
+        </ContextMenu.List>
+      </ContextMenu>
+    </Card>
+  )
+}
+
+interface MultiCardTemplateArgs extends TemplateArgs {
+  caption: string
+}
+const MultiCardTemplate: Story<MultiCardTemplateArgs> = ({
+  text,
+  caption,
+}: MultiCardTemplateArgs) => {
+  const toast = useToast()
+
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Box position="relative">
+      <Card
+        onClick={() =>
+          toast({
+            title: "Account created.",
+            description: "We've created your account for you.",
+            status: "success",
+            duration: 9000,
+            isClosable: true,
+          })
+        }
+        variant="multi"
+      >
+        <CardBody>
+          <Icon as={BiFolder} fontSize="1.5rem" fill="icon.alt" />
+          <Text textStyle="body-1" color="text.label" noOfLines={3}>
+            {text}
+          </Text>
+        </CardBody>
+        <CardFooter textAlign="left" ml="2.5rem">
+          <Text textStyle="caption-2" color="text.helper" noOfLines={1}>
+            {caption}
+          </Text>
+        </CardFooter>
+      </Card>
+      <ContextMenu>
+        <ContextMenu.Button />
+        <ContextMenu.List>
+          <ContextMenu.Item>Something</ContextMenu.Item>
+        </ContextMenu.List>
+      </ContextMenu>
+    </Box>
+  )
+}
+
+export const SingleCard = SingleCardTemplate.bind({})
+SingleCard.args = {
+  text: "Folder 1",
+}
+
+export const MultiCard = MultiCardTemplate.bind({})
+MultiCard.args = {
+  text: "Folder 2",
+  caption: "something",
+}
+
+export default cardMeta
---
+++ src/theme/components/Card.ts
@@ -0,0 +1,78 @@
+import { ComponentMultiStyleConfig } from "@chakra-ui/theme"
+import { anatomy, PartsStyleObject } from "@chakra-ui/theme-tools"
+
+import { colours } from "theme/foundations/colours"
+
+export const CARD_THEME_KEY = "card"
+
+// Determines whether the card is single-line or
+// is multi-line (has a bottom action drawer)
+export type CardVariant = "single" | "multi"
+
+export const parts = anatomy(CARD_THEME_KEY).parts(
+  "container",
+  "body",
+  "footer"
+)
+
+const borderColour = (colours.border.action as Record<string, string>).default
+
+const variantSingle: PartsStyleObject<typeof parts> = {
+  container: {
+    gridTemplateColumns: "1fr 2.5rem",
+    gridTemplateAreas: "'body button'",
+  },
+}
+
+const variantMulti: PartsStyleObject<typeof parts> = {
+  container: {
+    gridTemplateColumns: "1fr 2.5rem",
+    gridTemplateRows: "4.5rem 1fr",
+    // This is a 2x2 grid
+    gridTemplateAreas: "'body button' 'footer button'",
+    gap: "1rem 0rem",
+  },
+  footer: {
+    gridArea: "footer",
+  },
+}
+
+export const Card: ComponentMultiStyleConfig = {
+  parts: parts.keys,
+  baseStyle: {
+    container: {
+      display: "grid",
+      backgroundColor: "background.action.defaultInverse",
+      justifyContent: "flex-start",
+      boxSizing: "border-box",
+      paddingInlineEnd: "2rem",
+      paddingInlineStart: "2rem",
+      paddingY: "1.5rem",
+      border: "1px solid",
+      borderColor: "border.action.light",
+      borderRadius: "4px",
+      w: "100%",
+      _hover: {
+        backgroundColor: "background.action.altInverse",
+      },
+      _focus: {
+        boxShadow: `0 0 0 2px ${borderColour}`,
+      },
+    },
+    body: {
+      justifyContent: "flex-start",
+      display: "flex",
+      alignItems: "flex-start",
+      textAlign: "left",
+      gridArea: "body",
+      height: "full",
+    },
+  },
+  variants: {
+    single: variantSingle,
+    multi: variantMulti,
+  },
+  defaultProps: {
+    variant: "single",
+  },
+}
