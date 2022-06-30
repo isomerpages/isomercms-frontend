e5d1b76ca652f7d0193f490cc9a67442b867b079
seaerchin <jiachin@open.gov.sg>
2022-06-30 13:55:58 +0800

    fix(card): fix card styling

--- src/theme/components/Card.ts
+++ src/theme/components/Card.ts
@@ -58,6 +58,7 @@ export const Card: ComponentMultiStyleConfig = {
       _focus: {
         boxShadow: `0 0 0 2px ${borderColour}`,
       },
+      overflow: "auto",
     },
     body: {
       height: "full",
