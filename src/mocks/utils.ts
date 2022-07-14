ef3bcebdcffb941fe30b29a4121d0f28e1c83bcd
seaerchin <jiachin@open.gov.sg>
2022-07-14 21:52:21 +0800

    refactor(utils): add base method for building data

--- src/mocks/utils.ts
+++ src/mocks/utils.ts
@@ -1,20 +1,28 @@
-import { rest } from "msw"
+import { DefaultBodyType, rest } from "msw"
 
 import { DirectoryData, PageData } from "types/directory"
+
+const apiDataBuilder = (endpoint: string) => (
+  mockData: DefaultBodyType,
+  delay?: number | "infinite"
+): ReturnType<typeof rest["get"]> => {
+  return rest.get(endpoint, (req, res, ctx) => {
+    return res(delay ? ctx.delay(delay) : ctx.delay(0), ctx.json(mockData))
+  })
+}
 
 export const buildPagesData = (
   mockPagesData: PageData[],
   delay?: number | "infinite"
 ): ReturnType<typeof rest["get"]> => {
-  return rest.get(`*/sites/:siteName/pages`, (req, res, ctx) => {
-    return res(delay ? ctx.delay(delay) : ctx.delay(0), ctx.json(mockPagesData))
-  })
+  return apiDataBuilder("*/sites/:siteName/pages")(mockPagesData, delay)
 }
 
 export const buildDirData = (
   mockDirData: DirectoryData[],
   delay?: number | "infinite"
 ): ReturnType<typeof rest["get"]> => {
+  return apiDataBuilder("*/sites/:siteName/collections")(mockDirData, delay)
   return rest.get(`*/sites/:siteName/collections`, (req, res, ctx) => {
     return res(delay ? ctx.delay(delay) : ctx.delay(0), ctx.json(mockDirData))
   })
