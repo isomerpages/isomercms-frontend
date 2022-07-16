e695d3c411bafd66eba922b1f7db5c028a29100a
seaerchin <jiachin@open.gov.sg>
2022-07-16 22:21:57 +0800

    chore(mocks): add new constants and utils for resource category

--- src/mocks/constants.ts
+++ src/mocks/constants.ts
@@ -1,4 +1,4 @@
-import { DirectoryData, PageData } from "types/directory"
+import { DirectoryData, PageData, ResourcePageData } from "types/directory"
 
 export const MOCK_PAGES_DATA: PageData[] = [
   {
@@ -17,6 +17,34 @@ export const MOCK_PAGES_DATA: PageData[] = [
   {
     name: "extra page",
     type: "file",
+  },
+]
+export const MOCK_RESOURCE_CATEGORY_PAGES_DATA: ResourcePageData[] = [
+  {
+    name: "some page",
+    title: "i am a title",
+    date: "2022-08-04",
+    resourceType: "post",
+  },
+  {
+    name: "another page",
+    title: "this a random blog post by a random blogger",
+    date: "2012-02-04",
+    resourceType: "post",
+  },
+  {
+    name:
+      "some page with an extremely long name that cannot fit inside the textbox",
+    title:
+      "some page with an extremely long name that cannot fit inside the textbox. in fact, this is so long it should be an essay but whatever",
+    date: "1966-12-03",
+    resourceType: "file",
+  },
+  {
+    name: "extra page",
+    title: "this is an extra page",
+    date: "2000-01-02",
+    resourceType: "file",
   },
 ]
 
@@ -55,3 +83,5 @@ export const MOCK_FOLDER_NAME = "mock-folder"
 export const MOCK_SUBFOLDER_NAME = "mock-subfolder"
 
 export const MOCK_RESOURCE_ROOM_NAME = "mock-resource-room"
+
+export const MOCK_RESOURCE_CATEGORY_NAME = "mock-resource-category"
