11a1778f60c881a9d5c86e5e990a92a327e39734
seaerchin <jiachin@open.gov.sg>
2022-07-18 17:26:29 +0800

    fix(pagesettingsmodal): use chakra modal and form components

--- src/components/PageSettingsModal/PageSettingsModal.jsx
+++ src/components/PageSettingsModal/PageSettingsModal.jsx
@@ -1,5 +1,23 @@
-import { CloseButton, HStack } from "@chakra-ui/react"
+import {
+  Input,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  Box,
+  Text,
+  Center,
+  Spinner,
+  FormControl,
+} from "@chakra-ui/react"
 import { yupResolver } from "@hookform/resolvers/yup"
+import {
+  FormErrorMessage,
+  FormLabel,
+  ModalCloseButton,
+} from "@opengovsg/design-system-react"
 import axios from "axios"
 import { Breadcrumb } from "components/folders/Breadcrumb"
 import {
@@ -8,8 +26,6 @@ import {
   FormTitle,
   FormDescription,
 } from "components/Form"
-import FormField from "components/FormField"
-import FormFieldHorizontal from "components/FormFieldHorizontal"
 import FormFieldMedia from "components/FormFieldMedia"
 import { LoadingButton } from "components/LoadingButton"
 import ResourceFormFields from "components/ResourceFormFields"
@@ -92,141 +108,137 @@ export const PageSettingsModal = ({
   }
 
   return (
-    <>
-      <div className={elementStyles.overlay}>
-        <div className={elementStyles["modal-settings"]}>
-          <div className={elementStyles.modalHeader}>
-            <h1>{!fileName ? "Create new page" : "Page settings"}</h1>
-            <CloseButton id="settings-CLOSE" onClick={onClose} />
-          </div>
-          <div className={elementStyles.modalContent}>
-            {fileName && !pageData ? (
-              <center>
-                <div className="spinner-border text-primary" role="status" />
-              </center>
-            ) : (
-              <>
-                <div>
-                  {!fileName ? "You may edit page details anytime. " : ""}
-                  To edit page content, simply click on the page title. <br />
-                  <Breadcrumb params={params} title={watch("title")} />
-                  {/* Title */}
-                  <FormContext isRequired hasError={!!errors.title?.message}>
-                    <FormTitle>Page title</FormTitle>
-                    <FormField
-                      placeholder="Page title"
-                      id="title"
-                      // eslint-disable-next-line react/jsx-props-no-spreading
-                      {...register("title", { required: true })}
-                    />
-                    <FormError>{errors.title?.message}</FormError>
-                  </FormContext>
-                  <br />
-                  {/* Permalink */}
-                  {watch("layout") !== "file" && (
-                    <>
-                      <FormContext
-                        hasError={!!errors.permalink?.message}
-                        isRequired
-                        isDisabled={watch("layout") === "file"}
-                      >
-                        <FormTitle>Page URL</FormTitle>
-                        <FormDescription>{siteUrl}</FormDescription>
-                        <FormFieldHorizontal
-                          // eslint-disable-next-line react/jsx-props-no-spreading
-                          {...register("permalink", { required: true })}
-                          id="permalink"
-                          placeholder="Page URL"
-                        />
-                        <FormError>{errors.permalink?.message}</FormError>
-                      </FormContext>
-                      <br />
-                    </>
-                  )}
-                  {resourceRoomName && (
-                    <>
-                      <p className={elementStyles.formLabel}>
-                        Resources details
-                      </p>
-                      <ResourceFormFields
-                        register={register}
-                        errors={errors}
-                        watch={watch}
-                        setValue={setValue}
-                        trigger={trigger}
-                      />
-                    </>
-                  )}
-                  <br />
-                  <p className={elementStyles.formLabel}>Page details</p>
-                  <FormContext hasError={!!errors.description?.message}>
-                    <FormTitle>Meta Description (Optional)</FormTitle>
-                    <FormDescription>
-                      Description snippet shown in search results.{" "}
-                      <a
-                        href="https://go.gov.sg/isomer-meta"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Learn more
-                      </a>
-                    </FormDescription>
-                    <FormField
-                      placeholder="Meta Description (Optional)"
-                      id="description"
-                      // eslint-disable-next-line react/jsx-props-no-spreading
-                      {...register("description")}
-                    />
-                    <FormError>{errors.description?.message}</FormError>
-                  </FormContext>
-                  <br />
-                  <FormContext
-                    hasError={!!errors.image?.message}
-                    onFieldChange={(e) => setValue("image", e.target.value)}
+    <Modal isOpen onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalCloseButton id="settings-CLOSE" />
+        <ModalHeader>
+          <Text as="h1">{!fileName ? "Create new page" : "Page settings"}</Text>
+        </ModalHeader>
+        <ModalBody>
+          {fileName && !pageData ? (
+            <Center>
+              <Spinner />
+            </Center>
+          ) : (
+            <Box>
+              {!fileName ? "You may edit page details anytime. " : ""}
+              To edit page content, simply click on the page title. <br />
+              <Breadcrumb params={params} title={watch("title")} />
+              {/* Title */}
+              <FormControl isRequired isInvalid={!!errors.title?.message}>
+                <FormLabel>Page title</FormLabel>
+                <Input
+                  placeholder="Page title"
+                  id="title"
+                  // eslint-disable-next-line react/jsx-props-no-spreading
+                  {...register("title", { required: true })}
+                />
+                <FormErrorMessage>{errors.title?.message}</FormErrorMessage>
+              </FormControl>
+              <br />
+              {/* Permalink */}
+              {watch("layout") !== "file" && (
+                <>
+                  <FormControl
+                    isInvalid={!!errors.permalink?.message}
+                    isRequired
+                    isDisabled={watch("layout") === "file"}
                   >
-                    <FormTitle>Meta Image URL (Optional)</FormTitle>
-                    <FormDescription>
-                      Image shown when link is shared on social media.{" "}
-                      <a
-                        href="https://go.gov.sg/isomer-meta"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Learn more
-                      </a>
-                    </FormDescription>
-                    <FormFieldMedia
-                      register={register}
-                      placeholder="Meta Image URL (Optional)"
-                      id="image"
-                      inlineButtonText="Select Image"
+                    <Box mb="0.75rem">
+                      <FormLabel mb={0}>Page URL</FormLabel>
+                      <FormLabel.Description color="text.description">
+                        {siteUrl}
+                      </FormLabel.Description>
+                    </Box>
+                    <Input
+                      // eslint-disable-next-line react/jsx-props-no-spreading
+                      {...register("permalink", { required: true })}
+                      id="permalink"
+                      placeholder="Page URL"
                     />
-                    <FormError>{errors.image?.message}</FormError>
-                  </FormContext>
-                </div>
-                <HStack
-                  w="100%"
-                  justify="flex-end"
-                  paddingInlineEnd={1}
-                  pt="20px"
-                >
-                  <LoadingButton
-                    onClick={handleSubmit(onSubmit)}
-                    isDisabled={
-                      !fileName
-                        ? !_.isEmpty(errors)
-                        : !_.isEmpty(errors) || !pageData?.sha
-                    }
+                    <FormErrorMessage>
+                      {errors.permalink?.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                  <br />
+                </>
+              )}
+              {resourceRoomName && (
+                <>
+                  <p className={elementStyles.formLabel}>Resources details</p>
+                  <ResourceFormFields
+                    register={register}
+                    errors={errors}
+                    watch={watch}
+                    setValue={setValue}
+                    trigger={trigger}
+                  />
+                </>
+              )}
+              <br />
+              <Text textStyle="h4">Page details</Text>
+              <FormControl isInvalid={!!errors.description?.message}>
+                <Box mb="0.75rem">
+                  <FormLabel mb={0} textColor="text.label">
+                    Meta Description
+                  </FormLabel>
+                  <FormLabel.Description useMarkdown color="text.description">
+                    Description snippet shown in search results. [Learn
+                    more](https://go.gov.sg/isomer-meta)
+                  </FormLabel.Description>
+                </Box>
+                <Input
+                  placeholder="Meta Description (Optional)"
+                  id="description"
+                  // eslint-disable-next-line react/jsx-props-no-spreading
+                  {...register("description")}
+                />
+                <FormErrorMessage>
+                  {errors.description?.message}
+                </FormErrorMessage>
+              </FormControl>
+              <br />
+              <FormContext
+                hasError={!!errors.image?.message}
+                onFieldChange={(e) => setValue("image", e.target.value)}
+              >
+                <FormTitle>Meta Image URL (Optional)</FormTitle>
+                <FormDescription>
+                  Image shown when link is shared on social media.{" "}
+                  <a
+                    href="https://go.gov.sg/isomer-meta"
+                    target="_blank"
+                    rel="noreferrer"
                   >
-                    Save
-                  </LoadingButton>
-                </HStack>
-              </>
-            )}
-          </div>
-        </div>
-      </div>
-    </>
+                    Learn more
+                  </a>
+                </FormDescription>
+                <FormFieldMedia
+                  register={register}
+                  placeholder="Meta Image URL (Optional)"
+                  id="image"
+                  inlineButtonText="Select Image"
+                />
+                <FormError>{errors.image?.message}</FormError>
+              </FormContext>
+            </Box>
+          )}
+        </ModalBody>
+        <ModalFooter>
+          <LoadingButton
+            onClick={handleSubmit(onSubmit)}
+            isDisabled={
+              !fileName
+                ? !_.isEmpty(errors)
+                : !_.isEmpty(errors) || !pageData?.sha
+            }
+          >
+            Save
+          </LoadingButton>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   )
 }
 
