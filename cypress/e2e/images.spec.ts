9fcfdd9fa8d2d34814cb670f05df78213ed0c8fc
seaerchin <jiachin@open.gov.sg>
2022-08-05 03:57:48 +0800

    refactor(cypress specs): use new commands and constants

--- cypress/e2e/images.spec.ts
+++ cypress/e2e/images.spec.ts
@@ -1,15 +1,8 @@
 import "cypress-file-upload"
 
-import {
-  E2E_EXTENDED_TIMEOUT,
-  E2E_DEFAULT_WAIT_TIME,
-} from "../fixtures/constants"
+import { TEST_REPO_NAME, Interceptors } from "../fixtures/constants"
 
 describe("Images", () => {
-  const COOKIE_NAME = Cypress.env("COOKIE_NAME")
-  const COOKIE_VALUE = Cypress.env("COOKIE_VALUE")
-  const TEST_REPO_NAME = Cypress.env("TEST_REPO_NAME")
-
   const IMAGE_TITLE = "singapore.jpg"
   const OTHER_IMAGE_TITLE = "america.jpg"
   const TEST_IMAGE_PATH = "images/singapore.jpg"
@@ -18,8 +11,8 @@ describe("Images", () => {
 
   describe("Create image, delete image, edit image settings, and move images in image albums", () => {
     before(() => {
-      cy.setCookie(COOKIE_NAME, COOKIE_VALUE)
-      window.localStorage.setItem("userId", "test")
+      cy.setSessionDefaults()
+      cy.setupDefaultInterceptors()
 
       cy.visit(`/sites/${TEST_REPO_NAME}/media/images/mediaDirectory/images`)
       cy.contains("Create new album").click()
@@ -31,59 +24,46 @@ describe("Images", () => {
       cy.get("button")
         .contains(/^Skip$/)
         .click()
+        .wait(Interceptors.POST)
 
       // Assert
-      cy.wait(E2E_DEFAULT_WAIT_TIME)
-      cy.contains(ALBUM_TITLE, { timeout: E2E_EXTENDED_TIMEOUT }).should(
-        "exist"
-      )
+      cy.contains(ALBUM_TITLE).should("exist")
     })
 
     beforeEach(() => {
-      cy.setCookie(COOKIE_NAME, COOKIE_VALUE)
-      window.localStorage.setItem("userId", "test")
+      cy.setupDefaultInterceptors()
+      cy.setSessionDefaults()
 
       cy.visit(
         `/sites/${TEST_REPO_NAME}/media/images/mediaDirectory/images%2F${ALBUM_TITLE}`
-      )
+      ).wait(Interceptors.GET)
     })
 
     it("Should be able to add image to image album", () => {
-      cy.uploadMedia(IMAGE_TITLE, TEST_IMAGE_PATH)
+      cy.uploadMedia(IMAGE_TITLE, TEST_IMAGE_PATH).wait(Interceptors.POST)
+
       // ASSERTS
-      cy.wait(E2E_DEFAULT_WAIT_TIME)
-      cy.contains("Media file successfully uploaded", {
-        timeout: E2E_EXTENDED_TIMEOUT,
-      }).should("exist")
-      cy.contains(IMAGE_TITLE, { timeout: E2E_EXTENDED_TIMEOUT }).should(
-        "exist"
-      ) // image should be contained in Images
+      cy.contains("Media file successfully uploaded").should("exist")
+      cy.contains(IMAGE_TITLE).should("exist") // image should be contained in Images
     })
 
     it("Should be able to edit an image in image album", () => {
-      cy.contains(IMAGE_TITLE, { timeout: E2E_EXTENDED_TIMEOUT }).should(
-        "exist"
-      )
-      cy.renameMedia(IMAGE_TITLE, OTHER_IMAGE_TITLE)
+      cy.contains(IMAGE_TITLE).should("exist")
+      cy.renameMedia(IMAGE_TITLE, OTHER_IMAGE_TITLE).wait(Interceptors.POST)
+
       // ASSERTS
-      cy.wait(E2E_DEFAULT_WAIT_TIME)
-      cy.contains(OTHER_IMAGE_TITLE, { timeout: E2E_EXTENDED_TIMEOUT }).should(
-        "exist"
-      ) // Image should be contained in Images
+      cy.contains(OTHER_IMAGE_TITLE).should("exist") // Image should be contained in Images
     })
 
     it("Should be able to delete image from image album", () => {
-      cy.contains("button", OTHER_IMAGE_TITLE, {
-        timeout: E2E_EXTENDED_TIMEOUT,
-      })
+      cy.contains("button", OTHER_IMAGE_TITLE)
         .as("imagePreview")
         .should("exist")
       cy.clickContextMenuItem("@imagePreview", "Delete")
-      cy.contains("button", "delete").click()
+      cy.contains("button", "delete").click().wait(Interceptors.DELETE)
+
       // ASSERTS
-      cy.contains(OTHER_IMAGE_TITLE, { timeout: E2E_EXTENDED_TIMEOUT }).should(
-        "not.exist"
-      ) // Image file name should not exist in Images
+      cy.contains(OTHER_IMAGE_TITLE).should("not.exist") // Image file name should not exist in Images
     })
   })
 })
