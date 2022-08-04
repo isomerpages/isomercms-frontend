fd7162d4f88062575aef1eabde3a757190376d8b
seaerchin <jiachin@open.gov.sg>
2022-08-04 13:59:25 +0800

    test(cypress.config): update cypress config to hold only 10 tests in mem

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,12 @@ export default defineConfig({
   viewportWidth: 1366,
   projectId: "nxbty1",
 
+  // NOTE: Cypress keeps 50 tests in memory by default;
+  // Unfortunately, this leads to OOM issues occasionally.
+  // This has been reduced to 10 to help with the OOM issues.
+  // Refer here: https://docs.cypress.io/guides/references/configuration#Global for details.
+  numTestsKeptInMemory: 10,
+
   e2e: {
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
   },
