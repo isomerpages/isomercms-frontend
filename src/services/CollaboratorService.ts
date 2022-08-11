6ba0c4c0bdda3cb9905a5dde7d5c6738e8a59bc9
Preston Lim <prestonlimlianjie@gmail.com>
2022-08-11 13:43:09 +0800

    feat(api-service): add collaborator API service methods

---
+++ src/services/CollaboratorService.ts
@@ -0,0 +1,34 @@
+import { apiService } from "./ApiService"
+
+const getCollaboratorEndpoint = (siteName: string): string => {
+  return `/sites/${siteName}/collaborators`
+}
+
+export const getRole = async (siteName: string): Promise<any> => {
+  const endpoint = `${getCollaboratorEndpoint(siteName)}/role`
+  return apiService.get(endpoint).then((res) => res.data)
+}
+
+export const listCollaborators = async (siteName: string): Promise<any> => {
+  const endpoint = getCollaboratorEndpoint(siteName)
+  return apiService.get(endpoint).then((res) => res.data)
+}
+
+export const addCollaborator = async (
+  siteName: string,
+  email: string,
+  isAcknowledged: boolean
+): Promise<void> => {
+  const endpoint = getCollaboratorEndpoint(siteName)
+  return apiService
+    .post(endpoint, { email, acknowledge: isAcknowledged })
+    .then((res) => res.data)
+}
+
+export const deleteCollaborator = async (
+  siteName: string,
+  collaboratorId: string
+): Promise<void> => {
+  const endpoint = `${getCollaboratorEndpoint(siteName)}/${collaboratorId}`
+  return apiService.delete(endpoint).then((res) => res.data)
+}
