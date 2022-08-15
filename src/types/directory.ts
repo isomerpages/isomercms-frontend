309b7b164be461466d06fe6409e8a3ed5401d387
seaerchin <jiachin@open.gov.sg>
2022-08-15 11:59:02 +0800

    chore(directory): add util methods

--- src/types/directory.ts
+++ src/types/directory.ts
@@ -23,6 +23,10 @@ export interface DirectoryData {
   children: string[]
 }
 
+export const isDirectoryData = (data: unknown): data is DirectoryData => {
+  return (data as DirectoryData).type === "dir"
+}
+
 export interface PageData {
   name: string
   title?: string
@@ -36,6 +40,14 @@ export interface MediaData {
   name: string
   sha: string
   type: "file"
+}
+
+// NOTE: This assumes that mediaPath is proof that this is a media file.
+// The corresponding backend code (mediaFiles.js) does not appear to guarantee the existence of
+// this property on media files (or indeed, ANY property) and this is a best effort attempt,
+// which might fail in the future if new media files are discovered to be returned w/o mediaPath.
+export const isMediaData = (data: unknown): data is MediaData => {
+  return (data as MediaData).type === "file" && !!(data as MediaData).mediaPath
 }
 
 export type ResourcePageData = Required<Omit<PageData, "type">> & {
