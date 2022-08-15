03277a0214ba154b124c6bd8a2f81f97da073410
Alexander Lee <alexander@open.gov.sg>
2022-08-15 16:45:08 +0800

    Chore: cleanup api file

--- src/api.js
+++ src/api.js
@@ -586,17 +586,6 @@ const verifyMobileNumberOtp = async (mobile, otp) => {
   return res.data
 }
 
-const sendLoginOtp = async (email) => {
-  const endpoint = `${BACKEND_URL_V2}/auth/login`
-  return axios.post(endpoint, { email })
-}
-
-const verifyLoginOtp = async (email, otp) => {
-  const endpoint = `${BACKEND_URL_V2}/auth/verify`
-  const res = await axios.post(endpoint, { email, otp })
-  return res.data
-}
-
 export {
   getDirectoryFile,
   setDirectoryFile,
@@ -636,6 +625,4 @@ export {
   verifyEmailOtp,
   getMobileNumberOtp,
   verifyMobileNumberOtp,
-  sendLoginOtp,
-  verifyLoginOtp,
 }
