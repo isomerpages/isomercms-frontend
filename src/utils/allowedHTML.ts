65e70d38306a7a04eddde43459a830e6a1b0120c
Kishore <42832651+kishore03109@users.noreply.github.com>
2022-08-17 12:04:13 +0800

    Fix/hitting character limit when adding <iframe/> (#1011)
    * feat(Description): allow iframe input without affecting char length
    
    * fix(infobar description): add html tags inforbar description
    
    * feat(infobar desc): only allowing iframe tag for infobar desc
    
    * ref(regex for Iframe): refactored method for clarity
    
    Co-authored-by: seaerchin <44049504+seaerchin@users.noreply.github.com>
    
    * fix(allowedhtmltags): fix eslint problems
    
    Co-authored-by: seaerchin <44049504+seaerchin@users.noreply.github.com>

---
+++ src/utils/allowedHTML.ts
@@ -0,0 +1,13 @@
+/**
+ * HTML tag that do not count towards the total
+ * character limit.
+ */
+const IFRAME_TAG_REGEX = new RegExp("(<iframe.*/iframe>)", "gm")
+
+/**
+ * @param text raw text input by user
+ * @returns the character length after removing specifc HTML tags
+ */
+export const getLengthWithoutTags = (text: string): number => {
+  return text.replace(IFRAME_TAG_REGEX, "").length
+}
