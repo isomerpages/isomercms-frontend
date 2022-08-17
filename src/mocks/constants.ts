93ceae53de1ebc52632304aec61ee6d4c246f05b
seaerchin <44049504+seaerchin@users.noreply.github.com>
2022-08-17 14:22:58 +0800

    ref(storybook): add settings stories (#984)
    * chore(mocks): add utils/constants for settings related mocks
    
    * fix(settings): fix skeleton height
    
    * feat(settings.stories): add settings stories
    
    * chore(settings/footer): updated ui of settings/footer to match figma

--- src/mocks/constants.ts
+++ src/mocks/constants.ts
@@ -1,4 +1,5 @@
 import { DirectoryData, PageData, ResourcePageData } from "types/directory"
+import { BackendSiteSettings } from "types/settings"
 
 export const MOCK_PAGES_DATA: PageData[] = [
   {
@@ -76,6 +77,55 @@ export const MOCK_USER = {
   userId: "mockUser",
   email: "mockUser@open.gov.sg",
   contactNumber: "98765432",
+}
+
+export const MOCK_BE_SETTINGS: BackendSiteSettings = {
+  title: "isomer",
+  description: "isomer is a website builder",
+  // NOTE: No shareicon/logo/favicon as no uploaded img within the story
+  facebook_pixel: "many pixel",
+  google_analytics: "some analytics",
+  linkedin_insights: "some insightful comment",
+  is_government: true,
+  colors: {
+    "primary-color": "#261EFF",
+    "secondary-color": "#26342F",
+    "media-colors": [
+      {
+        title: "some colour",
+        color: "#000000",
+      },
+      {
+        title: "some colour",
+        color: "#000000",
+      },
+      {
+        title: "some colour",
+        color: "#000000",
+      },
+      {
+        title: "some colour",
+        color: "#000000",
+      },
+      {
+        title: "some colour",
+        color: "#000000",
+      },
+    ],
+  },
+  contact_us: "/isomie/indomie",
+  feedback: "/feed/me/back",
+  faq: "/no/questions/allowed",
+  show_reach: "true",
+  social_media: {
+    facebook: "https://www.facebook.com/iloveisomer",
+    linkedin: "https://www.linkedin.com/company/isomerites",
+    twitter: "https://www.twitter.com/isotwit",
+    youtube: "https://www.youtube.com/isotubes",
+    instagram: "https://www.instagram.com/isogram/",
+    telegram: "https://t.me/isogram",
+    tiktok: "https://www.tiktok.com/isotok",
+  },
 }
 
 export const MOCK_FOLDER_NAME = "mock-folder"
