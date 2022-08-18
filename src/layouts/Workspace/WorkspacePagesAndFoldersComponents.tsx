da44d05a326bcaea32432decb5e938eed23e8eb9
Kishore <e0323826@u.nus.edu>
2022-08-18 11:12:42 +0800

    style(workspace): remove redundant fragments

--- src/layouts/Workspace/WorkspacePagesAndFoldersComponents.tsx
+++ src/layouts/Workspace/WorkspacePagesAndFoldersComponents.tsx
@@ -81,31 +81,27 @@ export const UngroupedPages = (props: {
   const { pagesData, url } = props
   const { setRedirectToPage } = useRedirectHook()
   return (
-    <>
-      <Section>
-        <Box w="100%">
-          <SectionHeader label="Ungrouped Pages">
-            <CreateButton
-              onClick={() => setRedirectToPage(`${url}/createPage`)}
-            >
-              Create page
-            </CreateButton>
-          </SectionHeader>
-          <SectionCaption label="NOTE: " icon={BiInfoCircle}>
-            Pages here do not belong to any folders.
-          </SectionCaption>
-        </Box>
-        <Skeleton isLoaded={!!pagesData} w="full">
-          <SimpleGrid columns={3} spacing="1.5rem">
-            {pagesData &&
-              pagesData.length > 0 &&
-              pagesData
-                .filter((page) => page.name !== "contact-us.md")
-                .map(({ name }) => <PageCard title={name} />)}
-          </SimpleGrid>
-        </Skeleton>
-      </Section>
-    </>
+    <Section>
+      <Box w="100%">
+        <SectionHeader label="Ungrouped Pages">
+          <CreateButton onClick={() => setRedirectToPage(`${url}/createPage`)}>
+            Create page
+          </CreateButton>
+        </SectionHeader>
+        <SectionCaption label="NOTE: " icon={BiInfoCircle}>
+          Pages here do not belong to any folders.
+        </SectionCaption>
+      </Box>
+      <Skeleton isLoaded={!!pagesData} w="full">
+        <SimpleGrid columns={3} spacing="1.5rem">
+          {pagesData &&
+            pagesData.length > 0 &&
+            pagesData
+              .filter((page) => page.name !== "contact-us.md")
+              .map(({ name }) => <PageCard title={name} />)}
+        </SimpleGrid>
+      </Skeleton>
+    </Section>
   )
 }
 
@@ -113,121 +109,115 @@ export const EmptyFolder = (props: { url: string }): JSX.Element => {
   const { url } = props
   const { setRedirectToPage } = useRedirectHook()
   return (
-    <>
-      <Box as="form" w="full">
-        {/* Resource Room does not exist */}
-        <VStack spacing={5}>
-          <EmptyBoxImage />
-          <Center>
-            <VStack spacing={0}>
-              <Text textStyle="subhead-1">{" There's nothing here yet. "}</Text>
-              <Text textStyle="body-2">Create a new item to get started.</Text>
-            </VStack>
-          </Center>
+    <Box as="form" w="full">
+      {/* Resource Room does not exist */}
+      <VStack spacing={5}>
+        <EmptyBoxImage />
+        <Center>
+          <VStack spacing={0}>
+            <Text textStyle="subhead-1">{" There's nothing here yet. "}</Text>
+            <Text textStyle="body-2">Create a new item to get started.</Text>
+          </VStack>
+        </Center>
 
-          <CreateButton
-            onClick={() => setRedirectToPage(`${url}/createDirectory`)}
-          >
-            Create folder
-          </CreateButton>
-        </VStack>
-      </Box>
-    </>
+        <CreateButton
+          onClick={() => setRedirectToPage(`${url}/createDirectory`)}
+        >
+          Create folder
+        </CreateButton>
+      </VStack>
+    </Box>
   )
 }
 
 export const EmptyPage = (props: { url: string }): JSX.Element => {
   const { url } = props
   const { setRedirectToPage } = useRedirectHook()
+
   return (
-    <>
-      <Box as="form" w="full">
-        {/* Resource Room does not exist */}
-        <VStack spacing={5}>
-          <EmptyBoxImage />
-          <Center>
-            <VStack spacing={0}>
-              <Text textStyle="subhead-1">There&apos;s nothing here yet.</Text>
-              <Text textStyle="body-2">Create a new item to get started.</Text>
-            </VStack>
-          </Center>
-          <CreateButton onClick={() => setRedirectToPage(`${url}/createPage`)}>
-            Create page
-          </CreateButton>
-        </VStack>
-      </Box>
-    </>
+    <Box as="form" w="full">
+      <VStack spacing={5}>
+        <EmptyBoxImage />
+        <Center>
+          <VStack spacing={0}>
+            <Text textStyle="subhead-1">There&apos;s nothing here yet.</Text>
+            <Text textStyle="body-2">Create a new item to get started.</Text>
+          </VStack>
+        </Center>
+        <CreateButton onClick={() => setRedirectToPage(`${url}/createPage`)}>
+          Create page
+        </CreateButton>
+      </VStack>
+    </Box>
   )
 }
 
 export const EmptyPageAndFolder = (props: { url: string }): JSX.Element => {
   const { url } = props
   return (
-    <>
-      <Box as="form" w="full">
-        {/* Resource Room does not exist */}
-        <VStack spacing={5}>
-          <EmptyBoxImage />
-          <Center>
-            <VStack spacing={0}>
-              <Text textStyle="subhead-1">There&apos;s nothing here yet.</Text>
-              <Text textStyle="body-2">Create a new item to get started.</Text>
-            </VStack>
-          </Center>
-          <ContextMenu placement="bottom-end" offset={[0, 0]}>
-            {({ isOpen }) => (
-              <ButtonGroup isAttached variant="outline">
-                {/* NOTE: This is to avoid the 2 joined buttons both having 1px padding,
+    <Box as="form" w="full">
+      {/* Resource Room does not exist */}
+      <VStack spacing={5}>
+        <EmptyBoxImage />
+        <Center>
+          <VStack spacing={0}>
+            <Text textStyle="subhead-1">There&apos;s nothing here yet.</Text>
+            <Text textStyle="body-2">Create a new item to get started.</Text>
+          </VStack>
+        </Center>
+        <ContextMenu placement="bottom-end" offset={[0, 0]}>
+          {({ isOpen }) => (
+            <ButtonGroup isAttached variant="outline">
+              {/* NOTE: This is to avoid the 2 joined buttons both having 1px padding,
             which results in a larger border at the attached area. */}
-                <Button
-                  borderRight="0px"
-                  borderRightRadius={0}
+              <Button
+                borderRight="0px"
+                borderRightRadius={0}
+                as={Link}
+                to={`${url}/createPage`}
+                variant="outline"
+              >
+                Create page
+              </Button>
+              <ContextMenu.Button
+                position="inherit"
+                as={IconButton}
+                borderLeftRadius={0}
+                aria-label="Select options"
+                variant="outline"
+                _active={{ bg: "white" }}
+                icon={
+                  <Icon
+                    as={isOpen ? BiChevronUp : BiChevronDown}
+                    fontSize="1rem"
+                    fill="blue"
+                  />
+                }
+              />
+              <ContextMenu.List minWidth={150}>
+                <ContextMenu.Item
                   as={Link}
                   to={`${url}/createPage`}
-                  variant="outline"
+                  icon={<BiFileBlank fontSize="1.25rem" fill="icon.alt" />}
                 >
-                  Create page
-                </Button>
-                <ContextMenu.Button
-                  position="inherit"
-                  as={IconButton}
-                  borderLeftRadius={0}
-                  aria-label="Select options"
-                  variant="outline"
-                  _active={{ bg: "white" }}
-                  icon={
-                    <Icon
-                      as={isOpen ? BiChevronUp : BiChevronDown}
-                      fontSize="1rem"
-                      fill="blue"
-                    />
-                  }
-                />
-                <ContextMenu.List minWidth={150}>
-                  <ContextMenu.Item
-                    as={Link}
-                    to={`${url}/createPage`}
-                    icon={<BiFileBlank fontSize="1.25rem" fill="icon.alt" />}
-                  >
-                    <Text textStyle="body-1" fill="text.body">
-                      Create page
-                    </Text>
-                  </ContextMenu.Item>
-                  <ContextMenu.Item
-                    as={Link}
-                    to={`${url}/createDirectory`}
-                    icon={<BiFolder fontSize="1.25rem" fill="icon.alt" />}
-                  >
-                    <Text textStyle="body-1" fill="text.body">
-                      Create folder
-                    </Text>
-                  </ContextMenu.Item>
-                </ContextMenu.List>
-              </ButtonGroup>
-            )}
-          </ContextMenu>
-        </VStack>
-      </Box>
-    </>
+                  <Text textStyle="body-1" fill="text.body">
+                    Create page
+                  </Text>
+                </ContextMenu.Item>
+                <ContextMenu.Item
+                  as={Link}
+                  to={`${url}/createDirectory`}
+                  icon={<BiFolder fontSize="1.25rem" fill="icon.alt" />}
+                >
+                  <Text textStyle="body-1" fill="text.body">
+                    Create folder
+                  </Text>
+                </ContextMenu.Item>
+              </ContextMenu.List>
+            </ButtonGroup>
+          )}
+        </ContextMenu>
+      </VStack>
+    </Box>
   )
 }
