aa9619a7cfe7ef9295f26964c6f0dd989a4e145f
Kishore <e0323826@u.nus.edu>
2022-08-18 10:23:07 +0800

    refactor(directoryhooks): rename hook for clarity

--- src/layouts/screens/ReorderingScreen.jsx
+++ src/layouts/screens/ReorderingScreen.jsx
@@ -1,11 +1,14 @@
 import ReorderingModal from "components/folders/ReorderingModal"
 import PropTypes from "prop-types"
 
-import { useGetFolders, useReorderDirectoryHook } from "hooks/directoryHooks"
+import {
+  useGetFoldersAndPages,
+  useReorderDirectoryHook,
+} from "hooks/directoryHooks"
 
 export const ReorderingScreen = ({ match, onClose }) => {
   const { params, decodedParams } = match
-  const { data: dirData } = useGetFolders(params, { initialData: [] })
+  const { data: dirData } = useGetFoldersAndPages(params, { initialData: [] })
   const { mutateAsync: reorderHandler } = useReorderDirectoryHook(params, {
     onSettled: onClose,
   })
