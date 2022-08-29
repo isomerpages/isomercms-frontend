bb362c00023c866d4785f19762fb376f86b7fdc3
seaerchin <jiachin@open.gov.sg>
2022-08-29 10:24:39 +0800

    fix(folderitem): remove extra code and chakra.button
    this component is solely used in reordering modal, where it's already wrapped by an draggable. the
    button interferes with the drag drop, and hence has been removed

--- src/components/folders/FolderItem.jsx
+++ src/components/folders/FolderItem.jsx
@@ -1,40 +1,12 @@
-import {
-  chakra,
-  LinkOverlay,
-  LinkBox,
-  Divider,
-  HStack,
-  Icon,
-  Text,
-  Box,
-} from "@chakra-ui/react"
-import { ContextMenu } from "components/ContextMenu"
-import { useMemo } from "react"
-import {
-  BiChevronRight,
-  BiEditAlt,
-  BiFolder,
-  BiTrash,
-  BiWrench,
-} from "react-icons/bi"
-import { Link as RouterLink, useRouteMatch } from "react-router-dom"
+import { Box } from "@chakra-ui/react"
 
 import elementStyles from "styles/isomer-cms/Elements.module.scss"
 import contentStyles from "styles/isomer-cms/pages/Content.module.scss"
 
 import { pageFileNameToTitle } from "utils"
 
-const FolderItem = ({ item, isDisabled }) => {
-  const { url } = useRouteMatch()
-
-  const { name, type, children } = item
-  const encodedName = encodeURIComponent(name)
-
-  const generatedLink = useMemo(() => {
-    return type === "dir"
-      ? `${url}/subfolders/${encodedName}`
-      : `${url}/editPage/${encodedName}`
-  }, [encodedName, type, url])
+const FolderItem = ({ item }) => {
+  const { name } = item
 
   return (
     <Box
@@ -42,84 +14,14 @@ const FolderItem = ({ item, isDisabled }) => {
       className={`${contentStyles.component} ${contentStyles.card}`}
       position="relative"
     >
-      <LinkBox w="100%">
-        <LinkOverlay as={RouterLink} to={!isDisabled && generatedLink}>
-          <chakra.button
-            className={`${elementStyles.card} ${contentStyles.card} ${elementStyles.folderItem}`}
-          >
-            <div className={contentStyles.contentContainerFolderRow}>
-              {type === "file" ? (
-                <i
-                  className={`bx bxs-file-blank ${elementStyles.folderItemIcon}`}
-                />
-              ) : (
-                <i
-                  className={`bx bxs-folder ${elementStyles.folderItemIcon}`}
-                />
-              )}
-              <span className={`${elementStyles.folderItemText} mr-auto`}>
-                {pageFileNameToTitle(name)}
-              </span>
-              {children ? (
-                <span className={`${elementStyles.folderItemText} mr-5`}>
-                  {children.length} item{children.length === 1 ? "" : "s"}
-                </span>
-              ) : null}
-            </div>
-          </chakra.button>
-        </LinkOverlay>
-      </LinkBox>
-      {!isDisabled && (
-        <ContextMenu>
-          <ContextMenu.Button right="1.875rem" bottom="1.875rem" />
-          <ContextMenu.List>
-            <ContextMenu.Item
-              icon={<BiEditAlt />}
-              as={RouterLink}
-              to={generatedLink}
-            >
-              <Text>Edit</Text>
-            </ContextMenu.Item>
-            <ContextMenu.Item
-              icon={<BiWrench />}
-              as={RouterLink}
-              to={
-                type === "dir"
-                  ? `${url}/editDirectorySettings/${encodedName}`
-                  : `${url}/editPageSettings/${encodedName}`
-              }
-            >
-              Settings
-            </ContextMenu.Item>
-            {type !== "dir" && (
-              <ContextMenu.Item
-                icon={<BiFolder />}
-                as={RouterLink}
-                to={`${url}/movePage/${encodedName}`}
-              >
-                <HStack spacing="4rem" alignItems="center">
-                  <Text>Move to</Text>
-                  <Icon as={BiChevronRight} fontSize="1.25rem" />
-                </HStack>
-              </ContextMenu.Item>
-            )}
-            <>
-              <Divider />
-              <ContextMenu.Item
-                icon={<BiTrash />}
-                as={RouterLink}
-                to={
-                  type === "dir"
-                    ? `${url}/deleteDirectory/${encodedName}`
-                    : `${url}/deletePage/${encodedName}`
-                }
-              >
-                Delete
-              </ContextMenu.Item>
-            </>
-          </ContextMenu.List>
-        </ContextMenu>
-      )}
+      <Box className={`${elementStyles.card} ${elementStyles.folderItem}`}>
+        <div className={contentStyles.contentContainerFolderRow}>
+          <i className={`bx bxs-file-blank ${elementStyles.folderItemIcon}`} />
+          <span className={`${elementStyles.folderItemText} mr-auto`}>
+            {pageFileNameToTitle(name)}
+          </span>
+        </div>
+      </Box>
     </Box>
   )
 }
