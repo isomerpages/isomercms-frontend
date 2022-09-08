2b0fc7a2919935df3f917033d6d75eb736275533
Alexander Lee <alexander@open.gov.sg>
2022-09-08 16:44:29 +0800

    Feat: update permalink check for resource pages

--- src/components/PageSettingsModal/PageSettingsSchema.jsx
+++ src/components/PageSettingsModal/PageSettingsSchema.jsx
@@ -4,6 +4,7 @@ import * as Yup from "yup"
 import {
   permalinkRegexTest,
   specialCharactersRegexTest,
+  resourcePermalinkRegexText,
   PAGE_SETTINGS_PERMALINK_MIN_LENGTH,
   PAGE_SETTINGS_PERMALINK_MAX_LENGTH,
   PAGE_SETTINGS_TITLE_MIN_LENGTH,
@@ -32,9 +33,27 @@ export const PageSettingsSchema = (existingTitlesArray = []) =>
         "Title is already in use. Please choose a different title.",
         (value) => !_.includes(existingTitlesArray, value)
       ),
-    permalink: Yup.string().when("layout", (layout, schema) =>
-      layout !== "file"
-        ? schema
+    permalink: Yup.string().when("layout", (layout, schema) => {
+      switch (layout) {
+        case "file":
+          return schema
+        case "post":
+          return schema
+            .required("Permalink is required")
+            .min(
+              PAGE_SETTINGS_PERMALINK_MIN_LENGTH,
+              `Permalink must be longer than ${PAGE_SETTINGS_PERMALINK_MIN_LENGTH} characters`
+            )
+            .max(
+              PAGE_SETTINGS_PERMALINK_MAX_LENGTH,
+              `Permalink must be shorter than ${PAGE_SETTINGS_PERMALINK_MAX_LENGTH} characters`
+            )
+            .matches(
+              resourcePermalinkRegexText,
+              "Permalink should contain alphanumeric characters separated by hyphens and slashes only"
+            )
+        default:
+          return schema
             .required("Permalink is required")
             .min(
               PAGE_SETTINGS_PERMALINK_MIN_LENGTH,
@@ -48,8 +67,8 @@ export const PageSettingsSchema = (existingTitlesArray = []) =>
               permalinkRegexTest,
               "Permalink should start with a slash, and contain alphanumeric characters separated by hyphens and slashes only"
             )
-        : schema
-    ),
+      }
+    }),
     layout: Yup.string().when("$type", (type, schema) =>
       type === "resourcePage"
         ? schema
