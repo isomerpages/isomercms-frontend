8eba250526bcbc1f137fad89ee3d07fba188d727
seaerchin <jiachin@open.gov.sg>
2022-09-30 02:23:27 +0800

    feat(editingblockedmodal): add new modal

---
+++ src/layouts/ReviewRequest/components/EditingBlockedModal/EditingBlockedModal.stories.tsx
@@ -0,0 +1,26 @@
+import { useDisclosure } from "@chakra-ui/react"
+import { Button } from "@opengovsg/design-system-react"
+import { ComponentMeta, Story } from "@storybook/react"
+
+import { EditingBlockedModal } from "./EditingBlockedModal"
+
+const modalMeta = {
+  title: "Components/ReviewRequest/Editing Blocked Modal",
+  component: EditingBlockedModal,
+} as ComponentMeta<typeof EditingBlockedModal>
+
+const Template: Story<never> = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure({ defaultIsOpen: true })
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+      <EditingBlockedModal isOpen={isOpen} onClose={onClose}>
+        <Button colorScheme="danger">Click me</Button>
+      </EditingBlockedModal>
+    </>
+  )
+}
+
+export const Playground = Template.bind({})
+
+export default modalMeta
