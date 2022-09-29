5392f6b1b5df662a66af3cc641762c8e7ea7d2af
seaerchin <jiachin@open.gov.sg>
2022-09-29 20:33:39 +0800

    feat(cancelrequestmodal): add new modal

---
+++ src/layouts/ReviewRequest/components/CancelRequestModal/CancelRequestModal.tsx
@@ -0,0 +1,48 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalProps,
+  Text,
+} from "@chakra-ui/react"
+import { Button, ModalCloseButton } from "@opengovsg/design-system-react"
+
+export const CancelRequestModal = (props: ModalProps): JSX.Element => {
+  const { onClose } = props
+
+  return (
+    <Modal {...props}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>
+          <Text textStyle="h4" color="text.title.alt">
+            Cancel request?
+          </Text>
+        </ModalHeader>
+        <ModalCloseButton />
+
+        <ModalBody>
+          <Text textStyle="body-2">
+            The request to review will be cancelled but changes made will
+            remain.
+          </Text>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button
+            textColor="text.body"
+            variant="clear"
+            mr="1rem"
+            onClick={onClose}
+          >
+            Go back
+          </Button>
+          <Button colorScheme="danger">Yes, cancel</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
