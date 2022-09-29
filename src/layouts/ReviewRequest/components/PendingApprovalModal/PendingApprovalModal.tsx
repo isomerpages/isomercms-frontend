a53edc4f7fe0e8845756d735c33991788ceea0bc
seaerchin <jiachin@open.gov.sg>
2022-09-30 02:19:45 +0800

    feat(pendingapprovalmodal): add new modall

---
+++ src/layouts/ReviewRequest/components/PendingApprovalModal/PendingApprovalModal.tsx
@@ -0,0 +1,49 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalProps,
+  Text,
+} from "@chakra-ui/react"
+import { Button, ModalCloseButton } from "@opengovsg/design-system-react"
+
+export const PendingApprovalModal = (props: ModalProps): JSX.Element => {
+  const { onClose } = props
+
+  return (
+    <Modal {...props}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>
+          <Text textStyle="h4" textColor="text.title.alt">
+            A Review request is pending approval
+          </Text>
+        </ModalHeader>
+        <ModalCloseButton />
+
+        <ModalBody>
+          <Text textStyle="body-1">
+            Any changes you make now will be added to the existing Review
+            request, and published with the changes in it.
+          </Text>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button
+            variant="clear"
+            mr="1rem"
+            onClick={onClose}
+            colorScheme="secondary"
+            textColor="text.title.brandSecondary"
+          >
+            Back to Site Dashboard
+          </Button>
+          <Button>Continue to edit</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
