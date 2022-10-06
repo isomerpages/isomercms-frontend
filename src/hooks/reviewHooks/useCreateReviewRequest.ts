34fc4f4aa851a8b87fcd9174eda8fa7454890426
seaerchin <jiachin@open.gov.sg>
2022-10-07 02:46:21 +0800

    feat(usecreaterr): add new hook

---
+++ src/hooks/reviewHooks/useCreateReviewRequest.ts
@@ -0,0 +1,27 @@
+import { AxiosError } from "axios"
+import { UseMutationResult, useMutation, useQueryClient } from "react-query"
+
+import { REVIEW_REQUEST_QUERY_KEY } from "constants/queryKeys"
+
+import { ErrorDto } from "types/error"
+import { ReviewRequestInfo } from "types/reviewRequest"
+
+import * as ReviewService from "../../services/ReviewService"
+
+export const useCreateReviewRequest = (
+  siteName: string
+): UseMutationResult<number, AxiosError<ErrorDto>, ReviewRequestInfo> => {
+  const queryClient = useQueryClient()
+  return useMutation(
+    ({ reviewers, ...rest }) =>
+      ReviewService.createReviewRequest(siteName, {
+        ...rest,
+        reviewers: reviewers.map(({ value }) => value),
+      }),
+    {
+      onSettled: () => {
+        queryClient.invalidateQueries([REVIEW_REQUEST_QUERY_KEY, siteName])
+      },
+    }
+  )
+}
