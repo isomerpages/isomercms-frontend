d5756f39c913019b974de33e259700aa8a96dcd8
seaerchin <jiachin@open.gov.sg>
2022-10-07 02:52:26 +0800

    feat(empty rr): add linkage to rr modal

--- src/layouts/SiteDashboard/components/EmptyReviewRequest.tsx
+++ src/layouts/SiteDashboard/components/EmptyReviewRequest.tsx
@@ -1,25 +1,33 @@
-import { VStack, Icon, Text } from "@chakra-ui/react"
+import { VStack, Icon, Text, useDisclosure } from "@chakra-ui/react"
 import { Button } from "@opengovsg/design-system-react"
 import { DisplayCard, DisplayCardContent } from "components/DisplayCard"
 import { BiCheckCircle } from "react-icons/bi"
 
+import { ReviewRequestModal } from "layouts/ReviewRequest"
+
 import { EmptyWhiteBoxImage } from "assets"
 
 export const EmptyReviewRequest = (): JSX.Element => {
+  const { isOpen, onClose, onOpen } = useDisclosure()
   return (
-    <DisplayCard variant="onlyContent" w="100%">
-      <DisplayCardContent justifyContent="center" alignItems="center">
-        <VStack spacing="1rem" py="2.15rem">
-          <EmptyWhiteBoxImage />
-          <Text textStyle="caption-2" color="text.helper">
-            There are no pending reviews.
-          </Text>
-          {/* TODO: To be replaced by review request modal */}
-          <Button leftIcon={<Icon as={BiCheckCircle} fontSize="1.25rem" />}>
-            Request a Review
-          </Button>
-        </VStack>
-      </DisplayCardContent>
-    </DisplayCard>
+    <>
+      <DisplayCard variant="content" w="100%">
+        <DisplayCardContent justifyContent="center" alignItems="center">
+          <VStack spacing="1rem" py="2.15rem">
+            <EmptyWhiteBoxImage />
+            <Text textStyle="caption-2" color="text.helper">
+              There are no pending reviews.
+            </Text>
+            <Button
+              leftIcon={<Icon as={BiCheckCircle} fontSize="1.25rem" />}
+              onClick={onOpen}
+            >
+              Request a Review
+            </Button>
+          </VStack>
+        </DisplayCardContent>
+      </DisplayCard>
+      <ReviewRequestModal isOpen={isOpen} onClose={onClose} />
+    </>
   )
 }
