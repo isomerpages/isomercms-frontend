ef6968e912aad5f0676aaba192ca388db5488c4a
seaerchin <jiachin@open.gov.sg>
2022-10-07 02:54:39 +0800

    feat(rr form/modal): integrate with be

--- src/layouts/ReviewRequest/components/ReviewRequestForm/ReviewRequestForm.tsx
+++ src/layouts/ReviewRequest/components/ReviewRequestForm/ReviewRequestForm.tsx
@@ -1,5 +1,6 @@
 import { VStack, FormControl } from "@chakra-ui/react"
 import { FormLabel, Input, Textarea } from "@opengovsg/design-system-react"
+import { Controller, useFormContext } from "react-hook-form"
 import Select from "react-select"
 
 import { User } from "types/reviewRequest"
@@ -11,15 +12,22 @@ export interface ReviewRequestFormProps {
 export const ReviewRequestForm = ({
   admins,
 }: ReviewRequestFormProps): JSX.Element => {
+  const { register } = useFormContext()
+
   return (
     <VStack align="flex-start" spacing="1.5rem" mt="2rem">
       <FormControl isRequired>
         <FormLabel>Title</FormLabel>
-        <Input type="text" placeholder="Title your request" />
+        <Input
+          type="text"
+          placeholder="Title your request"
+          {...register("title", { required: true })}
+        />
       </FormControl>
       <FormControl>
         <FormLabel>Comments</FormLabel>
         <Textarea
+          {...register("description")}
           minH="9rem"
           placeholder="Briefly describe the changes you’ve made, and add a review deadline (if applicable)"
         />
@@ -38,10 +46,16 @@ export const ReviewRequestForm = ({
           Your changes can be published when at least **one** reviewer approves
           your request
         </FormLabel.Description>
-        <Select
-          options={admins}
-          isMulti
-          placeholder="Select Admins to add as reviewers"
+        <Controller
+          name="reviewers"
+          render={({ field }) => (
+            <Select
+              {...field}
+              options={admins}
+              isMulti
+              placeholder="Select Admins to add as reviewers"
+            />
+          )}
         />
       </FormControl>
     </VStack>
