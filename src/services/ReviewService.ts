c247e1fcb2838fac5bfc53558ee86c4dd85a466e
seaerchin <jiachin@open.gov.sg>
2022-10-07 02:48:39 +0800

    feat(reviewservice): add new service to retrieve data from be

---
+++ src/services/ReviewService.ts
@@ -0,0 +1,43 @@
+import {
+  EditedItemProps,
+  ReviewRequest,
+  ReviewRequestDto,
+  UserDto,
+} from "types/reviewRequest"
+
+import { apiService } from "./ApiService"
+
+export const getDiff = async (siteName: string): Promise<EditedItemProps[]> => {
+  const endpoint = `/sites/${siteName}/review/compare`
+  const { items } = await apiService
+    .get<{ items: EditedItemProps[] }>(endpoint)
+    .then(({ data }) => data)
+
+  return items
+}
+export const getCollaborators = async (
+  siteName: string
+): Promise<UserDto[]> => {
+  const endpoint = `/sites/${siteName}/collaborators`
+  return apiService
+    .get<{ collaborators: UserDto[] }>(endpoint)
+    .then(({ data }) => data.collaborators)
+}
+
+export const createReviewRequest = async (
+  siteName: string,
+  reviewData: ReviewRequestDto
+): Promise<number> => {
+  const endpoint = `/sites/${siteName}/review/request`
+  return apiService.post<number>(endpoint, reviewData).then(({ data }) => data)
+}
+
+export const getReviewRequest = async (
+  siteName: string,
+  reviewId: number
+): Promise<ReviewRequest> => {
+  const endpoint = `/sites/${siteName}/review/${reviewId}`
+  return apiService
+    .get<{ reviewRequest: ReviewRequest }>(endpoint)
+    .then(({ data }) => data.reviewRequest)
+}
