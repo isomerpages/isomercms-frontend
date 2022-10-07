29760cc3d0d8cc99ab6e9912cb6480fbc01eb729
seaerchin <jiachin@open.gov.sg>
2022-10-08 03:00:17 +0800

    fix(usecreaterr): invalidate all requests to force refetch

--- src/hooks/reviewHooks/useCreateReviewRequest.ts
+++ src/hooks/reviewHooks/useCreateReviewRequest.ts
@@ -1,7 +1,10 @@
 import { AxiosError } from "axios"
 import { UseMutationResult, useMutation, useQueryClient } from "react-query"
 
-import { REVIEW_REQUEST_QUERY_KEY } from "constants/queryKeys"
+import {
+  REVIEW_REQUEST_QUERY_KEY,
+  SITE_DASHBOARD_REVIEW_REQUEST_KEY,
+} from "constants/queryKeys"
 
 import { ErrorDto } from "types/error"
 import { ReviewRequestInfo } from "types/reviewRequest"
@@ -21,6 +24,10 @@ export const useCreateReviewRequest = (
     {
       onSettled: () => {
         queryClient.invalidateQueries([REVIEW_REQUEST_QUERY_KEY, siteName])
+        queryClient.invalidateQueries([
+          SITE_DASHBOARD_REVIEW_REQUEST_KEY,
+          siteName,
+        ])
       },
     }
   )
