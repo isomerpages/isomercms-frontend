5554c9a86a43bb55c32e6778a123631c54d5e516
Alexander Lee <alexander@open.gov.sg>
2022-10-10 14:37:06 +0800

    Feat: add CommentsDrawer

---
+++ src/assets/images/ChatImage.tsx
@@ -0,0 +1,39 @@
+export const ChatImage = (
+  props: React.SVGProps<SVGSVGElement>
+): JSX.Element => {
+  return (
+    <svg
+      width="238"
+      height="122"
+      viewBox="0 0 238 122"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg" // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+    >
+      <path
+        d="M132.592 1H218.958C225.361 1 230.551 6.19027 230.551 12.5928V61.051L237.268 76.2239L218.372 61.051H132.592C126.189 61.051 120.999 55.8607 120.999 49.4582V31.0255V12.5928C121.002 6.19141 126.19 1.00274 132.592 1Z"
+        fill="white"
+        stroke="#A0A0A0"
+        strokeLinecap="round"
+      />
+      <path d="M213 32H139H213Z" fill="#A0A0A0" />
+      <path d="M213 32H139" stroke="#A0A0A0" />
+      <path d="M213 18.5H139H213Z" fill="#A0A0A0" />
+      <path d="M213 18.5H139" stroke="#A0A0A0" />
+      <path d="M188 45.5H139H188Z" fill="#A0A0A0" />
+      <path d="M188 45.5H139" stroke="#A0A0A0" />
+      <path
+        d="M85.4064 45.7761H19.0397C12.6372 45.7761 7.44689 50.9663 7.44689 57.3689V105.827L0.730469 121L19.626 105.827H85.4064C91.8089 105.827 96.9992 100.637 96.9992 94.2343V57.3689C96.9964 50.9675 91.8077 45.7788 85.4064 45.7761Z"
+        fill="white"
+        stroke="#A0A0A0"
+        strokeLinecap="round"
+      />
+      <path d="M24.7988 76.6686H79.6423H24.7988Z" fill="#A0A0A0" />
+      <path d="M24.7988 76.6686H79.6423" stroke="#A0A0A0" />
+      <path d="M24.7988 63.2358H79.6423H24.7988Z" fill="#A0A0A0" />
+      <path d="M24.7988 63.2358H79.6423" stroke="#A0A0A0" />
+      <path d="M43.2676 90.1044H79.984H43.2676Z" fill="#A0A0A0" />
+      <path d="M43.2676 90.1044H79.984" stroke="#A0A0A0" />
+    </svg>
+  )
+}
---
+++ src/layouts/ReviewRequest/components/Comments/CommentsDrawer.tsx
@@ -0,0 +1,165 @@
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Stack,
+  UseDisclosureReturn,
+  Text,
+  Flex,
+  Divider,
+  HStack,
+  useDisclosure,
+  Skeleton,
+} from "@chakra-ui/react"
+import { IconButton } from "@opengovsg/design-system-react"
+import { PropsWithChildren } from "react"
+import { BiCommentDetail } from "react-icons/bi"
+
+import {
+  useGetComments,
+  useUpdateComments,
+  useUpdateReadComments,
+} from "hooks/commentsHooks"
+
+import { getAxiosErrorMessage } from "utils/axios"
+import { getDateTimeFromUnixTime } from "utils/date"
+
+import { ChatImage } from "assets/images/ChatImage"
+import { CommentProps } from "types/comments"
+
+import { CommentFormProps, SendCommentForm } from "./SendCommentForm"
+
+export interface CommentItemProps {
+  commenterName: string
+  commentTime: number
+  isNew: boolean
+}
+
+const CommentItem = ({
+  commenterName,
+  commentTime,
+  isNew,
+  children,
+}: PropsWithChildren<CommentItemProps>): JSX.Element => {
+  const { date, time } = getDateTimeFromUnixTime(commentTime)
+  return (
+    <Box backgroundColor={isNew ? "blue.100" : "blue.50"} p="1rem" mb="1rem">
+      <HStack
+        fontSize="0.625rem"
+        color="text.helper"
+        justifyContent="space-between"
+        pb="0.5rem"
+      >
+        <Text overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap">
+          {commenterName}
+        </Text>
+        <Text whiteSpace="nowrap">{`${date}, ${time}`}</Text>
+      </HStack>
+      {children}
+    </Box>
+  )
+}
+
+export type CommentsDrawerProps = Pick<
+  UseDisclosureReturn,
+  "onClose" | "isOpen"
+>
+
+export const CommentsDrawer = ({ siteName, requestId }: CommentProps) => {
+  const {
+    isOpen: isCommentsOpen,
+    onOpen: onCommentsOpen,
+    onClose: onCommentsClose,
+  } = useDisclosure()
+
+  const { data: commentsData, isLoading: isCommentsLoading } = useGetComments({
+    siteName,
+    requestId,
+  })
+
+  const { mutateAsync: updateReadComments } = useUpdateReadComments()
+  const {
+    mutateAsync: updateNotifications,
+    error: updateNotificationsError,
+  } = useUpdateComments()
+
+  const handleUpdateNotifications = async ({ comment }: CommentFormProps) => {
+    await updateNotifications({ siteName, requestId, message: comment })
+  }
+
+  return (
+    <>
+      <IconButton
+        backgroundColor="background.action.defaultInverse"
+        onClick={() => {
+          onCommentsOpen()
+          // updateReadComments({ siteName, requestId })
+        }}
+        aria-label="Open comments"
+        icon={<BiCommentDetail />}
+        variant="clear"
+        boxShadow="0 0 0 2px var(--chakra-colors-gray-100)"
+      />
+      <Drawer
+        isOpen={isCommentsOpen}
+        onClose={onCommentsClose}
+        placement="right"
+        size="sm"
+      >
+        <DrawerOverlay />
+        <DrawerContent px="1rem">
+          <DrawerCloseButton
+            variant="clear"
+            colorScheme="secondary"
+            top="1.25rem"
+          />
+          <DrawerHeader fontSize="1.5rem" color="text.title.alt">
+            Comments
+          </DrawerHeader>
+          <DrawerBody
+            fontSize="0.875rem"
+            whiteSpace="pre-line"
+            color="secondary.500"
+          >
+            <Stack spacing="2rem" height="100%">
+              <Text color="text.helper">
+                Comments apply to all items in this Review request
+              </Text>
+              <Skeleton isLoaded={!isCommentsLoading} flex={1}>
+                {commentsData && commentsData.length > 0 ? (
+                  commentsData.map((comment) => (
+                    <CommentItem
+                      commenterName={comment.email}
+                      commentTime={comment.createdAt}
+                      isNew={comment.isNew}
+                    >
+                      {comment.message}
+                    </CommentItem>
+                  ))
+                ) : (
+                  <Flex
+                    flex={1}
+                    h="100%"
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    <ChatImage />
+                  </Flex>
+                )}
+              </Skeleton>
+              <Divider borderColor="border.divider.alt" />
+              <SendCommentForm
+                onSubmit={handleUpdateNotifications}
+                errorMessage={getAxiosErrorMessage(updateNotificationsError)}
+              />
+            </Stack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
