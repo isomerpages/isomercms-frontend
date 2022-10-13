04103547ca67d3a5f4f73947861e83ef669a6dc8
seaerchin <jiachin@open.gov.sg>
2022-10-13 11:11:13 +0800

    feat(axios): add method to extract message given if axios has be dto

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,12 +1,26 @@
 import { AxiosError } from "axios"
 
+import { ErrorDto } from "types/error"
 import { DEFAULT_RETRY_MSG } from "utils"
 
 export const isAxiosError = (err: unknown): err is AxiosError => {
   return (err as AxiosError).isAxiosError
 }
 
-export const getAxiosErrorMessage = (error: null | AxiosError): string => {
+const isBackendError = (
+  err: AxiosError<unknown>
+): err is AxiosError<ErrorDto> => {
+  return !!(err as AxiosError<ErrorDto>).response?.data.message
+}
+
+export const getAxiosErrorMessage = (
+  error: null | AxiosError<ErrorDto> | AxiosError
+): string => {
   if (!error) return ""
-  return error?.response?.data?.error?.message || DEFAULT_RETRY_MSG
+
+  if (isBackendError(error)) {
+    return error.response?.data.message || DEFAULT_RETRY_MSG
+  }
+
+  return error.response?.data?.error?.message || DEFAULT_RETRY_MSG
 }
