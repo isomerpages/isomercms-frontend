63f71df4c779f4dec621dfffc379141f53192a08
seaerchin <jiachin@open.gov.sg>
2022-10-19 14:22:28 +0800

    chore(constants): removed unused stuff

--- src/components/CollaboratorModal/constants.ts
+++ src/components/CollaboratorModal/constants.ts
@@ -1,9 +1,3 @@
-export enum CollaboratorModalState {
-  Default = "DEFAULT",
-  Acknowledgement = "ACKNOWLEDGEMENT",
-  RemoveCollaborator = "REMOVE_COLLABORATOR",
-}
-
 export const ACK_REQUIRED_ERROR_MESSAGE = "Acknowledgement required"
 
 export const TEXT_FONT_SIZE = "0.875rem"
