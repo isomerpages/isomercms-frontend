9e8fd50990f36dfe477f95085e6a733279481d1a
seaerchin <jiachin@open.gov.sg>
2022-10-19 15:51:25 +0800

    chore(collaboratorhooks): update error import

--- src/hooks/collaboratorHooks/useAddCollaboratorHook.ts
+++ src/hooks/collaboratorHooks/useAddCollaboratorHook.ts
@@ -4,13 +4,13 @@ import { UseMutationResult, useQueryClient, useMutation } from "react-query"
 import { LIST_COLLABORATORS_KEY } from "constants/queryKeys"
 
 import { CollaboratorService } from "services"
-import { CollaboratorError } from "types/collaborators"
+import { MiddlewareError } from "types/error"
 
 export const useAddCollaboratorHook = (
   siteName: string
 ): UseMutationResult<
   void,
-  AxiosError<{ error: CollaboratorError }>,
+  AxiosError<{ error: MiddlewareError }>,
   { newCollaboratorEmail: string; isAcknowledged: boolean }
 > => {
   const queryClient = useQueryClient()
