7c68220fd1738aea1952dd45a41f8cc71d5eb334
seaerchin <jiachin@open.gov.sg>
2022-11-07 15:43:07 +0800

    fix(approvedreviewredirect): removed redir for github users on error

--- src/routing/ApprovedReviewRedirect.tsx
+++ src/routing/ApprovedReviewRedirect.tsx
@@ -2,6 +2,8 @@ import { Greyscale } from "components/Greyscale"
 import { PropsWithChildren, useEffect } from "react"
 import { Redirect, RedirectProps, useParams } from "react-router-dom"
 
+import { useLoginContext } from "contexts/LoginContext"
+
 import { useGetReviewRequests } from "hooks/siteDashboardHooks"
 
 import { getAxiosErrorMessage } from "utils/axios"
@@ -23,17 +25,20 @@ export const ApprovedReviewRedirect = ({
   const errorToast = useErrorToast()
   const warningToast = useWarningToast()
 
+  const { userId } = useLoginContext()
+  const isGithubUser = !!userId
+
   const hasApprovedReviewRequest =
     reviewRequests &&
     reviewRequests.filter((req) => req.status === "APPROVED").length > 0
 
   useEffect(() => {
-    if (isError) {
+    if (!isGithubUser && isError) {
       errorToast({
         description: getAxiosErrorMessage(error),
       })
     }
-  }, [isError, error, errorToast])
+  }, [isError, error, errorToast, isGithubUser])
 
   useEffect(() => {
     if (hasApprovedReviewRequest) {
@@ -44,7 +49,9 @@ export const ApprovedReviewRedirect = ({
     }
   }, [hasApprovedReviewRequest, warningToast])
 
-  return (
+  return isGithubUser ? (
+    <>{children}</>
+  ) : (
     <Greyscale isActive={isLoading || !reviewRequests}>
       {hasApprovedReviewRequest && (
         <Redirect {...rest} to={`/sites/${siteName}/dashboard`} />
@@ -53,7 +60,7 @@ export const ApprovedReviewRedirect = ({
        * If we fail to retrieve the list of review requests,
        * we take the conservative route and prevent the user from editing.
        */}
-      {!hasApprovedReviewRequest && isError && (
+      {!hasApprovedReviewRequest && !isGithubUser && isError && (
         <Redirect {...rest} to="/sites" />
       )}
       {children}
