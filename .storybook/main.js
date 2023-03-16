e3e1c8079253220ce49f1cfcb62641b7b1bc0925
Harish V <harish@open.gov.sg>
2023-03-16 21:37:34 +0800

    fix: storybook config for wp 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,35 +10,10 @@ module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "storybook-preset-craco",
+    "@storybook/preset-create-react-app",
   ],
   framework: "@storybook/react",
-  // webpackFinal setup retrieved from ChakraUI's own Storybook setup
-  // https://github.com/chakra-ui/chakra-ui/blob/main/.storybook/main.js
-  webpackFinal: async (storybookConfig) => {
-    // Required to sync aliases between storybook and overriden configs
-    return {
-      ...storybookConfig,
-      module: {
-        rules: [
-          ...storybookConfig.module.rules,
-          {
-            type: "javascript/auto",
-            test: /\.mjs$/,
-            include: /node_modules/,
-          },
-        ],
-      },
-      resolve: {
-        ...storybookConfig.resolve,
-        alias: {
-          ...storybookConfig.resolve.alias,
-          // Required so storybook knows where the npm package is to render ChakraUI components
-          // as this is not directly installed in package.json.
-          "@emotion/core": "@emotion/react",
-          "emotion-theming": "@emotion/react",
-        },
-      },
-    }
+  core: {
+    builder: "webpack5",
   },
 }
