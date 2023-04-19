94641f7c3d7130601fabcd6db788b006f11c0a21
seaerchin <jiachin@open.gov.sg>
2023-04-19 19:02:32 +0800

    chore(sites.spec): fix e2e

--- cypress/e2e/sites.spec.ts
+++ cypress/e2e/sites.spec.ts
@@ -1,19 +1,27 @@
-import { CMS_BASEURL, TEST_REPO_NAME } from "../fixtures/constants"
+import {
+  CMS_BASEURL,
+  Interceptors,
+  TEST_REPO_NAME,
+} from "../fixtures/constants"
 
 describe("Sites page", () => {
   beforeEach(() => {
     cy.setSessionDefaults()
-    // NOTE: This is to v1 so we are redefining it here
-    cy.intercept("GET", "/v1/**").as("getRequest")
+    cy.setupDefaultInterceptors()
   })
 
   it("Sites page should have sites header", () => {
-    cy.visit(`${CMS_BASEURL}/sites`).wait("@getRequest")
-    cy.contains("Sites")
+    cy.visit(`${CMS_BASEURL}/sites`).wait(Interceptors.GET)
+    cy.contains("Get help").should("exist")
   })
 
   it("Sites page should allow user to click into a test site repo", () => {
-    cy.visit(`${CMS_BASEURL}/sites`).wait("@getRequest")
+    cy.visit(`${CMS_BASEURL}/sites`)
+      // NOTE: `/whoami` and `/sites`
+      .wait(Interceptors.GET)
+      .wait(Interceptors.GET)
+
+    cy.contains("Loading sites").should("not.exist")
 
     cy.contains(TEST_REPO_NAME).click()
     cy.url().should(
