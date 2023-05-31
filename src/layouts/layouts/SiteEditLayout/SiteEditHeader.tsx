4cc13e559d0829a6813c3d338ed676c69b9ae32f
Harish <harish@open.gov.sg>
2023-05-31 13:58:43 +0800

    fix: add checks to Header component (#1286)

--- src/layouts/layouts/SiteEditLayout/SiteEditHeader.tsx
+++ src/layouts/layouts/SiteEditLayout/SiteEditHeader.tsx
@@ -49,8 +49,12 @@ export const SiteEditHeader = (): JSX.Element => {
     isLoading: isReviewRequestsLoading,
   } = useGetReviewRequests(siteName)
 
+  // Note: if PR is in APPROVED status, it will auto-redirect to dashboard as no edits should happen
+  // But have added here to be explicit of the status checks
   const openReviewRequests = reviewRequests
-    ? reviewRequests.filter((request) => request.status === "OPEN")
+    ? reviewRequests.filter(
+        (request) => request.status === "OPEN" || request.status === "APPROVED"
+      )
     : []
 
   const shouldDisableReviewRequestButton =
