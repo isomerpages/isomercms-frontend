b68a0b115731cfb705190e5dfba0e850c3e7f565
Alexander Lee <alexander@open.gov.sg>
2023-06-01 17:53:41 +0800

    Fix: trim whitespace (#1291)

--- src/services/CollaboratorService.ts
+++ src/services/CollaboratorService.ts
@@ -29,7 +29,10 @@ export const addCollaborator = async (
 ): Promise<void> => {
   const endpoint = getCollaboratorEndpoint(siteName)
   return apiService
-    .post(endpoint, { email: email.toLowerCase(), acknowledge: isAcknowledged })
+    .post(endpoint, {
+      email: email.trim().toLowerCase(),
+      acknowledge: isAcknowledged,
+    })
     .then((res) => res.data)
 }
 
