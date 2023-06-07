28e4597cbb634795ee6edd3752827e216d444fb4
seaerchin <jiachin@open.gov.sg>
2023-06-07 16:38:06 +0800

    chore(navigation): add return type

--- cypress/utils/navigation.ts
+++ cypress/utils/navigation.ts
@@ -1,6 +1,6 @@
 import { BACKEND_URL, E2E_EMAIL_TEST_SITE } from "../fixtures/constants"
 
-export const visitE2eEmailTestRepo = () => {
+export const visitE2eEmailTestRepo = (): void => {
   cy.visit(`${BACKEND_URL}/sites/${E2E_EMAIL_TEST_SITE.repo}/dashboard`)
   cy.contains(E2E_EMAIL_TEST_SITE.repo).should("be.visible")
 }
