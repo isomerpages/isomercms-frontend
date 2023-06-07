2e325775b198110d0403d6e08fc43c27386f0f4f
seaerchin <jiachin@open.gov.sg>
2023-06-07 21:09:05 +0800

    fix(navigation): fix url

--- cypress/utils/navigation.ts
+++ cypress/utils/navigation.ts
@@ -1,6 +1,6 @@
-import { BACKEND_URL, E2E_EMAIL_TEST_SITE } from "../fixtures/constants"
+import { CMS_BASEURL, E2E_EMAIL_TEST_SITE } from "../fixtures/constants"
 
 export const visitE2eEmailTestRepo = (): void => {
-  cy.visit(`${BACKEND_URL}/sites/${E2E_EMAIL_TEST_SITE.repo}/dashboard`)
+  cy.visit(`${CMS_BASEURL}/sites/${E2E_EMAIL_TEST_SITE.repo}/dashboard`)
   cy.contains(E2E_EMAIL_TEST_SITE.repo).should("be.visible")
 }
