4bade8303359814b0a6760891e28267b29a23148
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-06-08 13:51:24 +0800

    feat(storybook): add in site lunch storybook for dashboard

--- src/layouts/SiteDashboard/SiteDashboard.stories.tsx
+++ src/layouts/SiteDashboard/SiteDashboard.stories.tsx
@@ -23,6 +23,7 @@ import {
   buildSiteDashboardReviewRequests,
   buildSiteLaunchDto,
 } from "mocks/utils"
+import { SITE_LAUNCH_TASKS_LENGTH } from "types/siteLaunch"
 
 import { SiteDashboard } from "./SiteDashboard"
 
