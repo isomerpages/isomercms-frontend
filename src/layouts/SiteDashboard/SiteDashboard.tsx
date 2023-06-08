7cd2470e95b67803c3f4fe90b63d98f0648fe902
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-06-08 13:51:24 +0800

    feat(storybook): add in site lunch storybook for dashboard

--- src/layouts/SiteDashboard/SiteDashboard.tsx
+++ src/layouts/SiteDashboard/SiteDashboard.tsx
@@ -26,9 +26,10 @@ import {
   MenuDropdownItem,
 } from "components/MenuDropdownButton"
 import _ from "lodash"
-import { useEffect } from "react"
+import { useEffect, useEffect } from "react"
 import { BiCheckCircle, BiCog, BiEditAlt, BiGroup } from "react-icons/bi"
 import { useParams, Link as RouterLink } from "react-router-dom"
+import { isUserUsingSiteLaunchFeature } from "services/SiteLaunchService"
 
 import { LOCAL_STORAGE_KEYS } from "constants/localStorage"
 
