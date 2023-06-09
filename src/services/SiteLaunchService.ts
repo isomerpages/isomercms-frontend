3e0dbb4f8aba670ef52e7b8353896ed776a31eaa
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-06-09 13:37:54 +0800

    style(siteLaunchService): add comments for clarity

--- src/services/SiteLaunchService.ts
+++ src/services/SiteLaunchService.ts
@@ -3,6 +3,9 @@ const whiteListedRepos = process.env.SITE_LAUNCH_FEATURE_WHITELISTED_REPOS?.spli
 )
 
 export const isUserUsingSiteLaunchFeature = (siteName: string): boolean => {
+  // The name of our sites for our storybooks is "storybook".
+  // Therefore, to allow the storybook to use the site launch feature, we need to
+  // explicitly check for it.
   if (siteName === "storybook") {
     return true
   }
