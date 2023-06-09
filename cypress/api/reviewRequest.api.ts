bccb1f4a598d31a49e90596cc96641e5f8a72d81
seaerchin <jiachin@open.gov.sg>
2023-06-09 13:01:50 +0800

    feat(cypress api): add some api for rr

--- cypress/api/reviewRequest.api.ts
+++ cypress/api/reviewRequest.api.ts
@@ -1,21 +1,40 @@
-import { Interceptors } from "../fixtures/constants"
-
 import { E2E_EMAIL_SITE_API_URL } from "./constants"
 
+const BASE_URL = `${E2E_EMAIL_SITE_API_URL}/review`
+
 /**
- * @precondition Assumes that the default interceptors are set up.
  * This creates a review request and waits for it using the default interceptors
  * Note that this creates a review request for the test site using the
  * session of the currently active user.
  */
-export const createReviewRequest = async (
+export const createReviewRequest = (
   title: string,
   reviewers: string[],
   description?: string
-): Promise<void> => {
-  cy.request("POST", `${E2E_EMAIL_SITE_API_URL}/review/request`, {
-    reviewers,
-    title,
-    description,
-  }).wait(Interceptors.POST)
+): Cypress.Chainable<number> => {
+  return cy
+    .request("POST", `${BASE_URL}/request`, {
+      reviewers,
+      title,
+      description,
+    })
+    .then(({ body }) => body.pullRequestNumber)
+}
+
+export const listReviewRequests = (): Cypress.Chainable<{ id: number }[]> => {
+  return cy
+    .request("GET", `${BASE_URL}/summary`)
+    .then(({ body }) => body.reviews)
+}
+
+export const closeReviewRequests = (): void => {
+  listReviewRequests().then((reviewRequests) => {
+    reviewRequests.forEach(({ id }) => {
+      cy.request("DELETE", `${BASE_URL}/${id}`)
+    })
+  })
+}
+
+export const approveReviewRequest = (id: number): void => {
+  cy.request("POST", `${BASE_URL}/${id}/approve`)
 }
