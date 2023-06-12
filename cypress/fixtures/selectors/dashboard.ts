a707d2e59fb703963e8b721bf3574da6b270d5d8
seaerchin <jiachin@open.gov.sg>
2023-06-12 13:46:45 +0800

    feat(session/dashboard): add const and chnage email based on type

---
+++ cypress/fixtures/selectors/dashboard.ts
@@ -0,0 +1,6 @@
+export const COMMENTS_DRAWER_BUTTON_SELECTOR =
+  "button[aria-label='Open comments']"
+
+export const COMMENTS_INPUT_SELECTOR = "input[name='comment'][id='comment']"
+export const SUBMIT_COMMENT_BUTTON_SELECTOR =
+  "button[type='submit'][aria-label='link to send comment']"
