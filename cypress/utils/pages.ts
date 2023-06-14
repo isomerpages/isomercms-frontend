19eb3791c72bb481a21b67caafdf192d0836a0fd
seaerchin <jiachin@open.gov.sg>
2023-06-14 12:49:55 +0800

    chore(pages): add default length

--- cypress/utils/pages.ts
+++ cypress/utils/pages.ts
@@ -1,5 +1,5 @@
 // NOTE: generates a random string.
 // Length is set to 7 by default
-export const genRandomString = (length: number): string => {
+export const genRandomString = (length = 7): string => {
   return Math.random().toString(36).substring(length)
 }
