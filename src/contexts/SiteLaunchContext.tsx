553a0391e79b2a288e7a330b6e0a63b676879c14
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-06-16 19:39:24 +0800

    style(site launch context): use useEffect instead

--- src/contexts/SiteLaunchContext.tsx
+++ src/contexts/SiteLaunchContext.tsx
@@ -4,7 +4,6 @@ import { useParams } from "react-router-dom"
 import { useGetSiteLaunchStatus } from "hooks/siteDashboardHooks"
 
 import {
-  SiteLaunchDto,
   SiteLaunchFrontEndStatus,
   SiteLaunchStatusProps,
   SITE_LAUNCH_TASKS_LENGTH,
@@ -32,38 +31,6 @@ export const useSiteLaunchContext = (): SiteLaunchContextProps => {
   return SiteLaunchContextData
 }
 
-function updateSiteLaunchStatusOnApiCall(
-  siteLaunchDto: SiteLaunchDto,
-  siteLaunchStatusProps: SiteLaunchStatusProps,
-  setSiteLaunchStatusProps: (
-    siteLaunchStatusProps: SiteLaunchStatusProps
-  ) => void
-): void {
-  if (
-    siteLaunchDto.siteStatus === "NOT_LAUNCHED" &&
-    siteLaunchStatusProps.siteLaunchStatus === "LOADING"
-  ) {
-    setSiteLaunchStatusProps({
-      siteLaunchStatus: "NOT_LAUNCHED",
-      stepNumber: 0,
-    })
-  }
-  // this condition is added to prevent redundant re-renders
-  const isSiteLaunchFEAndBESynced =
-    siteLaunchStatusProps.siteLaunchStatus === siteLaunchDto?.siteStatus
-  if (
-    (siteLaunchDto.siteStatus === "LAUNCHED" ||
-      siteLaunchDto.siteStatus === "LAUNCHING") &&
-    !isSiteLaunchFEAndBESynced
-  ) {
-    setSiteLaunchStatusProps({
-      siteLaunchStatus: siteLaunchDto.siteStatus,
-      stepNumber: SITE_LAUNCH_TASKS_LENGTH,
-      dnsRecords: siteLaunchDto.dnsRecords,
-    })
-  }
-}
-
 export const SiteLaunchProvider = ({
   children,
   initialSiteLaunchStatus,
@@ -81,13 +48,34 @@ export const SiteLaunchProvider = ({
 
   const { data: siteLaunchDto } = useGetSiteLaunchStatus(siteName)
 
-  if (siteLaunchDto) {
-    updateSiteLaunchStatusOnApiCall(
-      siteLaunchDto,
-      siteLaunchStatusProps,
-      setSiteLaunchStatusProps
-    )
-  }
+  // using a UseEffect to sync the siteLaunchStatusProps with the siteLaunchDto
+  // this is needed because the siteLaunchDto is fetched asynchronously
+  useEffect(() => {
+    if (!siteLaunchDto) return
+    if (
+      siteLaunchDto.siteStatus === "NOT_LAUNCHED" &&
+      siteLaunchStatusProps.siteLaunchStatus === "LOADING"
+    ) {
+      setSiteLaunchStatusProps({
+        siteLaunchStatus: "NOT_LAUNCHED",
+        stepNumber: 0,
+      })
+    }
+    // this condition is added to prevent redundant re-renders
+    const isSiteLaunchFEAndBESynced =
+      siteLaunchStatusProps.siteLaunchStatus === siteLaunchDto?.siteStatus
+    if (
+      (siteLaunchDto.siteStatus === "LAUNCHED" ||
+        siteLaunchDto.siteStatus === "LAUNCHING") &&
+      !isSiteLaunchFEAndBESynced
+    ) {
+      setSiteLaunchStatusProps({
+        siteLaunchStatus: siteLaunchDto.siteStatus,
+        stepNumber: SITE_LAUNCH_TASKS_LENGTH,
+        dnsRecords: siteLaunchDto.dnsRecords,
+      })
+    }
+  }, [siteLaunchDto, siteLaunchStatusProps.siteLaunchStatus])
 
   return (
     <SiteLaunchContext.Provider
