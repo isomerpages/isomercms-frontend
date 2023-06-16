509e197d1ed8ae7bad9fbf0304d1745aa9cb1c01
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-06-16 19:39:24 +0800

    style(site launch context): use useEffect instead

--- src/contexts/SiteLaunchContext.tsx
+++ src/contexts/SiteLaunchContext.tsx
@@ -4,7 +4,6 @@ import { useParams } from "react-router-dom"
 import { useGetSiteLaunchStatus } from "hooks/siteDashboardHooks"
 
 import {
-  SiteLaunchDto,
   SiteLaunchFrontEndStatus,
   SiteLaunchStatusProps,
   SiteLaunchTaskTypeIndex,
