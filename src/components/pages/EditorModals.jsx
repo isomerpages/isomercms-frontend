86d7a41f6591cf7559b11a40162ce08c2a875c8e
Harish <harish@open.gov.sg>
2023-06-26 16:22:07 +0800

    IS-271: fix bugs on simple mde editor (#1323)
    * feat: allow selection of images
    
    * draft: enable hyperlink and image insertion
    
    * fix: remove console logs and dangling props
    
    * fix: code cleanup
    
    * fix: remove unused var

--- src/components/pages/EditorModals.jsx
+++ src/components/pages/EditorModals.jsx
@@ -1,29 +1,60 @@
 import HyperlinkModal from "components/HyperlinkModal"
 import MediaModal from "components/media/MediaModal"
 import PropTypes from "prop-types"
+import { useEffect, useState } from "react"
 
-const EditorModals = ({ mdeRef, onSave, modalType, onClose, mediaType }) => {
+const EditorModals = ({
+  onSave,
+  modalType,
+  onClose,
+  mediaType,
+  simpleMde,
+  lineAndCursor,
+}) => {
+  const [linePos, setLinePos] = useState({ line: 0, ch: 0, sticky: null })
+
+  // this fixes the issue where line and cursor are being reset to 0 upon opening modals
+  useEffect(() => {
+    if (!lineAndCursor && simpleMde) {
+      setLinePos(lineAndCursor)
+      simpleMde.codemirror.setSelection(lineAndCursor)
+    }
+    if (
+      lineAndCursor &&
+      !(lineAndCursor.line === 0 && lineAndCursor.ch === 0)
+    ) {
+      setLinePos(lineAndCursor)
+    }
+  }, [lineAndCursor])
+
   const onHyperlinkSave = (text, link) => {
-    const cm = mdeRef.current.simpleMde.codemirror
+    const cm = simpleMde.codemirror
+    cm.setCursor(linePos)
+    cm.setSelection(linePos)
     cm.replaceSelection(`[${text}](${link})`)
     // set state so that rerender is triggered and path is shown
-    onSave(mdeRef.current.simpleMde.codemirror.getValue())
+    onSave(simpleMde.codemirror.getValue())
     onClose()
   }
 
   const onMediaSave = (data) => {
     const { selectedMediaPath, altText } = data
-    const cm = mdeRef.current.simpleMde.codemirror
+    const cm = simpleMde.codemirror
+
+    cm.setCursor(linePos)
+    cm.setSelection(linePos)
+
     if (mediaType === "files")
       cm.replaceSelection(
         `[${altText}](${selectedMediaPath.replaceAll(" ", "%20")})`
       )
-    if (mediaType === "images")
+    if (mediaType === "images") {
       cm.replaceSelection(
         `![${altText}](${selectedMediaPath.replaceAll(" ", "%20")})`
       )
+    }
     // set state so that rerender is triggered and image is shown
-    onSave(mdeRef.current.simpleMde.codemirror.getValue())
+    onSave(simpleMde.codemirror.getValue())
     onClose()
   }
 
@@ -39,7 +70,7 @@ const EditorModals = ({ mdeRef, onSave, modalType, onClose, mediaType }) => {
       )}
       {modalType === "hyperlink" && (
         <HyperlinkModal
-          text={mdeRef.current.simpleMde.codemirror.getSelection()}
+          text={simpleMde.codemirror.getSelection()}
           onSave={onHyperlinkSave}
           onClose={onClose}
         />
@@ -49,14 +80,23 @@ const EditorModals = ({ mdeRef, onSave, modalType, onClose, mediaType }) => {
 }
 
 EditorModals.propTypes = {
-  mdeRef: PropTypes.oneOfType([
-    // https://stackoverflow.com/questions/48007326/what-is-the-correct-proptype-for-a-ref-in-react
-    PropTypes.func,
-    PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
-  ]),
   onSave: PropTypes.func.isRequired,
   modalType: PropTypes.oneOf(["hyperlink", "media"]).isRequired,
   onClose: PropTypes.func.isRequired,
+  simpleMde: PropTypes.shape({
+    codemirror: PropTypes.shape({
+      getSelection: PropTypes.func,
+      getValue: PropTypes.func,
+      replaceSelection: PropTypes.func,
+      setCursor: PropTypes.func,
+    }),
+  }),
+  lineAndCursor: PropTypes.shape({
+    line: PropTypes.number,
+    ch: PropTypes.number,
+    sticky: PropTypes.string,
+    xRel: PropTypes.number,
+  }),
 }
 
 export default EditorModals
