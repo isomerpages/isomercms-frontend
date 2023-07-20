46c3d0a0be98c4aca72e86732f6c33f9026d8b8c
caando <zhangjikun907475@gmail.com>
2023-07-21 04:05:04 +0800

    feat(preview): added preview images

---
+++ src/hooks/allSitesHooks/useSitePreview.ts
@@ -0,0 +1,25 @@
+import { useQuery } from "react-query"
+import type { UseQueryResult } from "react-query"
+
+import { SITE_PREVIEW_KEY } from "constants/queryKeys"
+
+import * as AllSitesService from "services/AllSitesService"
+
+import { SitePreviewRequest } from "types/sites"
+
+export const useSitePreview = (
+  userEmail: string,
+  sites: string[]
+): UseQueryResult<SitePreviewRequest[]> => {
+  return useQuery<SitePreviewRequest[]>(
+    [`${SITE_PREVIEW_KEY}`, userEmail],
+    () => AllSitesService.getSitePreview(sites),
+    {
+      retry: false,
+      refetchOnWindowFocus: false,
+      onError: (error) => {
+        console.error(error)
+      },
+    }
+  )
+}
--- src/services/AllSitesService.ts
+++ src/services/AllSitesService.ts
@@ -1,8 +1,15 @@
-import { SiteDataRequest } from "types/sites"
+import { SiteDataRequest, SitePreviewRequest } from "types/sites"
 
 import { apiService } from "./ApiService"
 
 export const getAllSites = async (): Promise<SiteDataRequest> => {
   const endpoint = `/sites`
   return apiService.get(endpoint).then((res) => res.data)
+}
+
+export const getSitePreview = async (
+  sites: string[]
+): Promise<SitePreviewRequest[]> => {
+  const endpoint = `/sites/preview`
+  return apiService.post(endpoint, { sites }).then((res) => res.data)
 }
