fcfb3c765131c1b42feb4a0d12d256f33e9dd2a9
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-07-21 11:09:53 +0800

    feat(notif): full suite notif e2e

--- cypress/e2e/notifications.spec.ts
+++ cypress/e2e/notifications.spec.ts
@@ -3,15 +3,17 @@ import {
   E2E_EMAIL_ADMIN,
   E2E_EMAIL_COLLAB,
   E2E_EMAIL_TEST_SITE,
-  EMPTY_NOTIFICATIONS_TEXT,
 } from "../fixtures/constants"
 import {
   genRandomString,
+  getNotificationList,
   getNotificationsButton,
   visitE2eEmailTestRepo,
 } from "../utils"
 
 const UNREAD_COMMENTS_BG_COLOR = "rgb(235, 248, 255)"
+const READ_COMMENTS_BG_COLOR = "rgba(0, 0, 0, 0)"
+
 const getReviewRequestedNotifText = (email: string) =>
   `${email} has sent you a review request.`
 
@@ -19,10 +21,13 @@ const getChangesMadeNotifText = (email: string) =>
   `${email} made changes to a review request.`
 
 const getApprovedReviewRequestNotifText = (email: string) =>
-  `${email} has approved a review request`
+  `${email} has approved a review request.`
 
 const getMergedReviewRequestNotifText = (email: string) =>
-  `${email} has published a review request`
+  `${email} has published a review request.`
+
+const getReviewCancelledNotifText = (email: string) =>
+  `${email} has cancelled a review request.`
 
 const getNewNotifBadge = () =>
   getNotificationsButton().within(() => {
@@ -51,7 +56,7 @@ describe("notifications", () => {
     // This is because previous tests may have created notifications.
     api.markNotificationsAsRead()
 
-    cy.setEmailSessionDefaults("Email collaborator")
+    cy.setEmailSessionDefaults("Email admin")
     cy.setupDefaultInterceptors()
     api.closeReviewRequests()
     // NOTE: Mark all notifications as read as email admin.
@@ -70,12 +75,18 @@ describe("notifications", () => {
     // Act
     getNotificationsButton().click()
 
+    // NOTE: We don't check for the existence of no notifications as
+    // we want the tests to be idempotent
     // Assert
-    cy.contains(EMPTY_NOTIFICATIONS_TEXT).should("be.visible")
+    getNotificationList().should(
+      "have.css",
+      "background-color",
+      READ_COMMENTS_BG_COLOR
+    )
   })
   it("should receive a notification on successful creation of a review request", () => {
     // Arrange
-    cy.setEmailSessionDefaults("Email admin")
+    cy.setEmailSessionDefaults("Email collaborator")
     // NOTE: we only refetch on a refresh, so we need to revisit the dashboard
     visitE2eEmailTestRepo()
 
@@ -100,7 +111,11 @@ describe("notifications", () => {
     )
     // NOTE: Editor should not see a notification
     getNotificationsButton().click()
-    cy.contains(EMPTY_NOTIFICATIONS_TEXT).should("be.visible")
+    getNotificationList().should(
+      "have.css",
+      "background-color",
+      READ_COMMENTS_BG_COLOR
+    )
     // NOTE: All other reviewers should see a notification
     cy.setEmailSessionDefaults("Email admin")
     visitE2eEmailTestRepo()
@@ -126,12 +141,12 @@ describe("notifications", () => {
   })
   it("should receive a notification on approval of a review request", () => {
     // Arrange
-    cy.setEmailSessionDefaults("Email admin")
+    cy.setEmailSessionDefaults("Email collaborator")
     visitE2eEmailTestRepo()
     api.approveReviewRequest(reviewId)
 
     // Act
-    cy.setEmailSessionDefaults("Email collaborator")
+    cy.setEmailSessionDefaults("Email admin")
     visitE2eEmailTestRepo()
     getNotificationsButton().click()
 
@@ -147,7 +162,7 @@ describe("notifications", () => {
     api.mergeReviewRequest(reviewId)
 
     // Act
-    cy.setEmailSessionDefaults("Email collaborator")
+    cy.setEmailSessionDefaults("Email collaborator", "collab@e2e.gov.sg")
     visitE2eEmailTestRepo()
     getNotificationsButton().click()
 
@@ -158,17 +173,37 @@ describe("notifications", () => {
   })
   it("should not send a notification if the user is a github user", () => {
     // Arrange
+    cy.setGithubSessionDefaults()
+
     // Act
+    // NOTE: this would have a redirect since GH users don't have a dashboard
+    visitE2eEmailTestRepo()
+
     // Assert
+    // Button should not exist
+    getNotificationsButton().should("throw")
   })
   it("should receive a notification on closing of review request", () => {
     // Arrange
+    api.closeReviewRequest(reviewId)
     // Act
+    cy.setEmailSessionDefaults("Email collaborator")
+    visitE2eEmailTestRepo()
     // Assert
+    getNotificationsButton().click()
+    cy.contains(getReviewCancelledNotifText(E2E_EMAIL_ADMIN.email))
   })
   it("should mark all notifications as read on clicking the button", () => {
     // Arrange
+    getNotificationsButton().click()
+    visitE2eEmailTestRepo()
     // Act
+    getNotificationsButton().click()
     // Assert
+    getNotificationList().should(
+      "have.css",
+      "background-color",
+      READ_COMMENTS_BG_COLOR
+    )
   })
 })
