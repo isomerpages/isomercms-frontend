4065ed9dabca8c36e4d81259550e8f73fa307063
seaerchin <jiachin@open.gov.sg>
2023-07-24 17:47:43 +0800

    feat(feedbak): add initial new modal

--- src/components/FeedbackModal/FeedbackModal.tsx
+++ src/components/FeedbackModal/FeedbackModal.tsx
@@ -5,51 +5,18 @@ import {
   ModalHeader,
   ModalBody,
   Heading,
+  HStack,
+  Text,
+  ModalFooter,
+  VStack,
+  Box,
 } from "@chakra-ui/react"
-import { ModalCloseButton } from "@opengovsg/design-system-react"
-
-const FeedbackForm = () => {
-  return (
-    <>
-      <div
-        style={{
-          fontFamily: "sans-serif",
-          fontSize: "15px",
-          color: "#000",
-          opacity: "0.9",
-          paddingTop: "5px",
-          paddingBottom: "8px",
-        }}
-      >
-        If the form below is not loaded, you can also fill it in{" "}
-        <a href="https://form.gov.sg/64b7a11823e54700118bad90">here</a>.
-      </div>
-
-      <iframe
-        title="FormSG Feedback form for Isomer"
-        id="iframe"
-        src="https://form.gov.sg/64b7a11823e54700118bad90"
-        width="100%"
-        height="650px"
-      />
-
-      <div
-        style={{
-          fontFamily: "sans-serif",
-          fontSize: "12px",
-          color: "#999",
-          opacity: "0.5",
-          paddingTop: "5px",
-        }}
-      >
-        Powered by{" "}
-        <a href="https://form.gov.sg" color="#999">
-          Form
-        </a>
-      </div>
-    </>
-  )
-}
+import {
+  ModalCloseButton,
+  Button,
+  Textarea,
+} from "@opengovsg/design-system-react"
+import { useState } from "react"
 
 export interface FeedbackModalProps {
   isOpen: boolean
@@ -59,18 +26,58 @@ export const FeedbackModal = ({
   isOpen,
   onClose,
 }: FeedbackModalProps): JSX.Element => {
+  const [shouldShowTextArea, setShouldShowTextArea] = useState(false)
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>
-          <Heading as="h4">Help make Isomer better!</Heading>
-        </ModalHeader>
-        <ModalCloseButton />
-        <ModalBody pb={6}>
-          <FeedbackForm />
-        </ModalBody>
-      </ModalContent>
-    </Modal>
+    <form>
+      <Modal isOpen={isOpen} onClose={onClose} size="4xl">
+        <ModalOverlay />
+        <ModalContent py="1.5rem" px="2rem">
+          <ModalHeader p={0}>
+            <Heading as="h5">
+              How&apos;s your IsomerCMS experience today?
+            </Heading>
+          </ModalHeader>
+          <ModalCloseButton right="2rem" top="2rem" />
+          <ModalBody p={0} mt="1.5rem">
+            <VStack spacing="1.5rem" align="flex-start" p={0}>
+              <HStack spacing="1.5rem" px="18.5">
+                {Array(11)
+                  .fill(null)
+                  .map((_, index) => index)
+                  .map((idx) => (
+                    <Button
+                      onClick={() => setShouldShowTextArea(true)}
+                      variant="reverse"
+                      fill="inherit"
+                    >
+                      <Text
+                        textStyle="body-2"
+                        textColor="color.base.content.default"
+                      >
+                        {idx}
+                      </Text>
+                    </Button>
+                  ))}
+              </HStack>
+              {shouldShowTextArea && (
+                <Box width="full">
+                  <Text>Share why you gave us this rating</Text>
+                  <Textarea mt="0.25rem" placeholder="Leave us some feedback" />
+                </Box>
+              )}
+            </VStack>
+          </ModalBody>
+          {shouldShowTextArea && (
+            <ModalFooter mt="0.5rem">
+              <HStack spacing="1rem">
+                <Button>Cancel</Button>
+                <Button>Done</Button>
+              </HStack>
+            </ModalFooter>
+          )}
+        </ModalContent>
+      </Modal>
+    </form>
   )
 }
