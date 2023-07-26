24ff565d6c275907ce5c90998c545e497a6b6e40
seaerchin <jiachin@open.gov.sg>
2023-07-26 13:02:23 +0800

    feat(feedbackmodal): submit to be

--- src/components/FeedbackModal/FeedbackModal.tsx
+++ src/components/FeedbackModal/FeedbackModal.tsx
@@ -18,6 +18,11 @@ import {
 } from "@opengovsg/design-system-react"
 import { Rating } from "components/Rating/Rating"
 import { useState } from "react"
+import { useForm } from "react-hook-form"
+
+import { useLoginContext } from "contexts/LoginContext"
+
+import { useSubmitFeedback } from "hooks/useSubmitFeedback"
 
 export interface FeedbackModalProps {
   isOpen: boolean
@@ -29,13 +34,20 @@ export const FeedbackModal = ({
 }: FeedbackModalProps): JSX.Element => {
   const [shouldShowTextArea, setShouldShowTextArea] = useState(false)
   const [activeIdx, setActiveIdx] = useState<number>(-1)
+  const { register, handleSubmit } = useForm<{ feedback: string }>()
+  const { userType, email } = useLoginContext()
+  const { mutateAsync: submitFeedback, isLoading } = useSubmitFeedback()
 
   return (
-    // modal content, button, header, footer
-    <form>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <form
+          onSubmit={handleSubmit(async ({ feedback }) => {
+            submitFeedback({ feedback, email, userType, rating: activeIdx })
+            onClose()
+          })}
+        >
           <ModalHeader>How satisfied are you with Isomer?</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={shouldShowTextArea ? "auto" : "1.5rem"}>
@@ -48,11 +60,13 @@ export const FeedbackModal = ({
                 activeIdx={activeIdx}
               />
               {shouldShowTextArea && (
-                <FormControl isRequired>
-                  <FormLabel isRequired>
-                    Share why you gave us this rating
-                  </FormLabel>
-                  <Textarea mt="0.25rem" placeholder="Leave us some feedback" />
+                <FormControl>
+                  <FormLabel>Share why you gave us this rating</FormLabel>
+                  <Textarea
+                    mt="0.25rem"
+                    placeholder="Leave us some feedback"
+                    {...register("feedback")}
+                  />
                 </FormControl>
               )}
             </VStack>
@@ -61,18 +75,26 @@ export const FeedbackModal = ({
             <ModalFooter>
               <HStack spacing="1rem">
                 <ButtonGroup>
-                  <Button colorScheme="neutral" variant="clear">
+                  <Button
+                    colorScheme="neutral"
+                    variant="clear"
+                    onClick={onClose}
+                  >
                     Cancel
                   </Button>
-                  <Button colorScheme="main" isDisabled>
+                  <Button
+                    colorScheme="main"
+                    type="submit"
+                    isLoading={isLoading}
+                  >
                     Done
                   </Button>
                 </ButtonGroup>
               </HStack>
             </ModalFooter>
           )}
-        </ModalContent>
-      </Modal>
-    </form>
+        </form>
+      </ModalContent>
+    </Modal>
   )
 }
