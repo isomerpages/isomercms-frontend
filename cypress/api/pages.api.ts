036e58ba409700c7c4823fdc236e89afac728015
Alexander Lee <alexander@open.gov.sg>
2023-08-01 13:29:46 +0800

    feat: add additional api utils

--- cypress/api/pages.api.ts
+++ cypress/api/pages.api.ts
@@ -1,5 +1,70 @@
+import { DirectoryData } from "types/directory"
+
 import { UnlinkedPageDto } from "../../src/types/pages"
 import { BACKEND_URL } from "../fixtures/constants"
+
+export const listCollectionPages = (
+  site: string,
+  collectionName: string
+): Cypress.Chainable<DirectoryData[]> => {
+  return cy
+    .request(
+      "GET",
+      `${BACKEND_URL}/sites/${site}/collections/${collectionName}`
+    )
+    .then(({ body }) => body)
+}
+
+export const listUnlinkedPages = (
+  site: string
+): Cypress.Chainable<DirectoryData[]> => {
+  return cy
+    .request("GET", `${BACKEND_URL}/sites/${site}/pages`)
+    .then(({ body }) => body)
+}
+
+export const addCollectionPage = (
+  pageName: string,
+  collectionName: string,
+  title: string,
+  permalink: string,
+  pageContent: string,
+  site: string
+): void => {
+  cy.request(
+    "POST",
+    `${BACKEND_URL}/sites/${site}/collections/${collectionName}/pages`,
+    {
+      content: {
+        frontMatter: {
+          title,
+          permalink,
+        },
+        pageBody: pageContent,
+      },
+      newFileName: pageName,
+    }
+  )
+}
+
+export const addUnlinkedPage = (
+  pageName: string,
+  title: string,
+  permalink: string,
+  pageContent: string,
+  site: string
+): void => {
+  cy.request("POST", `${BACKEND_URL}/sites/${site}/pages/pages`, {
+    content: {
+      frontMatter: {
+        title,
+        permalink,
+      },
+      pageBody: pageContent,
+    },
+    newFileName: pageName,
+  })
+}
 
 export const editUnlinkedPage = (
   pageName: string,
@@ -28,4 +93,81 @@ export const readUnlinkedPage = (
   return cy
     .request("GET", `${BACKEND_URL}/sites/${site}/pages/pages/${pageName}`)
     .then(({ body }) => body)
+}
+
+export const readCollectionPage = (
+  pageName: string,
+  collectionName: string,
+  site: string
+): Cypress.Chainable<UnlinkedPageDto> => {
+  return cy
+    .request(
+      "GET",
+      `${BACKEND_URL}/sites/${site}/collections/${collectionName}/pages/${pageName}`
+    )
+    .then(({ body }) => body)
+}
+
+export const deleteUnlinkedPage = (pageName: string, site: string): void => {
+  readUnlinkedPage(pageName, site).then(({ sha }) => {
+    return cy.request(
+      "DELETE",
+      `${BACKEND_URL}/sites/${site}/pages/pages/${pageName}`,
+      {
+        sha,
+      }
+    )
+  })
+}
+
+export const deleteCollectionPage = (
+  pageName: string,
+  collectionName: string,
+  site: string
+): void => {
+  readCollectionPage(pageName, collectionName, site).then(({ sha }) => {
+    return cy.request(
+      "DELETE",
+      `${BACKEND_URL}/sites/${site}/collections/${collectionName}/pages/${pageName}`,
+      {
+        sha,
+      }
+    )
+  })
+}
+
+export const renameUnlinkedPage = (
+  pageName: string,
+  newPageName: string,
+  site: string
+): void => {
+  readUnlinkedPage(pageName, site).then(({ sha, content }) => {
+    return cy.request(
+      "POST",
+      `${BACKEND_URL}/sites/${site}/pages/pages/${pageName}`,
+      {
+        content,
+        sha,
+        newFileName: newPageName,
+      }
+    )
+  })
+}
+
+export const moveUnlinkedPage = (
+  pageName: string,
+  targetCollectionName: string,
+  site: string
+): void => {
+  cy.request("POST", `${BACKEND_URL}/sites/${site}/pages/move`, {
+    target: {
+      collectionName: targetCollectionName,
+    },
+    items: [
+      {
+        name: pageName,
+        type: "file",
+      },
+    ],
+  })
 }
