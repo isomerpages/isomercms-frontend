04e58ddcc1eba4af0652a933d12b49d497c79872
seaerchin <jiachin@open.gov.sg>
2023-08-01 17:25:08 +0800

    chore(tabs): removed custom styling

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -2,13 +2,11 @@ import { Breadcrumb } from "./Breadcrumb"
 import { Card, CARD_THEME_KEY } from "./Card"
 import { DISPLAY_CARD_THEME_KEY, DisplayCard } from "./DisplayCard"
 import { Infobox } from "./Infobox"
-import { Tabs } from "./Tabs"
 
 // eslint-disable-next-line import/prefer-default-export
 export const components = {
   [CARD_THEME_KEY]: Card,
   [DISPLAY_CARD_THEME_KEY]: DisplayCard,
-  Tabs,
   Breadcrumb,
   Infobox,
 }
