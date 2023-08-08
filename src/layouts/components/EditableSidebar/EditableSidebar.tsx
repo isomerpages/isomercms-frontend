27125ec3c5bee24e3838aa5df501e087f08a47f7
seaerchin <jiachin@open.gov.sg>
2023-08-08 16:21:01 +0800

    feat(editablesidebar): first pass

--- src/layouts/components/EditableSidebar/EditableSidebar.tsx
+++ src/layouts/components/EditableSidebar/EditableSidebar.tsx
@@ -1,12 +1,33 @@
-import { Box, Center, Flex, Heading, Text, VStack } from "@chakra-ui/react"
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  Icon,
+  Text,
+  VStack,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+} from "@chakra-ui/react"
 import { PropsWithChildren } from "react"
+import { BiFolder } from "react-icons/bi"
+
+import { ContextMenu } from "components/ContextMenu"
 
 import { HomepageStartEditingImage } from "assets"
 
 type SidebarHeaderProps = Pick<EditableSidebarProps, "title">
 const SidebarHeader = ({ title }: SidebarHeaderProps) => {
   return (
-    <Flex pos="sticky" alignContent="center">
+    <Flex
+      w="100%"
+      bg="base.canvas.default"
+      pos="sticky"
+      justify="center"
+      py="1rem"
+    >
       <Text textStyle="subhead-3" color="base.content.brand">
         {title}
       </Text>
@@ -16,19 +37,26 @@ const SidebarHeader = ({ title }: SidebarHeaderProps) => {
 
 const EmptySideBarBody = () => {
   return (
-    <VStack spacing="0.5rem" alignItems="flex-start">
-      <Heading as="h5">Customise Sections</Heading>
+    <VStack spacing="0.5rem" alignItems="flex-start" px="1.5rem" pt="1.5rem">
+      <Text textStyle="h5">Customise Sections</Text>
       <Text textStyle="body-2">Drag and drop sections to rearrange them</Text>
-      <Box>
-        <Center>
-          <HomepageStartEditingImage />
-          <Text> Sections you add will appear here</Text>
-          <Text textStyle="caption-2">
-            Add informative content to your website from images to text by
-            clicking “Add section” below
-          </Text>
-        </Center>
-      </Box>
+      <Flex
+        alignItems="center"
+        flexDir="column"
+        p="3.75rem 1.5rem"
+        justifyContent="center"
+      >
+        <HomepageStartEditingImage />
+        <Text> Sections you add will appear here</Text>
+        <Text
+          textStyle="caption-2"
+          textColor="base.content.medium"
+          textAlign="center"
+        >
+          Add informative content to your website from images to text by
+          clicking “Add section” below
+        </Text>
+      </Flex>
     </VStack>
   )
 }
@@ -41,9 +69,9 @@ export const EditableSidebar = ({
   children,
 }: PropsWithChildren<EditableSidebarProps>) => {
   return (
-    <Flex flexDir="column" px="1.5rem" pt="1.5rem">
+    <Flex flexDir="column" bg="base.canvas.alt">
       <SidebarHeader title={title} />
-      {children ? <div /> : <EmptySideBarBody />}
+      {children ? <>{children} </> : <EmptySideBarBody />}
     </Flex>
   )
 }
