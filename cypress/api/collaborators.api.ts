de0aac056e1e35f623b538373a152a91e6b2cb3a
Alexander Lee <alexander@open.gov.sg>
2023-08-08 12:14:24 +0800

    fix: move removeOtherCollaborators to use api call directly (#1395)
    * fix: move removeOtherCollaborators to use api call directly
    
    * feat: modifyremoveOtherCollaborators to take email

--- cypress/api/collaborators.api.ts
+++ cypress/api/collaborators.api.ts
@@ -1,5 +1,10 @@
-import { E2E_EMAIL_TEST_SITE } from "../fixtures/constants"
+import { CollaboratorDto } from "../../src/types/collaborators"
+import { E2E_EMAIL_ADMIN, E2E_EMAIL_TEST_SITE } from "../fixtures/constants"
 import { USER_TYPES } from "../fixtures/users"
+
+import { E2E_EMAIL_SITE_API_URL } from "./constants"
+
+const BASE_URL = `${E2E_EMAIL_SITE_API_URL}/collaborators`
 
 export const addAdmin = (admin: string): void => {
   cy.createEmailUser(
@@ -8,4 +13,18 @@ export const addAdmin = (admin: string): void => {
     USER_TYPES.Email.Admin,
     E2E_EMAIL_TEST_SITE.name
   )
+}
+
+export const listCollaborators = (): Cypress.Chainable<CollaboratorDto[]> => {
+  return cy.request("GET", `${BASE_URL}`).then(({ body }) => body.collaborators)
+}
+
+export const removeOtherCollaborators = (remainingEmail: string): void => {
+  listCollaborators().then((collaborators) => {
+    collaborators.forEach((collaborator) => {
+      if (collaborator.email === remainingEmail) return
+      const { id } = collaborator
+      cy.request("DELETE", `${BASE_URL}/${id}`)
+    })
+  })
 }
