de63384583ebc48a97563fff84879c9db3c069f7
seaerchin <jiachin@open.gov.sg>
2023-08-08 18:36:49 +0800

    feat(icon): add new draggable icon

---
+++ src/assets/icons/BxDraggable.tsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line import/prefer-default-export
+export const BxDraggable = (
+  props: React.SVGProps<SVGSVGElement>
+): JSX.Element => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <circle cx="6" cy="9" r="2" fill="#BABECB" />
+      <circle cx="12" cy="9" r="2" fill="#BABECB" />
+      <circle cx="18" cy="9" r="2" fill="#BABECB" />
+      <circle cx="6" cy="15" r="2" fill="#BABECB" />
+      <circle cx="12" cy="15" r="2" fill="#BABECB" />
+      <circle cx="18" cy="15" r="2" fill="#BABECB" />
+    </svg>
+  )
+}
