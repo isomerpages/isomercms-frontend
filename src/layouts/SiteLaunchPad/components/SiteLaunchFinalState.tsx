02713243706be839525b62abc248677bcddf620f
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-08-11 18:34:27 +0800

    feat(site launch): add storybook for final state screens (#1407)
    * fix(checklist): fix wrong checklist
    
    * fix(site launch checklist): fix wrongful rebase
    
    * feat(siteLaunch): final state images
    
    * feat(siteLaunch): add failure state for dto+props
    
    * feat(site launch): add failure state screen
    
    * feat(site launch): integrate final screen with site launch pad
    
    * feat(site launch): add storybooks
    
    * fix(site launch): fix copy for failure screen
    
    * feat(sl final state): fix copy
    
    * fix(sl styling): button variant

---
+++ src/layouts/SiteLaunchPad/components/SiteLaunchFinalState.tsx
@@ -0,0 +1,84 @@
+import { HStack, Text, VStack } from "@chakra-ui/react"
+import { Button } from "@opengovsg/design-system-react"
+
+import { useSiteLaunchContext } from "contexts/SiteLaunchContext"
+
+import { SiteLaunchFailureImage, SiteLaunchSuccessImage } from "assets"
+
+const SiteLaunchSuccessState = (): JSX.Element => {
+  return (
+    <>
+      <Text
+        as="h3"
+        textStyle="h3"
+        textColor="base.content.strong"
+        mb="1.5rem"
+        mt="3rem"
+      >
+        We have lift off! Your site has successfully launched.
+      </Text>
+      <SiteLaunchSuccessImage />
+
+      <Text textStyle="body-1" textColor="base.content.default" mt="1.5rem">
+        View your site status from the Dashboard.
+      </Text>
+      <Text textStyle="body-1" textColor="base.content.default" mb="3rem">
+        You can find your site&apos;s DNS records in the settings page.
+      </Text>
+      <HStack spacing="3rem">
+        <Button variant="link" colorScheme="neutral">
+          Back to tasklist
+        </Button>
+        <Button>Visit live site</Button>
+      </HStack>
+    </>
+  )
+}
+
+const SiteLaunchFailureState = (): JSX.Element => {
+  return (
+    <>
+      <Text
+        as="h3"
+        textStyle="h3"
+        textColor="base.content.strong"
+        mb="1.5rem"
+        mt="3rem"
+      >
+        Site failed to launch, please contact Isomer support.
+      </Text>
+      <SiteLaunchFailureImage />
+
+      <Text
+        textStyle="body-1"
+        textColor="base.content.default"
+        mt="1.5rem"
+        textAlign="center"
+      >
+        The most common reason for site launch failure is unknown CloudFront
+        records. Isomer support will get back to you as soon as we can.
+      </Text>
+
+      <Button
+        mt="1.5rem"
+        as="a"
+        href="mailto:support@isomer.gov.sg?subject=Site%20Launch%20Failure&body=Hello%20Isomer%20Support,"
+      >
+        <Text textColor="base.content.inverse" textStyle="subhead-1">
+          Email Isomer support
+        </Text>
+      </Button>
+    </>
+  )
+}
+
+export const SiteLaunchFinalState = (): JSX.Element => {
+  const { siteLaunchStatusProps } = useSiteLaunchContext()
+  const isSuccess = siteLaunchStatusProps?.siteLaunchStatus === "LAUNCHED"
+
+  return (
+    <VStack w="65%">
+      {isSuccess ? <SiteLaunchSuccessState /> : <SiteLaunchFailureState />}
+    </VStack>
+  )
+}
