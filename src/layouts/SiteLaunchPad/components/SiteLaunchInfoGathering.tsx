f382c758ca8848715eae35c95da1e41d51134686
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-08-11 11:40:01 +0800

    feat(sl info gathering): use DS form contrl

--- src/layouts/SiteLaunchPad/components/SiteLaunchInfoGathering.tsx
+++ src/layouts/SiteLaunchPad/components/SiteLaunchInfoGathering.tsx
@@ -8,8 +8,14 @@ import {
   InputGroup,
   InputLeftAddon,
   HStack,
+  FormControl,
 } from "@chakra-ui/react"
-import { Badge, Input, Radio } from "@opengovsg/design-system-react"
+import {
+  Badge,
+  FormErrorMessage,
+  Input,
+  Radio,
+} from "@opengovsg/design-system-react"
 import { useEffect, useState } from "react"
 import { useForm } from "react-hook-form"
 import { BiRightArrowAlt } from "react-icons/bi"
@@ -106,119 +112,132 @@ export const SiteLaunchInfoCollectorBody = ({
 
   return (
     <SiteLaunchPadBody>
-      <Text textStyle="subhead-1">What domain are you launching with?</Text>
-      <InputGroup mt="4" mb="1">
-        <InputLeftAddon>https://</InputLeftAddon>
-        <Input
-          placeholder="isomer.gov.sg"
-          {...register("domain", {
-            required: "Domain is required",
-            validate: (value) => {
-              if (
-                value.startsWith("www.") ||
-                value.startsWith("http://") ||
-                value.startsWith("https://")
-              ) {
-                return "Your domain cannot begin with 'http://', 'https://' or 'www.'"
-              }
+      <FormControl isInvalid={!!errors.domain}>
+        <Text textStyle="subhead-1">What domain are you launching with?</Text>
+        <InputGroup mt="4" mb="1">
+          <InputLeftAddon>https://</InputLeftAddon>
+          <Input
+            placeholder="isomer.gov.sg"
+            {...register("domain", {
+              required: "Domain is required",
+              validate: (value) => {
+                if (
+                  value.startsWith("www.") ||
+                  value.startsWith("http://") ||
+                  value.startsWith("https://")
+                ) {
+                  return "Your domain cannot begin with 'http://', 'https://' or 'www.'"
+                }
 
-              const invalidTld =
-                !value.endsWith(".com") && !value.endsWith(".sg")
-              if (value.endsWith("/") || invalidTld) {
-                return "Your domain must end with '.com' or '.sg'\nCheck that your domain doesn't end with '/'"
-              }
+                const invalidTld =
+                  !value.endsWith(".com") && !value.endsWith(".sg")
+                if (value.endsWith("/") || invalidTld) {
+                  return `Your domain must end with '.com' or '.sg'\nCheck that your domain doesn't end with '/'`
+                }
 
-              if (value.startsWith(".")) {
-                return "Check that your domain is valid and try again"
-              }
-              return true
-            },
-          })}
-        />
-      </InputGroup>
-      {errors.domain && (
-        <Text textStyle="subhead-2">{errors.domain.message}</Text>
-      )}
+                if (value.startsWith(".")) {
+                  return "Check that your domain is valid and try again"
+                }
+                return true
+              },
+            })}
+          />
+        </InputGroup>
+        <FormErrorMessage>
+          <Box>
+            {errors.domain?.message?.split("\n").map((msg) => {
+              return (
+                <Text
+                  textStyle="subhead-2"
+                  textColor="utility.feedback.critical"
+                >
+                  {msg}
+                </Text>
+              )
+            })}
+          </Box>
+        </FormErrorMessage>
 
-      <Text textStyle="subhead-1" mt="1.5rem">
-        What is the nature of the domain you are launching with?
-      </Text>
-      <RadioGroup
-        {...register("nature")}
-        onChange={handleDomainNatureQn}
-        mt="0.25rem"
-        value={selectedDomainNature}
-      >
-        <Radio value={SiteNature.New} mb="0.25rem">
-          <Text textStyle="body-1" color="black">
-            It is a brand new domain
-          </Text>
-        </Radio>
-        <Radio value={SiteNature.Live}>
-          <Text textStyle="body-1" color="black">
-            This domain is currently live or was previously live
-          </Text>
-        </Radio>
-      </RadioGroup>
+        <Text textStyle="subhead-1" mt="1.5rem">
+          What is the nature of the domain you are launching with?
+        </Text>
+        <RadioGroup
+          {...register("nature")}
+          onChange={handleDomainNatureQn}
+          mt="0.25rem"
+          value={selectedDomainNature}
+        >
+          <Radio value={SiteNature.New} mb="0.25rem">
+            <Text textStyle="body-1" color="black">
+              It is a brand new domain
+            </Text>
+          </Radio>
+          <Radio value={SiteNature.Live}>
+            <Text textStyle="body-1" color="black">
+              This domain is currently live or was previously live
+            </Text>
+          </Radio>
+        </RadioGroup>
 
-      {displayWwwQn && (
-        <>
-          <Text textStyle="subhead-1" mt="1.5rem">
-            Do you want to host the both the www and non-www version of your
-            domain?
-          </Text>
-          <Text textStyle="subhead-2">
-            Select &quot;Yes&quot; if you are not sure
-          </Text>
-          <RadioGroup
-            {...register("useWww")}
-            onChange={handleWwwQn}
-            mt="0.25rem"
-            value={wwwSetting}
+        {displayWwwQn && (
+          <>
+            <Text textStyle="subhead-1" mt="1.5rem">
+              Do you want to host the both the www and non-www version of your
+              domain?
+            </Text>
+            <Text textStyle="subhead-2">
+              Select &quot;Yes&quot; if you are not sure
+            </Text>
+            <RadioGroup
+              {...register("useWww")}
+              onChange={handleWwwQn}
+              mt="0.25rem"
+              value={wwwSetting}
+            >
+              <Radio value="true" mb="0.25rem">
+                <HStack>
+                  <Text textStyle="body-1" color="black">
+                    Yes, host both www and non-www version
+                  </Text>
+                  {isWwwRecommended && <RecommendedBadge />}
+                </HStack>
+              </Radio>
+              <Radio value="false">
+                <HStack>
+                  <Text textStyle="body-1" color="black">
+                    No, host only the non-www version
+                  </Text>
+                  {!isWwwRecommended && <RecommendedBadge />}
+                </HStack>
+              </Radio>
+            </RadioGroup>
+          </>
+        )}
+        <Box display="flex" justifyContent="flex-end" mt="2rem">
+          <Button
+            variant="link"
+            mr={4}
+            onClick={() => setPageNumber(SITE_LAUNCH_PAGES.DISCLAIMER)}
+            height="2.75rem"
           >
-            <Radio value="true" mb="0.25rem">
-              <HStack>
-                <Text textStyle="body-1" color="black">
-                  Yes, host both www and non-www version
-                </Text>
-                {isWwwRecommended && <RecommendedBadge />}
-              </HStack>
-            </Radio>
-            <Radio value="false">
-              <HStack>
-                <Text textStyle="body-1" color="black">
-                  No, host only the non-www version
-                </Text>
-                {!isWwwRecommended && <RecommendedBadge />}
-              </HStack>
-            </Radio>
-          </RadioGroup>
-        </>
-      )}
-      <Box display="flex" justifyContent="flex-end" mt="2rem">
-        <Button
-          variant="link"
-          mr={4}
-          onClick={() => setPageNumber(SITE_LAUNCH_PAGES.DISCLAIMER)}
-          height="2.75rem"
-        >
-          <Text textColor="base.content.strong">Cancel</Text>
-        </Button>
-        <Button
-          rightIcon={<Icon as={BiRightArrowAlt} fontSize="1.25rem" />}
-          type="submit"
-          onClick={handleSubmit(onSubmit)}
-          isDisabled={
-            !!errors.domain ||
-            !selectedDomainNature ||
-            !watch("domain") ||
-            !wwwSetting
-          }
-          height="2.75rem"
-        >
-          Next
-        </Button>
-      </Box>
+            <Text textColor="base.content.strong">Cancel</Text>
+          </Button>
+          <Button
+            rightIcon={<Icon as={BiRightArrowAlt} fontSize="1.25rem" />}
+            type="submit"
+            onClick={handleSubmit(onSubmit)}
+            isDisabled={
+              !!errors.domain ||
+              !selectedDomainNature ||
+              !watch("domain") ||
+              !wwwSetting
+            }
+            height="2.75rem"
+          >
+            Next
+          </Button>
+        </Box>
+      </FormControl>
     </SiteLaunchPadBody>
   )
 }
