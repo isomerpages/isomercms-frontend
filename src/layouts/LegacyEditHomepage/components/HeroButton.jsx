b6da2a955617eafad49b82750980a554fd43d527
seaerchin <chin@Ers-MacBook-Pro.local>
2023-08-23 17:13:09 +0800

    fix(legacyedithomepage): add back missing imports

---
+++ src/layouts/LegacyEditHomepage/components/HeroButton.jsx
@@ -0,0 +1,48 @@
+import PropTypes from "prop-types"
+
+import FormContext from "components/Form/FormContext"
+import FormError from "components/Form/FormError"
+import FormTitle from "components/Form/FormTitle"
+import FormField from "components/FormField"
+
+/* eslint
+  react/no-array-index-key: 0
+ */
+
+const HeroButton = ({ button, url, sectionIndex, onFieldChange, errors }) => (
+  <>
+    <FormContext hasError={!!errors.button}>
+      <FormTitle>Hero button</FormTitle>
+      <FormField
+        placeholder="Hero button"
+        id={`section-${sectionIndex}-hero-button`}
+        value={button}
+        onChange={onFieldChange}
+      />
+      <FormError>{errors.button}</FormError>
+    </FormContext>
+    <FormContext hasError={!!errors.url}>
+      <FormTitle>Hero button URL</FormTitle>
+      <FormField
+        placeholder="Insert /page-url or https://"
+        id={`section-${sectionIndex}-hero-url`}
+        value={url}
+        onChange={onFieldChange}
+      />
+      <FormError>{errors.url}</FormError>
+    </FormContext>
+  </>
+)
+
+export default HeroButton
+
+HeroButton.propTypes = {
+  button: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  sectionIndex: PropTypes.number.isRequired,
+  onFieldChange: PropTypes.func.isRequired,
+  errors: PropTypes.shape({
+    button: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+  }).isRequired,
+}
---
+++ src/layouts/LegacyEditHomepage/components/KeyHighlight.jsx
@@ -0,0 +1,109 @@
+import { Button, IconButton } from "@opengovsg/design-system-react"
+import PropTypes from "prop-types"
+
+import FormContext from "components/Form/FormContext"
+import FormError from "components/Form/FormError"
+import FormTitle from "components/Form/FormTitle"
+import FormField from "components/FormField"
+
+import elementStyles from "styles/isomer-cms/Elements.module.scss"
+
+/* eslint
+  react/no-array-index-key: 0
+ */
+
+const KeyHighlight = ({
+  title,
+  description,
+  url,
+  highlightIndex,
+  onFieldChange,
+  shouldDisplay,
+  displayHandler,
+  deleteHandler,
+  errors,
+}) => (
+  <div className={elementStyles.card}>
+    <div className={elementStyles.cardHeader}>
+      <h2>{title}</h2>
+      <IconButton
+        variant="clear"
+        id={`highlight-${highlightIndex}-toggle`}
+        onClick={displayHandler}
+      >
+        <i
+          className={`bx ${
+            shouldDisplay ? "bx-chevron-down" : "bx-chevron-right"
+          }`}
+          id={`highlight-${highlightIndex}-icon`}
+        />
+      </IconButton>
+    </div>
+    {/* Core highlight fields */}
+    {shouldDisplay && (
+      <>
+        <div className={elementStyles.cardContent}>
+          <FormContext isRequired hasError={!!errors.title}>
+            <FormTitle>Highlight title</FormTitle>
+            <FormField
+              placeholder="Highlight title"
+              id={`highlight-${highlightIndex}-title`}
+              value={title}
+              onChange={onFieldChange}
+            />
+            <FormError>{errors.title}</FormError>
+          </FormContext>
+          <FormContext isRequired hasError={!!errors.description}>
+            <FormTitle>Highlight description</FormTitle>
+            <FormField
+              placeholder="Highlight description"
+              id={`highlight-${highlightIndex}-description`}
+              value={description}
+              onChange={onFieldChange}
+            />
+            <FormError>{errors.description}</FormError>
+          </FormContext>
+          <FormContext isRequired hasError={!!errors.url}>
+            <FormTitle>Highlight URL</FormTitle>
+            <FormField
+              placeholder="Insert permalink or external URL"
+              id={`highlight-${highlightIndex}-url`}
+              value={url}
+              onChange={onFieldChange}
+            />
+            <FormError>{errors.url}</FormError>
+          </FormContext>
+        </div>
+        <div className={elementStyles.inputGroup}>
+          <Button
+            id={`highlight-${highlightIndex}-delete`}
+            onClick={deleteHandler}
+            colorScheme="critical"
+            key={`${highlightIndex}-delete`}
+            w="100%"
+          >
+            Delete highlight
+          </Button>
+        </div>
+      </>
+    )}
+  </div>
+)
+
+export default KeyHighlight
+
+KeyHighlight.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  highlightIndex: PropTypes.number.isRequired,
+  onFieldChange: PropTypes.func.isRequired,
+  deleteHandler: PropTypes.func.isRequired,
+  shouldDisplay: PropTypes.bool.isRequired,
+  displayHandler: PropTypes.func.isRequired,
+  errors: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+  }).isRequired,
+}
