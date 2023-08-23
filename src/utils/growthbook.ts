8d7ee9b7577fabf6a48dc5cce73edb248ef6c5a2
Harish V <harish@open.gov.sg>
2023-08-23 21:35:42 +0800

    feat: add default dev mode to be false

--- src/utils/growthbook.ts
+++ src/utils/growthbook.ts
@@ -2,10 +2,10 @@ import { GrowthBook } from "@growthbook/growthbook-react"
 
 const GROWTHBOOK_API_HOST = "https://cdn.growthbook.io"
 
-export const getGrowthBookInstance = (clientKey: string) => {
+export const getGrowthBookInstance = (clientKey: string, isDev = false) => {
   return new GrowthBook({
     apiHost: GROWTHBOOK_API_HOST,
     clientKey,
-    enableDevMode: true, // TODO:remove for prod
+    enableDevMode: isDev, // enable only for local dev
   })
 }
