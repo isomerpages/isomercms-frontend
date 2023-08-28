62dde3d95e9234577e3e2b29f77a2cce860ef991
Harish V <harish@open.gov.sg>
2023-08-28 23:06:11 +0800

    fix: uncomment code

--- cypress/e2e/workspace.spec.ts
+++ cypress/e2e/workspace.spec.ts
@@ -53,139 +53,139 @@ describe("Workspace Pages flow", () => {
     PARSED_EDITED_TEST_FOLDER_WITH_PAGES_TITLE
   )
 
-  // describe("Create page, delete page, edit page settings in Workspace", () => {
-  //   it("Test site workspace page should have unlinked pages section", () => {
-  //     cy.contains("Create page")
-  //   })
-  //   it("Should be able to create a new page with valid title and permalink", () => {
-  //     // Act
-  //     cy.get(".chakra-button")
-  //       .contains("Create page")
-  //       .should("exist")
-  //       .click({ force: true })
-  //     cy.get("#title").clear().type(TEST_PAGE_TITLE)
-  //     cy.get("#permalink").clear().type(TEST_PAGE_PERMALNK)
-  //     cy.contains("Save").click()
-  //     cy.wait(Interceptors.POST)
-  //       .its("request.body")
-  //       .should("deep.equal", {
-  //         content: {
-  //           frontMatter: {
-  //             title: TEST_PAGE_TITLE,
-  //             permalink: TEST_PAGE_PERMALNK,
-  //             description: "",
-  //           },
-  //         },
-  //         newFileName: `${TEST_PAGE_TITLE}.md`,
-  //       })
-  //     // Assert
-  //     // 1. User should be redirected to correct EditPage
-  //     cy.url().should(
-  //       "include",
-  //       `${CMS_BASEURL}/sites/${TEST_REPO_NAME}/editPage/${TEST_PAGE_ENCODED}`
-  //     )
-  //     cy.contains(TEST_PAGE_TITLE).should("exist")
-  //     // 2. If user goes back to the workspace, they should be able to see that the page exists
-  //     cy.contains("Back to Workspace")
-  //       .should("exist")
-  //       .siblings("button")
-  //       .click()
-  //     cy.contains("a", TEST_PAGE_TITLE).should("exist")
-  //   })
-  //   it("Should not be able to create page with invalid title", () => {
-  //     // Arrange
-  //     const INVALID_TEST_PAGE_TITLES = [
-  //       "Ab",
-  //       "Lorem Ipsum<",
-  //       "^Lorem Ipsum",
-  //       "~%Lorem Ipsum",
-  //       "/Lorem Ipsum",
-  //       ";Lorem Ipsum",
-  //       ">Lorem Ipsum",
-  //       "[Lorem Ipsum",
-  //       "]Lorem Ipsum",
-  //     ]
-  //     // Act
-  //     cy.contains("a", "Create page").click({ force: true })
-  //     // Cannot use titles shorter than 4 characters or containing symbols ~!@#$%^&*_+-./\`:;~{}()[]"'<>,?
-  //     INVALID_TEST_PAGE_TITLES.forEach((invalidTitle) => {
-  //       cy.get("#title").clear().type(invalidTitle).blur()
-  //       cy.contains("button", "Save").should("be.disabled")
-  //     })
-  //     // Assert
-  //     // Page title must not already exist
-  //     cy.get("#title").clear().type(TEST_PAGE_TITLE).blur()
-  //     cy.contains("Title is already in use. Please choose a different title.")
-  //     cy.contains("button", "Save").should("be.disabled")
-  //   })
-  //   it("Should not be able to create page with invalid permalink", () => {
-  //     // Arrange
-  //     const INVALID_TEST_PAGE_PERMALINKS = ["/12", "test-", "/abcd?"]
-  //     // Act
-  //     cy.contains("a", "Create page").click({ force: true })
-  //     // Assert
-  //     // Permalink needs to be longer than 4 characters, should start with a slash, and contain alphanumeric characters separated by hyphens and slashes only
-  //     INVALID_TEST_PAGE_PERMALINKS.forEach((invalidPermalink) => {
-  //       cy.get("#permalink").clear().type(invalidPermalink).blur()
-  //       cy.contains("button", "Save").should("be.disabled")
-  //     })
-  //   })
-  //   it("Should be able to edit existing page details with Chinese title and valid permalink", () => {
-  //     // Arrange
-  //     cy.contains("button", TEST_PAGE_TITLE)
-  //       .parent()
-  //       .parent()
-  //       .as("pageCard")
-  //       .should("exist")
-  //     // Act
-  //     // User should be able edit page details
-  //     cy.clickContextMenuItem("@pageCard", "settings")
-  //     cy.get("#title").should("have.value", TEST_PAGE_TITLE)
-  //     cy.get("#title").clear().type(EDITED_TEST_PAGE_TITLE)
-  //     cy.contains("button", "Save").click()
-  //     cy.wait(Interceptors.POST)
-  //     // Assert
-  //     // New page title should be reflected in the Workspace
-  //     cy.contains(EDITED_TEST_PAGE_TITLE).should("exist")
-  //   })
-  //   it("Should be able to edit existing page details with Tamil title and valid permalink", () => {
-  //     // Arrange
-  //     cy.contains("button", EDITED_TEST_PAGE_TITLE)
-  //       .parent()
-  //       .parent()
-  //       .as("pageCard")
-  //       .should("exist")
-  //     // Act
-  //     // User should be able edit page details
-  //     cy.clickContextMenuItem("@pageCard", "settings")
-  //     cy.get("#title").should("have.value", EDITED_TEST_PAGE_TITLE)
-  //     cy.get("#title").clear().type(EDITED_TEST_PAGE_TITLE_2)
-  //     cy.contains("button", "Save").click()
-  //     cy.wait(Interceptors.POST)
-  //     // Asserts
-  //     // Should show modal
-  //     cy.contains(SUCCESSFUL_EDIT_PAGE_TOAST).should("exist")
-  //     // New page title should be reflected in Folders
-  //     cy.contains(EDITED_TEST_PAGE_TITLE_2).should("exist")
-  //   })
-  //   it("Should be able to delete existing page on workspace", () => {
-  //     // Arrange
-  //     // Ensure that the frontend has been updated
-  //     cy.contains(EDITED_TEST_PAGE_TITLE).should("not.exist")
-  //     // Act
-  //     // User should be able to remove the created test page card
-  //     cy.contains("button", EDITED_TEST_PAGE_TITLE_2)
-  //       .parent()
-  //       .parent()
-  //       .as("pageCard")
-  //       .should("exist")
-  //     cy.clickContextMenuItem("@pageCard", "Delete")
-  //     cy.contains("button", "delete").should("exist").click()
-  //     cy.wait(Interceptors.DELETE)
-  //     // Assert
-  //     cy.contains(EDITED_TEST_PAGE_TITLE_2).should("not.exist")
-  //   })
-  // })
+  describe("Create page, delete page, edit page settings in Workspace", () => {
+    it("Test site workspace page should have unlinked pages section", () => {
+      cy.contains("Create page")
+    })
+    it("Should be able to create a new page with valid title and permalink", () => {
+      // Act
+      cy.get(".chakra-button")
+        .contains("Create page")
+        .should("exist")
+        .click({ force: true })
+      cy.get("#title").clear().type(TEST_PAGE_TITLE)
+      cy.get("#permalink").clear().type(TEST_PAGE_PERMALNK)
+      cy.contains("Save").click()
+      cy.wait(Interceptors.POST)
+        .its("request.body")
+        .should("deep.equal", {
+          content: {
+            frontMatter: {
+              title: TEST_PAGE_TITLE,
+              permalink: TEST_PAGE_PERMALNK,
+              description: "",
+            },
+          },
+          newFileName: `${TEST_PAGE_TITLE}.md`,
+        })
+      // Assert
+      // 1. User should be redirected to correct EditPage
+      cy.url().should(
+        "include",
+        `${CMS_BASEURL}/sites/${TEST_REPO_NAME}/editPage/${TEST_PAGE_ENCODED}`
+      )
+      cy.contains(TEST_PAGE_TITLE).should("exist")
+      // 2. If user goes back to the workspace, they should be able to see that the page exists
+      cy.contains("Back to Workspace")
+        .should("exist")
+        .siblings("button")
+        .click()
+      cy.contains("a", TEST_PAGE_TITLE).should("exist")
+    })
+    it("Should not be able to create page with invalid title", () => {
+      // Arrange
+      const INVALID_TEST_PAGE_TITLES = [
+        "Ab",
+        "Lorem Ipsum<",
+        "^Lorem Ipsum",
+        "~%Lorem Ipsum",
+        "/Lorem Ipsum",
+        ";Lorem Ipsum",
+        ">Lorem Ipsum",
+        "[Lorem Ipsum",
+        "]Lorem Ipsum",
+      ]
+      // Act
+      cy.contains("a", "Create page").click({ force: true })
+      // Cannot use titles shorter than 4 characters or containing symbols ~!@#$%^&*_+-./\`:;~{}()[]"'<>,?
+      INVALID_TEST_PAGE_TITLES.forEach((invalidTitle) => {
+        cy.get("#title").clear().type(invalidTitle).blur()
+        cy.contains("button", "Save").should("be.disabled")
+      })
+      // Assert
+      // Page title must not already exist
+      cy.get("#title").clear().type(TEST_PAGE_TITLE).blur()
+      cy.contains("Title is already in use. Please choose a different title.")
+      cy.contains("button", "Save").should("be.disabled")
+    })
+    it("Should not be able to create page with invalid permalink", () => {
+      // Arrange
+      const INVALID_TEST_PAGE_PERMALINKS = ["/12", "test-", "/abcd?"]
+      // Act
+      cy.contains("a", "Create page").click({ force: true })
+      // Assert
+      // Permalink needs to be longer than 4 characters, should start with a slash, and contain alphanumeric characters separated by hyphens and slashes only
+      INVALID_TEST_PAGE_PERMALINKS.forEach((invalidPermalink) => {
+        cy.get("#permalink").clear().type(invalidPermalink).blur()
+        cy.contains("button", "Save").should("be.disabled")
+      })
+    })
+    it("Should be able to edit existing page details with Chinese title and valid permalink", () => {
+      // Arrange
+      cy.contains("button", TEST_PAGE_TITLE)
+        .parent()
+        .parent()
+        .as("pageCard")
+        .should("exist")
+      // Act
+      // User should be able edit page details
+      cy.clickContextMenuItem("@pageCard", "settings")
+      cy.get("#title").should("have.value", TEST_PAGE_TITLE)
+      cy.get("#title").clear().type(EDITED_TEST_PAGE_TITLE)
+      cy.contains("button", "Save").click()
+      cy.wait(Interceptors.POST)
+      // Assert
+      // New page title should be reflected in the Workspace
+      cy.contains(EDITED_TEST_PAGE_TITLE).should("exist")
+    })
+    it("Should be able to edit existing page details with Tamil title and valid permalink", () => {
+      // Arrange
+      cy.contains("button", EDITED_TEST_PAGE_TITLE)
+        .parent()
+        .parent()
+        .as("pageCard")
+        .should("exist")
+      // Act
+      // User should be able edit page details
+      cy.clickContextMenuItem("@pageCard", "settings")
+      cy.get("#title").should("have.value", EDITED_TEST_PAGE_TITLE)
+      cy.get("#title").clear().type(EDITED_TEST_PAGE_TITLE_2)
+      cy.contains("button", "Save").click()
+      cy.wait(Interceptors.POST)
+      // Asserts
+      // Should show modal
+      cy.contains(SUCCESSFUL_EDIT_PAGE_TOAST).should("exist")
+      // New page title should be reflected in Folders
+      cy.contains(EDITED_TEST_PAGE_TITLE_2).should("exist")
+    })
+    it("Should be able to delete existing page on workspace", () => {
+      // Arrange
+      // Ensure that the frontend has been updated
+      cy.contains(EDITED_TEST_PAGE_TITLE).should("not.exist")
+      // Act
+      // User should be able to remove the created test page card
+      cy.contains("button", EDITED_TEST_PAGE_TITLE_2)
+        .parent()
+        .parent()
+        .as("pageCard")
+        .should("exist")
+      cy.clickContextMenuItem("@pageCard", "Delete")
+      cy.contains("button", "delete").should("exist").click()
+      cy.wait(Interceptors.DELETE)
+      // Assert
+      cy.contains(EDITED_TEST_PAGE_TITLE_2).should("not.exist")
+    })
+  })
 
   describe("Create folder, rename folder, and delete folder from Workspace", () => {
     it("Test site workspace page should have folders section", () => {
