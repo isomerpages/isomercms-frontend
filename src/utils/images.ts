ac625de783a2ac4fe525c84d617ab150c4ffcd0a
Alexander Lee <alexander@open.gov.sg>
2023-08-30 16:20:41 +0800

    chore: fix styling

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -2,7 +2,9 @@
  * Util method to retrieve image details from /images folder from the relative file path,
  * e.g. "/images/album%/picture$.jpg" -> { imageDirectory: "images%2Falbum%25", fileName: "picture%24.jpg" }
  */
-export const getImageDetails = (imageLink: string): { fileName: string; imageDirectory: string } => {
+export const getImageDetails = (
+  imageLink: string
+): { fileName: string; imageDirectory: string } => {
   const cleanImagePath = decodeURI(imageLink).replace(/^\//, "")
   const filePathArr = cleanImagePath
     .split("/")
