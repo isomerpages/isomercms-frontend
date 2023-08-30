d33392d56254627e677947832925474f6303222c
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2023-08-30 12:32:09 +0800

    Fix name for FeatureFlaggedContactUs

--- src/layouts/FeatureFlaggedContactUs.tsx
+++ src/layouts/FeatureFlaggedContactUs.tsx
@@ -5,7 +5,7 @@ import { OGP_SITES, TESTING_SITES } from "constants/sites"
 import EditContactUs from "./EditContactUs"
 import LegacyEditContactUs from "./LegacyEditContactUs"
 
-export const FeatureFlaggedHomepage = (): JSX.Element => {
+export const FeatureFlaggedContactUs = (): JSX.Element => {
   const params = useParams<{ siteName: string }>()
   const isOgpSite =
     OGP_SITES.includes(params.siteName) ||
