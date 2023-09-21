48236e7896f9f15d692c367f8f0ccd2ce932ebe7
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2023-09-21 11:36:28 +0800

    feat: introduce new help overlay for add section button

--- src/components/Editable/AddSectionButton.tsx
+++ src/components/Editable/AddSectionButton.tsx
@@ -1,5 +1,20 @@
-import { HStack, Icon, MenuItemProps, Text } from "@chakra-ui/react"
+import {
+  Box,
+  HStack,
+  Icon,
+  MenuItemProps,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverProps,
+  PopoverTrigger,
+  Portal,
+  Text,
+  VStack,
+} from "@chakra-ui/react"
 import { Menu, MenuButtonProps } from "@opengovsg/design-system-react"
+import { PropsWithChildren } from "react"
 import { BiPlus } from "react-icons/bi"
 
 type AddSectionButtonProps = MenuButtonProps & {
@@ -54,5 +69,48 @@ const AddSectionButtonOption = ({
   )
 }
 
+export interface HelpOverlayProps
+  extends Omit<PopoverProps, "children">,
+    PropsWithChildren {
+  title: string
+  description: string
+  image?: JSX.Element
+}
+const HelpOverlay = ({
+  title,
+  description,
+  image,
+  children,
+  ...rest
+}: HelpOverlayProps) => {
+  return (
+    <Popover
+      placement="end"
+      trigger="hover"
+      arrowSize={16}
+      offset={[0, 11]}
+      {...rest}
+    >
+      <PopoverTrigger>
+        <Box>{children}</Box>
+      </PopoverTrigger>
+      {/* Portal is needed to avoid PopoverArrow from jumping around */}
+      <Portal>
+        <PopoverContent p="0.75rem">
+          <PopoverArrow />
+          <PopoverBody>
+            <VStack align="left">
+              {image && <Box pb="1.5rem">{image}</Box>}
+              <Text textStyle="subhead-1">{title}</Text>
+              <Text textStyle="body-1">{description}</Text>
+            </VStack>
+          </PopoverBody>
+        </PopoverContent>
+      </Portal>
+    </Popover>
+  )
+}
+
 AddSectionButton.Option = AddSectionButtonOption
 AddSectionButton.List = Menu.List
+AddSectionButton.HelpOverlay = HelpOverlay
