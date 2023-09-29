05e9d69f72dfe644e0912966265267999862569e
Alexander Lee <alexander@open.gov.sg>
2023-09-29 13:47:56 +0800

    chore: move warning banner to growthbook

--- src/constants/featureFlags.ts
+++ src/constants/featureFlags.ts
@@ -2,4 +2,5 @@ export const FEATURE_FLAGS = {
   STYLING_REVAMP: "styles",
   HOMEPAGE_TEMPLATES: "homepage_new_templates",
   REPO_PRIVATISATION: "repo_privatisation",
+  BANNER: "banner",
 } as const
