4d351f42a796b632257b6afea9ebbb4f783f5584
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2023-10-19 16:10:00 +0800

    fix(navbar): fix race condition when retrieving second level data (#1608)
    * fix(navbar): fix race condition when retrieving second level data
    
    * chore: remove comment

--- src/hooks/directoryHooks/useGetSecondLevelFoldersAndPages.ts
+++ src/hooks/directoryHooks/useGetSecondLevelFoldersAndPages.ts
@@ -1,4 +1,4 @@
-import { useQueries, UseQueryResult } from "react-query"
+import { useQuery, UseQueryResult } from "react-query"
 
 import { DIR_SECOND_LEVEL_DIRECTORIES_KEY } from "constants/queryKeys"
 
@@ -15,27 +15,42 @@ interface OrderedCollection {
   order: string[]
 }
 
+const getSecondLevelFoldersAndPages = ({
+  siteName,
+  collectionsData = [],
+}: SecondLevelFoldersAndPagesParams) => {
+  return Promise.all(
+    collectionsData
+      .filter<DirectoryData>((item): item is DirectoryData =>
+        isDirectoryData(item)
+      )
+      .map(
+        ({ name }) =>
+          DirectoryService.getCollection({
+            siteName,
+            collectionName: name,
+          })
+            .then((data) => ({
+              collectionName: name,
+              order: data.map(({ name: directoryName }) => directoryName),
+            }))
+            .catch((_) => null) as Promise<OrderedCollection | null>
+      )
+  )
+}
+
 export const useGetSecondLevelFoldersAndPages = ({
   siteName,
   collectionsData = [],
-}: SecondLevelFoldersAndPagesParams): UseQueryResult<OrderedCollection[]>[] => {
-  const directoryQueries = collectionsData
-    .filter<DirectoryData>((item): item is DirectoryData =>
-      isDirectoryData(item)
-    )
-    .map(({ name }) => ({
-      queryKey: [DIR_SECOND_LEVEL_DIRECTORIES_KEY, siteName, name],
-      queryFn: () =>
-        DirectoryService.getCollection({
-          siteName,
-          collectionName: name,
-        }).then((data) => ({
-          collectionName: name,
-          order: data.map(({ name: directoryName }) => directoryName),
-        })),
-    }))
-
-  return useQueries<OrderedCollection[]>(directoryQueries) as UseQueryResult<
-    OrderedCollection[]
-  >[]
+}: SecondLevelFoldersAndPagesParams): UseQueryResult<
+  (OrderedCollection | null)[]
+> => {
+  return useQuery<(OrderedCollection | null)[]>(
+    [DIR_SECOND_LEVEL_DIRECTORIES_KEY, ...collectionsData],
+    () => getSecondLevelFoldersAndPages({ siteName, collectionsData }),
+    {
+      retry: false,
+      cacheTime: 0, // We want to refetch data on every page load because file order may have changed
+    }
+  )
 }
