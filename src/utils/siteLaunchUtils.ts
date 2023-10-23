0dc411991950f9bd1b879b7b4525a2dc0be87565
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2023-10-23 10:29:44 +0800

    feat: allow Isomer admins to edit on email-login (#1604)

--- src/utils/siteLaunchUtils.ts
+++ src/utils/siteLaunchUtils.ts
@@ -2,6 +2,8 @@ import { WHITELISTED_REPOS } from "constants/siteLaunch"
 
 import { SiteMemberRole } from "types/collaborators"
 
+import { isAdminUser } from "./collaborators"
+
 const isRepoWhitelistedForSiteLaunch = (siteName: string): boolean => {
   // The name of our sites for our storybooks is "storybook".
   // Therefore, to allow the storybook to use the site launch feature, we need to
@@ -10,10 +12,6 @@ const isRepoWhitelistedForSiteLaunch = (siteName: string): boolean => {
     return true
   }
   return WHITELISTED_REPOS ? WHITELISTED_REPOS.includes(siteName) : false
-}
-
-const isAdminUser = (role?: SiteMemberRole): boolean => {
-  return role === `ADMIN`
 }
 
 export const isSiteLaunchEnabled = (
