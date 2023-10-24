7da8151b3f13bd3b172972360ea8f27bad2c73af
seaerchin <jiachin@open.gov.sg>
2023-10-25 03:52:25 +0800

    refactor(page-preview): use back old chunk

--- src/components/pages/PagePreview.jsx
+++ src/components/pages/PagePreview.jsx
@@ -2,30 +2,50 @@ import { Box } from "@chakra-ui/react"
 import PropTypes from "prop-types"
 import { useParams } from "react-router-dom"
 
+import editorStyles from "styles/isomer-cms/pages/Editor.module.scss"
+
 import CollectionPageTemplate from "templates/pages/CollectionPageTemplate"
 import PageTemplate from "templates/pages/PageTemplate"
 import ResourcePageTemplate from "templates/pages/ResourcePageTemplate"
 
 import { getDecodedParams } from "utils"
 
-const PagePreview = ({ title }) => {
+const PagePreview = ({ title, chunk }) => {
   const params = useParams()
   const pageParams = getDecodedParams(params)
   const { collectionName, resourceRoomName, resourceCategoryName } = pageParams
   const RenderedPreview = () => {
     if (collectionName) {
-      return <CollectionPageTemplate pageParams={pageParams} title={title} />
+      return (
+        <CollectionPageTemplate
+          chunk={chunk}
+          pageParams={pageParams}
+          title={title}
+        />
+      )
     }
 
     if (resourceRoomName && resourceCategoryName) {
-      return <ResourcePageTemplate pageParams={pageParams} title={title} />
+      return (
+        <ResourcePageTemplate
+          chunk={chunk}
+          pageParams={pageParams}
+          title={title}
+        />
+      )
     }
 
-    return <PageTemplate title={title} pageParams={pageParams} />
+    return <PageTemplate chunk={chunk} title={title} pageParams={pageParams} />
   }
 
   return (
-    <Box w="100%" h="100%" bg="white" overflowY="auto">
+    <Box
+      w="100%"
+      h="100%"
+      bg="white"
+      overflowY="auto"
+      className={editorStyles.pageEditorMain}
+    >
       <RenderedPreview />
     </Box>
   )
@@ -33,6 +53,7 @@ const PagePreview = ({ title }) => {
 
 PagePreview.propTypes = {
   title: PropTypes.string.isRequired,
+  chunk: PropTypes.string.isRequired,
 }
 
 export default PagePreview
--- src/templates/pageComponentsV2/PageBody.jsx
+++ src/templates/pageComponentsV2/PageBody.jsx
@@ -1,13 +1,7 @@
-import { useEditorContext } from "contexts/EditorContext"
-
-export const PageBody = () => {
-  const { editor } = useEditorContext()
+export const PageBody = ({ chunk }) => {
   return (
     <div className="col is-8 is-offset-1-desktop is-12-touch print-content page-content-body">
-      <div
-        className="content"
-        dangerouslySetInnerHTML={{ __html: editor?.getHTML() }}
-      />
+      <div className="content" dangerouslySetInnerHTML={{ __html: chunk }} />
     </div>
   )
 }
