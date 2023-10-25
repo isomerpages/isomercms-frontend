3947363ca6594a508f4c46df4126ca70a3e6d69a
seaerchin <jiachin@open.gov.sg>
2023-10-25 12:51:22 +0800

    refactor(tablemenu): use menuitem

--- src/layouts/components/Editor/MenuItem.tsx
+++ src/layouts/components/Editor/MenuItem.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@chakra-ui/react"
 import { IconButton } from "@opengovsg/design-system-react"
 import { MouseEventHandler } from "react"
 import remixiconUrl from "remixicon/fonts/remixicon.symbol.svg"
@@ -8,13 +7,17 @@ interface MenuItemProps {
   title?: string
   action?: MouseEventHandler<HTMLButtonElement>
   isActive?: null | (() => boolean)
+  isRound?: boolean
 }
 
-export default ({ icon, title, action, isActive = null }: MenuItemProps) => (
+export default ({
+  icon,
+  title,
+  action,
+  isRound,
+  isActive = null,
+}: MenuItemProps) => (
   <IconButton
-    // variant="unstyled"
-    type="button"
-    className="menu-item"
     _hover={{ bg: "gray.100" }}
     _active={{ bg: "gray.200" }}
     onClick={action}
@@ -25,6 +28,7 @@ export default ({ icon, title, action, isActive = null }: MenuItemProps) => (
     w="1.75rem"
     p="0.25rem"
     aria-label={title || "divider"}
+    isRound={isRound}
   >
     <svg className="remix" height="1.25rem" width="1.25rem">
       <use xlinkHref={`${remixiconUrl}#ri-${icon}`} />
