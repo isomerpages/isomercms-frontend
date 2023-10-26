5eeea8d6401a046858eab657d6780c89559bff0e
seaerchin <jiachin@open.gov.sg>
2023-10-26 22:41:18 +0800

    feat(editpage): shift modals over to layout

--- src/layouts/EditPage/EditPageLayout.tsx
+++ src/layouts/EditPage/EditPageLayout.tsx
@@ -1,21 +1,36 @@
-import { Flex, Spacer, Button, useDisclosure } from "@chakra-ui/react"
+import {
+  Flex,
+  Spacer,
+  Button,
+  useDisclosure,
+  Box,
+  Code,
+  Text,
+} from "@chakra-ui/react"
 import { AxiosError } from "axios"
-import { PropsWithChildren, useEffect } from "react"
+import DOMPurify from "dompurify"
+import _ from "lodash"
+import { marked } from "marked"
+import { PropsWithChildren, useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 import { Footer } from "components/Footer"
 import { Greyscale } from "components/Greyscale"
 import Header from "components/Header"
 import { OverwriteChangesModal } from "components/OverwriteChangesModal"
+import { WarningModal } from "components/WarningModal"
 
+import { useGetMultipleMediaHook } from "hooks/mediaHooks"
 import { useGetPageHook, useUpdatePageHook } from "hooks/pageHooks"
-import { useGetSiteColorsHook } from "hooks/settingsHooks"
+import { useCspHook, useGetSiteColorsHook } from "hooks/settingsHooks"
 import useRedirectHook from "hooks/useRedirectHook"
 
 import { isWriteActionsDisabled } from "utils/reviewRequests"
 
 import { PageVariant } from "types/pages"
 import { createPageStyleSheet, getDecodedParams } from "utils"
+
+import { sanitiseRawHtml, updateHtmlWithMediaData } from "./utils"
 
 interface EditPageLayoutProps {
   getEditorContent: () => string
@@ -29,12 +44,14 @@ export const EditPageLayout = ({
 }: PropsWithChildren<EditPageLayoutProps>) => {
   const params = useParams<{ siteName: string }>()
   const decodedParams = getDecodedParams(params)
+  const [mediaSrcs, setMediaSrcs] = useState(new Set(""))
+
   const {
     isOpen: isOverwriteOpen,
     onOpen: onOverwriteOpen,
     onClose: onOverwriteClose,
   } = useDisclosure()
-
+  const { data: csp } = useCspHook()
   const {
     mutateAsync: updatePageHandler,
     isLoading: isSavingPage,
@@ -52,8 +69,37 @@ export const EditPageLayout = ({
     onError: () => setRedirectToNotFound(siteName),
   })
   const { data: siteColorsData } = useGetSiteColorsHook(params)
+  const { data: mediaData } = useGetMultipleMediaHook({
+    siteName,
+    mediaSrcs,
+  })
+  const [isContentViolation, setIsContentViolation] = useState(false)
+  const [isXSSViolation, setIsXSSViolation] = useState(false)
+  const {
+    isOpen: isXSSWarningModalOpen,
+    onOpen: onXSSWarningModalOpen,
+    onClose: onXSSWarningModalClose,
+  } = useDisclosure()
 
   const isWriteDisabled = isWriteActionsDisabled(siteName)
+  const editorContent = getEditorContent()
+
+  useEffect(() => {
+    if (!csp || _.isEmpty(csp) || !editorContent) return
+
+    const isLegacyPage = variant === "markdown" || !variant
+    const html = isLegacyPage ? marked.parse(editorContent) : editorContent
+    const { sanitisedHtml } = sanitiseRawHtml(csp, html)
+
+    const {
+      html: processedChunk,
+      isXssViolation,
+      isContentViolation: isCspViolation,
+    } = updateHtmlWithMediaData(mediaSrcs, sanitisedHtml, mediaData)
+
+    setIsXSSViolation(isXssViolation)
+    setIsContentViolation(isCspViolation)
+  }, [mediaData, csp, mediaSrcs, onXSSWarningModalOpen, editorContent, variant])
 
   useEffect(() => {
     if (siteColorsData)
@@ -90,6 +136,51 @@ export const EditPageLayout = ({
         }}
       />
 
+      <WarningModal
+        isOpen={isXSSViolation && isXSSWarningModalOpen}
+        onClose={onXSSWarningModalClose}
+        displayTitle="Warning"
+        // DOMPurify removed object format taken from https://github.com/cure53/DOMPurify/blob/dd63379e6354f66d4689bb80b30cb43a6d8727c2/src/purify.js
+        displayText={
+          <Box>
+            <Text>
+              There is unauthorised JS detected in the following snippet
+              {DOMPurify.removed.length > 1 ? "s" : ""}:
+            </Text>
+            {DOMPurify.removed.map((elem, i) => (
+              <>
+                <br />
+                <Code>{i + 1}</Code>:
+                <Code>
+                  {elem.attribute?.nodeName || elem.element?.outerHTML || elem}
+                </Code>
+              </>
+            ))}
+            <br />
+            <br />
+            Before saving, the editor input will be automatically sanitised to
+            prevent security vulnerabilities.
+            <br />
+            <br />
+            To save the sanitised editor input, press Acknowledge. To return to
+            the editor without sanitising, press Cancel.
+          </Box>
+        }
+      >
+        <Button colorScheme="critical" onClick={onXSSWarningModalClose}>
+          Cancel
+        </Button>
+        <Button
+          onClick={() => {
+            setIsXSSViolation(false)
+            onSave()
+            onXSSWarningModalClose()
+          }}
+        >
+          Acknowledge
+        </Button>
+      </WarningModal>
+
       <Greyscale isActive={isWriteDisabled}>
         <Flex flexDir="column" h="full">
           <Header
@@ -106,7 +197,17 @@ export const EditPageLayout = ({
           </Flex>
           <Spacer />
           <Footer>
-            <Button onClick={onSave} isLoading={isSavingPage}>
+            <Button
+              onClick={() => {
+                if (isXSSViolation) onXSSWarningModalOpen()
+                else onSave()
+              }}
+              // TODO: Add an alert/modal
+              // to warn the user when they violate our csp
+              // so they know why + can take action to remedy
+              isDisabled={isContentViolation}
+              isLoading={isSavingPage}
+            >
               Save
             </Button>
           </Footer>
--- src/layouts/LegacyEditPage.jsx
+++ src/layouts/LegacyEditPage.jsx
@@ -1,7 +1,5 @@
-import { useDisclosure, Text, Box, Code } from "@chakra-ui/react"
-import { Button } from "@opengovsg/design-system-react"
+import { useDisclosure, Box } from "@chakra-ui/react"
 import axios from "axios"
-import DOMPurify from "dompurify"
 import _ from "lodash"
 import { marked } from "marked"
 import PropTypes from "prop-types"
@@ -9,61 +7,20 @@ import { useEffect, useState } from "react"
 
 import MarkdownEditor from "components/pages/MarkdownEditor"
 import PagePreview from "components/pages/PagePreview"
-import { WarningModal } from "components/WarningModal"
 
 import { useGetMultipleMediaHook } from "hooks/mediaHooks"
-import { useGetPageHook, useUpdatePageHook } from "hooks/pageHooks"
-import { useCspHook, useGetSiteColorsHook } from "hooks/settingsHooks"
+import { useGetPageHook } from "hooks/pageHooks"
+import { useCspHook } from "hooks/settingsHooks"
 import useRedirectHook from "hooks/useRedirectHook"
 
-// Isomer utils
-import checkCSP from "utils/cspUtils"
-import { createPageStyleSheet } from "utils/siteColorUtils"
-
-import { getMediaSrcsFromHtml, adjustImageSrcs } from "utils"
+import { getMediaSrcsFromHtml } from "utils"
 
 import "easymde/dist/easymde.min.css"
 import { EditPageLayout } from "./EditPage/EditPageLayout"
+import { sanitiseRawHtml, updateHtmlWithMediaData } from "./EditPage/utils"
 
 // axios settings
 axios.defaults.withCredentials = true
-
-DOMPurify.setConfig({
-  ADD_TAGS: ["iframe", "#comment", "script"],
-  ADD_ATTR: [
-    "allow",
-    "allowfullscreen",
-    "frameborder",
-    "scrolling",
-    "marginheight",
-    "marginwidth",
-    "target",
-    "async",
-  ],
-  // required in case <script> tag appears as the first line of the markdown
-  FORCE_BODY: true,
-})
-DOMPurify.addHook("uponSanitizeElement", (node, data) => {
-  // Allow script tags if it has a src attribute
-  // Script sources are handled by our CSP sanitiser
-  if (
-    data.tagName === "script" &&
-    !(node.hasAttribute("src") && node.innerHTML === "")
-  ) {
-    // Adapted from https://github.com/cure53/DOMPurify/blob/e0970d88053c1c564b6ccd633b4af7e7d9a10375/src/purify.js#L719-L736
-    DOMPurify.removed.push({ element: node })
-    try {
-      node.parentNode.removeChild(node)
-    } catch (e) {
-      try {
-        // eslint-disable-next-line no-param-reassign
-        node.outerHTML = ""
-      } catch (ex) {
-        node.remove()
-      }
-    }
-  }
-})
 
 const EditPage = ({ match }) => {
   const { params, decodedParams } = match
@@ -74,77 +31,28 @@ const EditPage = ({ match }) => {
   const [htmlChunk, setHtmlChunk] = useState("")
 
   // TODO: Migrate the below 4 to the new `EditPageLayout`
-  const [currSha, setCurrSha] = useState("")
+  const [, setCurrSha] = useState("")
   const [hasChanges, setHasChanges] = useState(false)
   const [, setIsContentViolation] = useState(false)
-  const [isXSSViolation, setIsXSSViolation] = useState(false)
+  const [, setIsXSSViolation] = useState(false)
 
-  const {
-    isOpen: isXSSWarningModalOpen,
-    onClose: onXSSWarningModalClose,
-  } = useDisclosure()
-
-  const { onOpen: onOverwriteOpen } = useDisclosure()
+  const { onOpen: onXSSWarningModalOpen } = useDisclosure()
 
   const { setRedirectToNotFound } = useRedirectHook()
 
   const { data: pageData, isLoading: isLoadingPage } = useGetPageHook(params, {
     onError: () => setRedirectToNotFound(siteName),
   })
-  const { mutateAsync: updatePageHandler } = useUpdatePageHook(params, {
-    onError: (err) => {
-      if (err.response.status === 409) onOverwriteOpen()
-    },
-    onSuccess: () => setHasChanges(false),
-  })
 
   const { data: csp } = useCspHook()
-  const { data: siteColorsData } = useGetSiteColorsHook(params)
   const { data: mediaData } = useGetMultipleMediaHook({
     siteName,
     mediaSrcs,
   })
 
-  const updateMediaSrcs = () => {
-    if (!csp || _.isEmpty(csp) || !editorValue) return
-    const html = marked.parse(editorValue)
-    const { sanitisedHtml: CSPSanitisedHtml } = checkCSP(csp, html)
-    const DOMCSPSanitisedHtml = DOMPurify.sanitize(CSPSanitisedHtml)
-    setMediaSrcs(getMediaSrcsFromHtml(DOMCSPSanitisedHtml))
-  }
-
-  const updateHtmlWithMediaData = () => {
-    if (!csp || _.isEmpty(csp) || !editorValue) return
-    const html = marked.parse(editorValue)
-    const {
-      isCspViolation: checkedIsCspViolation,
-      sanitisedHtml: CSPSanitisedHtml,
-    } = checkCSP(csp, html)
-    const DOMCSPSanitisedHtml = DOMPurify.sanitize(CSPSanitisedHtml)
-    const processedChunk = adjustImageSrcs(DOMCSPSanitisedHtml, mediaData || [])
-
-    // Using FORCE_BODY adds a fake <remove></remove>
-    DOMPurify.removed = DOMPurify.removed.filter(
-      (el) => el.element?.tagName !== "REMOVE"
-    )
-
-    setIsXSSViolation(DOMPurify.removed.length > 0)
-    setIsContentViolation(checkedIsCspViolation)
-    setHtmlChunk(processedChunk)
-  }
-
   /** ******************************** */
   /*     useEffects to load data     */
   /** ******************************** */
-
-  useEffect(() => {
-    if (siteColorsData)
-      createPageStyleSheet(
-        siteName,
-        siteColorsData.primaryColor,
-        siteColorsData.secondaryColor
-      )
-  }, [siteColorsData, siteName])
 
   useEffect(() => {
     if (pageData && !hasChanges) {
@@ -158,70 +66,33 @@ const EditPage = ({ match }) => {
       setHasChanges(pageData.content.pageBody.trim() !== editorValue)
   }, [pageData, editorValue])
 
+  // TODO: We need to migrate this over to `EditPageLayout`
   useEffect(() => {
-    updateMediaSrcs()
+    if (!csp || _.isEmpty(csp) || !editorValue) return
+    const html = marked.parse(editorValue)
+
+    const { sanitisedHtml: DOMCSPSanitisedHtml } = sanitiseRawHtml(csp, html)
+    setMediaSrcs(getMediaSrcsFromHtml(DOMCSPSanitisedHtml))
   }, [csp, editorValue])
 
   useEffect(() => {
-    updateHtmlWithMediaData()
-  }, [mediaData, editorValue])
+    if (!csp || _.isEmpty(csp) || !editorValue) return
+    const html = marked.parse(editorValue)
+    const { sanitisedHtml } = sanitiseRawHtml(csp, html)
+
+    const {
+      html: processedChunk,
+      isXssViolation,
+      isContentViolation,
+    } = updateHtmlWithMediaData(mediaSrcs, sanitisedHtml, mediaData)
+
+    setIsXSSViolation(isXssViolation)
+    setIsContentViolation(isContentViolation)
+    setHtmlChunk(processedChunk)
+  }, [mediaData, editorValue, csp, mediaSrcs, onXSSWarningModalOpen])
 
   return (
     <EditPageLayout getEditorContent={() => editorValue}>
-      {/* XSS violation warning modal */}
-      <WarningModal
-        isOpen={isXSSViolation && isXSSWarningModalOpen}
-        onClose={onXSSWarningModalClose}
-        displayTitle="Warning"
-        // DOMPurify removed object format taken from https://github.com/cure53/DOMPurify/blob/dd63379e6354f66d4689bb80b30cb43a6d8727c2/src/purify.js
-        displayText={
-          <Box>
-            <Text>
-              There is unauthorised JS detected in the following snippet
-              {DOMPurify.removed.length > 1 ? "s" : ""}:
-            </Text>
-            {DOMPurify.removed.map((elem, i) => (
-              <>
-                <br />
-                <Code>{i + 1}</Code>:
-                <Code>
-                  {elem.attribute?.nodeName || elem.element?.outerHTML || elem}
-                </Code>
-              </>
-            ))}
-            <br />
-            <br />
-            Before saving, the editor input will be automatically sanitised to
-            prevent security vulnerabilities.
-            <br />
-            <br />
-            To save the sanitised editor input, press Acknowledge. To return to
-            the editor without sanitising, press Cancel.`
-          </Box>
-        }
-      >
-        <Button colorScheme="critical" onClick={onXSSWarningModalClose}>
-          Cancel
-        </Button>
-        <Button
-          onClick={() => {
-            const sanitizedEditorValue = DOMPurify.sanitize(editorValue)
-            setEditorValue(sanitizedEditorValue)
-            setIsXSSViolation(false)
-            updatePageHandler({
-              pageData: {
-                frontMatter: pageData.content.frontMatter,
-                sha: currSha,
-                pageBody: sanitizedEditorValue,
-              },
-            })
-            onXSSWarningModalClose()
-          }}
-        >
-          Acknowledge
-        </Button>
-      </WarningModal>
-
       {/* Editor */}
       <Box w="50%" p="1.25rem" maxH="100%" overflowY="scroll">
         <MarkdownEditor
