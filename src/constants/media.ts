2ecfc2c94db461ae4a846030280c08b8d08ceb05
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2023-10-27 16:39:35 +0800

    feat(media): introduce enhancement to media pages

--- src/constants/media.ts
+++ src/constants/media.ts
@@ -1,1 +1,2 @@
+export const MAX_MEDIA_LEVELS = 3
 export const MEDIA_PAGINATION_SIZE = 15
--- src/layouts/Media/Media.tsx
+++ src/layouts/Media/Media.tsx
@@ -1,13 +1,30 @@
-import { SimpleGrid, Box, Text, Skeleton, Center } from "@chakra-ui/react"
-import { Button, Pagination } from "@opengovsg/design-system-react"
+import {
+  Box,
+  Center,
+  HStack,
+  Icon,
+  SimpleGrid,
+  Skeleton,
+  Spacer,
+  Text,
+  VStack,
+} from "@chakra-ui/react"
+import {
+  Badge,
+  Button,
+  Menu,
+  Pagination,
+  Tooltip,
+} from "@opengovsg/design-system-react"
 import _ from "lodash"
-import { useEffect } from "react"
-import { BiBulb, BiUpload } from "react-icons/bi"
-import { Link, Switch, useRouteMatch, useHistory } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { BiFolderOpen, BiFolderPlus, BiTrash } from "react-icons/bi"
+import { Link, Switch, useHistory, useRouteMatch } from "react-router-dom"
 
 import { Greyscale } from "components/Greyscale"
+import { ImagePreviewCard } from "components/ImagePreviewCard"
 
-import { MEDIA_PAGINATION_SIZE } from "constants/pagination"
+import { MEDIA_PAGINATION_SIZE, MAX_MEDIA_LEVELS } from "constants/media"
 
 import { useGetAllMediaFiles } from "hooks/directoryHooks/useGetAllMediaFiles"
 import { useListMediaFolderFiles } from "hooks/directoryHooks/useListMediaFolderFiles"
@@ -25,22 +42,20 @@ import { ProtectedRouteWithProps } from "routing/ProtectedRouteWithProps"
 
 import { isWriteActionsDisabled } from "utils/reviewRequests"
 
-import {
-  CreateButton,
-  Section,
-  SectionCaption,
-  SectionHeader,
-} from "../components"
+import { EmptyAlbumImage } from "assets"
+
+import { CreateButton } from "../components"
 import { SiteEditLayout } from "../layouts"
 
 import {
-  MediaDirectoryCard,
-  ImagePreviewCard,
+  // ImagePreviewCard,
   FilePreviewCard,
   MediaBreadcrumbs,
+  MediaDirectoryCard,
 } from "./components"
 
 interface MediaLabels {
+  articleLabel: "a" | "an"
   singularMediaLabel: "file" | "image"
   pluralMediaLabel: "files" | "images"
   singularDirectoryLabel: "directory" | "album"
@@ -52,6 +67,7 @@ interface MediaLabels {
 const getMediaLabels = (mediaType: "files" | "images"): MediaLabels => {
   if (mediaType === "files") {
     return {
+      articleLabel: "a",
       singularMediaLabel: "file",
       pluralMediaLabel: "files",
       singularDirectoryLabel: "directory",
@@ -60,6 +76,7 @@ const getMediaLabels = (mediaType: "files" | "images"): MediaLabels => {
   }
 
   return {
+    articleLabel: "an",
     singularMediaLabel: "image",
     pluralMediaLabel: "images",
     singularDirectoryLabel: "album",
@@ -67,7 +84,59 @@ const getMediaLabels = (mediaType: "files" | "images"): MediaLabels => {
   }
 }
 
+const getSubheadText = (
+  countSubdirectories: number,
+  countFiles: number,
+  mediaLabels: MediaLabels,
+  isUngrouped: boolean
+) => {
+  const results = []
+  if (countSubdirectories > 0) {
+    results.push(
+      `${countSubdirectories} ${
+        countSubdirectories === 1
+          ? mediaLabels.singularDirectoryLabel
+          : mediaLabels.pluralDirectoryLabel
+      }`
+    )
+  }
+
+  results.push(
+    `${countFiles} ${isUngrouped ? "ungrouped " : ""}${
+      countFiles === 1
+        ? mediaLabels.singularMediaLabel
+        : mediaLabels.pluralMediaLabel
+    }`
+  )
+
+  return results.join(" and ")
+}
+
 export const Media = (): JSX.Element => {
+  const CreateAlbumButton = () => (
+    <Greyscale isActive={isWriteDisabled}>
+      <CreateButton
+        as={Link}
+        to={
+          mediaDirectoryName.split("%2F").length < MAX_MEDIA_LEVELS
+            ? `${url}/createDirectory`
+            : "#"
+        }
+        isDisabled={mediaDirectoryName.split("%2F").length >= MAX_MEDIA_LEVELS}
+      >
+        {`Create ${singularDirectoryLabel}`}
+      </CreateButton>
+    </Greyscale>
+  )
+
+  const handleSelect = (filePath: string) => {
+    if (selectedImages.includes(filePath)) {
+      setSelectedImages(selectedImages.filter((img) => img !== filePath))
+    } else {
+      setSelectedImages([...selectedImages, filePath])
+    }
+  }
+
   const history = useHistory()
   const [curPage, setCurPage] = usePaginate()
   const { params, path, url } = useRouteMatch<{
@@ -75,7 +144,8 @@ export const Media = (): JSX.Element => {
     mediaRoom: "files" | "images"
     mediaDirectoryName: string
   }>()
-  const { siteName, mediaRoom: mediaType } = params
+  const { siteName, mediaRoom: mediaType, mediaDirectoryName } = params
+  const [selectedImages, setSelectedImages] = useState<string[]>([])
 
   useEffect(() => {
     // NOTE: Because this component is shared between different media types + subfolders,
@@ -118,32 +188,138 @@ export const Media = (): JSX.Element => {
   )
 
   const {
-    singularMediaLabel,
+    articleLabel,
     pluralMediaLabel,
     singularDirectoryLabel,
     pluralDirectoryLabel,
   } = getMediaLabels(mediaType)
+
   const isWriteDisabled = isWriteActionsDisabled(siteName)
+  const countSubdirectories =
+    mediaFolderSubdirectories?.directories?.length || 0
+  const countFiles = mediaFolderFiles?.total || 0
+  const isUngrouped = !mediaDirectoryName.includes("%2F")
 
   return (
     <>
       <SiteEditLayout overflow="hidden">
-        <Section>
-          <Box>
-            <Text as="h4" textStyle="h4">
-              {_.upperFirst(mediaType)}
-            </Text>
-            <MediaBreadcrumbs />
-          </Box>
-        </Section>
-        <Section>
-          <SectionHeader label={_.upperFirst(pluralDirectoryLabel)}>
-            <Greyscale isActive={isWriteDisabled}>
-              <CreateButton as={Link} to={`${url}/createDirectory`}>
-                {`Create ${singularDirectoryLabel}`}
-              </CreateButton>
-            </Greyscale>
-          </SectionHeader>
+        <VStack spacing="2rem" w="100%">
+          {/* Header section */}
+          <HStack w="100%">
+            <VStack spacing="0.5rem" align="left" w="40%">
+              <MediaBreadcrumbs />
+              <Text as="h4" textStyle="h4">
+                All {mediaType}
+              </Text>
+              <Text textStyle="subhead-2" mt="0.25rem">
+                {getSubheadText(
+                  countSubdirectories,
+                  countFiles,
+                  getMediaLabels(mediaType),
+                  isUngrouped
+                )}
+              </Text>
+            </VStack>
+            {files &&
+              mediaFolderFiles &&
+              (countSubdirectories !== 0 ||
+                countFiles !== 0 ||
+                selectedImages.length > 0) && (
+                <>
+                  <Spacer />
+                  {selectedImages.length > 0 ? (
+                    <>
+                      <Box mt="auto">
+                        <Button
+                          variant="clear"
+                          color="base.content.strong"
+                          onClick={() => setSelectedImages([])}
+                        >
+                          Deselect all
+                        </Button>
+                      </Box>
+                      <Box mt="auto">
+                        <Menu isStretch={false}>
+                          <Menu.Button
+                            variant="clear"
+                            colorScheme="slate"
+                            sx={{
+                              color: "base.content.strong",
+                              _hover: {
+                                backgroundColor:
+                                  "interaction.tinted.main.hover",
+                              },
+                              _active: {
+                                backgroundColor:
+                                  "interaction.tinted.main.active",
+                              },
+                            }}
+                          >
+                            Edit selected
+                            <Badge
+                              borderRadius="3.125rem"
+                              bgColor="interaction.sub-subtle.default"
+                              color="interaction.sub.default"
+                              ml="0.5rem"
+                            >
+                              {selectedImages.length}
+                            </Badge>
+                          </Menu.Button>
+                          <Menu.List>
+                            <Menu.Item>
+                              <Icon
+                                as={BiFolderOpen}
+                                mr="1rem"
+                                fontSize="1.25rem"
+                              />
+                              Move images to album
+                            </Menu.Item>
+                            <Menu.Item>
+                              <Icon
+                                as={BiFolderPlus}
+                                mr="1rem"
+                                fontSize="1.25rem"
+                              />
+                              Create new album with images
+                            </Menu.Item>
+                            <Menu.Item color="interaction.critical.default">
+                              <Icon as={BiTrash} mr="1rem" fontSize="1.25rem" />
+                              Delete all
+                            </Menu.Item>
+                          </Menu.List>
+                        </Menu>
+                      </Box>
+                    </>
+                  ) : (
+                    <>
+                      <Box mt="auto">
+                        {mediaDirectoryName.split("%2F").length >=
+                        MAX_MEDIA_LEVELS ? (
+                          <Tooltip
+                            label={`You can only add up to ${MAX_MEDIA_LEVELS} levels of ${pluralMediaLabel}`}
+                            hasArrow
+                            gutter={16}
+                          >
+                            <CreateAlbumButton />
+                          </Tooltip>
+                        ) : (
+                          <CreateAlbumButton />
+                        )}
+                      </Box>
+                      <Box mt="auto">
+                        <Greyscale isActive={isWriteDisabled}>
+                          <Button as={Link} to={`${url}/createMedia`}>
+                            {`Upload ${pluralMediaLabel}`}
+                          </Button>
+                        </Greyscale>
+                      </Box>
+                    </>
+                  )}
+                </>
+              )}
+          </HStack>
+
+          {/* Subdirectories section */}
           <Skeleton
             w="100%"
             h={
@@ -157,77 +333,92 @@ export const Media = (): JSX.Element => {
               })}
             </SimpleGrid>
           </Skeleton>
-        </Section>
-        <Section>
-          <Box w="100%">
-            <SectionHeader label={`Ungrouped ${pluralMediaLabel}`}>
-              <Greyscale isActive={isWriteDisabled}>
-                <Button
-                  as={Link}
-                  to={`${url}/createMedia`}
-                  leftIcon={<BiUpload fontSize="1.5rem" />}
-                  variant="outline"
-                >
-                  {`Upload ${singularMediaLabel}`}
-                </Button>
-              </Greyscale>
-            </SectionHeader>
-            <SectionCaption label="PRO TIP: " icon={BiBulb}>
-              Upload {pluralMediaLabel} here to link to them in pages and
-              resources. The maximum {singularMediaLabel} size allowed is 5MB.{" "}
-              <br />
-              For {pluralMediaLabel} other than
-              {mediaType === "images"
-                ? ` 'png', 'jpg', '.jpeg', 'gif', 'tif', '.tiff', 'bmp', 'ico', 'svg'`
-                : ` 'pdf'`}
-              , please use
-              <Link to={{ pathname: `https://go.gov.sg` }} target="_blank">
-                {" "}
-                https://go.gov.sg{" "}
-              </Link>{" "}
-              to upload and link them to your Isomer site.
-            </SectionCaption>
-          </Box>
+
+          {/* Media section */}
           <Skeleton
             w="100%"
             h={isListMediaFilesLoading ? "4.5rem" : "fit-content"}
             isLoaded={!isListMediaFilesLoading}
+            mt="0.25rem"
           >
-            <SimpleGrid columns={3} spacing="1.5rem" w="100%">
-              {files.map(({ data, isLoading }) => {
-                return (
-                  // NOTE: Inner skeleton here is to allow for progressive load.
-                  // We show each individual card in a loading state and
-                  // allow them to fill in when possible.
-                  <Skeleton
-                    w="100%"
-                    h={isLoading ? "4.5rem" : "fit-content"}
-                    isLoaded={!isLoading}
-                  >
-                    {data && mediaType === "images" ? (
-                      <ImagePreviewCard
-                        name={data.name}
-                        mediaUrl={data.mediaUrl}
-                      />
-                    ) : (
-                      data && <FilePreviewCard name={data.name} />
-                    )}
-                  </Skeleton>
-                )
-              })}
-            </SimpleGrid>
-            {files && mediaFolderFiles && mediaFolderFiles?.total > 0 && (
-              <Center mt="1rem">
-                <Pagination
-                  totalCount={mediaFolderFiles.total}
-                  pageSize={MEDIA_PAGINATION_SIZE}
-                  currentPage={curPage}
-                  onPageChange={(page) => setCurPage(page)}
-                />
+            {files &&
+            mediaFolderFiles &&
+            countSubdirectories === 0 &&
+            countFiles === 0 ? (
+              <Center mt="5.75rem">
+                <VStack spacing={0}>
+                  <EmptyAlbumImage width="16.25rem" />
+                  <Text textStyle="subhead-1" mt="2.25rem">
+                    {_.upperFirst(pluralMediaLabel)}{" "}
+                    {mediaDirectoryName.split("%2F").length <
+                      MAX_MEDIA_LEVELS && ` and ${pluralDirectoryLabel}`}{" "}
+                    you add will appear here.
+                  </Text>
+                  <Text textStyle="body-2" mt="0.25rem">
+                    Upload {pluralMediaLabel} to link them on your pages.
+                  </Text>
+                  <Greyscale isActive={isWriteDisabled}>
+                    <Button as={Link} to={`${url}/createMedia`} mt="2rem">
+                      {`Upload ${pluralMediaLabel}`}
+                    </Button>
+                  </Greyscale>
+                  {mediaDirectoryName.split("%2F").length <
+                    MAX_MEDIA_LEVELS && (
+                    <Greyscale isActive={isWriteDisabled}>
+                      <Button
+                        as={Link}
+                        to={`${url}/createDirectory`}
+                        mt="1rem"
+                        variant="clear"
+                      >
+                        {`Or, create ${articleLabel} ${singularDirectoryLabel}`}
+                      </Button>
+                    </Greyscale>
+                  )}
+                </VStack>
               </Center>
+            ) : (
+              <>
+                <SimpleGrid columns={3} spacing="1.5rem" w="100%">
+                  {files.map(({ data, isLoading }) => {
+                    return (
+                      // NOTE: Inner skeleton here is to allow for progressive load.
+                      // We show each individual card in a loading state and
+                      // allow them to fill in when possible.
+                      <Skeleton
+                        w="100%"
+                        h={isLoading ? "4.5rem" : "fit-content"}
+                        isLoaded={!isLoading}
+                      >
+                        {data && mediaType === "images" ? (
+                          <ImagePreviewCard
+                            name={data.name}
+                            addedTime={data.addedTime}
+                            mediaUrl={data.mediaUrl}
+                            isSelected={selectedImages.includes(data.mediaPath)}
+                            onCheck={() => handleSelect(data.mediaPath)}
+                          />
+                        ) : (
+                          data && <FilePreviewCard name={data.name} />
+                        )}
+                      </Skeleton>
+                    )
+                  })}
+                </SimpleGrid>
+                {files && mediaFolderFiles && countFiles !== 0 && (
+                  <Center mt="3rem">
+                    <Pagination
+                      totalCount={mediaFolderFiles?.total || 0}
+                      pageSize={MEDIA_PAGINATION_SIZE}
+                      currentPage={curPage}
+                      onPageChange={(page) => setCurPage(page)}
+                    />
+                  </Center>
+                )}
+              </>
             )}
           </Skeleton>
-        </Section>
+        </VStack>
       </SiteEditLayout>
       <Switch>
         <ProtectedRouteWithProps
