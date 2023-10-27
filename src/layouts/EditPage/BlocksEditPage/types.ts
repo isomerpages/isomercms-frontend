f6da270957bfd08ea891c7b381a76828917f1b03
seaerchin <jiachin@open.gov.sg>
2023-10-27 15:38:05 +0800

    feat(blocks): add constants and types

---
+++ src/layouts/EditPage/BlocksEditPage/types.ts
@@ -0,0 +1,17 @@
+import { Editor, Extensions } from "@tiptap/core"
+import { IconType } from "react-icons"
+
+export interface Block {
+  content: string
+  extensions: Extensions
+  variant: BlockVariant
+}
+
+export interface BlockAddView {
+  title: string
+  description: string
+  icon: IconType
+  variant: BlockVariant
+}
+
+export type BlockVariant = "text" | "image" | "embed" | "table"
