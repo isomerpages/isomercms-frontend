9d05400afe85c1844f981d11c5c24c36dec0de7f
seaerchin <jiachin@open.gov.sg>
2023-10-29 20:44:37 +0800

    feat(views): add view and trim

--- src/layouts/components/Editor/extensions/Draggable/DraggableView.tsx
+++ src/layouts/components/Editor/extensions/Draggable/DraggableView.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Flex } from "@chakra-ui/react"
 import { Tag } from "@opengovsg/design-system-react"
 import { NodeViewContent, NodeViewProps, NodeViewWrapper } from "@tiptap/react"
 
@@ -7,7 +7,6 @@ import { useEditorContext } from "contexts/EditorContext"
 import { BxDraggableVertical } from "assets"
 
 const draggableStyles = {
-  display: "flex",
   padding: "0.5rem",
   margin: "0.5rem 0",
   borderRadius: "0.5rem",
@@ -21,23 +20,25 @@ export const DraggableView = ({ node }: NodeViewProps) => {
 
   return editor.isEditable ? (
     <Box as={NodeViewWrapper} {...draggableStyles}>
-      <Box
-        alignSelf="center"
-        // variant="clear"
-        cursor="grab"
-        aria-label="drag item"
-        mr="0.5rem"
-        contentEditable="false"
-        draggable
-        data-drag-handle
-        // icon={<BxDraggableVertical />}
-      >
-        <BxDraggableVertical />
-      </Box>
       <Box w="100%">
-        <Tag _hover={{}} _active={{}} mb="0.25rem">
-          {node.child(0).type.name}
-        </Tag>
+        <Flex flexDir="row" alignItems="center">
+          <Box
+            alignSelf="center"
+            // variant="clear"
+            cursor="grab"
+            aria-label="drag item"
+            mr="0.5rem"
+            contentEditable="false"
+            draggable
+            data-drag-handle
+            // icon={<BxDraggableVertical />}
+          >
+            <BxDraggableVertical />
+          </Box>
+          <Tag _hover={{}} _active={{}}>
+            {node.child(0).type.name}
+          </Tag>
+        </Flex>
         <NodeViewContent />
       </Box>
     </Box>
