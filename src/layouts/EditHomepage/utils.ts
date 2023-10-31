f27ae2e73720286fe9d83f40e02777b75b3ca46a
seaerchin <44049504+seaerchin@users.noreply.github.com>
2023-10-31 16:59:01 +0800

    feat(blocks): rollback handler (#1603)
    * feat(frontmatter): add rollbackhandler
    
    * fix(edithomepage): use new hook
    
    * refactor(edithomepage): use new hook
    
    * refactor(edithomepage): shift to new hook for features
    
    * fix(featureflags): change derived data
    
    * refactor(herobody): check for enabled change to function
    
    * refactor(featureflag): update namings
    
    * refactor(herobody): use feature key

--- src/layouts/EditHomepage/utils.ts
+++ src/layouts/EditHomepage/utils.ts
@@ -1,7 +1,13 @@
 import _ from "lodash"
 
+import { NUM_DEFAULT_HOMEPAGE_BLOCKS } from "constants/featureFlags"
+
 export const getErrorValues = (
   obj: Record<string, string>
 ): Record<string, string> => {
   return _.mapValues(obj, () => "")
+}
+
+export const getShouldShowNewLayouts = (blocks: unknown[]) => {
+  return blocks.length > NUM_DEFAULT_HOMEPAGE_BLOCKS
 }
