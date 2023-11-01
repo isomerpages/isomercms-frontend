b3e86ef48994f90f1f1cebf53ab9bf393815f76e
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2023-11-01 17:54:14 +0800

    feat(media): add delete media modal and story

---
+++ src/components/DeleteMediaModal/DeleteMediaModal.tsx
@@ -0,0 +1,157 @@
+import {
+  Box,
+  HStack,
+  Icon,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Tr,
+  VStack,
+} from "@chakra-ui/react"
+import {
+  Button,
+  Checkbox,
+  ModalCloseButton,
+} from "@opengovsg/design-system-react"
+import { useState } from "react"
+import { BiImage } from "react-icons/bi"
+
+import { LoadingButton } from "components/LoadingButton"
+
+import { MediaLabels, SelectedMediaDto } from "types/media"
+
+interface DeleteMediaModalProps {
+  selectedMedia: SelectedMediaDto[]
+  mediaLabels: MediaLabels
+  isWriteDisabled: boolean | undefined
+  isOpen: boolean
+  onClose: () => void
+  onProceed: (selectedMedia: SelectedMediaDto[]) => void
+}
+
+export const DeleteMediaModal = ({
+  selectedMedia,
+  mediaLabels,
+  isWriteDisabled,
+  isOpen,
+  onClose,
+  onProceed,
+}: DeleteMediaModalProps): JSX.Element => {
+  const { singularMediaLabel, pluralMediaLabel } = mediaLabels
+  const [isDeleteChecked, setIsDeleteChecked] = useState(false)
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalCloseButton />
+
+        {selectedMedia.length === 1 && (
+          <>
+            <ModalHeader>
+              <Text as="h4" textStyle="h4">
+                Delete {selectedMedia[0].filePath.split("/").pop()}?
+              </Text>
+            </ModalHeader>
+
+            <ModalBody mt="1rem">
+              Are you sure you want to delete this image? If you used this image
+              on any page, site visitors may see a broken image. This cannot be
+              undone.
+            </ModalBody>
+          </>
+        )}
+
+        {selectedMedia.length > 1 && (
+          <>
+            <ModalHeader>
+              <Text as="h4" textStyle="h4">
+                Delete {selectedMedia.length} {pluralMediaLabel}?
+              </Text>
+            </ModalHeader>
+
+            <ModalBody mt="1rem">
+              <VStack spacing="1.5rem">
+                <Text as="p" textStyle="body-1">
+                  Are you sure you want to delete {selectedMedia.length}{" "}
+                  {pluralMediaLabel}?
+                  <br />
+                  <br />
+                  If you used these {pluralMediaLabel} on any page, site
+                  visitors may see a broken {singularMediaLabel}. This cannot be
+                  undone.
+                </Text>
+
+                <Box w="100%">
+                  <Text textStyle="subhead-1">Selected images</Text>
+                  <Text textStyle="caption-2" mt="0.25rem">
+                    {selectedMedia.length} {pluralMediaLabel} will be deleted
+                  </Text>
+                  <TableContainer
+                    mt="0.75rem"
+                    overflowY="scroll"
+                    maxH="14.5rem"
+                  >
+                    <Table variant="simple">
+                      <Tbody borderTop="1px" borderColor="gray.100">
+                        {selectedMedia.map((selectedMediaData) => (
+                          <Tr key={selectedMediaData.filePath}>
+                            <Td w="2.75rem">
+                              <Icon as={BiImage} fontSize="1rem" />
+                            </Td>
+                            <Td>
+                              <Text textStyle="caption-2" noOfLines={1}>
+                                {selectedMediaData.filePath.split("/").pop()}
+                              </Text>
+                            </Td>
+                            <Td w="7rem">
+                              <Text textStyle="caption-2">
+                                {selectedMediaData.size}
+                              </Text>
+                            </Td>
+                          </Tr>
+                        ))}
+                      </Tbody>
+                    </Table>
+                  </TableContainer>
+                </Box>
+              </VStack>
+            </ModalBody>
+          </>
+        )}
+
+        <ModalFooter>
+          <HStack w="100%" spacing={4} justifyContent="flex-end" mt="0.5rem">
+            <Checkbox onChange={(e) => setIsDeleteChecked(e.target.checked)}>
+              Yes, delete{" "}
+              {selectedMedia.length === 1
+                ? singularMediaLabel
+                : `all ${selectedMedia.length} ${pluralMediaLabel}`}
+            </Checkbox>
+            <Button variant="clear" colorScheme="neutral" onClick={onClose}>
+              Cancel
+            </Button>
+            <LoadingButton
+              colorScheme="critical"
+              onClick={() => onProceed(selectedMedia)}
+              isDisabled={isWriteDisabled || !isDeleteChecked}
+            >
+              Delete{" "}
+              {selectedMedia.length === 1
+                ? singularMediaLabel
+                : pluralMediaLabel}
+            </LoadingButton>
+          </HStack>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
--- src/types/media.ts
+++ src/types/media.ts
@@ -2,3 +2,17 @@ export interface MultipleMediaParams {
   siteName: string
   mediaSrcs: Set<string>
 }
+
+export interface MediaLabels {
+  articleLabel: "a" | "an"
+  singularMediaLabel: "file" | "image"
+  pluralMediaLabel: "files" | "images"
+  singularDirectoryLabel: "directory" | "album"
+  pluralDirectoryLabel: "directories" | "albums"
+}
+
+export interface SelectedMediaDto {
+  filePath: string
+  size: number
+  sha: string
+}
