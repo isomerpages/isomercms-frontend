8524e38b0ae96562f801ffbbecd9f318bb7468e4
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-11-21 15:55:53 +0800

    chore(image): rm redundant overload

--- src/layouts/components/Editor/extensions/Image/Image.tsx
+++ src/layouts/components/Editor/extensions/Image/Image.tsx
@@ -4,14 +4,6 @@ import { ReactNodeViewRenderer } from "@tiptap/react"
 import { ImageView } from "./ImageView"
 
 export const IsomerImage = Image.extend({
-  parseHTML() {
-    return [
-      {
-        tag: "img",
-      },
-    ]
-  },
-
   renderHTML({ HTMLAttributes }) {
     return [
       "div",
