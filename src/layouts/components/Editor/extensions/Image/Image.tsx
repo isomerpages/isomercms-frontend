3cd940ceb1affbab0efffc228b45157cb36841fe
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-11-21 15:14:06 +0800

    fix(wrapAroundDiv): wrap the img ard a div... to prevent the p to be rendered

--- src/layouts/components/Editor/extensions/Image/Image.tsx
+++ src/layouts/components/Editor/extensions/Image/Image.tsx
@@ -4,6 +4,18 @@ import { ReactNodeViewRenderer } from "@tiptap/react"
 import { ImageView } from "./ImageView"
 
 export const IsomerImage = Image.extend({
+  parseHTML() {
+    return [
+      {
+        tag: "img",
+      },
+    ]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ["div", this.options.HTMLAttributes, ["img", HTMLAttributes]]
+  },
+
   addNodeView() {
     return ReactNodeViewRenderer(ImageView)
   },
