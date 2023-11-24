64eb37dc6e28746f85789234f0c5e457d298b9e9
Alexander Lee <alexander@open.gov.sg>
2023-11-24 17:09:50 +0800

    feat: select modal revamp for existing functionality

--- src/components/media/MediasSelectModal.jsx
+++ src/components/media/MediasSelectModal.jsx
@@ -16,6 +16,13 @@ import {
   HStack,
   BreadcrumbItem,
   BreadcrumbLink,
+  Tab,
+  Tabs,
+  TabList,
+  TabPanels,
+  TabPanel,
+  Grid,
+  GridItem,
 } from "@chakra-ui/react"
 import {
   Button,
@@ -66,7 +73,12 @@ const MediasSelectModal = ({
   const { params } = useRouteMatch()
   const { fileName } = params
 
-  const { singularMediaLabel, pluralMediaLabel } = getMediaLabels(mediaType)
+  const {
+    singularMediaLabel,
+    pluralMediaLabel,
+    articleLabel,
+    singularDirectoryLabel,
+  } = getMediaLabels(mediaType)
 
   const { mediaRoom, mediaDirectoryName } = queryParams
 
@@ -148,135 +160,207 @@ const MediasSelectModal = ({
               <ModalCloseButton onClick={onClose} position="static" />
             </HStack>
             <Text textStyle="body-1">
-              Choose from your {pluralMediaLabel} or upload a new{" "}
-              {singularMediaLabel}. You can organise your {pluralMediaLabel} in
-              Workspace &gt; {titleCase(pluralMediaLabel)}.
+              You can choose from your {pluralMediaLabel}, upload a new{" "}
+              {singularMediaLabel}, or add {articleLabel} {pluralMediaLabel}.
+              You can organise your {pluralMediaLabel} in Workspace &gt;{" "}
+              {titleCase(pluralMediaLabel)}.
             </Text>
-            <Flex w="100%" justifyContent="space-between">
-              {/* Search medias */}
-              <Box>
-                <Searchbar
-                  isExpanded
-                  onSearch={(val) => setSearchedValue(val)}
-                  placeholder="Press enter to search"
-                />
-              </Box>
-              {/* Upload medias */}
-              <Button
-                variant="inverse"
-                color="interaction.main.default"
-                onClick={onUpload}
-              >
-                Upload new {singularMediaLabel}
-              </Button>
-            </Flex>
           </VStack>
         </ModalHeader>
         <ModalBody>
-          <Breadcrumb pb="1.25rem">
-            {queryParams.mediaDirectoryName
-              ? queryParams.mediaDirectoryName.split("%2F").map((dir, idx) => (
-                  <BreadcrumbItem
-                    isCurrentPage={
-                      idx ===
-                      queryParams.mediaDirectoryName.split("%2F").length - 1
-                    }
+          <Tabs width="100%">
+            <TabList>
+              <Tab>Choose from {pluralMediaLabel}</Tab>
+              <Tab>Add by url</Tab>
+            </TabList>
+            <TabPanels pt="2rem" minHeight="16.5rem">
+              <TabPanel>
+                <Grid h="100%" templateColumns="15.25rem 1fr">
+                  <GridItem
+                    borderRight="1px solid"
+                    borderColor="base.divider.medium"
                   >
-                    <BreadcrumbLink
-                      onClick={(e) => {
-                        e.preventDefault()
-                        setQueryParams((prevState) => ({
-                          ...prevState,
-                          mediaDirectoryName: getMediaDirectoryName(
-                            queryParams.mediaDirectoryName,
-                            { end: idx + 1 }
-                          ),
-                        }))
-                      }}
+                    <Skeleton
+                      w="100%"
+                      h={
+                        isListMediaFolderSubdirectoriesLoading
+                          ? "4.5rem"
+                          : "fit-content"
+                      }
+                      isLoaded={!isListMediaFolderSubdirectoriesLoading}
+                      pb="2.25rem"
+                      pr="1.25rem"
                     >
-                      {deslugifyDirectory(dir)}
-                    </BreadcrumbLink>
-                  </BreadcrumbItem>
-                ))
-              : null}
-          </Breadcrumb>
-          <div
-            className={`${mediaStyles.mediaCards} justify-content-center pt-3 pl-2`}
-          >
-            <Skeleton
-              w="100%"
-              h={
-                isListMediaFolderSubdirectoriesLoading
-                  ? "4.5rem"
-                  : "fit-content"
-              }
-              isLoaded={!isListMediaFolderSubdirectoriesLoading}
-              pb="2.25rem"
-            >
-              <SimpleGrid w="100%" columns={3} spacing="1.5rem">
-                {/* Directories */}
-                {filteredDirectories.map((dir) => (
-                  <MediaDirectoryCard
-                    title={dir.name}
-                    onClick={() => {
-                      onMediaSelect("")
-                      setQueryParams((prevState) => {
-                        return {
-                          ...prevState,
-                          mediaDirectoryName: `${prevState.mediaDirectoryName}%2F${dir.name}`,
-                        }
-                      })
-                    }}
-                    isMenuNeeded={false}
-                  />
-                ))}
-              </SimpleGrid>
-            </Skeleton>
-
-            <Skeleton
-              w="100%"
-              h={isListMediaFilesLoading ? "4.5rem" : "fit-content"}
-              isLoaded={!isListMediaFilesLoading}
-            >
-              <SimpleGrid w="100%" columns={3} spacing="2.5%">
-                {files &&
-                  files
-                    .filter(({ data }) => filteredMedias.includes(data?.name))
-                    .map(({ data, isLoading }) => (
-                      <Skeleton
+                      {/* Directories */}
+                      {filteredDirectories.map((dir) => (
+                        <MediaDirectoryCard
+                          title={dir.name}
+                          onClick={() => {
+                            onMediaSelect("")
+                            setQueryParams((prevState) => {
+                              return {
+                                ...prevState,
+                                mediaDirectoryName: `${prevState.mediaDirectoryName}%2F${dir.name}`,
+                              }
+                            })
+                          }}
+                          isMenuNeeded={false}
+                        />
+                      ))}
+                    </Skeleton>
+                  </GridItem>
+                  <GridItem>
+                    <Box pl="1.25rem">
+                      <Flex
                         w="100%"
-                        h={isLoading ? "4.5rem" : "fit-content"}
-                        isLoaded={!isLoading}
-                        key={data.name}
+                        justifyContent="space-between"
+                        pb="1.25rem"
                       >
-                        {mediaType === "images" ? (
-                          <ImagePreviewCard
-                            name={data.name}
-                            addedTime={data.addedTime}
-                            mediaUrl={data.mediaUrl}
-                            isSelected={
-                              data.name === watch("selectedMedia")?.name
-                            }
-                            onClick={() => onMediaSelect(data)}
-                            isMenuNeeded={false}
+                        {/* Search medias */}
+                        <Box>
+                          <Searchbar
+                            isExpanded
+                            onSearch={(val) => setSearchedValue(val)}
+                            placeholder="Press enter to search"
                           />
-                        ) : (
-                          <FilePreviewCard
-                            name={data.name}
-                            addedTime={data.addedTime}
-                            mediaUrl={data.mediaUrl}
-                            isSelected={
-                              data.name === watch("selectedMedia")?.name
-                            }
-                            onClick={() => onMediaSelect(data)}
-                            isMenuNeeded={false}
-                          />
-                        )}
-                      </Skeleton>
-                    ))}
-              </SimpleGrid>
-            </Skeleton>
-          </div>
+                        </Box>
+                        {/* Upload medias */}
+                        <Button
+                          variant="inverse"
+                          color="interaction.main.default"
+                          onClick={onUpload}
+                        >
+                          Upload new {singularMediaLabel}
+                        </Button>
+                      </Flex>
+                      <Breadcrumb pb="1.25rem">
+                        {queryParams.mediaDirectoryName
+                          ? queryParams.mediaDirectoryName
+                              .split("%2F")
+                              .map((dir, idx) => (
+                                <BreadcrumbItem
+                                  isCurrentPage={
+                                    idx ===
+                                    queryParams.mediaDirectoryName.split("%2F")
+                                      .length -
+                                      1
+                                  }
+                                >
+                                  <BreadcrumbLink
+                                    onClick={(e) => {
+                                      e.preventDefault()
+                                      setQueryParams((prevState) => ({
+                                        ...prevState,
+                                        mediaDirectoryName: getMediaDirectoryName(
+                                          queryParams.mediaDirectoryName,
+                                          { end: idx + 1 }
+                                        ),
+                                      }))
+                                    }}
+                                  >
+                                    {deslugifyDirectory(dir)}
+                                  </BreadcrumbLink>
+                                </BreadcrumbItem>
+                              ))
+                          : null}
+                      </Breadcrumb>
+                      <div
+                        className={`${mediaStyles.mediaCards} justify-content-center pt-3 pl-2`}
+                      >
+                        <Skeleton
+                          w="100%"
+                          h={isListMediaFilesLoading ? "4.5rem" : "fit-content"}
+                          isLoaded={!isListMediaFilesLoading}
+                        >
+                          {files &&
+                            !isListMediaFilesLoading &&
+                            (searchValue
+                              ? filteredMedias.length === 0 && (
+                                  <VStack
+                                    h="100%"
+                                    w="100%"
+                                    alignItems="center"
+                                    justifyContent="center"
+                                  >
+                                    <Text>
+                                      We couldn’t find any {pluralMediaLabel}{" "}
+                                      with “{searchValue}”.
+                                    </Text>
+                                    {queryParams.mediaDirectoryName !==
+                                      pluralMediaLabel && (
+                                      <Button
+                                        variant="clear"
+                                        onClick={() => {
+                                          setQueryParams((prevState) => ({
+                                            ...prevState,
+                                            mediaDirectoryName: pluralMediaLabel,
+                                          }))
+                                        }}
+                                      >
+                                        Search in All {pluralMediaLabel} instead
+                                      </Button>
+                                    )}
+                                  </VStack>
+                                )
+                              : listMediaFilesData.total === 0 && (
+                                  <Text>
+                                    No {pluralMediaLabel} in this{" "}
+                                    {singularDirectoryLabel}.
+                                  </Text>
+                                ))}
+
+                          <SimpleGrid w="100%" columns={3} spacing="2.5%">
+                            {files &&
+                              files
+                                .filter(({ data }) =>
+                                  filteredMedias.includes(data?.name)
+                                )
+                                .map(({ data, isLoading }) => (
+                                  <Skeleton
+                                    w="100%"
+                                    h={isLoading ? "4.5rem" : "fit-content"}
+                                    isLoaded={!isLoading}
+                                    key={data.name}
+                                  >
+                                    {mediaType === "images" ? (
+                                      <ImagePreviewCard
+                                        name={data.name}
+                                        addedTime={data.addedTime}
+                                        mediaUrl={data.mediaUrl}
+                                        isSelected={
+                                          data.name ===
+                                          watch("selectedMedia")?.name
+                                        }
+                                        onClick={() => onMediaSelect(data)}
+                                        isMenuNeeded={false}
+                                      />
+                                    ) : (
+                                      <FilePreviewCard
+                                        name={data.name}
+                                        addedTime={data.addedTime}
+                                        mediaUrl={data.mediaUrl}
+                                        isSelected={
+                                          data.name ===
+                                          watch("selectedMedia")?.name
+                                        }
+                                        onClick={() => onMediaSelect(data)}
+                                        isMenuNeeded={false}
+                                      />
+                                    )}
+                                  </Skeleton>
+                                ))}
+                          </SimpleGrid>
+                        </Skeleton>
+                      </div>
+                    </Box>
+                  </GridItem>
+                </Grid>
+              </TabPanel>
+              <TabPanel>
+                <Text>panel 2</Text>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </ModalBody>
 
         <ModalFooter>
