595f69391357ba1e0033b6c45a82e2de25fdd670
seaerchin <44049504+seaerchin@users.noreply.github.com>
2023-12-05 15:30:41 +0800

    refactor(tiptap): change embeds view (#1723)
    ## Problem
    The old embed view is very different from how the rest of our editor looks like due to it not using our theme colours/styling.
    
    ## Solution
    Change the design to a design approved design!
    
    ## Screenshots
    <img width="599" alt="Screenshot 2023-12-05 at 3 09 54 PM" src="https://github.com/isomerpages/isomercms-frontend/assets/44049504/9792b585-1d18-4d0b-a7a6-6ffcf3d6c0ce">

--- src/layouts/components/Editor/extensions/FormSG/FormSGView.tsx
+++ src/layouts/components/Editor/extensions/FormSG/FormSGView.tsx
@@ -1,5 +1,5 @@
-import { Box, Text } from "@chakra-ui/react"
-import { NodeViewProps, NodeViewWrapper } from "@tiptap/react"
+import { Code } from "@chakra-ui/react"
+import { NodeViewProps } from "@tiptap/react"
 
 import { BlockWrapper } from "../../components/BlockWrapper"
 
@@ -8,33 +8,7 @@ export const FormSGView = ({ node, selected }: NodeViewProps) => {
 
   return (
     <BlockWrapper name="Embed" isSelected={selected}>
-      <Box
-        as={NodeViewWrapper}
-        bg="#FAF594"
-        border="3px solid #0d0d0d"
-        borderRadius="0.5rem"
-        margin="1rem 0"
-        position="relative"
-        data-drag-handle
-        contentEditable={false}
-      >
-        <Text
-          ml="1rem"
-          bgColor="#0d0d0d"
-          textStyle="caption-1"
-          fontWeight="bold"
-          color="#fff"
-          position="absolute"
-          top="0"
-          padding="0.25rem 0.75rem"
-          borderRadius="0 0 0.5rem 0.5rem"
-        >
-          FormSG Embed
-        </Text>
-        <Box mt="1.5rem" p="1rem">
-          {formSrc}
-        </Box>
-      </Box>
+      <Code paddingInline="0.75rem">{formSrc}</Code>
     </BlockWrapper>
   )
 }
