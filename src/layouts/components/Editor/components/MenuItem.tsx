3015df1e472a2feb3bdd7fff31f34f5dd5bd4012
Qilu Xie <qilu@open.gov.sg>
2023-12-14 08:02:18 +0800

    IS-734 Add delete icon to table control and other minor UI fixes (#1743)
    * added delete option to table floating menu
    
    * Added delete icon to floating menu of table
    
    * adjust table label font size to match the design
    
    * added z-index to bring table lable to the top of text

--- src/layouts/components/Editor/components/MenuItem.tsx
+++ src/layouts/components/Editor/components/MenuItem.tsx
@@ -1,4 +1,4 @@
-import { Icon, Tooltip } from "@chakra-ui/react"
+import { Icon, Tooltip, Divider } from "@chakra-ui/react"
 import { IconButton } from "@opengovsg/design-system-react"
 import { MouseEventHandler } from "react"
 import { IconType } from "react-icons/lib"
@@ -9,6 +9,8 @@ interface MenuItemProps {
   action?: MouseEventHandler<HTMLButtonElement>
   isActive?: null | (() => boolean)
   isRound?: boolean
+  color?: string
+  type?: string
 }
 
 export const MenuItem = ({
@@ -17,24 +19,51 @@ export const MenuItem = ({
   action,
   isRound,
   isActive = null,
+  color = "",
+  type = "item",
 }: MenuItemProps) => (
-  // NOTE: Delay opening by 500ms
   <Tooltip label={title || "divider"} hasArrow openDelay={500}>
-    <IconButton
-      _hover={{ bg: "gray.100" }}
-      _active={{ bg: "gray.200" }}
-      onClick={action}
-      bgColor={isActive && isActive() ? "gray.200" : "transparent"}
-      border="none"
-      h="1.75rem"
-      w="1.75rem"
-      minH="1.75rem"
-      minW="1.75rem"
-      p="0.25rem"
-      aria-label={title || "divider"}
-      isRound={isRound}
-    >
-      <Icon as={icon} fontSize="1.25rem" color="base.content.medium" />
-    </IconButton>
+    {type === "divider" ? (
+      <IconButton
+        _hover={{ bg: "transparent" }}
+        _active={{ bg: "gray.200" }}
+        bgColor="transparent"
+        border="none"
+        h="1.75rem"
+        w="1rem"
+        minH="1.75rem"
+        minW="1rem"
+        aria-label={title || "divider"}
+        isRound={isRound}
+      >
+        <Divider
+          orientation="vertical"
+          border="px solid"
+          borderColor="base.divider.strong"
+          h="1.25rem"
+        />
+      </IconButton>
+    ) : (
+      <IconButton
+        _hover={{ bg: "gray.100" }}
+        _active={{ bg: "gray.200" }}
+        onClick={action}
+        bgColor={isActive && isActive() ? "gray.200" : "transparent"}
+        border="none"
+        h="1.75rem"
+        w="1.75rem"
+        minH="1.75rem"
+        minW="1.75rem"
+        p="0.25rem"
+        aria-label={title || "divider"}
+        isRound={isRound}
+      >
+        <Icon
+          as={icon}
+          fontSize="1.25rem"
+          color={color || "base.content.medium"}
+        />
+      </IconButton>
+    )}
   </Tooltip>
 )
