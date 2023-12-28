070d72c5cb7d185baf74be3c7304bf8fa1366880
Park Se Hyun <>
2023-12-28 14:21:29 +0800

    fix(styles): fix overlapping page title issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -97,8 +97,9 @@ const Header = ({
         bg="white"
         h="4rem"
         w="100%"
+        spacing="0.5rem"
       >
-        <HStack spacing="1.25rem" flex={1}>
+        <HStack spacing="1.25rem" flex="1 1 content">
           {!showButton ? (
             <Box w="180px">
               <img
@@ -128,11 +129,16 @@ const Header = ({
         </HStack>
         {/* <Spacer /> */}
         {title ? (
-          <Flex minWidth="-webkit-min-content" alignItems="center">
-            <Text textStyle="h5">{title}</Text>
+          <Flex 
+            minWidth="-webkit-min-content" 
+            alignItems="center"
+            flex={1}
+            justifyContent="center"
+            >
+            <Text textStyle="h5" noOfLines={1}>{title}</Text>
           </Flex>
         ) : null}
-        <HStack flex={1} justifyContent="flex-end">
+        <HStack flex="0 1 content" justifyContent="flex-end" w="fit-content">
           (
           {isShowStagingBuildStatusEnabled && (
             <Box mr="-0.25rem">
