bcb7548307e1d6c2e35a26077302ad4e3e690d6d
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-01-31 13:56:06 +0800

    chore(modal): add comment on the use of the local Modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,10 @@
 import { Modal as ChakraModal, ModalProps } from "@chakra-ui/react"
 
+/**
+ * NOTE: We are wrapping the Modal component from Chakra UI by enforcing the
+ * motion preset to be "none" by default. This is because having animations
+ * makes the CMS appear very slow on GSIBs.
+ */
 export const Modal = ({ motionPreset, ...rest }: ModalProps) => {
   return <ChakraModal motionPreset={motionPreset || "none"} {...rest} />
 }
