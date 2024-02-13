0a7c10964894e9cfa85cae8b8d9d44d01b2d0b13
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-02-13 12:58:18 +0800

    fix: allow adding multiple collaborators consecutively

--- src/hooks/collaboratorHooks/useAddCollaboratorHook.ts
+++ src/hooks/collaboratorHooks/useAddCollaboratorHook.ts
@@ -1,5 +1,10 @@
 import { AxiosError } from "axios"
-import { UseMutationResult, useQueryClient, useMutation } from "react-query"
+import {
+  UseMutationResult,
+  useQueryClient,
+  useMutation,
+  UseMutationOptions,
+} from "react-query"
 
 import {
   LIST_COLLABORATORS_KEY,
@@ -7,17 +12,31 @@ import {
 } from "constants/queryKeys"
 
 import { CollaboratorService } from "services"
+import { AddCollaboratorDto } from "types/collaborators"
 import { MiddlewareError } from "types/error"
 
 export const useAddCollaboratorHook = (
-  siteName: string
+  siteName: string,
+  mutationOptions?: Omit<
+    UseMutationOptions<
+      void,
+      AxiosError<{ error: MiddlewareError }>,
+      AddCollaboratorDto
+    >,
+    "mutationFn" | "mutationKey"
+  >
 ): UseMutationResult<
   void,
   AxiosError<{ error: MiddlewareError }>,
-  { newCollaboratorEmail: string; isAcknowledged: boolean }
+  AddCollaboratorDto
 > => {
   const queryClient = useQueryClient()
-  return useMutation(
+
+  return useMutation<
+    void,
+    AxiosError<{ error: MiddlewareError }>,
+    AddCollaboratorDto
+  >(
     ({ newCollaboratorEmail, isAcknowledged }) =>
       CollaboratorService.addCollaborator(
         siteName,
@@ -25,12 +44,22 @@ export const useAddCollaboratorHook = (
         isAcknowledged
       ),
     {
-      onSuccess: () => {
+      ...mutationOptions,
+      onSuccess: (data, variables, context) => {
         queryClient.invalidateQueries([LIST_COLLABORATORS_KEY, siteName])
         queryClient.invalidateQueries([
           SITE_DASHBOARD_COLLABORATORS_KEY,
           siteName,
         ])
+
+        if (mutationOptions?.onSuccess) {
+          mutationOptions.onSuccess(data, variables, context)
+        }
+      },
+      onError: (err, variables, context) => {
+        if (mutationOptions?.onError) {
+          mutationOptions.onError(err, variables, context)
+        }
       },
     }
   )
