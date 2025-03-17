2c11186ed66906e548d4d2816f53410954700e9b
dcshzj <27919917+dcshzj@users.noreply.github.com>
2025-03-17 16:22:22 +0800

    fix: upgrade axios to 1.8.3

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -4,6 +4,7 @@ import axios from "axios"
 const API_BASE_URL_V2 = `${process.env.REACT_APP_BACKEND_URL_V2}`
 export const apiService = axios.create({
   baseURL: API_BASE_URL_V2,
+  allowAbsoluteUrls: false,
   // 100 secs
   timeout: 100000,
 })
